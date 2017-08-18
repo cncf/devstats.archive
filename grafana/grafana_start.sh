6760bbd7d29d65595a6d195b25cdd0b51949a524
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-18 14:25:54 +0000

    Replaced Docker Grafana with native & updated docs
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/grafana_start.sh
+++ grafana/grafana_start.sh
@@ -6,11 +6,13 @@ then
   echo "You need to set GRAFANA_PASS environment variable to run this script"
   exit 1
 fi
+
+# This is an alternative approach: use Grafana installed inside Docker container
 # -p host_port:container_port
 docker run --name grafana -d -p 3000:3000 -v /var/lib/grafana:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana
 
 # This is an alternative approach: use InfluxDB installed inside Docker container
-# docker run --name influxdb -d --volume=/var/influxdb:/var/influxdb -p 8083:8083 -p 8086:8086 tutum/influxdb
+docker run --name influxdb -d --volume=/var/influxdb:/var/influxdb -p 8083:8083 -p 8086:8086 tutum/influxdb
 docker ps
 
 # To configure influxdb when installed inside Docker container
