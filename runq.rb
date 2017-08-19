46b91f5d7e3eb0417cb842e55fd2ec34b82f4fe7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-19 14:18:14 +0000

    Replaced one letter variables in runq.rb
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- runq.rb
+++ runq.rb
@@ -7,66 +7,77 @@ require './conn' # All database details & setup there
 # rubocop:disable Lint/Debugger
 def runq(sql_file)
   # Connect to database
-  c = conn
+  con = conn
   sql = File.read(sql_file)
-  res = exec_sql(c, sql)
-  return unless res.count.positive?
-  hdr = res.first.keys
-  hdrl = {}
-  hdr.each do |k|
-    hdrl[k] = (res.map { |r| (r[k] || '').to_s } + [k]).max_by(&:length).length
+  # Results
+  results = exec_sql(con, sql)
+  return unless results.count.positive?
+
+  columns = results.first.keys
+  column_lengths = {}
+  columns.each do |column|
+    column_lengths[column] = (
+      results.map { |row| (row[column] || '').to_s } + [column]
+    ).max_by(&:length).length
   end
 
-  s = '/'
-  hdr.each do |k|
-    fmt = "%-#{hdrl[k]}s"
-    v = '-' * hdrl[k]
-    s += "#{fmt % v}+"
+  # Upper frame of the header row
+  output = '/'
+  columns.each do |column|
+    fmt = "%-#{column_lengths[column]}s"
+    value = '-' * column_lengths[column]
+    output += "#{fmt % value}+"
   end
-  s = s[0..-2] + "\\\n"
-  puts s
+  output = output[0..-2] + "\\\n"
+  puts output
 
-  s = '|'
-  hdr.each do |k|
-    fmt = "%-#{hdrl[k]}s"
-    s += "#{fmt % k}|"
+  # Header row
+  output = '|'
+  columns.each do |column|
+    fmt = "%-#{column_lengths[column]}s"
+    output += "#{fmt % column}|"
   end
-  s += "\n"
-  puts s
+  output += "\n"
+  puts output
 
-  s = '+'
-  hdr.each do |k|
-    fmt = "%-#{hdrl[k]}s"
-    v = '-' * hdrl[k]
-    s += "#{fmt % v}+"
+  # Frame between header row and data rows
+  output = '+'
+  columns.each do |column|
+    fmt = "%-#{column_lengths[column]}s"
+    value = '-' * column_lengths[column]
+    output += "#{fmt % value}+"
   end
-  s = s[0..-2] + "+\n"
-  puts s
+  output = output[0..-2] + "+\n"
+  puts output
 
-  res.each do |r|
-    s = '|'
-    r.each do |k, v|
-      fmt = "%-#{hdrl[k]}s"
-      s += "#{fmt % v}|"
+  # Data rows
+  results.each do |row|
+    output = '|'
+    row.each do |column, value|
+      fmt = "%-#{column_lengths[column]}s"
+      output += "#{fmt % value}|"
     end
-    s = s[0..-2] + "|\n"
-    puts s
+    output = output[0..-2] + "|\n"
+    puts output
   end
 
-  s = '\\'
-  hdr.each do |k|
-    fmt = "%-#{hdrl[k]}s"
-    v = '-' * hdrl[k]
-    s += "#{fmt % v}+"
+  # Frame below data rows
+  output = '\\'
+  columns.each do |column|
+    fmt = "%-#{column_lengths[column]}s"
+    v = '-' * column_lengths[column]
+    output += "#{fmt % v}+"
   end
-  s = s[0..-2] + "/\n"
-  puts s
-  puts "Rows: #{res.count}"
+  output = output[0..-2] + "/\n"
+  puts output
+
+  # Row count
+  puts "Rows: #{results.count}"
 rescue $DBError => e
   puts e.message
   binding.pry
 ensure
-  c&.close
+  con&.close
 end
 # rubocop:enable Lint/Debugger
 
