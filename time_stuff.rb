9bcd9480693aa030982af5a8bca19c244accc067
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-21 09:35:33 +0000

    Added all PRs merged metrics, updated runq to support params
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- time_stuff.rb
+++ time_stuff.rb
@@ -9,12 +9,41 @@ def seconds_in_week
   seconds_in_day * 7
 end
 
+def hour_start(dt)
+  Time.new(
+    dt.year,
+    dt.month,
+    dt.day,
+    dt.hour
+  ).utc
+end
+
 def day_start(dt)
   dt.to_date.to_time.utc
 end
 
+def week_start(dt)
+  day_start dt - ((dt.wday - 1) % 7) * seconds_in_day
+end
+
+def month_start(dt)
+  Time.new(dt.year, dt.month).utc
+end
+
+def year_start(dt)
+  Time.new(dt.year).utc
+end
+
+def next_hour_start(dt)
+  hour_start dt + 3600
+end
+
 def next_day_start(dt)
   day_start dt + seconds_in_day
+end
+
+def next_week_start(dt)
+  week_start dt + seconds_in_week
 end
 
 def next_month_start(dt)
@@ -32,22 +61,10 @@ def next_year_start(dt)
   Time.new(dt.year + 1).utc
 end
 
-def week_start(dt)
-  day_start dt - ((dt.wday - 1) % 7) * seconds_in_day
-end
-
-def next_week_start(dt)
-  week_start dt + seconds_in_week
-end
-
-def month_start(dt)
-  Time.new(dt.year, dt.month).utc
-end
-
-def year_start(dt)
-  Time.new(dt.year).utc
-end
-
 def to_ymd(dt)
   dt.strftime '%Y-%m-%d'
+end
+
+def to_ymdhms(dt)
+  dt.strftime '%Y-%m-%d %H:%M:%S'
 end
