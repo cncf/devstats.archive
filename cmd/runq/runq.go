c80178f130b819ad71251b4b7083c157b89ad042
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-22 16:03:20 +0000

    Runq ported to Go
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ cmd/runq/runq.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	lib "k8s.io/test-infra/gha2db"
+	"os"
+	"strings"
+)
+
+func runq(sqlFile string, params []string) {
+	// SQL arguments number
+	if len(params)%2 > 0 {
+		fmt.Printf("Must provide correct parameter value pairs.\n")
+		os.Exit(1)
+	}
+
+	// SQL arguments parse
+	replaces := make(map[string]string)
+	paramName := ""
+	for index, param := range params {
+		if index%2 == 0 {
+			replaces[param] = ""
+			paramName = param
+		} else {
+			replaces[paramName] = param
+			paramName = ""
+		}
+	}
+
+	// Read and eventually transform SQL file.
+	bytes, err := ioutil.ReadFile(sqlFile)
+	lib.FatalOnError(err)
+	sqlQuery := string(bytes)
+	for from, to := range replaces {
+		sqlQuery = strings.Replace(sqlQuery, from, to, -1)
+	}
+
+	// Connect to Postgres DB
+	c, err := lib.Conn()
+	lib.FatalOnError(err)
+	defer c.Close()
+
+	// Execute SQL
+	rows, err := lib.QuerySQLWithErr(c, sqlQuery)
+	lib.FatalOnError(err)
+	defer rows.Close()
+
+	// Now unknown rows, with unknown types
+	columns, err := rows.Columns()
+	lib.FatalOnError(err)
+
+	// Vals to hold any type as []interface{}
+	vals := make([]interface{}, len(columns))
+	for i := range columns {
+		vals[i] = new(sql.RawBytes)
+	}
+
+	// Get results into `results` appary of maps
+	var results []map[string]string
+	rowCount := 0
+	for rows.Next() {
+		rowMap := make(map[string]string)
+		err = rows.Scan(vals...)
+		lib.FatalOnError(err)
+		for index, val := range vals {
+			value := ""
+			if val != nil {
+				value = string(*val.(*sql.RawBytes))
+			}
+			rowMap[columns[index]] = value
+		}
+		results = append(results, rowMap)
+		rowCount++
+	}
+	err = rows.Err()
+	lib.FatalOnError(err)
+
+	if len(results) < 1 {
+		fmt.Printf("Metric returned no data\n")
+		return
+	}
+
+	// Compute column Lengths
+	columnLengths := make(map[string]int)
+	for _, column := range columns {
+		maxLen := len(column)
+		for _, row := range results {
+			valLen := len(row[column])
+			if valLen > maxLen {
+				maxLen = valLen
+			}
+		}
+		columnLengths[column] = maxLen
+	}
+
+	// Upper frame of the header row
+	output := "/"
+	for _, column := range columns {
+		strFormat := fmt.Sprintf("%%-%ds", columnLengths[column])
+		value := strings.Repeat("-", columnLengths[column])
+		output += fmt.Sprintf(strFormat, value) + "+"
+	}
+	output = output[:len(output)-1] + "\\\n"
+	fmt.Printf(output)
+
+	// Header row
+	output = "|"
+	for _, column := range columns {
+		strFormat := fmt.Sprintf("%%-%ds", columnLengths[column])
+		output += fmt.Sprintf(strFormat, column) + "|"
+	}
+	output += "\n"
+	fmt.Printf(output)
+
+	// Frame between header row and data rows
+	output = "+"
+	for _, column := range columns {
+		strFormat := fmt.Sprintf("%%-%ds", columnLengths[column])
+		value := strings.Repeat("-", columnLengths[column])
+		output += fmt.Sprintf(strFormat, value) + "+"
+	}
+	output = output[:len(output)-1] + "+\n"
+	fmt.Printf(output)
+
+	// Data rows loop
+	for _, row := range results {
+		// data row
+		output = "|"
+		for _, column := range columns {
+			value := row[column]
+			strFormat := fmt.Sprintf("%%-%ds", columnLengths[column])
+			output += fmt.Sprintf(strFormat, value) + "|"
+		}
+		output = output[:len(output)-1] + "|\n"
+		fmt.Printf(output)
+	}
+
+	// Frame below data rows
+	output = "\\"
+	for _, column := range columns {
+		strFormat := fmt.Sprintf("%%-%ds", columnLengths[column])
+		value := strings.Repeat("-", columnLengths[column])
+		output += fmt.Sprintf(strFormat, value) + "+"
+	}
+	output = output[:len(output)-1] + "/\n"
+	fmt.Printf(output)
+
+	fmt.Printf("Rows: %v\n", rowCount)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Required SQL file name [param1 value1 [param2 value2 ...]]\n")
+		os.Exit(1)
+	}
+	runq(os.Args[1], os.Args[2:])
+}
