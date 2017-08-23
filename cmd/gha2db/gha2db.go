d78443a787582b0586939385eca1408b354629ca
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-23 09:41:41 +0000

    gha2db wip, go routines & channels synced
    Working on GHA structure and import itself.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cmd/gha2db/gha2db.go
+++ cmd/gha2db/gha2db.go
@@ -1,22 +1,107 @@
 package main
 
 import (
-	//"database/sql"
+	"bytes"
+	"compress/gzip"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	lib "k8s.io/test-infra/gha2db"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func getGHAJSON(ch chan bool, dt time.Time, org []string, repo []string) {
-	fmt.Printf("Working on %v\n", dt)
-  if ch != nil {
-    ch <- true
-  }
+// parseJSON - Parse signle GHA JSON event
+func parseJSON(con *sql.DB, jsonStr []byte, dt time.Time, forg []string, frepo []string) (f int, e int) {
+	var h lib.GHA
+	if len(jsonStr) < 1 {
+		return
+	}
+	err := json.Unmarshal(jsonStr, &h)
+	if err != nil {
+		fmt.Printf("'%v'\n", jsonStr)
+	}
+	lib.FatalOnError(err)
+  fmt.Printf("repo: %v\n", h.Repo.Name)
+	/*
+	  h = JSON.parse json
+	  full_name = h['repo']['name']
+	  if repo_hit(full_name, forg, frepo)
+	    eid = h['id']
+	    if $json_out
+	      prt = JSON.pretty_generate(h)
+	      ofn = "jsons/#{dt.to_i}_#{eid}.json"
+	      File.write ofn, prt
+	    end
+	    if $db_out
+	      $ev[Thread.current.object_id] = h
+	      e = write_to_pg(con, h)
+	      $ev.delete(Thread.current.object_id)
+	    end
+	    puts "Processed: '#{dt}' event: #{eid}" if $debug >= 1
+	    f = 1
+	  end
+	  [f, e]
+	*/
+	return
 }
 
+// getGHAJSON - This is a work for single go routine - 1 hour of GHA data
+// Usually such JSON conatin about 15000 - 60000 singe GHA events
+// Boolean channel `ch` is used to synchronize go routines
+func getGHAJSON(ch chan bool, dt time.Time, forg []string, frepo []string) {
+	fmt.Printf("Working on %v\n", dt)
+
+	// Connect to Postgres DB
+	con, err := lib.Conn()
+	lib.FatalOnError(err)
+	defer con.Close()
+
+	fn := fmt.Sprintf(
+		"http://data.githubarchive.org/%04d-%02d-%02d-%d.json.gz",
+		dt.Year(), dt.Month(), dt.Day(), dt.Hour(),
+	)
+
+	// Get gzipped JSON array via HTTP
+	response, err := http.Get(fn)
+	lib.FatalOnError(err)
+	defer response.Body.Close()
+
+	// Decompress Gzipped response
+	reader, err := gzip.NewReader(response.Body)
+	lib.FatalOnError(err)
+	fmt.Printf("Opened %s\n", fn)
+	defer reader.Close()
+	jsonsBytes, err := ioutil.ReadAll(reader)
+	lib.FatalOnError(err)
+	fmt.Printf("Decompressed %s\n", fn)
+
+	// Split JSON array into separate JSONs
+	jsonsArray := bytes.Split(jsonsBytes, []byte("\n"))
+	fmt.Printf("Splitted %s, %d JSONs\n", fn, len(jsonsArray))
+
+	// Process JSONs one by one
+	n, f, e := 0, 0, 0
+	for _, json := range jsonsArray {
+		fi, ei := parseJSON(con, json, dt, forg, frepo)
+		n++
+		f += fi
+		e += ei
+	}
+	fmt.Printf(
+		"Parsed: %s: %d JSONs, found %d matching, events %d\n",
+		fn, n, f, e,
+	)
+	if ch != nil {
+		ch <- true
+	}
+}
+
+// gha2db - main work horse
 func gha2db(args []string) {
 	hourFrom, err := strconv.Atoi(args[1])
 	lib.FatalOnError(err)
@@ -62,20 +147,20 @@ func gha2db(args []string) {
 	if thrN > 1 {
 		chanPool := []chan bool{}
 		for dt.Before(dTo) || dt.Equal(dTo) {
-      ch := make(chan bool)
-      chanPool = append(chanPool, ch)
-      go getGHAJSON(ch, dt, org, repo)
+			ch := make(chan bool)
+			chanPool = append(chanPool, ch)
+			go getGHAJSON(ch, dt, org, repo)
 			dt = dt.Add(time.Hour)
-      if len(chanPool) == thrN {
-        ch = chanPool[0]
-        <-ch
-        chanPool = chanPool[1:]
-      }
+			if len(chanPool) == thrN {
+				ch = chanPool[0]
+				<-ch
+				chanPool = chanPool[1:]
+			}
 		}
-    fmt.Printf("Final threads join\n")
-    for _, ch := range chanPool {
-        <-ch
-    }
+		fmt.Printf("Final threads join\n")
+		for _, ch := range chanPool {
+			<-ch
+		}
 	} else {
 		fmt.Printf("Using single threaded version\n")
 		for dt.Before(dTo) || dt.Equal(dTo) {
@@ -84,27 +169,6 @@ func gha2db(args []string) {
 		}
 	}
 
-	/*
-	  dt = dFrom
-	  if $thr_n > 1
-	    thr_pool = []
-	    while dt <= d_to
-	      thr = Thread.new(dt) { |adt| get_gha_json(adt, org, repo) }
-	      thr_pool << thr
-	      dt += 3600
-	      # rubocop:disable Style/Next
-	      if thr_pool.length == $thr_n
-	        thr = thr_pool.first
-	        thr.join
-	        thr_pool = thr_pool[1..-1]
-	      end
-	      # rubocop:enable Style/Next
-	    end
-	    puts 'Final threads join'
-	    thr_pool.each(&:join)
-	  else
-	  end
-	*/
 	fmt.Printf("All done.\n")
 }
 
