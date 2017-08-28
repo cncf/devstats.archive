d91c010a688d3bb38ac00c6125127837f0e6b981
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-28 13:49:23 +0000

    Finished pg_conn.go coverage
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- test/pg_test.go
+++ test/pg_test.go
@@ -8,6 +8,167 @@ import (
 	lib "k8s.io/test-infra/gha2db"
 )
 
+func TestCleanUTF8(t *testing.T) {
+	// Test cases
+	var testCases = []struct {
+		value    string
+		expected string
+	}{
+		{value: "value", expected: "value"},
+		{value: "val\x00ue", expected: "value"},
+		{value: "val\u0000ue", expected: "value"},
+		{value: "v\x00a\U00000000l\u0000ue", expected: "value"},
+		{value: "平仮名, ひらがな", expected: "平仮名, ひらがな"},
+		{value: "\u0000平仮名\x00ひらがな\U00000000", expected: "平仮名ひらがな"},
+	}
+	// Execute test cases
+	for index, test := range testCases {
+		got := lib.CleanUTF8(test.value)
+		if got != test.expected {
+			t.Errorf("test number %d, expected %v, got %v", index+1, test.expected, got)
+		}
+	}
+}
+
+func TestTruncToBytes(t *testing.T) {
+	// Test cases
+	var testCases = []struct {
+		value       string
+		n           int
+		expectedStr string
+		expectedLen int
+	}{
+		{
+			value:       "value",
+			n:           3,
+			expectedStr: "val",
+			expectedLen: 3,
+		},
+		{
+			value:       "平仮名, ひらがな",
+			n:           6,
+			expectedStr: "平仮",
+			expectedLen: 6,
+		},
+		{
+			value:       "平仮名, ひらがな",
+			n:           8,
+			expectedStr: "平仮",
+			expectedLen: 6,
+		},
+		{
+			value:       "平仮名, ひらがな",
+			n:           9,
+			expectedStr: "平仮名",
+			expectedLen: 9,
+		},
+		{
+			value:       "\u0000平仮名, \x00ひら\U00000000がな",
+			n:           9,
+			expectedStr: "平仮名",
+			expectedLen: 9,
+		},
+	}
+	// Execute test cases
+	for index, test := range testCases {
+		gotStr := lib.TruncToBytes(test.value, test.n)
+		if gotStr != test.expectedStr {
+			t.Errorf("test number %d, expected string %v, got %v", index+1, test.expectedStr, gotStr)
+		}
+		gotLen := len(gotStr)
+		if gotLen != test.expectedLen {
+			t.Errorf("test number %d, expected length %v, got %v", index+1, test.expectedLen, gotLen)
+		}
+	}
+}
+
+func TestTruncStringOrNil(t *testing.T) {
+	// Test cases
+	sValues := []string{"value", "平仮名, ひらがな", "\u0000平仮名, \x00ひら\U00000000がな"}
+	var testCases = []struct {
+		value    *string
+		n        int
+		expected interface{}
+	}{
+		{
+			value:    nil,
+			n:        10,
+			expected: nil,
+		},
+		{
+			value:    &sValues[0],
+			n:        3,
+			expected: "val",
+		},
+		{
+			value:    &sValues[1],
+			n:        6,
+			expected: "平仮",
+		},
+		{
+			value:    &sValues[2],
+			n:        9,
+			expected: "平仮名",
+		},
+	}
+	// Execute test cases
+	for index, test := range testCases {
+		got := lib.TruncStringOrNil(test.value, test.n)
+		if got != test.expected {
+			t.Errorf("test number %d, expected %v, got %v", index+1, test.expected, got)
+		}
+	}
+}
+
+func TestBoolOrNil(t *testing.T) {
+	result := lib.BoolOrNil(nil)
+	if result != nil {
+		t.Errorf("test nil case: expected <nil>, got %v\n", result)
+	}
+	val := true
+	result = lib.BoolOrNil(&val)
+	if result != val {
+		t.Errorf("expected true, got %v\n", result)
+	}
+}
+
+func TestTimeOrNil(t *testing.T) {
+	result := lib.TimeOrNil(nil)
+	if result != nil {
+		t.Errorf("test nil case: expected <nil>, got %v\n", result)
+	}
+	val := time.Now()
+	result = lib.TimeOrNil(&val)
+	if result != val {
+		t.Errorf("expected %v, got %v\n", val, result)
+	}
+}
+
+func TestIntOrNil(t *testing.T) {
+	result := lib.IntOrNil(nil)
+	if result != nil {
+		t.Errorf("test nil case: expected <nil>, got %v\n", result)
+	}
+	val := 2
+	result = lib.IntOrNil(&val)
+	if result != val {
+		t.Errorf("expected %v, got %v\n", val, result)
+	}
+}
+
+func TestStringOrNil(t *testing.T) {
+	result := lib.StringOrNil(nil)
+	if result != nil {
+		t.Errorf("test nil case: expected <nil>, got %v\n", result)
+	}
+	val := "hello\x00 world"
+	expected := "hello world"
+	result = lib.StringOrNil(&val)
+	if result != expected {
+		t.Errorf("expected %v, got %v\n", val, result)
+	}
+}
+
 func TestPostgres(t *testing.T) {
 	// Environment context parse
 	var ctx lib.Ctx
