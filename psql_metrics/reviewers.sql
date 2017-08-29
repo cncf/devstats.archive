f5902293f941a6f909a171bd8c395c034750c74f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-08-29 16:09:10 +0000

    Speedup reviewers metric by using temp tables
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- psql_metrics/reviewers.sql
+++ psql_metrics/reviewers.sql
@@ -1,3 +1,4 @@
+create temp table matching as select event_id from gha_texts where created_at >= '{{from}}' and created_at < '{{to}}' and substring(body from '(?i)^\s*/lgtm\s*$') is not null;
 select
   count(distinct actor_id) as result
 from
@@ -5,27 +6,16 @@ from
 where
   actor_login not in ('googlebot')
   and actor_login not like 'k8s-%'
-  and (
-    id in (
-      select
-        min(event_id)
-      from
-        gha_issues_events_labels
-      where
-        created_at >= '{{from}}'
-        and created_at < '{{to}}'
-        and label_name in ('lgtm', 'Lgtm', 'lGtm', 'lgTm', 'lgtM', 'LGtm', 'LgTm', 'LgtM', 'lGTm', 'lGtM', 'lgTM', 'LGTm', 'LGtM', 'LgTM', 'lGTM', 'LGTM')
-      group by
-        issue_id
-    )
-    or id in (
-      select
-        event_id
-      from
-        gha_texts
-      where
-        created_at >= '{{from}}'
-        and created_at < '{{to}}'
-        and substring(body from '(?i)^\s*/lgtm\s*$') is not null
-    )
-  )
+  and id in (
+    select min(event_id)
+    from
+      gha_issues_events_labels
+    where
+      created_at >= '{{from}}'
+      and created_at < '{{to}}'
+      and label_name in ('lgtm', 'Lgtm', 'lGtm', 'lgTm', 'lgtM', 'LGtm', 'LgTm', 'LgtM', 'lGTm', 'lGtM', 'lgTM', 'LGTm', 'LGtM', 'LgTM', 'lGTM', 'LGTM')
+    group by
+      issue_id
+    union select event_id from matching
+  );
+drop table matching;
