dd2406a7a0c1049ebf49f7b8b76f036dbcf0201f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-06 10:00:40 +0000

    Pre 2015 events, added comment & member support
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- analysis.sh
+++ analysis.sh
@@ -2,4 +2,7 @@
 #ruby analysis.rb old '' jsons/*.json | tee analysis/old.txt
 #ruby analysis.rb old_repository 'repository' jsons/*.json | tee analysis/old_repository.txt
 #ruby analysis.rb old_payload 'payload' jsons/*.json | tee analysis/old_payload.txt
-ruby analysis.rb old_payload_pages 'payload,pages,i:0' jsons/*.json | tee analysis/old_payload_pages.txt
+#ruby analysis.rb old_payload_pages 'payload,pages,i:0' jsons/*.json | tee analysis/old_payload_pages.txt
+#ruby analysis.rb old_payload_member 'payload,member' jsons/*.json | tee analysis/old_payload_member.txt
+#ruby analysis.rb old_payload_comment 'payload,comment' jsons/*.json | tee analysis/old_payload_comment.txt
+ruby analysis.rb old_payload_comment_user 'payload,comment,user' jsons/*.json | tee analysis/old_payload_comment_user.txt
