f061cc6297bf2420eaa4637359dc20d2873758ce
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-06 09:27:57 +0000

    Pre 2015 events wip: finished commits and pages
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- analysis.sh
+++ analysis.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #ruby analysis.rb old '' jsons/*.json | tee analysis/old.txt
 #ruby analysis.rb old_repository 'repository' jsons/*.json | tee analysis/old_repository.txt
-ruby analysis.rb old_payload 'payload' jsons/*.json | tee analysis/old_payload.txt
+#ruby analysis.rb old_payload 'payload' jsons/*.json | tee analysis/old_payload.txt
+ruby analysis.rb old_payload_pages 'payload,pages,i:0' jsons/*.json | tee analysis/old_payload_pages.txt
--- kubernetes_test.sh
+++ kubernetes_test.sh
@@ -11,3 +11,9 @@ PG_DB=test GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 ./gha2db 2014-11-04 0 2014-11-06 0 'Go
 ##read a
 PG_DB=test GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure || exit 5
 echo "All done."
+
+
+# To test on unlimited repos
+#PG_DB=test ./gha2db 2017-08-29 0 2017-08-31 0 || exit 2
+#PG_DB=test GHA2DB_EXACT=1 ./gha2db 2015-07-07 0 2015-07-09 0 || exit 3
+#PG_DB=test GHA2DB_OLDFMT=1 ./gha2db 2014-11-04 0 2014-11-06 0 || exit 4
