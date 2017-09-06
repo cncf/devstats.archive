a1aa381beef0340a74c0e15787a2fca2667b56a1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-06 11:15:03 +0000

    Pre 2015 - added releases & assets support
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- analysis.sh
+++ analysis.sh
@@ -5,4 +5,8 @@
 #ruby analysis.rb old_payload_pages 'payload,pages,i:0' jsons/*.json | tee analysis/old_payload_pages.txt
 #ruby analysis.rb old_payload_member 'payload,member' jsons/*.json | tee analysis/old_payload_member.txt
 #ruby analysis.rb old_payload_comment 'payload,comment' jsons/*.json | tee analysis/old_payload_comment.txt
-ruby analysis.rb old_payload_comment_user 'payload,comment,user' jsons/*.json | tee analysis/old_payload_comment_user.txt
+#ruby analysis.rb old_payload_comment_user 'payload,comment,user' jsons/*.json | tee analysis/old_payload_comment_user.txt
+#ruby analysis.rb old_payload_release 'payload,release' jsons/*.json | tee analysis/old_payload_release.txt
+#ruby analysis.rb old_payload_release_author 'payload,release,author' jsons/*.json | tee analysis/old_payload_release_author.txt
+#ruby analysis.rb old_payload_release_assets 'payload,release,assets,i:0' jsons/*.json | tee analysis/old_payload_release_assets.txt
+#ruby analysis.rb old_payload_release_assets_uploader 'payload,release,assets,i:0,uploader' jsons/*.json | tee analysis/old_payload_release_assets_uploader.txt
