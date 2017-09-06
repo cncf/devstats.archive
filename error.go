73470790fc49685398e3e72d101aa7a8fe19a099
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-06 19:59:03 +0000

    Pre 2015 - failed 1st try, added logging tools & retrying
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- error.go
+++ error.go
@@ -2,12 +2,14 @@ package gha2db
 
 import (
 	"fmt"
+	"os"
 )
 
 // FatalOnError displays error message (if error present) and exits program
 func FatalOnError(err error) {
 	if err != nil {
 		fmt.Printf("Error:\n%v\nStacktrace:\n", err)
+		fmt.Fprintf(os.Stderr, "Error:\n%v\nStacktrace:\n", err)
 		panic("stacktrace")
 	}
 }
--- kubernetes.sh
+++ kubernetes.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-GHA2DB_MGETC=y ./structure || exit 1
-./gha2db 2015-08-01 0 today now 'kubernetes,kubernetes-client,kubernetes-incubator' || exit 2
-GHA2DB_EXACT=1 ./gha2db 2015-01-01 0 2016-01-01 0 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' || exit 3
-GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 ./gha2db 2014-06-01 0 2014-12-31 23 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' || exit 4
-GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure || exit 5
+> errors.txt
+> run.log
+GHA2DB_MGETC=y ./structure 2>>errors.txt | tee -a run.log || exit 1
+./gha2db 2015-08-01 0 today now 'kubernetes,kubernetes-client,kubernetes-incubator' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_EXACT=1 ./gha2db 2015-01-01 0 2016-01-01 0 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 ./gha2db 2014-06-01 0 2014-12-31 23 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' 2>>errors.txt | tee -a run.log || exit 4
+GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 5
 echo "All done."
