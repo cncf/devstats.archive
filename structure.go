dc4cdf9213c590a7f5d146a90546528cb3aef160
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-07 15:31:24 +0000

    Pres 2015 - hopefully last fixes for this format
    data regenerated, now testing if PRs - Issues connections works as
    needed for metrics (before 2015 there was no Issue data in JSONs), so I
    had to create artificial Issues on PullRequest events. They have very
    special IDs etc.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- structure.go
+++ structure.go
@@ -988,8 +988,8 @@ func Structure(ctx *Ctx) {
 			"insert into gha_texts(event_id, body, created_at, repo_id, repo_name, actor_id, actor_login, type) "+
 				"select event_id, body, created_at, dup_repo_id, dup_repo_name, dup_actor_id, dup_actor_login, dup_type from gha_comments "+
 				"where body != '' and event_id > %v union "+
-				"select c.event_id, c.message, e.created_at, e.repo_id, e.dup_repo_name, e.actor_id, e.dup_actor_login, e.type from gha_commits c, gha_events e "+
-				"where c.event_id = e.id and c.message != '' and e.id > %v union "+
+				"select event_id, message, dup_created_at, dup_repo_id, dup_repo_name, dup_actor_id, dup_actor_login, dup_type from gha_commits "+
+				"where message != '' and event_id > %v union "+
 				"select event_id, title, created_at, dup_repo_id, dup_repo_name, dup_actor_id, dup_actor_login, dup_type from gha_issues where "+
 				"title != '' and event_id > %v union "+
 				"select event_id, body, created_at, dup_repo_id, dup_repo_name, dup_actor_id, dup_actor_login, dup_type from gha_issues where "+
---
+++ util_sql/prs_issues_conn.sql
@@ -0,0 +1,20 @@
+select
+  ipr.issue_id,
+  pr.id,
+  ipr.created_at,
+  count(*) as cnt
+from
+  gha_issues_pull_requests ipr,
+  gha_pull_requests pr
+where
+  pr.id = ipr.pull_request_id
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+  and pr.event_id = (
+    select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
+  )
+group by
+  ipr.issue_id, pr.id, ipr.created_at
+order by
+  cnt desc,
+  ipr.issue_id;
