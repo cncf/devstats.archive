ca380b4f8f0653d3d5b9887eded2952e83bee346
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-12 11:45:59 +0000

    SIG mentions using labels wip
    Need to update analysis file that I need to reference in my metrics
    feedback doc.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/wip.sql
+++ util_sql/wip.sql
@@ -1,51 +1,47 @@
-create temp table prs as
-select ipr.issue_id, pr.created_at, min(pr.merged_at) as merged_at
+/*select
+  substring(sig from 13) as sig,
+  count(*) as count_value
 from
-  gha_issues_pull_requests ipr,
-  gha_pull_requests pr
+  (
+    select lower(coalesce(
+        substring(
+          body from '(?i)(?:^|\s)+(@kubernetes/sig-[\w\d-]+)(?:-bug|-feature-request|-pr-review|-api-review|-misc|-proposal|-design-proposal|-test-failure)s?(?:$|[^\w\d-]+)'
+        ),
+        substring(
+          body from '(?i)(?:^|\s)+(@kubernetes/sig-[\w\d-]*[\w\d]+)(?:$|[^\w\d-]+)'
+        )
+      )) as sig
+    from
+      gha_texts
+    where
+      created_at >= '{{from}}'
+      and created_at < '{{to}}'
+  ) sel
 where
-  pr.id = ipr.pull_request_id
-  and pr.created_at >= '{{from}}'
-  and pr.created_at < '{{to}}'
+  sel.sig is not null
 group by
-  ipr.issue_id, pr.created_at;
-
-create temp table pr_lgtm as
-select issue_id, min(created_at) as lgtm_at
-from
-  gha_issues_events_labels
-where
-  label_name = 'lgtm'
-  and issue_id in (select issue_id from prs)
-group by
-  issue_id;
-
-create temp table pr_approve as
-select issue_id, min(created_at) as approve_at
-from
-  gha_issues_events_labels
-where
-  label_name = 'approved'
-  and issue_id in (select issue_id from prs)
-group by
-  issue_id;
-
+  sel.sig
+order by
+  count_value desc,
+  sel.sig asc;
+*/
 select
-  'hours_open_to_lgtm,hours_lgtm_to_approve,hours_approve_to_merge' as name,
-  avg(extract(epoch from least(lgtm.lgtm_at, prs.merged_at) - prs.created_at)/3600) as hours_open_to_lgtm,
-  avg(extract(epoch from least(approve.approve_at, prs.merged_at) - least(lgtm.lgtm_at, prs.merged_at))/3600) as hours_lgtm_to_approve,
-  avg(extract(epoch from prs.merged_at - least(approve.approve_at, prs.merged_at))/3600) as hours_approve_to_merge
-from
-  prs
--- left join
-join
-  pr_lgtm lgtm on prs.issue_id = lgtm.issue_id
--- left join
-join
-  pr_approve approve on prs.issue_id = approve.issue_id
+  sel.sig as sig,
+  count(distinct issue_id) as cnt
+from (
+  select i.id as issue_id,
+    lower(substring(il.dup_label_name from '(?i)sig/(.*)')) as sig
+  from
+    gha_issues_labels il,
+    gha_issues i
+  where
+    i.id = il.issue_id
+    and il.dup_created_at >= '{{from}}'
+    and il.dup_created_at < '{{to}}'
+  ) sel
 where
-  prs.merged_at is not null;
-
-drop table pr_lgtm;
-drop table pr_approve;
-drop table prs;
+  sel.sig is not null
+group by
+  sig
+order by
+  cnt desc
