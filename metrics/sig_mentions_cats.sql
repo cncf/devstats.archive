3d3ebc2bf419864b108adad1ebed90fc87c49bc8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-12 09:20:20 +0000

    All metrics fixed (added fill gaps call where needed)
    Updated documentation, metrics, exported new JSONs.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/sig_mentions_cats.sql
@@ -0,0 +1,21 @@
+select
+  substring(sig from 2) as sig,
+  count(*) as count_value
+from
+  (
+    select substring(
+          body from '(?:^|\s)+(?:@kubernetes/sig-[\w\d-]+)(-bug|-feature-request|-pr-review|-api-review|-misc|-proposal|-design-proposal|-test-failure)s?(?:$|[^\w\d-]+)'
+        ) as sig
+    from
+      gha_texts
+    where
+      created_at >= '{{from}}'
+      and created_at < '{{to}}'
+  ) sel
+where
+  sel.sig is not null
+group by
+  sel.sig
+order by
+  count_value desc,
+  sel.sig asc;
