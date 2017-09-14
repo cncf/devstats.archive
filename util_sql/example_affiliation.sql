282a4fe141eb6a1d8d4d40f87eba85ea66bb63bc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-14 12:59:14 +0000

    Finished cncf/gitdm:github_users.json affiliations import
    Now have to regenerate all data due to structure change. After  that we
    have developers affiliations (and emails) available in: gha_actors,
    gha_actors_emails, gha_actors_affiliations, gha_companies tables.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/example_affiliation.sql
@@ -0,0 +1,24 @@
+select
+  a.id as ID,
+  a.login as Author,
+  a.name as Name,
+  ae.email as Email,
+  aa.company_name as Employer,
+  to_char(aa.dt_from, 'YYYY-MM-DD') as From,
+  to_char(aa.dt_to, 'YYYY-MM-DD') as To
+from
+  gha_actors a,
+  gha_actors_emails ae,
+  gha_actors_affiliations aa
+where
+  a.id = ae.actor_id
+  and a.id = aa.actor_id
+  and (
+    aa.company_name = 'CNCF'
+    or a.login = 'lukaszgryglicki'
+  )
+order by
+  a.name asc,
+  ae.email asc,
+  aa.dt_from asc
+;
