8ad405a9feb91b8d2eeae5df5d1e4366273adef3
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-14 07:56:24 +0000

    Affiliations wip checkpoint
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cmd/import_affs/import_affs.go
+++ cmd/import_affs/import_affs.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"time"
 
 	lib "k8s.io/test-infra/gha2db"
@@ -20,8 +23,47 @@ type GitHubUser struct {
 	Name        string `json:"name"`
 }
 
+// stringSet - set of strings
+type stringSet map[string]struct{}
+
+// mapSet - this is a map from string to Set of strings
+type mapSet map[string]stringSet
+
+// decode emails with ! instead of @
+func emailDecode(line string) string {
+	//line.gsub(/[^\s!]+![^\s!]+/) { |email| email.sub('!', '@') }
+	re := regexp.MustCompile(`([^\s!]+)!([^\s!]+)`)
+	return re.ReplaceAllString(line, `$1@$2`)
+}
+
+// Search for given actor using his/her login
+func findActor(db *sql.DB, ctx *lib.Ctx, login string) (aid int, ok bool) {
+	rows := lib.QuerySQLWithErr(
+		db,
+		ctx,
+		fmt.Sprintf("select id from gha_actors where login=%s", lib.NValue(1)),
+		login,
+	)
+	defer rows.Close()
+	for rows.Next() {
+		lib.FatalOnError(rows.Scan(&aid))
+		ok = true
+	}
+	lib.FatalOnError(rows.Err())
+	return
+}
+
 // Imports given JSON file.
 func importAffs(jsonFN string) {
+	// Environment context parse
+	var ctx lib.Ctx
+	ctx.Init()
+
+	// Connect to Postgres DB
+	con := lib.PgConn(&ctx)
+	defer con.Close()
+
+	// Parse github_users.json
 	var users GitHubUsers
 	data, err := ioutil.ReadFile(jsonFN)
 	if err != nil {
@@ -29,7 +71,68 @@ func importAffs(jsonFN string) {
 		return
 	}
 	lib.FatalOnError(json.Unmarshal(data, &users))
-	//lib.Printf("%+v\n", users)
+
+	// Process users affiliations
+	emptyVal := struct{}{}
+	loginEmails := make(mapSet)
+	loginNames := make(mapSet)
+	loginAffs := make(mapSet)
+	for _, user := range users {
+		// Email decode ! --> @
+		user.Email = emailDecode(user.Email)
+		login := user.Login
+		// Email
+		email := user.Email
+		if email != "" {
+			_, ok := loginEmails[login]
+			if !ok {
+				loginEmails[login] = stringSet{}
+			}
+			loginEmails[login][email] = emptyVal
+		}
+		// Name
+		name := user.Name
+		if name != "" {
+			_, ok := loginNames[login]
+			if !ok {
+				loginNames[login] = stringSet{}
+			}
+			loginNames[login][name] = emptyVal
+		}
+		// Affiliation
+		aff := user.Affiliation
+		if aff != "NotFound" {
+			_, ok := loginAffs[login]
+			if !ok {
+				loginAffs[login] = stringSet{}
+			}
+			loginAffs[login][aff] = emptyVal
+		}
+	}
+
+	// Login - Names should be 1:1
+	for login, names := range loginNames {
+		if len(names) > 1 {
+			lib.FatalOnError(fmt.Errorf("login has multiple names: %v: %+v\n", login, names))
+		}
+	}
+
+	// Login - Email(s) 1:N
+	for login, emails := range loginEmails {
+		for email := range emails {
+			fmt.Printf("%v - %v\n", login, email)
+		}
+	}
+
+	// Login - Affiliation should be 1:1
+	// There are some ambigous affiliations in github_users.json
+	// For such cases we're picking up the one with most entries
+	// And then if more than 1 with the same number of entries, then pick up first
+	for login, affs := range loginAffs {
+		if len(affs) > 1 {
+			fmt.Printf("%v: %+v\n", login, affs)
+		}
+	}
 }
 
 func main() {
