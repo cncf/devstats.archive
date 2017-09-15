2f8d952745074f87177fe8a538ebeddf2345e1e4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-15 05:37:57 +0000

    Added SQl metric that measues development velocity for companies
    Measures: activity, issues, PRs, commits, comments in given data periods
    groupped by companies
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/company_activity.sql
@@ -0,0 +1,43 @@
+select sub.*,
+  sub.review_comments + sub.issue_comments + sub.commit_comments as comments
+from (
+  select
+    affs.company_name as company,
+    count(distinct ev.id) as activity,
+    count(distinct ev.dup_actor_login) as authors,
+    sum(case ev.type when 'IssuesEvent' then 1 else 0 end) as issues,
+    sum(case ev.type when 'PullRequestEvent' then 1 else 0 end) as prs,
+    sum(case ev.type when 'PushEvent' then 1 else 0 end) as commits,
+    sum(case ev.type when 'PullRequestReviewCommentEvent' then 1 else 0 end) as review_comments,
+    sum(case ev.type when 'IssueCommentEvent' then 1 else 0 end) as issue_comments,
+    sum(case ev.type when 'CommitCommentEvent' then 1 else 0 end) as commit_comments
+  from
+    gha_events ev,
+    gha_actors_affiliations affs
+  where
+    ev.actor_id = affs.actor_id
+    and affs.dt_from <= ev.created_at
+    and affs.dt_to > ev.created_at
+    and ev.created_at >= '{{from}}'
+    and ev.created_at < '{{to}}'
+    and ev.type in (
+      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+    )
+    and dup_actor_login not in (
+      'googlebot', 'k8s-ci-robot', 'k8s-merge-robot', 'k8s-bot',
+      'k8s-teamcity-mesosphere', 'k8s-reviewable', 'k8s-cherrypick-bot',
+      'k8s-publish-robot'
+    )
+  group by
+    affs.company_name
+  order by
+    activity desc
+  ) sub
+where
+  sub.authors > 1
+  and sub.issues > 0
+  and sub.prs > 0
+  and sub.commits > 0
+  and sub.review_comments + sub.issue_comments > 0
+;
