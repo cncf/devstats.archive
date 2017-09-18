c94d7331f959460b8c8bb9a9e22257f7dabfc86e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-18 16:40:16 +0000

    Fixes to crontab, when changed make install to use "go install"
    Go install makes sure that gha2db package is OK, but it puts binaries in
    $GOROOT/bin. GOROOT is not set and $GOROOT/bin is not a part of PATH
    while running from cron. Updated scripts and entry to reflect this while
    still have pacjage maintained by b"go install" and not by cp into
    `/usr/bin`.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cron_gha2db_sync.sh
+++ cron_gha2db_sync.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-set -o pipefail
-date > /tmp/gha2db_sync.err
-date > /tmp/gha2db_sync.log
-gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator' 2>> /tmp/gha2db_sync.err | tee -a /tmp/gha2db_sync.log
+gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator'
