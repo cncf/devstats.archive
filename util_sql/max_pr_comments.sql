3f79bdf6c85e31cdaeb880e622ababf0a2197883
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-19 11:12:45 +0000

    Updated PR comments dashboard to use percentiles instead of max
    Updated all YAML configs and dashboards as well.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/max_pr_comments.sql
@@ -0,0 +1,15 @@
+select
+  count(distinct pl.comment_id) as cnt
+from
+  gha_payloads pl,
+  gha_pull_requests pr
+where
+  pl.pull_request_id = pr.id
+  and pl.comment_id is not null
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+group by
+  pl.pull_request_id
+union select 0 as cnt
+order by cnt desc
+limit 1;
