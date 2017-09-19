ad55692abb86d55552a21ac448094eeb64196b4f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-19 11:12:45 +0000

    Updated PR comments dashboard to use percentiles instead of max
    Updated all YAML configs and dashboards as well.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/pr_comments.sql
+++ metrics/pr_comments.sql
@@ -1,4 +1,5 @@
-select
+create temp table pr_comments as
+select pl.pull_request_id as pr,
   count(distinct pl.comment_id) as cnt
 from
   gha_payloads pl,
@@ -9,7 +10,14 @@ where
   and pr.created_at >= '{{from}}'
   and pr.created_at < '{{to}}'
 group by
-  pull_request_id
-union select 0 as cnt
-order by cnt desc
-limit 1;
+  pl.pull_request_id;
+
+select
+  'pr_comments_median,pr_comments_percentile_85,pr_comments_percentile_95' as name,
+  percentile_disc(0.5) within group (order by cnt asc) as median,
+  percentile_disc(0.85) within group (order by cnt asc) as perentile_85,
+  percentile_disc(0.95) within group (order by cnt asc) as perentile_95
+from
+  pr_comments;
+
+drop table pr_comments;
