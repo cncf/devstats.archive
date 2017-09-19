0688e19173f1b3b2c54344b5b5b516e46776fba5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-19 09:04:02 +0000

    Repo groups DB structure and script to set them
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/repo_groups.sql
@@ -0,0 +1,13 @@
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
+
