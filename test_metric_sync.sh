360c7d46f6548eaec2438b727daa4f4a09154b04
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-25 12:10:34 +0000

    Companies velocity with repo-groups
    Work in progress
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ test_metric_sync.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=2 GHA2DB_DEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_ANNOTATIONS_YAML=metrics/empty.yaml GHA2DB_METRICS_YAML=metrics.yaml GHA2DB_GAPS_YAML=gaps.yaml GHA2DB_LOCAL=1 IDB_DB=test ./gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator'
