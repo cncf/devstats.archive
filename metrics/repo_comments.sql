23f678cf3d4b37dc6defcaaaa6822114e5a1a102
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-26 09:20:01 +0000

    Added "repository comments" dashboard
    No test coverage yet.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/repo_comments.sql
@@ -0,0 +1,28 @@
+select
+  'repo_comments,All' as repo_group,
+  count(distinct event_id) as result
+from
+  gha_texts
+where
+  created_at >= '{{from}}'
+  and created_at < '{{to}}'
+  and actor_login not in ('googlebot')
+  and actor_login not like 'k8s-%'
+union select 'repo_comments,' || r.repo_group as repo_group,
+  count(distinct t.event_id) as result
+from
+  gha_texts t,
+  gha_repos r
+where
+  r.id = t.repo_id
+  and r.repo_group is not null
+  and t.created_at >= '{{from}}'
+  and t.created_at < '{{to}}'
+  and t.actor_login not in ('googlebot')
+  and t.actor_login not like 'k8s-%'
+group by
+  r.repo_group
+order by
+  result desc,
+  repo_group asc
+;
