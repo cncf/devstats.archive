86dcda4d5566095024108b7320704cbb5ee1ab8e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-26 05:56:43 +0000

    Reviewers metric now have repo-group drop-down
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- test_metric_sync.sh
+++ test_metric_sync.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+make || exit 1
+./grafana/influxdb_recreate.sh test
 GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=2 GHA2DB_DEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_ANNOTATIONS_YAML=metrics/empty.yaml GHA2DB_METRICS_YAML=metrics.yaml GHA2DB_GAPS_YAML=gaps.yaml GHA2DB_LOCAL=1 IDB_DB=test ./gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator'
