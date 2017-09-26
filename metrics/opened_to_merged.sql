027dafe34664d0bb8a0e0cc3d06f83958836fdd8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-26 07:03:19 +0000

    Opened to merged repo group drop-down ready
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/opened_to_merged.sql
+++ metrics/opened_to_merged.sql
@@ -12,17 +12,52 @@ where
     select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
   );
 
+create temp table prs_groups as
+select r.repo_group,
+  ipr.issue_id,
+  pr.created_at,
+  pr.merged_at as merged_at
+from
+  gha_issues_pull_requests ipr,
+  gha_pull_requests pr,
+  gha_repos r
+where
+  r.id = ipr.repo_id
+  and r.repo_group is not null
+  and pr.id = ipr.pull_request_id
+  and pr.merged_at is not null
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+  and pr.event_id = (
+    select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
+  );
+
 create temp table tdiffs as
-select extract(epoch from prs.merged_at - prs.created_at) / 3600 as open_to_merge
+select extract(epoch from merged_at - created_at) / 3600 as open_to_merge
 from prs;
 
+create temp table tdiffs_groups as
+select repo_group, extract(epoch from merged_at - created_at) / 3600 as open_to_merge
+from prs_groups;
+
 select
-  'opened_to_merged_percentile_15,opened_to_merged_median,opened_to_merged_percentile_85' as name,
+  'opened_to_merged;All;percentile_15,median,percentile_85' as name,
   percentile_disc(0.15) within group (order by open_to_merge asc) as open_to_merge_15_percentile,
   percentile_disc(0.5) within group (order by open_to_merge asc) as open_to_merge_median,
   percentile_disc(0.85) within group (order by open_to_merge asc) as open_to_merge_85_percentile
 from
-  tdiffs;
+  tdiffs_groups
+union select 'opened_to_merged;' || repo_group || ';percentile_15,median,percentile_85' as name,
+  percentile_disc(0.15) within group (order by open_to_merge asc) as open_to_merge_15_percentile,
+  percentile_disc(0.5) within group (order by open_to_merge asc) as open_to_merge_median,
+  percentile_disc(0.85) within group (order by open_to_merge asc) as open_to_merge_85_percentile
+from
+  tdiffs_groups
+group by
+  repo_group
+;
 
+drop table tdiffs_groups;
+drop table prs_groups;
 drop table tdiffs;
 drop table prs;
