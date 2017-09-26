e5e62ed95df32cc01a75f1abdb76dfae11707535
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-26 06:00:22 +0000

    Fixed infrastructure to test and add single metrics
    Git-ignore files used by testing new metrics on separate InfluxDB.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- add_single_metric.sh
+++ add_single_metric.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_ANNOTATIONS_YAML=annotations.yaml GHA2DB_METRICS_YAML=metrics.yaml GHA2DB_GAPS_YAML=gaps.yaml GHA2DB_LOCAL=1 ./gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator'
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_ANNOTATIONS_YAML=test_annotations.yaml GHA2DB_METRICS_YAML=test_metrics.yaml GHA2DB_GAPS_YAML=gatest_ps.yaml GHA2DB_LOCAL=1 ./gha2db_sync 'kubernetes,kubernetes-client,kubernetes-incubator'
