6d8cc8afed2250aa8813e2d38f4e2b64d691f83a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-28 06:06:04 +0000

    PRs age dashboard wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/prs_age.sql
@@ -0,0 +1,60 @@
+create temp table prs as
+select pr.id, pr.created_at, pr.merged_at
+from
+  gha_pull_requests pr
+where
+  pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+  and pr.event_id = (
+    select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
+  );
+
+create temp table prs_groups as
+select r.repo_group,
+  pr.id,
+  pr.created_at,
+  pr.merged_at as merged_at
+from
+  gha_pull_requests pr,
+  gha_repos r
+where
+  r.id = pr.dup_repo_id
+  and r.repo_group is not null
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+  and pr.event_id = (
+    select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
+  );
+
+create temp table tdiffs as
+select id, extract(epoch from coalesce(merged_at - created_at, now() - created_at)) / 3600 as age
+from prs;
+
+create temp table tdiffs_groups as
+select repo_group, id, extract(epoch from coalesce(merged_at - created_at, now() - created_at)) / 3600 as age
+from prs_groups;
+
+select
+  'prs_age;All;number,median,percentile_85' as name,
+  round(count(distinct id) / {{n}}, 2) as num,
+  percentile_disc(0.5) within group (order by age asc) as age_median,
+  percentile_disc(0.85) within group (order by age asc) as age_85_percentile
+from
+  tdiffs
+union select 'prs_age;' || repo_group || ';number,median,percentile_85' as name,
+  round(count(distinct id) / {{n}}, 2) as num,
+  percentile_disc(0.5) within group (order by age asc) as age_median,
+  percentile_disc(0.85) within group (order by age asc) as age_85_percentile
+from
+  tdiffs_groups
+group by
+  repo_group
+order by
+  num desc,
+  name asc
+;
+
+drop table tdiffs_groups;
+drop table prs_groups;
+drop table tdiffs;
+drop table prs;
