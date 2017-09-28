857117ff8d714bd0df54315aad44324ff925e55b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-28 14:14:41 +0000

    Top commenters dashboard wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/repo_comments.sql
+++ metrics/repo_comments.sql
@@ -1,25 +1,27 @@
 select
   'repo_comments,All' as repo_group,
-  round(count(distinct event_id) / {{n}}, 2) as result
+  round(count(distinct id) / {{n}}, 2) as result
 from
-  gha_texts
+  gha_comments
 where
   created_at >= '{{from}}'
   and created_at < '{{to}}'
-  and actor_login not in ('googlebot')
-  and actor_login not like 'k8s-%'
+  and dup_actor_login not in ('googlebot')
+  and dup_actor_login not like 'k8s-%'
+  and dup_actor_login not like '%-bot'
 union select 'repo_comments,' || r.repo_group as repo_group,
-  round(count(distinct t.event_id) / {{n}}, 2) as result
+  round(count(distinct t.id) / {{n}}, 2) as result
 from
-  gha_texts t,
+  gha_comments t,
   gha_repos r
 where
-  r.id = t.repo_id
+  r.id = t.dup_repo_id
   and r.repo_group is not null
   and t.created_at >= '{{from}}'
   and t.created_at < '{{to}}'
-  and t.actor_login not in ('googlebot')
-  and t.actor_login not like 'k8s-%'
+  and t.dup_actor_login not in ('googlebot')
+  and t.dup_actor_login not like 'k8s-%'
+  and t.dup_actor_login not like '%-bot'
 group by
   r.repo_group
 order by
