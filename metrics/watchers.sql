28af5e23fa864e6fad0f2c0e32e9230a5f454a14
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-09-29 09:03:54 +0000

    New watchers, forks, open issues metric wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/watchers.sql
@@ -0,0 +1,20 @@
+select
+  'contrib;' || r.repo_group || ';watchers,forks,open_issues' as name,
+  max(f.watchers) as watchers,
+  max(f.forks) as forks,
+  max(f.open_issues) as open_issues
+from
+  gha_forkees f,
+  gha_repos r
+where
+  r.id = f.dup_repo_id
+  and r.repo_group is not null
+  and f.updated_at < '{{to}}'
+group by
+  r.repo_group
+order by
+  watchers desc,
+  forks desc,
+  open_issues desc,
+  name asc
+;
