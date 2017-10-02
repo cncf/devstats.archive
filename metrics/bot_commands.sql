76b9178b0b82e37739587ebcb0fc717b65bef454
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-02 09:39:55 +0000

    New dashboard "First non-author activity" ready
    Pending test coverage.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/bot_commands.sql
@@ -0,0 +1,25 @@
+select
+  'bot_commands,' || substring(cmd from 1) as command,
+  count(*) as count_value
+from
+  (
+    select lower(
+        substring(
+          body from '(?i)(?:^|\s)+(/[a-zA-Z]+)(?:$|\s)+'
+        )
+      ) as cmd
+    from
+      gha_texts
+    where
+      created_at >= '{{from}}'
+      and created_at < '{{to}}'
+  ) sel
+where
+  sel.cmd is not null
+group by
+  sel.cmd
+having
+  count(*) >= 5
+order by
+  count_value desc,
+  sel.cmd asc;
