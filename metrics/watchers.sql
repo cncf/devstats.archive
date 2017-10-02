ac8e30a4b0b12953da35a2216865789a4367414c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-02 05:20:57 +0000

    Fixed community stats dashboard
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/watchers.sql
+++ metrics/watchers.sql
@@ -1,5 +1,5 @@
 select
-  'contrib;' || r.repo_group || ';watchers,forks,open_issues' as name,
+  'contrib;' || r.name || ';watchers,forks,open_issues' as name,
   max(f.watchers) as watchers,
   max(f.forks) as forks,
   max(f.open_issues) as open_issues
@@ -8,10 +8,10 @@ from
   gha_repos r
 where
   r.name = f.full_name
-  and r.repo_group is not null
+  and f.updated_at >= '{{from}}'
   and f.updated_at < '{{to}}'
 group by
-  r.repo_group
+  r.name
 order by
   watchers desc,
   forks desc,
