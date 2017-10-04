1da487d6ec4e2155e2501af6370e0352586124a4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-04 05:58:39 +0000

    Changed Grafana icon & favicon to K8s one
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ grafana/change_title_and_icons.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# GRAFANA_DATA=/usr/share/grafana/
+for f in `find ${GRAFANA_DATA} -type f -exec grep -l "Grafana -" "{}" \; | sort | uniq`
+do
+  ls -l "$f"
+  vim -c "%s/Grafana -/K8s DevStats -/g|wq" "$f"
+done
+cp -n ${GRAFANA_DATA}public/img/grafana_icon.svg ${GRAFANA_DATA}public/img/grafana_icon.svg.bak
+cp grafana/img/k8s.svg ${GRAFANA_DATA}public/img/grafana_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}public/img/fav32.png ${GRAFANA_DATA}public/img/fav32.png.bak
+cp grafana/img/k8s32.png ${GRAFANA_DATA}public/img/fav32.png || exit 1
+echo 'OK'
