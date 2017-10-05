020f4ec4ba8b3dfd575369701c21c0d44675b049
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-05 07:32:11 +0000

    Bot commands dashboard now use repo group
    And also addressed Brian's feedback about bot commands list beeing a
    fixed list from K8s docs.
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/bot_commands.sql
+++ metrics/bot_commands.sql
@@ -1,29 +1,50 @@
-select
-  'bot_commands,' || substring(cmd from 2) as command,
-  round(count(*) / {{n}}, 2) as count_value
-from
-  (
-    select lower(
+create temp table matching as
+select cmd, eid, repo_group
+from (
+    select regexp_replace(
+      lower(
         substring(
-          body from '(?i)(?:^|\s)+(/[a-zA-Z]+[a-zA-Z0-9]+)(?:$|\s)+'
+          t.body from '(?i)(?:^|\s)+(/(approve|approve\s+no-issue|approve\s+cancel|area|remove\s+area|assign|unassign|cc|uncc|close|reopen|hold|hold\s+cancel|joke|kind|remove-kind|lgtm|lgtm\s+cancel|ok-to-test|test|test\s+all|retest|priority|remove-priority|sig|remove-sig|release-note|release-note-action-required|release-note-none))(?:$|\s)+'
         )
-      ) as cmd
-    from
-      gha_texts
-    where
-      created_at >= '{{from}}'
-      and created_at < '{{to}}'
-      and actor_login not in ('googlebot')
-      and actor_login not like 'k8s-%'
-      and actor_login not like '%-bot'
-      and actor_login not like '%-robot'
-  ) sel
+      ),
+      '\s+',
+      ' ',
+      'g'
+    ) as cmd,
+    t.event_id as eid,
+    r.repo_group as repo_group
+  from
+    gha_texts t,
+    gha_repos r
+  where
+    r.id = t.repo_id
+    and r.repo_group is not null
+    and t.created_at >= '{{from}}'
+    and t.created_at < '{{to}}'
+    and t.actor_login not in ('googlebot')
+    and t.actor_login not like 'k8s-%'
+    and t.actor_login not like '%-bot'
+    and t.actor_login not like '%-robot'
+  ) sub
 where
-  sel.cmd is not null
+  sub.cmd is not null;
+
+select
+  'bot_commands,' || substring(cmd from 2) || '`All' as command,
+  round(count(distinct eid) / {{n}}, 2) as count_value
+from
+  matching
 group by
-  sel.cmd
-having
-  count(*) >= 5
+  cmd
+union select 'bot_commands,' || substring(cmd from 2) || '`' || repo_group as command,
+  round(count(distinct eid) / {{n}}, 2) as count_value
+from
+  matching
+group by
+  cmd,
+  repo_group
 order by
   count_value desc,
-  sel.cmd asc;
+  command asc;
+
+drop table matching;
