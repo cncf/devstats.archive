89171b24702122ba98b7185a5e91fbf1a0c3978b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-06 11:30:59 +0000

    Updated "SIG mentions using labels" dashboard
    Added "7 Days MA" period.
    Added multi-value drop-down to select SIG categories.
    Pending test coverage
    
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/sig_mentions_labels_sigs_tags.sql
@@ -0,0 +1,22 @@
+select
+  -- string_agg(sub.sig, ',')
+  sub.sig
+from (
+  select sel.sig as sig,
+  count(distinct issue_id) as cnt
+  from (
+    select distinct issue_id,
+      lower(substring(dup_label_name from '(?i)sig/(.*)')) as sig
+    from
+      gha_issues_labels
+    ) sel
+  where
+    sel.sig is not null
+  group by
+    sig
+  order by
+    cnt desc,
+    sig asc
+) sub
+limit 30
+;
