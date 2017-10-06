901302db3e025995992c0475cb7d92b670dae40a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-06 05:29:39 +0000

    Added "All" to repository group drop-down for "Community stats"
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/watchers.sql
+++ metrics/watchers.sql
@@ -1,5 +1,28 @@
 select
-  'contrib;' || r.alias || ';watchers,forks,open_issues' as name,
+  'contrib;All;watchers,forks,open_issues' as name,
+  sum(sub.watchers) as watchers,
+  sum(sub.forks) as forks,
+  sum(sub.open_issues) as open_issues
+from (
+  select r.alias as name,
+    max(f.watchers) as watchers,
+    max(f.forks) as forks,
+    max(f.open_issues) as open_issues
+  from
+    gha_forkees f,
+    gha_repos r
+  where
+    r.name = f.full_name
+    and f.updated_at >= '{{from}}'
+    and f.updated_at < '{{to}}'
+  group by
+    r.alias
+  having
+    max(f.watchers) > 1
+    and max(f.forks) > 1
+    and max(f.open_issues) > 1
+  ) sub
+union select 'contrib;' || r.alias || ';watchers,forks,open_issues' as name,
   max(f.watchers) as watchers,
   max(f.forks) as forks,
   max(f.open_issues) as open_issues
@@ -12,10 +35,6 @@ where
   and f.updated_at < '{{to}}'
 group by
   r.alias
-having
-  max(f.watchers) > 0
-  and max(f.forks) > 0
-  and max(f.open_issues) > 0
 order by
   watchers desc,
   forks desc,
