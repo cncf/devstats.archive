614e4245dad5dc1f00714e6921ae82280cd57e8d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-06 08:45:47 +0000

    WIP: SIG mentions breakdown by category
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/sig_mentions_cats.sql
+++ metrics/sig_mentions_cats.sql
@@ -1,9 +1,10 @@
 select
-  'cat,' || substring(sig from 2) as sig,
-  count(*) as count_value
+  'sig_mentions_texts_cat,' || substring(sig from 2) as sig,
+  round(count(distinct eid) / {{n}}, 2) as count_value
 from
   (
-    select lower(substring(
+    select event_id as eid,
+      lower(substring(
           body from '(?i)(?:^|\s)+(?:@kubernetes/sig-[\w\d-]+)(-bug|-feature-request|-pr-review|-api-review|-misc|-proposal|-design-proposal|-test-failure)s?(?:$|[^\w\d-]+)'
         )) as sig
     from
