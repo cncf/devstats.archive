f96bc817d3dabb32dd666481255f711b0c5930cc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-11 13:39:09 +0000

    Handle https webhooks
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- test_webhook.sh
+++ test_webhook.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 curl -H "Content-Type: application/json" -d "@cmd/webhook/example_webhook_payload.dat" -X POST http://cncftest.io:1982/hook
+curl -H "Content-Type: application/json" -d "@cmd/webhook/example_webhook_payload.dat" -X POST https://cncftest.io:2982/hook
