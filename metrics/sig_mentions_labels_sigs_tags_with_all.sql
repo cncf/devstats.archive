3f3e3b3a7eae0b4798fe8cb532499760e0ed2fb0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-10-17 07:37:18 +0000

    New issues metric wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/sig_mentions_labels_sigs_tags_with_all.sql
@@ -0,0 +1,22 @@
+select
+  sub.sig
+from (
+  select sel.sig as sig,
+  count(distinct issue_id) as cnt
+  from (
+    select distinct issue_id,
+      lower(substring(dup_label_name from '(?i)sig/(.*)')) as sig
+    from
+      gha_issues_labels
+    ) sel
+  where
+    sel.sig is not null
+  group by
+    sig
+  order by
+    cnt desc,
+    sig asc
+  limit 30
+) sub
+union select 'All'
+;
