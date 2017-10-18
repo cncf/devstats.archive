a2dec9ddcd012d5a82f111f7f57c7b64942290ba
root <root@cncftest.io>
2017-10-18 09:27:47 +0000

    Added PRs approved/awaiting approval metric
    Signed-off-by: root <root@cncftest.io>

---
+++ metrics/prs_state.sql
@@ -0,0 +1,67 @@
+create temp table all_prs as
+select distinct id,
+  dup_repo_name
+from
+  gha_issues
+where
+  is_pull_request = true
+  and created_at >= '{{from}}'
+  and created_at < '{{to}}'
+;
+
+create temp table approved_prs as
+select distinct i.id,
+  i.dup_repo_name
+from
+  gha_issues i,
+  gha_comments c,
+  gha_issues_pull_requests ipr,
+  gha_pull_requests pr
+where
+  ipr.issue_id = i.id
+  and ipr.pull_request_id = pr.id
+  and i.event_id = c.event_id
+  and i.is_pull_request = true
+  and i.created_at >= '{{from}}'
+  and i.created_at < '{{to}}'
+  and (
+    pr.merged_at is not null
+    or substring(c.body from '(?i)(?:^|\n|\r)\s*/(approve|lgtm)\s*(?:\n|\r|$)') is not null
+ )
+;
+
+select
+  'prs_approve_state;All;all,approved;awaiting' as name,
+  round(count(distinct prs.id) / {{n}}, 2) as all_prs,
+  round(count(distinct prs.id) filter (where a.id is not null) / {{n}}, 2) as approved,
+  round(count(distinct prs.id) filter (where a.id is null) / {{n}}, 2) as awaiting
+from
+  all_prs prs
+left join 
+  approved_prs a
+on
+  prs.id = a.id
+union select 'prs_approve_state;' || r.repo_group ||';all,approved;awaiting' as name,
+  round(count(distinct prs.id) / {{n}}, 2) as all_prs,
+  round(count(distinct prs.id) filter (where a.id is not null) / {{n}}, 2) as approved,
+  round(count(distinct prs.id) filter (where a.id is null) / {{n}}, 2) as awaiting
+from
+  gha_repos r
+join
+  all_prs prs
+on
+  prs.dup_repo_name = r.name
+  and r.repo_group is not null
+left join 
+  approved_prs a
+on
+  prs.id = a.id
+group by
+  r.repo_group
+order by
+  all_prs desc,
+  name asc
+;
+
+drop table all_prs;
+drop table approved_prs;
