6dd18b131ebe71e24980dae85e57bdfc44504bd7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-02 16:39:24 +0000

    First attempt to import prometheus data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/prometheus/repo_groups.sql
@@ -0,0 +1,17 @@
+-- Add repository groups
+-- This is a stub, repo_group = repo name in Prometheus
+update gha_repos set repo_group = name;
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
+
