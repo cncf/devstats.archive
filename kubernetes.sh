e43cd7da9e0fce03347fece3bf29090b6f011fa3
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-02 07:35:11 +0000

    Added kubernetes-helm org, regenerating data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,7 +4,7 @@ set -o pipefail
 > run.log
 GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 # Next 3 calls takes about: 1h43m, 26m, 17m
-GHA2DB_LOCAL=1 ./gha2db 2015-08-06 0 today now 'kubernetes,kubernetes-client,kubernetes-incubator' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_LOCAL=1 ./gha2db 2015-08-06 0 today now 'kubernetes,kubernetes-client,kubernetes-incubator,kubernetes-helm' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_LOCAL=1 GHA2DB_EXACT=1 ./gha2db 2015-01-01 0 2015-08-14 0 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 ./gha2db 2014-06-02 0 2014-12-31 23 'GoogleCloudPlatform/kubernetes,kubernetes,kubernetes-client' 2>>errors.txt | tee -a run.log || exit 4
 # This generates index and summary tables, it takes 5m32s
