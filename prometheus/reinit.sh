70e509b57af394a4403d5c8e4c039dab62ee0514
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-04 10:41:42 +0000

    Updated reinit script for prometheus
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prometheus/reinit.sh
+++ prometheus/reinit.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./gha2db_sync
+./grafana/influxdb_recreate.sh prom_temp || exit 1
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prom_temp ./gha2db_sync || exit 1
+./grafana/influxdb_recreate.sh prometheus || exit 1
+./idb_backup prom_temp prometheus
