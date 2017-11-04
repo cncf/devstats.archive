61700f9df0e4872f38a04747a7231d9149bd65c4
root <root@cncftest.io>
2017-11-04 11:32:35 +0000

    Update reinit script for k8s
    Signed-off-by: root <root@cncftest.io>

--- kubernetes/reinit_all.sh
+++ kubernetes/reinit_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./grafana/influxdb_recreate.sh temp || exit 1
-IDB_DB=temp GHA2DB_LOCAL=1 GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 ./gha2db_sync || exit 1
+./grafana/influxdb_recreate.sh gha_temp || exit 1
+GHA2DB_PROJECT=kubernetes IDB_DB=gha_temp GHA2DB_LOCAL=1 GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh gha || exit 1
-./idb_backup temp gha
+./idb_backup gha_temp gha
