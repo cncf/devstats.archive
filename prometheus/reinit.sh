0ba095a1d72173fe5e895f7005b100d66943026a
root <root@cncftest.io>
2017-11-04 11:28:30 +0000

    Updated reinit script for prometheus
    Signed-off-by: root <root@cncftest.io>

--- prometheus/reinit.sh
+++ prometheus/reinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./grafana/influxdb_recreate.sh prom_temp || exit 1
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prom_temp ./gha2db_sync || exit 1
+./grafana/influxdb_recreate.sh prometheus_temp || exit 1
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus_temp ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh prometheus || exit 1
-./idb_backup prom_temp prometheus
+./idb_backup prometheus_temp prometheus
