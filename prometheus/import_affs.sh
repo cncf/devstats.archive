536112bfa387e2067bd1fbd8ecfaffeb3cb83462
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-06 14:59:32 +0000

    Added new affiliations for prometheus, k8s pending
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- prometheus/import_affs.sh
+++ prometheus/import_affs.sh
@@ -2,3 +2,6 @@
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./runq scripts/clean_affiliations.sql
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./import_affs github_users.json
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./idb_tags
+echo 'Now when company affiliations changes, You should run all companies releted tags manually, get results and possibly update metrics/prometheus/gaps.yaml'
+echo 'In this case: ./metrics/prometheus/companies_tags.sql'
+echo 'And then regenerate all InfluxData via ./prometheus/reinit.sh'
