b99418b5812453be55c76ccee1959472ef44daf4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-06 06:48:42 +0000

    New 'PRs approval stacked' dashboard for prometheus and updated drop downs for repository group to limit to top 25
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/prometheus/repo_groups_tags.sql
+++ metrics/prometheus/repo_groups_tags.sql
@@ -1,9 +1,19 @@
 select
-  distinct repo_group
-from
-  gha_repos
-where
-  repo_group is not null
-order by
-  repo_group asc
+  sel.repo_group
+from (
+  select r.repo_group,
+    count(distinct e.id) as cnt
+  from
+    gha_repos r,
+    gha_events e
+  where
+    e.repo_id = r.id
+    and r.repo_group is not null
+  group by
+    r.repo_group
+  order by
+    cnt desc,
+    r.repo_group asc
+  limit 25
+  ) sel
 ;
