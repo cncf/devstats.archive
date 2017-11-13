26e781564b329e8bbd6549279f79131e480bc776
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-13 12:56:26 +0000

    Multiple dockerized grafanas, updated docs
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ grafana/opentracing/docker_grafana_first_run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -z "${GRAFANA_PASS}" ]
+then
+  echo "You need to set GRAFANA_PASS environment variable to run this script"
+  exit 1
+fi
+docker run -d -p 3002:3000 -v /var/lib/grafana.opentracing:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana:master
