a146fb3944886106e94d2aacc605dfa80f03fc2b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-13 12:01:38 +0000

    Added opentracing scripts, generated psql database
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- opentracing/opentracing.sh
+++ opentracing/opentracing.sh
@@ -4,7 +4,7 @@ set -o pipefail
 > errors.txt
 > run.log
 GHA2DB_PROJECT=opentracing IDB_DB=opentracing PG_DB=opentracing GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
-GHA2DB_PROJECT=opentracing IDB_DB=opentracing PG_DB=opentracing GHA2DB_LOCAL=1 ./gha2db 2015-01-01 0 today now 'opentracing' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=opentracing IDB_DB=opentracing PG_DB=opentracing GHA2DB_LOCAL=1 ./gha2db 2015-11-26 0 today now 'opentracing' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=opentracing IDB_DB=opentracing PG_DB=opentracing GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
 ./grafana/influxdb_recreate.sh opentracing
 ./opentracing/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
