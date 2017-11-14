cdfa32193d63b47841f331af7d301764f4be0b2e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-14 12:10:06 +0000

    Fixed rebrand scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/opentracing/change_title_and_icons.sh
+++ grafana/opentracing/change_title_and_icons.sh
@@ -10,14 +10,14 @@ do
   ls -l "$f"
   vim -c '%s/"Grafana - "/"OpenTracing DevStats - "/g|wq' "$f"
 done
-cp -n ${GRAFANA_DATA}public/img/grafana_icon.svg ${GRAFANA_DATA}public/img/grafana_icon.svg.bak
-cp grafana/img/opentracing.svg ${GRAFANA_DATA}public/img/grafana_icon.svg || exit 1
-cp -n ${GRAFANA_DATA}public/img/fav32.png ${GRAFANA_DATA}public/img/fav32.png.bak
-cp grafana/img/opentracing32.png ${GRAFANA_DATA}public/img/fav32.png || exit 1
-cp -n ${GRAFANA_DATA}public/img/fav16.png ${GRAFANA_DATA}public/img/fav16.png.bak
-cp grafana/img/opentracing32.png ${GRAFANA_DATA}public/img/fav16.png || exit 1
-cp -n ${GRAFANA_DATA}public/img/fav_dark_16.png ${GRAFANA_DATA}public/img/fav_dark_16.png.bak
-cp grafana/img/opentracing32.png ${GRAFANA_DATA}public/img/fav_dark_16.png || exit 1
-cp -n ${GRAFANA_DATA}public/img/fav_dark_32.png ${GRAFANA_DATA}public/img/fav_dark_32.png.bak
-cp grafana/img/opentracing32.png ${GRAFANA_DATA}public/img/fav_dark_32.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/grafana_icon.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg.bak
+cp grafana/img/opentracing.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav32.png ${GRAFANA_DATA}/public/img/fav32.png.bak
+cp grafana/img/opentracing32.png ${GRAFANA_DATA}/public/img/fav32.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav16.png ${GRAFANA_DATA}/public/img/fav16.png.bak
+cp grafana/img/opentracing32.png ${GRAFANA_DATA}/public/img/fav16.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav_dark_16.png ${GRAFANA_DATA}/public/img/fav_dark_16.png.bak
+cp grafana/img/opentracing32.png ${GRAFANA_DATA}/public/img/fav_dark_16.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav_dark_32.png ${GRAFANA_DATA}/public/img/fav_dark_32.png.bak
+cp grafana/img/opentracing32.png ${GRAFANA_DATA}/public/img/fav_dark_32.png || exit 1
 echo 'OK'
