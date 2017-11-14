cc99a3faaee2fff890fd5b8c33e5f40598177ac0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-14 08:54:56 +0000

    Updated references to static page - it no longer needs to be in /web/ subdirectory
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cron_db_backup.sh
+++ cron_db_backup.sh
@@ -3,6 +3,6 @@ echo "Backup start:" && date
 cd /tmp || exit 1
 sudo -u postgres pg_dump $1 > /tmp/$1.sql || exit 2
 xz -9 /tmp/$1.sql || exit 3
-mv /tmp/$1.sql.xz /var/www/html/web/$1.sql.xz || exit 4
+mv /tmp/$1.sql.xz /var/www/html/$1.sql.xz || exit 4
 date && echo "Backup OK"
 
