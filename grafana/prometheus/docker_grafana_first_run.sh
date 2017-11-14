b294732a1b14108c2e55e3f91bd84d1ca9e21b05
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-14 11:14:45 +0000

    Maybe fixed apparmour docker problem
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/prometheus/docker_grafana_first_run.sh
+++ grafana/prometheus/docker_grafana_first_run.sh
@@ -4,4 +4,4 @@ then
   echo "You need to set GRAFANA_PASS environment variable to run this script"
   exit 1
 fi
-docker run -d -p 3001:3000 -v /var/lib/grafana.prometheus:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana:master
+docker run --security-opt "apparmor:unconfined" -d -p 3001:3000 -v /var/lib/grafana.prometheus:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana:master
