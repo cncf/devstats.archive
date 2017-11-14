1492e0873c9a055561625563d10902a62209ce3e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-14 10:31:16 +0000

    Added config for running kubernetes grafana in docker
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ grafana/kubernetes/docker_grafana_restart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./grafana/kubernetes/docker_grafana_stop.sh
+./grafana/kubernetes/docker_grafana_start.sh
---
+++ grafana/kubernetes/docker_grafana_shell.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo 'Bash into running kubernetes grafana container'
+sudo docker exec -i -t k8s_grafana /bin/bash
