8112e28bcb15a1ab0c29d3d906ed9a8ee089050c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-14 11:14:45 +0000

    Maybe fixed apparmour docker problem
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/opentracing/docker_grafana_first_run.sh
+++ grafana/opentracing/docker_grafana_first_run.sh
@@ -4,4 +4,4 @@ then
   echo "You need to set GRAFANA_PASS environment variable to run this script"
   exit 1
 fi
-docker run -d -p 3002:3000 -v /var/lib/grafana.opentracing:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana:master
+docker run --security-opt "apparmor:unconfined" -d -p 3002:3000 -v /var/lib/grafana.opentracing:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" grafana/grafana:master
