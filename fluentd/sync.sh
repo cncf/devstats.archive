63b25b8c89c95081b71d04556224fed3e73813eb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-15 15:37:19 +0000

    Fluentd and other project - set influx start date
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fluentd/sync.sh
+++ fluentd/sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=fluentd PG_DB=fluentd IDB_DB=fluentd ./gha2db_sync
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=fluentd GHA2DB_STARTDT=2012-07 PG_DB=fluentd IDB_DB=fluentd ./gha2db_sync
