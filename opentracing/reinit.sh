47a173018639d2026241706ec828c0851144776b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-15 15:37:19 +0000

    Fluentd and other project - set influx start date
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- opentracing/reinit.sh
+++ opentracing/reinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./grafana/influxdb_recreate.sh opentracing_temp || exit 1
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=opentracing PG_DB=opentracing IDB_DB=opentracing_temp ./gha2db_sync || exit 1
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=opentracing GHA2DB_STARTDT=2015-11-26 PG_DB=opentracing IDB_DB=opentracing_temp ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh opentracing || exit 1
 ./idb_backup opentracing_temp opentracing
--- prometheus/reinit.sh
+++ prometheus/reinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./grafana/influxdb_recreate.sh prometheus_temp || exit 1
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus_temp ./gha2db_sync || exit 1
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus GHA2DB_STARTDT=2014-03-03 PG_DB=prometheus IDB_DB=prometheus_temp ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh prometheus || exit 1
 ./idb_backup prometheus_temp prometheus
