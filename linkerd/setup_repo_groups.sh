7ce57db896332ec5503286c95e9d04c4f22fe01f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-16 11:17:20 +0000

    Added linkerd
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ linkerd/setup_repo_groups.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Run this script from the repository top level: ./linkerd/setup_repo_groups.sh
+echo "Setting up linkerd repository groups"
+GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./runq scripts/linkerd/repo_groups.sql
---
+++ scripts/linkerd/repo_groups.sql
@@ -0,0 +1,20 @@
+-- Add repository groups
+-- This is a stub, repo_group = repo name in Prometheus
+update gha_repos set repo_group = name;
+
+update gha_repos set alias = name;
+
+-- update gha_repos set repo_group = 'fluentd' where name in ('fluentd', 'fluent/fluentd');
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
