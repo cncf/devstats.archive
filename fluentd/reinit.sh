8109181bf40b39f1eea28caff8b228a48156b83e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-16 07:28:58 +0000

    Added project start date to projects.yaml
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fluentd/reinit.sh
+++ fluentd/reinit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./grafana/influxdb_recreate.sh fluentd_temp || exit 1
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_STARTDT=2012-07 GHA2DB_PROJECT=fluentd PG_DB=fluentd IDB_DB=fluentd_temp ./gha2db_sync || exit 1
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=fluentd PG_DB=fluentd IDB_DB=fluentd_temp ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh fluentd || exit 1
 ./idb_backup fluentd_temp fluentd
--- prometheus/sync.sh
+++ prometheus/sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus GHA2DB_STARTDT=2014-03-03 PG_DB=prometheus IDB_DB=prometheus ./gha2db_sync
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./gha2db_sync
