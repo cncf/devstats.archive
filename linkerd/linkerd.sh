bd5bd8d3a0af4ca15f674529a635dd3e8fd5f7e7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-16 14:52:15 +0000

    Final linkerd scripts, confirmed and already working on cncftest
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- linkerd/linkerd.sh
+++ linkerd/linkerd.sh
@@ -5,7 +5,7 @@ set -o pipefail
 > run.log
 GHA2DB_PROJECT=linkerd IDB_DB=linkerd PG_DB=linkerd GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 GHA2DB_PROJECT=linkerd IDB_DB=linkerd PG_DB=linkerd GHA2DB_LOCAL=1 ./gha2db 2017-01-23 0 today now 'linkerd' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=linkerd IDB_DB=linkerd PG_DB=linkerd GHA2DB_LOCAL=1 GHA2DB_EXACT=1 ./gha2db 2016-01-13 0 today now 'BuoyantIO/linkerd' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=linkerd IDB_DB=linkerd PG_DB=linkerd GHA2DB_LOCAL=1 GHA2DB_EXACT=1 ./gha2db 2016-01-13 0 2017-01-24 0 'BuoyantIO/linkerd' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=linkerd IDB_DB=linkerd PG_DB=linkerd GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
 ./grafana/influxdb_recreate.sh linkerd
 ./linkerd/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
