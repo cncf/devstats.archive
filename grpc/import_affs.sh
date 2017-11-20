a5310d6d88db259d4f7a027c279df7d781aa410e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-20 13:43:07 +0000

    gRPC continue work - wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grpc/import_affs.sh
+++ grpc/import_affs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./runq scripts/clean_affiliations.sql
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./import_affs github_users.json
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./idb_tags
-echo 'Now when company affiliations changes, You should run all companies releted tags manually, get results and possibly update metrics/prometheus/gaps.yaml'
-echo 'In this case: ./metrics/prometheus/companies_tags.sql: ./prometheus/top_n_companies.sh'
-echo 'And then regenerate all InfluxData via ./prometheus/reinit.sh'
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=grpc PG_DB=grpc IDB_DB=grpc ./runq scripts/clean_affiliations.sql
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=grpc PG_DB=grpc IDB_DB=grpc ./import_affs github_users.json
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=grpc PG_DB=grpc IDB_DB=grpc ./idb_tags
+echo 'Now when company affiliations changes, You should run all companies releted tags manually, get results and possibly update metrics/grpc/gaps.yaml'
+echo 'In this case: ./metrics/grpc/companies_tags.sql: ./grpc/top_n_companies.sh'
+echo 'And then regenerate all InfluxData via ./grpc/reinit.sh'
--- grpc/top_n_repos_groups.sh
+++ grpc/top_n_repos_groups.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PG_DB=prometheus ./runq metrics/prometheus/repo_groups_tags_with_all.sql {{lim}} $1 ' sel.repo_group' " string_agg(sel.repo_group, ',')"
+PG_DB=grpc ./runq metrics/grpc/repo_groups_tags_with_all.sql {{lim}} $1 ' sel.repo_group' " string_agg(sel.repo_group, ',')"
