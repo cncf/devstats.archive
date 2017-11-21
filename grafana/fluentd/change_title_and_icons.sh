9b6792a1a9cb19fb5e73f56e892bc54c27d8db1f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-21 09:59:39 +0000

    Fixes for companies list and auth page branding
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/fluentd/change_title_and_icons.sh
+++ grafana/fluentd/change_title_and_icons.sh
@@ -12,6 +12,10 @@ do
 done
 cp -n ${GRAFANA_DATA}/public/img/grafana_icon.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg.bak
 cp grafana/img/fluentd.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg.bak
+cp grafana/img/fluentd.svg ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/grafana_net_logo.svg ${GRAFANA_DATA}/public/img/grafana_net_logo.svg.svg.bak
+cp grafana/img/fluentd.svg ${GRAFANA_DATA}/public/img/grafana_net_logo.svg || exit 1
 cp -n ${GRAFANA_DATA}/public/img/fav32.png ${GRAFANA_DATA}/public/img/fav32.png.bak
 cp grafana/img/fluentd32.png ${GRAFANA_DATA}/public/img/fav32.png || exit 1
 cp -n ${GRAFANA_DATA}/public/img/fav16.png ${GRAFANA_DATA}/public/img/fav16.png.bak
