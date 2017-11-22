8991b8bc0eb7c06401cccdcf14fe844164695415
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-22 18:16:36 +0000

    Fix top unknowns SQL script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/top_unknowns.sql
+++ util_sql/top_unknowns.sql
@@ -1,27 +1,31 @@
 select
-  e.dup_actor_login as actor,
+  a.login as actor,
+  e.actor_id,
   count(distinct e.id) as cnt
 from
+  gha_actors a,
   gha_events e
 left join
-  gha_actors_affiliations a
+  gha_actors_affiliations af
 on
-  e.actor_id = a.actor_id
-  and a.dt_from <= e.created_at
-  and a.dt_to > e.created_at
+  e.actor_id = af.actor_id
+  and af.dt_from <= e.created_at
+  and af.dt_to > e.created_at
 where
-  e.dup_actor_login not in ('googlebot')
-  and e.dup_actor_login not like 'k8s-%'
-  and e.dup_actor_login not like '%-bot'
-  and e.dup_actor_login not like '%-robot'
+  a.id = e.actor_id
+  and a.login not in ('googlebot')
+  and a.login not like 'k8s-%'
+  and a.login not like '%-bot'
+  and a.login not like '%-robot'
   and e.type in (
     'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
     'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
   )
-  and a.actor_id is null
+  and af.actor_id is null
   and e.created_at >= now() - '{{ago}}'::interval
 group by
-  e.dup_actor_login
+  a.login,
+  e.actor_id
 order by
   cnt desc
 limit
