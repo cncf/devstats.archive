2fc53ed8d15a8709923b22081b7080bdfd0f9732
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-11-22 10:30:40 +0000

    Changes contributing companies algorithm in other projects too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/linkerd/num_stats.sql
+++ metrics/linkerd/num_stats.sql
@@ -1,33 +1,46 @@
 select
-  'num_stats;All;companies,developers' as name,
+  'num_stats;All;companies,developers,unknowns' as name,
   count(distinct affs.company_name) as n_companies,
-  count(distinct ev.dup_actor_login) as n_authors
+  count(distinct ev.dup_actor_login) as n_authors,
+  count(distinct ev.dup_actor_login) filter (where affs.company_name is null) as n_unknown_authors
 from
-  gha_events ev,
+  gha_events ev
+left join
   gha_actors_affiliations affs
-where
+on
   ev.actor_id = affs.actor_id
   and affs.dt_from <= ev.created_at
   and affs.dt_to > ev.created_at
-  and ev.created_at >= '{{from}}'
+where
+  ev.created_at >= '{{from}}'
   and ev.created_at < '{{to}}'
-  and affs.company_name not in ('Self')
-union select 'num_stats;' || r.repo_group || ';companies,developers' as name,
+  and ev.type in (
+    'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+    'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+  )
+union select 'num_stats;' || r.repo_group || ';companies,developers,unknowns' as name,
   count(distinct affs.company_name) as n_companies,
-  count(distinct ev.dup_actor_login) as n_authors
+  count(distinct ev.dup_actor_login) as n_authors,
+  count(distinct ev.dup_actor_login) filter (where affs.company_name is null) as n_unknown_authors
 from
-  gha_events ev,
-  gha_actors_affiliations affs,
-  gha_repos r
+  gha_repos r,
+  gha_events ev
+left join
+  gha_actors_affiliations affs
+on
+  ev.actor_id = affs.actor_id
+  and affs.dt_from <= ev.created_at
+  and affs.dt_to > ev.created_at
 where
   r.name = ev.dup_repo_name
   and r.repo_group is not null
-  and ev.actor_id = affs.actor_id
-  and affs.dt_from <= ev.created_at
-  and affs.dt_to > ev.created_at
   and ev.created_at >= '{{from}}'
   and ev.created_at < '{{to}}'
-  and affs.company_name not in ('Self')
+  and ev.type in (
+    'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+    'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+    'PushEvent'
+  )
 group by
   r.repo_group
 order by
