0f587590fac166e7345bacd86ba4eee535f67908
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-15 17:07:24 +0000

    Added info to annotation test scripts that one more series drop is needed
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- kubernetes/annotations.sh
+++ kubernetes/annotations.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo 'To clear old annotations use: `influx -host '172.17.0.1' -username gha_admin -password ... -database gha`, then `drop series from annotations; drop series from quick_ranges;`'
+echo 'To clear old annotations use: `influx -host '172.17.0.1' -username gha_admin -password ... -database gha`, then `drop series from annotations; drop series from quick_ranges; drop series from computed;`'
 GHA2DB_PROJECT=kubernetes GHA2DB_DEBUG=1 GHA2DB_LOCAL=1 ./annotations
