6815eba0479879397bc00e84480117234610fa2b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-15 17:07:24 +0000

    Added info to annotation test scripts that one more series drop is needed
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fluentd/annotations.sh
+++ fluentd/annotations.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo 'To clear old annotations use: `influx -host '172.17.0.1' -username gha_admin -password ... -database fluentd`, then `drop series from annotations; drop series from quick_ranges;`'
+echo 'To clear old annotations use: `influx -host '172.17.0.1' -username gha_admin -password ... -database fluentd`, then `drop series from annotations; drop series from quick_ranges; drop series from computed;`'
 GHA2DB_PROJECT=fluentd GHA2DB_DEBUG=1 GHA2DB_LOCAL=1 PG_DB=fluentd IDB_DB=fluentd ./annotations
