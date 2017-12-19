a1c3d96179be15963f91fada262db5f1709404c1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-19 11:18:11 +0000

    Sync with cncf/artwork icons paths
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 # This script assumes that You have cncf/artwork cloned in ~/dev/cncf/artwork 
-cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_mask_icon.svg
-cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_mask_icon.svg
-cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_mask_icon.svg
-cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_mask_icon.svg
-cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_mask_icon.svg
-cp ~/dev/cncf/artwork/grpc/color/grpc-color.svg /usr/share/grafana.grpc/public/img/grafana_icon.svg
-cp ~/dev/cncf/artwork/grpc/color/grpc-color.svg /usr/share/grafana.grpc/public/img/grafana_com_auth_icon.svg
-cp ~/dev/cncf/artwork/grpc/color/grpc-color.svg /usr/share/grafana.grpc/public/img/grafana_net_logo.svg
-cp ~/dev/cncf/artwork/grpc/color/grpc-color.svg /usr/share/grafana.grpc/public/img/grafana_mask_icon.svg
+cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_icon.svg || exit 1
+cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_com_auth_icon.svg || exit 2
+cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_net_logo.svg || exit 3
+cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.svg /usr/share/grafana.k8s/public/img/grafana_mask_icon.svg || exit 4
+cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_icon.svg || exit 5
+cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_com_auth_icon.svg || exit 6
+cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_net_logo.svg || exit 7
+cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.svg /usr/share/grafana.prometheus/public/img/grafana_mask_icon.svg || exit 8
+cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_icon.svg || exit 9
+cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_com_auth_icon.svg || exit 10
+cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_net_logo.svg || exit 11
+cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.svg /usr/share/grafana.opentracing/public/img/grafana_mask_icon.svg || exit 12
+cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_icon.svg || exit 13
+cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_com_auth_icon.svg || exit 14
+cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_net_logo.svg || exit 15
+cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.svg /usr/share/grafana.fluentd/public/img/grafana_mask_icon.svg || exit 16
+cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_icon.svg || exit 17
+cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_com_auth_icon.svg || exit 18
+cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_net_logo.svg || exit 19
+cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.svg /usr/share/grafana.linkerd/public/img/grafana_mask_icon.svg || exit 20
+cp ~/dev/cncf/artwork/grpc/icon/color/grpc-icon-color.svg /usr/share/grafana.grpc/public/img/grafana_icon.svg || exit 21
+cp ~/dev/cncf/artwork/grpc/icon/color/grpc-icon-color.svg /usr/share/grafana.grpc/public/img/grafana_com_auth_icon.svg || exit 22
+cp ~/dev/cncf/artwork/grpc/icon/color/grpc-icon-color.svg /usr/share/grafana.grpc/public/img/grafana_net_logo.svg || exit 23
+cp ~/dev/cncf/artwork/grpc/icon/color/grpc-icon-color.svg /usr/share/grafana.grpc/public/img/grafana_mask_icon.svg || exit 24
+echo 'OK'
