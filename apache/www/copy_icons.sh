53b845030f7d1e5096c75a4182313aa1d97ed789
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-19 11:18:11 +0000

    Sync with cncf/artwork icons paths
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # This script assumes that You have cncf/artwork cloned in ~/dev/cncf/artwork 
-cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.png /var/www/html/img/
-cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.png /var/www/html/img/
-cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.png /var/www/html/img/
-cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.png /var/www/html/img/
-cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.png /var/www/html/img/
-cp ~/dev/cncf/artwork/grpc/color/grpc-color.png /var/www/html/img/
+cp ~/dev/cncf/artwork/kubernetes/icon/color/kubernetes-icon-noborder-color.png /var/www/html/img/ || exit 1
+cp ~/dev/cncf/artwork/prometheus/icon/color/prometheus-icon-color.png /var/www/html/img/ || exit 2
+cp ~/dev/cncf/artwork/opentracing/icon/color/opentracing-icon-color.png /var/www/html/img/ || exit 3
+cp ~/dev/cncf/artwork/fluentd/icon/color/fluentd-icon-color.png /var/www/html/img/ || exit 4
+cp ~/dev/cncf/artwork/linkerd/icon/color/linkerd-icon-color.png /var/www/html/img/ || exit 5
+cp ~/dev/cncf/artwork/grpc/icon/color/grpc-icon-color.png /var/www/html/img/ || exit 6
+echo 'OK'
