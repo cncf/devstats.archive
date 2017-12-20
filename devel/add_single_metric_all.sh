1d9a4248bfffce8ad2401e1f05b288167d701002
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-20 11:57:45 +0000

    Fix dangerous PG/IDB name missing in test scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/add_single_metric_all.sh
+++ devel/add_single_metric_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-GHA2DB_PROJECT=kubernetes ./devel/add_single_metric.sh
-GHA2DB_PROJECT=prometheus ./devel/add_single_metric.sh
-GHA2DB_PROJECT=opentracing ./devel/add_single_metric.sh
-GHA2DB_PROJECT=fluentd ./devel/add_single_metric.sh
-GHA2DB_PROJECT=linkerd ./devel/add_single_metric.sh
-GHA2DB_PROJECT=grpc ./devel/add_single_metric.sh
+GHA2DB_PROJECT=kubernetes PG_DB=gha IDB_DB=gha ./devel/add_single_metric.sh
+GHA2DB_PROJECT=prometheus PG_DB=prometheus IDB_DB=prometheus ./devel/add_single_metric.sh
+GHA2DB_PROJECT=opentracing PG_DB=opentracing IDB_DB=opentracing ./devel/add_single_metric.sh
+GHA2DB_PROJECT=fluentd PG_DB=fluentd IDB_DB=fluentd ./devel/add_single_metric.sh
+GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./devel/add_single_metric.sh
+GHA2DB_PROJECT=grpc PG_DB=grpc IDB_DB=grpc ./devel/add_single_metric.sh
