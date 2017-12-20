3a19ffb109307b32a136977c13ca8daf0b3411a2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-20 12:28:10 +0000

    One more fix
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/add_single_metric.sh
+++ devel/add_single_metric.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_GAPS_YAML=devel/test_gaps.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=gha IDB_DB=gha ./gha2db_sync
+if [ -z "${PG_DB}" ]
+then
+  echo "You need to set PG_DB environment variable to run this script"
+  exit 1
+fi
+if [ -z "${IDB_DB}" ]
+then
+  echo "You need to set IDB_DB environment variable to run this script"
+  exit 1
+fi
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_GAPS_YAML=devel/test_gaps.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 ./gha2db_sync
