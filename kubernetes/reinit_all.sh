0f2c21f62010f72dc53551656c439600f091ed95
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-12-20 11:57:45 +0000

    Fix dangerous PG/IDB name missing in test scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- kubernetes/reinit_all.sh
+++ kubernetes/reinit_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./grafana/influxdb_recreate.sh gha_temp || exit 1
-GHA2DB_PROJECT=kubernetes IDB_DB=gha_temp GHA2DB_LOCAL=1 GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 ./gha2db_sync || exit 1
+GHA2DB_PROJECT=kubernetes IDB_DB=gha_temp GHA2DB_LOCAL=1 GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 PG_DB=gha IDB_DB=gha ./gha2db_sync || exit 1
 ./grafana/influxdb_recreate.sh gha || exit 1
 ./idb_backup gha_temp gha
