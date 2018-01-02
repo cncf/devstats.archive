f6a0b5a9580427006580381ca85c553d451b2c34
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-02 15:26:46 +0000

    Added script to do partial sync for a new metric (without InfluxDB reset)
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/sync_single_metric.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ -z "${PG_DB}" ]
+then
+  echo "You need to set PG_DB environment variable to run this script"
+  exit 1
+fi
+if [ -z "${IDB_DB}" ]
+then
+  echo "You need to set IDB_DB environment variable to run this script"
+  exit 1
+fi
+GHA2DB_CMDDEBUG=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_GAPS_YAML=devel/test_gaps.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 ./gha2db_sync
