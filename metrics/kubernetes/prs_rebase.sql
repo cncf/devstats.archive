242dca262bf1861372b4ea4f3df493132741f622
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-02 15:19:22 +0000

    (All) needs rebase PRs reimplement - wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/prs_rebase.sql
+++ metrics/kubernetes/prs_rebase.sql
@@ -13,7 +13,6 @@ where
   and i.number = pr.number
   and i.dup_repo_id = pr.dup_repo_id
   and i.is_pull_request = true
-  and i.created_at >= '{{from}}'
   and i.created_at < '{{to}}'
 group by
   i.id,
@@ -21,8 +20,9 @@ group by
 ;
 
 create temp table rebases as
-select distinct i.id,
-  i.repo_name
+select distinct il.issue_id,
+  i.repo_name,
+  max(il.dup_created_at) as rebase_dt
 from
   issues i,
   gha_issues_labels il
@@ -30,27 +30,55 @@ where
   il.issue_id = i.id
   and il.dup_label_name = 'needs-rebase'
   and il.dup_created_at >= i.created
+  and il.dup_created_at < '{{to}}'
   and (
     i.closed is null
-    or il.dup_created_at < i.closed
+    or (
+      il.dup_created_at <= i.closed
+      and i.closed >= '{{to}}'
+    )
   )
+group by
+  il.issue_id,
+  i.repo_name
+;
+
+create temp table removed_rebases as
+select il.issue_id,
+  r.repo_name,
+  min(il.dup_created_at) as removed_dt
+from
+  gha_issues_labels il,
+  rebases r
+where
+  r.issue_id = il.issue_id
+  and il.dup_created_at > r.rebase_dt
+  and il.dup_created_at < '{{to}}'
+group by
+  il.issue_id,
+  r.repo_name
 ;
 
 select
-  'pr_needs_rebase,' || r.repo_group as repo_group,
-  round(count(distinct rb.id) / {{n}}, 2) as need_rebase_count
+  'pr_needs_rebase,' || re.repo_group as repo_group,
+  count(distinct r.issue_id) as need_rebase_count
 from
-  rebases rb,
-  gha_repos r
+  gha_repos re
+join
+  rebases r
+on
+  r.repo_name = re.name
+  and re.repo_group is not null
+left join
+  removed_rebases rr
+on
+  r.issue_id = rr.issue_id
 where
-  r.name = rb.repo_name
-  and r.repo_group is not null
+  rr.issue_id is null
 group by
-  r.repo_group
-order by
-  need_rebase_count desc,
-  repo_group asc
+  re.repo_group
 ;
 
+drop table removed_rebases;
 drop table rebases;
 drop table issues;
