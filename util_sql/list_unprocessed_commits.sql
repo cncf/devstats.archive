4129ddea6062c8b2860a07aa0409234a67107705
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-05 14:17:48 +0000

    Added per file commits granularity - very wip
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/list_unprocessed_commits.sql
+++ util_sql/list_unprocessed_commits.sql
@@ -1,17 +1,16 @@
 select 
-  distinct sub.sha
+  distinct sub.sha, sub.repo
 from (
-  select distinct sha from gha_branches
-  union select distinct commit_id as sha from gha_comments
-  union select distinct original_commit_id as sha from gha_comments
-  union select distinct sha from gha_commits
-  union select distinct sha from gha_pages
-  union select distinct head as sha from gha_payloads
-  union select distinct befor as sha from gha_payloads
-  union select distinct commit as sha from gha_payloads
-  union select distinct base_sha as sha from gha_pull_requests
-  union select distinct head_sha as sha from gha_pull_requests
-  union select distinct merge_commit_sha as sha from gha_pull_requests
+  select distinct commit_id as sha, dup_repo_name as repo from gha_comments
+  union select distinct original_commit_id as sha, dup_repo_name as repo from gha_comments
+  union select distinct sha, dup_repo_name as repo from gha_commits
+  union select distinct sha, dup_repo_name as repo from gha_pages
+  union select distinct head as sha, dup_repo_name as repo from gha_payloads
+  union select distinct befor as sha, dup_repo_name as repo from gha_payloads
+  union select distinct commit as sha, dup_repo_name as repo from gha_payloads
+  union select distinct base_sha as sha, dup_repo_name as repo from gha_pull_requests
+  union select distinct head_sha as sha, dup_repo_name as repo from gha_pull_requests
+  union select distinct merge_commit_sha as sha, dup_repo_name as repo from gha_pull_requests
   ) sub
 where
   sub.sha is not null
