b0dfa02ba4715fca6e0c753fb9e55000b8eff5ba
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-08 15:59:21 +0000

    Major refactor, separate table for only sha-path connection, new table to connect events to SHAs - there can be multiple events for single SHA
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/list_unprocessed_commits.sql
+++ util_sql/list_unprocessed_commits.sql
@@ -1,16 +1,16 @@
 select 
-  distinct sub.sha, sub.repo, sub.event_id
+  distinct sub.sha, sub.repo
 from (
-  select distinct commit_id as sha, dup_repo_name as repo, event_id from gha_comments
-  union select distinct original_commit_id as sha, dup_repo_name as repo, event_id from gha_comments
-  union select distinct sha, dup_repo_name as repo, event_id from gha_commits
-  union select distinct sha, dup_repo_name as repo, event_id from gha_pages
-  union select distinct head as sha, dup_repo_name as repo, event_id from gha_payloads
-  union select distinct befor as sha, dup_repo_name as repo, event_id from gha_payloads
-  union select distinct commit as sha, dup_repo_name as repo, event_id from gha_payloads
-  union select distinct base_sha as sha, dup_repo_name as repo, event_id from gha_pull_requests
-  union select distinct head_sha as sha, dup_repo_name as repo, event_id from gha_pull_requests
-  union select distinct merge_commit_sha as sha, dup_repo_name as repo, event_id from gha_pull_requests
+  select distinct commit_id as sha, dup_repo_name as repo from gha_comments
+  union select distinct original_commit_id as sha, dup_repo_name as repo from gha_comments
+  union select distinct sha, dup_repo_name as repo from gha_commits
+  union select distinct sha, dup_repo_name as repo from gha_pages
+  union select distinct head as sha, dup_repo_name as repo from gha_payloads
+  union select distinct befor as sha, dup_repo_name as repo from gha_payloads
+  union select distinct commit as sha, dup_repo_name as repo from gha_payloads
+  union select distinct base_sha as sha, dup_repo_name as repo from gha_pull_requests
+  union select distinct head_sha as sha, dup_repo_name as repo from gha_pull_requests
+  union select distinct merge_commit_sha as sha, dup_repo_name as repo from gha_pull_requests
   ) sub
 where
   sub.sha is not null
