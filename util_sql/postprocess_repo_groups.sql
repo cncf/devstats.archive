b3c6c6ed3831c4e2427eb5c4189ab76704da0efc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-09 15:09:01 +0000

    Postprocess repo groups - take a look on PR reviews too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/postprocess_repo_groups.sql
+++ util_sql/postprocess_repo_groups.sql
@@ -4,6 +4,29 @@ update
 set
   repo_group = 'Cluster lifecycle'
 where
-  path like 'kubernetes/kubernetes/cmd/kubeadm/%'
-  or path like 'kubernetes/kubernetes/cluster/%'
+  repo_group is null
+  and (
+    path like 'kubernetes/kubernetes/cmd/kubeadm/%'
+    or path like 'kubernetes/kubernetes/cluster/%'
+  )
+;
+
+update
+  gha_events_commits_files
+set
+  repo_group = 'Cluster lifecycle'
+where
+  repo_group is null
+  and event_id in (
+    select
+      event_id
+    from
+      gha_comments
+    where
+      dup_repo_name = 'kubernetes/kubernetes'
+      and (
+        path like 'cmd/kubeadm/%'
+        or path like 'cluster/%'
+      )
+  )
 ;
