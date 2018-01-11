6787373cdb770752d7229613c0fed4c9a32dc6ba
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-11 13:46:08 +0000

    Added project activity util SQL
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/proj_activity.sql
@@ -0,0 +1,18 @@
+select
+  f.dt,
+  e.type as event_type,
+  e.dup_actor_login as actor,
+  f.path,
+  f.size,
+  f.dup_repo_name as repo,
+  f.repo_group,
+  f.sha as commit_SHA
+from
+  gha_events_commits_files f,
+  gha_events e
+where
+  e.id = f.event_id
+order by
+  f.dt desc
+limit 100
+;
