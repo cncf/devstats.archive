4afccb39e00efa02648d8eaf2f20d5e658fe1333
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-12 15:09:50 +0000

    Updated 'Approvers' dashboard to use commit files + updated test coverage
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/approvers.sql
+++ metrics/kubernetes/approvers.sql
@@ -8,25 +8,34 @@ where
   and substring(body from '(?i)(?:^|\n|\r)\s*/approve\s*(?:\n|\r|$)') is not null;
 
 select
-  'approvers,' || r.repo_group as repo_group,
-  count(distinct e.dup_actor_login) as result
-from
-  gha_events e,
-  gha_repos r
+  sub.repo_group,
+  count(distinct sub.actor) as result
+from (
+  select 'approvers,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    e.dup_actor_login as actor
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    e.repo_id = r.id
+    and e.dup_actor_login not in ('googlebot')
+    and e.dup_actor_login not like 'k8s-%'
+    and e.dup_actor_login not like '%-bot'
+    and e.dup_actor_login not like '%-robot'
+    and e.id in (
+      select event_id
+      from
+        matching
+    )
+  ) sub
 where
-  e.repo_id = r.id
-  and r.repo_group is not null
-  and e.dup_actor_login not in ('googlebot')
-  and e.dup_actor_login not like 'k8s-%'
-  and e.dup_actor_login not like '%-bot'
-  and e.dup_actor_login not like '%-robot'
-  and e.id in (
-    select event_id
-    from
-      matching
-  )
+  sub.repo_group is not null
 group by
-  r.repo_group
+  sub.repo_group
 order by
   result desc,
   repo_group asc
