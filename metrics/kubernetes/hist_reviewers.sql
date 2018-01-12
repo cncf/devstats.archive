5aab98cf99838136164284cdc127efa4656b3930
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-12 14:30:49 +0000

    Updated 'reviewers histogram' metric to use new commit data and updated test coverage
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/hist_reviewers.sql
+++ metrics/kubernetes/hist_reviewers.sql
@@ -15,36 +15,46 @@ where
   and type in ('PullRequestReviewCommentEvent');
 
 select
-  'reviewers_hist,' || r.repo_group as repo_group,
-  e.dup_actor_login as actor,
-  count(distinct e.id) as reviews
-from
-  gha_events e,
-  gha_repos r
+  sub.repo_group,
+  sub.actor,
+  count(distinct sub.id) as reviews
+from (
+  select 'reviewers_hist,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    e.dup_actor_login as actor,
+    e.id
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    e.repo_id = r.id
+    and e.dup_actor_login not in ('googlebot')
+    and e.dup_actor_login not like 'k8s-%'
+    and e.dup_actor_login not like '%-bot'
+    and e.dup_actor_login not like '%-robot'
+    and e.id in (
+      select min(event_id)
+      from
+        gha_issues_events_labels
+      where
+        {{period:created_at}}
+        and label_name in ('lgtm', 'approved')
+      group by
+        issue_id
+      union select event_id from matching
+      union select event_id from reviews
+    )
+  ) sub
 where
-  e.repo_id = r.id
-  and r.repo_group is not null
-  and e.dup_actor_login not in ('googlebot')
-  and e.dup_actor_login not like 'k8s-%'
-  and e.dup_actor_login not like '%-bot'
-  and e.dup_actor_login not like '%-robot'
-  and e.id in (
-    select min(event_id)
-    from
-      gha_issues_events_labels
-    where
-      {{period:created_at}}
-      and label_name in ('lgtm', 'approved')
-    group by
-      issue_id
-    union select event_id from matching
-    union select event_id from reviews
-  )
+  sub.repo_group is not null
 group by
-  r.repo_group,
-  e.dup_actor_login
+  sub.repo_group,
+  sub.actor
 having
-  count(distinct e.id) >= 3
+  count(distinct sub.id) >= 3
 union select 'reviewers_hist,All' as repo_group,
   dup_actor_login as actor,
   count(distinct id) as reviews
