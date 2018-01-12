b77e4e8638ff14d8d09653fdc5ffae6a561a1666
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-12 09:56:23 +0000

    Reviewers dashboard updated to use new SQL using commit files repo groups definition
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/reviewers.sql
+++ metrics/kubernetes/reviewers.sql
@@ -26,32 +26,44 @@ where
       issue_id
     union select event_id from matching
   )
-union select 'reviewers,' || r.repo_group as repo_group,
-  count(distinct e.dup_actor_login) as result
-from
-  gha_events e,
-  gha_repos r
+union select sub.repo_group,
+  count(distinct sub.actor) as result
+from (
+  select 'reviewers,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    e.dup_actor_login as actor
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    e.repo_id = r.id
+    and e.dup_actor_login not in ('googlebot')
+    and e.dup_actor_login not like 'k8s-%'
+    and e.dup_actor_login not like '%-bot'
+    and e.dup_actor_login not like '%-robot'
+    and e.id in (
+      select min(event_id)
+      from
+        gha_issues_events_labels
+      where
+        created_at >= '{{from}}'
+        and created_at < '{{to}}'
+        and label_name in ('lgtm', 'approved')
+      group by
+        issue_id
+      union select event_id from matching
+    )
+  ) sub
 where
-  e.repo_id = r.id
-  and r.repo_group is not null
-  and e.dup_actor_login not in ('googlebot')
-  and e.dup_actor_login not like 'k8s-%'
-  and e.dup_actor_login not like '%-bot'
-  and e.dup_actor_login not like '%-robot'
-  and e.id in (
-    select min(event_id)
-    from
-      gha_issues_events_labels
-    where
-      created_at >= '{{from}}'
-      and created_at < '{{to}}'
-      and label_name in ('lgtm', 'approved')
-    group by
-      issue_id
-    union select event_id from matching
-  )
-group by r.repo_group
-order by result desc, repo_group asc
+  sub.repo_group is not null
+group by
+  sub.repo_group
+order by
+  result desc,
+  repo_group asc
 ;
 
 drop table matching;
