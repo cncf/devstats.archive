b1e0343a563816037d926c85b086a26eb022bab6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-15 08:32:32 +0000

    Updated 'Approvers histogram' to use git files data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/hist_approvers.sql
+++ metrics/kubernetes/hist_approvers.sql
@@ -4,32 +4,43 @@ from
   gha_texts
 where
   {{period:created_at}}
-  and substring(body from '(?i)(?:^|\n|\r)\s*/(?:lgtm|approve)\s*(?:\n|\r|$)') is not null;
+  and substring(body from '(?i)(?:^|\n|\r)\s*/(?:approve)\s*(?:\n|\r|$)') is not null;
 
 select
-  'approvers_hist,' || r.repo_group as repo_group,
-  e.dup_actor_login as actor,
-  count(distinct e.id) as approves
-from
-  gha_events e,
-  gha_repos r
+  sub.repo_group,
+  sub.actor,
+  count(distinct sub.id) as approves
+from (
+  select 'approvers_hist,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    e.dup_actor_login as actor,
+    e.id
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    e.repo_id = r.id
+    and r.repo_group is not null
+    and e.dup_actor_login not in ('googlebot')
+    and e.dup_actor_login not like 'k8s-%'
+    and e.dup_actor_login not like '%-bot'
+    and e.dup_actor_login not like '%-robot'
+    and e.id in (
+      select event_id
+      from
+        matching
+      )
+    ) sub
 where
-  e.repo_id = r.id
-  and r.repo_group is not null
-  and e.dup_actor_login not in ('googlebot')
-  and e.dup_actor_login not like 'k8s-%'
-  and e.dup_actor_login not like '%-bot'
-  and e.dup_actor_login not like '%-robot'
-  and e.id in (
-    select event_id
-    from
-      matching
-  )
+  sub.repo_group is not null
 group by
-  r.repo_group,
-  e.dup_actor_login
+  sub.repo_group,
+  sub.actor
 having
-  count(distinct e.id) >= 2
+  count(distinct sub.id) >= 2
 union select 'approvers_hist,All' as repo_group,
   dup_actor_login as actor,
   count(distinct id) as approves
@@ -56,3 +67,4 @@ order by
 ;
 
 drop table matching;
+--
