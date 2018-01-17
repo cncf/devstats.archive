619c2a2cd0fe1a363ed19bbf5b64d3cd08749a32
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 12:29:12 +0000

    Updated 'PR authors companies histogram' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/hist_pr_companies.sql
+++ metrics/kubernetes/hist_pr_companies.sql
@@ -1,27 +1,37 @@
 select
-  'hist_pr_companies,' || r.repo_group as repo_group,
-  a.company_name as company,
-  count(distinct pr.id) as prs
-from
-  gha_pull_requests pr,
-  gha_repos r,
-  gha_actors_affiliations a
+  sub.repo_group,
+  sub.company,
+  count(distinct sub.id) as prs
+from (
+  select 'hist_pr_companies,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    a.company_name as company,
+    pr.id
+  from
+    gha_repos r,
+    gha_actors_affiliations a,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    pr.dup_actor_id = a.actor_id
+    and a.dt_from <= pr.created_at
+    and a.dt_to > pr.created_at
+    and {{period:pr.created_at}}
+    and pr.dup_repo_id = r.id
+    and pr.dup_actor_login not in ('googlebot')
+    and pr.dup_actor_login not like 'k8s-%'
+    and pr.dup_actor_login not like '%-bot'
+    and pr.dup_actor_login not like '%-robot'
+  ) sub
 where
-  pr.dup_actor_id = a.actor_id
-  and a.dt_from <= pr.created_at
-  and a.dt_to > pr.created_at
-  and {{period:pr.created_at}}
-  and pr.dup_repo_id = r.id
-  and r.repo_group is not null
-  and pr.dup_actor_login not in ('googlebot')
-  and pr.dup_actor_login not like 'k8s-%'
-  and pr.dup_actor_login not like '%-bot'
-  and pr.dup_actor_login not like '%-robot'
+  sub.repo_group is not null
 group by
-  r.repo_group,
-  a.company_name
+  sub.repo_group,
+  sub.company
 having
-  count(distinct pr.id) >= 3
+  count(distinct sub.id) >= 3
 union select 'hist_pr_companies,All' as repo_group,
   a.company_name as company,
   count(distinct pr.id) as prs
