3f3f597f5ec49b1bab6f7cb0b81da35a9363ce5d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 14:36:23 +0000

    Updated 'PR authors' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/prs_authors.sql
+++ metrics/kubernetes/prs_authors.sql
@@ -10,22 +10,31 @@ where
   and dup_actor_login not like 'k8s-%'
   and dup_actor_login not like '%-bot'
   and dup_actor_login not like '%-robot'
-union select 'prs_authors,' || r.repo_group as repo_group,
-  round(count(distinct pr.dup_actor_login) / {{n}}, 2) as authors
-from
-  gha_pull_requests pr,
-  gha_repos r
+union select sub.repo_group,
+  round(count(distinct sub.actor) / {{n}}, 2) as authors
+from (
+  select 'prs_authors,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.dup_actor_login as actor
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    pr.dup_repo_id = r.id
+    and pr.created_at >= '{{from}}'
+    and pr.created_at < '{{to}}'
+    and pr.dup_actor_login not in ('googlebot')
+    and pr.dup_actor_login not like 'k8s-%'
+    and pr.dup_actor_login not like '%-bot'
+    and pr.dup_actor_login not like '%-robot'
+ ) sub
 where
-  pr.dup_repo_id = r.id
-  and r.repo_group is not null
-  and pr.created_at >= '{{from}}'
-  and pr.created_at < '{{to}}'
-  and pr.dup_actor_login not in ('googlebot')
-  and pr.dup_actor_login not like 'k8s-%'
-  and pr.dup_actor_login not like '%-bot'
-  and pr.dup_actor_login not like '%-robot'
+  sub.repo_group is not null
 group by
-  r.repo_group
+  sub.repo_group
 order by
   authors desc,
   repo_group asc
