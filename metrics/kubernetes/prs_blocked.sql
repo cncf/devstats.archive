05553a66afcb36029fd0edaa7213fb3ef26b377a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 14:52:52 +0000

    Updated 'Blocked PRs' metric to use file-level granularity repo groups + fixed 'Project stats' test coverage
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/prs_blocked.sql
+++ metrics/kubernetes/prs_blocked.sql
@@ -1,6 +1,7 @@
 create temp table issues as
 select distinct i.id as issue_id,
   pr.id as pr_id,
+  pr.event_id as event_id,
   i.dup_repo_name
 from
   gha_issues_pull_requests ipr,
@@ -41,34 +42,44 @@ on
     )
     or il.dup_label_name like 'do-not-merge%'
   )
-union select r.repo_group,
-  round(count(distinct i.pr_id) / {{n}}, 2) as all_prs,
-  round(count(distinct i.pr_id) filter (where il.dup_label_name = 'needs-ok-to-test') / {{n}}, 2) as needs_ok_to_test,
-  round(count(distinct i.pr_id) filter (where il.dup_label_name = 'release-note-label-needed') / {{n}}, 2) as release_note_label_needed,
-  round(count(distinct i.pr_id) filter (where il.dup_label_name = 'lgtm') / {{n}}, 2) as lgtm,
-  round(count(distinct i.pr_id) filter (where il.dup_label_name = 'approved') / {{n}}, 2) as approved,
-  round(count(distinct i.pr_id) filter (where il.dup_label_name like 'do-not-merge%') / {{n}}, 2) as do_not_merge
-from
-  issues i
-join
-  gha_repos r
-on
-  i.dup_repo_name = r.name
-  and r.repo_group is not null
-left join
-  gha_issues_labels il
-on
-  il.issue_id = i.issue_id
-  and il.dup_created_at >= '{{from}}'
-  and il.dup_created_at < '{{to}}'
-  and (
-    il.dup_label_name in (
-      'needs-ok-to-test', 'release-note-label-needed', 'lgtm', 'approved'
+union select sub.repo_group,
+  round(count(distinct sub.pr_id) / {{n}}, 2) as all_prs,
+  round(count(distinct sub.pr_id) filter (where sub.dup_label_name = 'needs-ok-to-test') / {{n}}, 2) as needs_ok_to_test,
+  round(count(distinct sub.pr_id) filter (where sub.dup_label_name = 'release-note-label-needed') / {{n}}, 2) as release_note_label_needed,
+  round(count(distinct sub.pr_id) filter (where sub.dup_label_name = 'lgtm') / {{n}}, 2) as lgtm,
+  round(count(distinct sub.pr_id) filter (where sub.dup_label_name = 'approved') / {{n}}, 2) as approved,
+  round(count(distinct sub.pr_id) filter (where sub.dup_label_name like 'do-not-merge%') / {{n}}, 2) as do_not_merge
+from (
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    i.pr_id,
+    il.dup_label_name
+  from
+    gha_repos r
+  join
+    issues i
+  on
+    i.dup_repo_name = r.name
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = i.event_id
+  left join
+    gha_issues_labels il
+  on
+    il.issue_id = i.issue_id
+    and il.dup_created_at >= '{{from}}'
+    and il.dup_created_at < '{{to}}'
+    and (
+      il.dup_label_name in (
+        'needs-ok-to-test', 'release-note-label-needed', 'lgtm', 'approved'
+      )
+      or il.dup_label_name like 'do-not-merge%'
     )
-    or il.dup_label_name like 'do-not-merge%'
-  )
+  ) sub
+where
+  sub.repo_group is not null
 group by
-  r.repo_group
+  sub.repo_group
 ;
 
 select
