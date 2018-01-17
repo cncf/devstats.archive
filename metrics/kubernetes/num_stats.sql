55ee0c05a7d9ba9d986fbcdd49b1f923a01e8260
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 13:01:07 +0000

    Updated 'Num stats' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/num_stats.sql
+++ metrics/kubernetes/num_stats.sql
@@ -18,31 +18,41 @@ where
     'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
     'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
   )
-union select 'num_stats;' || r.repo_group || ';companies,developers,unknowns' as name,
-  count(distinct affs.company_name) as n_companies,
-  count(distinct ev.actor_id) as n_authors,
-  count(distinct ev.actor_id) filter (where affs.company_name is null) as n_unknown_authors
-from
-  gha_repos r,
-  gha_events ev
-left join
-  gha_actors_affiliations affs
-on
-  ev.actor_id = affs.actor_id
-  and affs.dt_from <= ev.created_at
-  and affs.dt_to > ev.created_at
+union select sub.name,
+  count(distinct sub.company_name) as n_companies,
+  count(distinct sub.actor_id) as n_authors,
+  count(distinct sub.actor_id) filter (where sub.company_name is null) as n_unknown_authors
+from (
+    select 'num_stats;' || coalesce(ecf.repo_group, r.repo_group) || ';companies,developers,unknowns' as name,
+    affs.company_name,
+    ev.actor_id
+  from
+    gha_repos r,
+    gha_events ev
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = ev.id
+  left join
+    gha_actors_affiliations affs
+  on
+    ev.actor_id = affs.actor_id
+    and affs.dt_from <= ev.created_at
+    and affs.dt_to > ev.created_at
+  where
+    r.name = ev.dup_repo_name
+    and ev.created_at >= '{{from}}'
+    and ev.created_at < '{{to}}'
+    and ev.type in (
+      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+      'PushEvent'
+    )
+  ) sub
 where
-  r.name = ev.dup_repo_name
-  and r.repo_group is not null
-  and ev.created_at >= '{{from}}'
-  and ev.created_at < '{{to}}'
-  and ev.type in (
-    'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
-    'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
-    'PushEvent'
-  )
+  sub.name is not null
 group by
-  r.repo_group
+  sub.name
 order by
   n_companies desc,
   n_authors desc,
