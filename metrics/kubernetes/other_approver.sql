0011af567f1d7d242ff52492c7fe8ffb6f173845
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 15:24:55 +0000

    Updated 'Other approvers' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/other_approver.sql
+++ metrics/kubernetes/other_approver.sql
@@ -3,7 +3,8 @@ select distinct i.id as issue_id,
   substring(
     c.body from '(?i)META={"approvers":\["([^"]+)"\]}'
   ) as approver,
-  i.dup_repo_name as repo_name
+  i.dup_repo_name as repo_name,
+  pl.event_id
 from
   gha_comments c,
   gha_payloads pl,
@@ -55,24 +56,36 @@ left join
   actual_approvers aa
 on
   aa.issue_id = sa.issue_id
-union select 'other_approvers;' || r.repo_group || ';all_suggested_approvers,no_approver,other_approver,suggested_approver' as name,
-  round(count(distinct sa.issue_id) / {{n}}, 2) as all_suggested_approvers,
-  round(count(distinct sa.issue_id) filter (where aa.issue_id is null) / {{n}}, 2) as no_approver,
-  round(count(distinct sa.issue_id) filter (where sa.approver != aa.approver) / {{n}}, 2) as other_approver,
-  round(count(distinct sa.issue_id) filter (where sa.approver = aa.approver) / {{n}}, 2) as suggested_approver
-from
-  gha_repos r
-join
-  suggested_approvers sa
-on
-  sa.repo_name = r.name
-  and r.repo_group is not null
-left join
-  actual_approvers aa
-on
-  aa.issue_id = sa.issue_id
+union select sub.name,
+  round(count(distinct sub.sa_issue_id) / {{n}}, 2) as all_suggested_approvers,
+  round(count(distinct sub.sa_issue_id) filter (where sub.aa_issue_id is null) / {{n}}, 2) as no_approver,
+  round(count(distinct sub.sa_issue_id) filter (where sub.sa_approver != sub.aa_approver) / {{n}}, 2) as other_approver,
+  round(count(distinct sub.sa_issue_id) filter (where sub.sa_approver = sub.aa_approver) / {{n}}, 2) as suggested_approver
+from (
+  select 'other_approvers;' || coalesce(ecf.repo_group, r.repo_group) || ';all_suggested_approvers,no_approver,other_approver,suggested_approver' as name,
+    sa.issue_id as sa_issue_id,
+    aa.issue_id as aa_issue_id,
+    sa.approver as sa_approver,
+    aa.approver as aa_approver
+  from
+    gha_repos r
+  join
+    suggested_approvers sa
+  on
+    sa.repo_name = r.name
+  left join
+    actual_approvers aa
+  on
+    aa.issue_id = sa.issue_id
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = sa.event_id
+  ) sub
+where
+  sub.name is not null
 group by
-  repo_group
+  sub.name
 order by
   all_suggested_approvers desc,
   name asc
