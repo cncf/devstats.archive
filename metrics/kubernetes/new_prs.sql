a7677f67350b666eaa5c05af12818eddee6a614e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 12:46:07 +0000

    Updated 'New PRs' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/new_prs.sql
+++ metrics/kubernetes/new_prs.sql
@@ -6,18 +6,27 @@ from
 where
   created_at >= '{{from}}'
   and created_at < '{{to}}'
-union select 'new_prs,' || r.repo_group as repo_group,
-  round(count(distinct pr.id) / {{n}}, 2) as new
-from
-  gha_pull_requests pr,
-  gha_repos r
+union select sub.repo_group,
+  round(count(distinct sub.id) / {{n}}, 2) as new
+from (
+    select 'new_prs,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    pr.dup_repo_id = r.id
+    and pr.created_at >= '{{from}}'
+    and pr.created_at < '{{to}}'
+  ) sub
 where
-  pr.dup_repo_id = r.id
-  and r.repo_group is not null
-  and pr.created_at >= '{{from}}'
-  and pr.created_at < '{{to}}'
+  sub.repo_group is not null
 group by
-  r.repo_group
+  sub.repo_group
 order by
   new desc,
   repo_group asc
