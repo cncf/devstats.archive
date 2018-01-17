5a0fee9e8d130301c665fe23cac06ea78ae03f97
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-17 14:29:38 +0000

    Updated 'PRs age' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/prs_age.sql
+++ metrics/kubernetes/prs_age.sql
@@ -10,21 +10,33 @@ where
   );
 
 create temp table prs_groups as
-select r.repo_group,
-  pr.id,
-  pr.created_at,
-  pr.merged_at as merged_at
-from
-  gha_pull_requests pr,
-  gha_repos r
+select distinct sub.repo_group,
+  sub.id,
+  sub.created_at,
+  sub.merged_at
+from (
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id,
+    pr.created_at,
+    pr.merged_at
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    r.id = pr.dup_repo_id
+    and pr.created_at >= '{{from}}'
+    and pr.created_at < '{{to}}'
+    and pr.event_id = (
+      select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
+    )
+  ) sub
 where
-  r.id = pr.dup_repo_id
-  and r.repo_group is not null
-  and pr.created_at >= '{{from}}'
-  and pr.created_at < '{{to}}'
-  and pr.event_id = (
-    select i.event_id from gha_pull_requests i where i.id = pr.id order by i.updated_at desc limit 1
-  );
+  sub.repo_group is not null
+;
 
 create temp table tdiffs as
 select id, extract(epoch from coalesce(merged_at - created_at, now() - created_at)) / 3600 as age
@@ -32,7 +44,8 @@ from prs;
 
 create temp table tdiffs_groups as
 select repo_group, id, extract(epoch from coalesce(merged_at - created_at, now() - created_at)) / 3600 as age
-from prs_groups;
+from
+  prs_groups;
 
 select
   'prs_age;All;number,median' as name,
