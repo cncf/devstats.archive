15136762b558bbc405f932031997f7c9d77713bc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-18 08:26:11 +0000

    Updated 'PRs merged repository groups' metric to use file-level granularity repo groups
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/prs_merged_groups.sql
+++ metrics/kubernetes/prs_merged_groups.sql
@@ -1,17 +1,26 @@
 select
-  'group_prs_merged,' || r.repo_group as repo_group,
-  round(count(distinct pr.id) / {{n}}, 2) as merge_count
-from
-  gha_pull_requests pr,
-  gha_repos r
+  sub.repo_group,
+  round(count(distinct sub.id) / {{n}}, 2) as merge_count
+from (
+  select 'group_prs_merged,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    r.name = pr.dup_repo_name
+    and pr.merged_at is not null
+    and pr.merged_at >= '{{from}}'
+    and pr.merged_at < '{{to}}'
+  ) sub
 where
-  r.name = pr.dup_repo_name
-  and r.repo_group is not null
-  and pr.merged_at is not null
-  and pr.merged_at >= '{{from}}'
-  and pr.merged_at < '{{to}}'
+  sub.repo_group is not null
 group by
-  r.repo_group
+  sub.repo_group
 order by
   merge_count desc,
   repo_group asc
