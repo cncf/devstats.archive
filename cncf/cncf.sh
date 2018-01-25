a2272e622e2c11d4e08400317327cd4fcb95cf08
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-25 08:03:03 +0000

    GHA is now fixed, so updating scripts not to skip previously broken GHA hour
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cncf/cncf.sh
+++ cncf/cncf.sh
@@ -4,13 +4,11 @@ set -o pipefail
 > errors.txt
 > run.log
 GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
-# Broken GitHub archive file 2017-11-08 1:00 AM: https://github.com/igrigorik/githubarchive.org/issues/169
-GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 ./gha2db 2015-12-07 18 2017-11-08 0 'cncf' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 ./gha2db 2017-11-08 2 today now 'cncf' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
+GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 ./gha2db 2015-12-07 18 today now 'cncf' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=cncf IDB_DB=cncf PG_DB=cncf GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
 ./grafana/influxdb_recreate.sh cncf
-./cncf/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
-./cncf/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
-./cncf/import_affs.sh 2>>errors.txt | tee -a run.log || exit 8
-./cncf/get_repos.sh 2>>errors.txt | tee -a run.log || exit 9
+./cncf/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
+./cncf/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 5
+./cncf/import_affs.sh 2>>errors.txt | tee -a run.log || exit 6
+./cncf/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
 echo "All done. You should run ./cncf/reinit.sh script now."
--- cni/cni.sh
+++ cni/cni.sh
@@ -4,14 +4,12 @@ set -o pipefail
 > errors.txt
 > run.log
 GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
-# Broken GitHub archive file 2017-11-08 1:00 AM: https://github.com/igrigorik/githubarchive.org/issues/169
-GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 ./gha2db 2016-05-04 0 2017-11-08 0 'containernetworking' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 ./gha2db 2017-11-08 2 today now 'containernetworking' 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 GHA2DB_EXACT=1 ./gha2db 2015-04-04 0 2016-05-05 0 'appc/cni' 2>>errors.txt | tee -a run.log || exit 4
-GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 ./gha2db 2016-05-04 0 today now 'containernetworking' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 GHA2DB_EXACT=1 ./gha2db 2015-04-04 0 2016-05-05 0 'appc/cni' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=cni IDB_DB=cni PG_DB=cni GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
 ./grafana/influxdb_recreate.sh cni
-./cni/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
-./cni/import_affs.sh 2>>errors.txt | tee -a run.log || exit 7
-./cni/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 8
-./cni/get_repos.sh 2>>errors.txt | tee -a run.log || exit 9
+./cni/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 5
+./cni/import_affs.sh 2>>errors.txt | tee -a run.log || exit 6
+./cni/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
+./cni/get_repos.sh 2>>errors.txt | tee -a run.log || exit 8
 echo "All done. You should run ./cni/reinit.sh script now."
