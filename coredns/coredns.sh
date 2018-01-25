f7b8ea121c8ea5e69370605a73933088085ab7e6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-25 06:29:36 +0000

    Updated previous projects config using BigQuery to find old repo names
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- coredns/coredns.sh
+++ coredns/coredns.sh
@@ -5,7 +5,7 @@ set -o pipefail
 > run.log
 GHA2DB_PROJECT=coredns IDB_DB=coredns PG_DB=coredns GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 # Broken GitHub archive file 2017-11-08 1:00 AM: https://github.com/igrigorik/githubarchive.org/issues/169
-GHA2DB_PROJECT=coredns IDB_DB=coredns PG_DB=coredns GHA2DB_LOCAL=1 ./gha2db 2017-02-21 0 2017-11-08 0 'coredns' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=coredns IDB_DB=coredns PG_DB=coredns GHA2DB_LOCAL=1 ./gha2db 2016-03-18 0 2017-11-08 0 'miekg/coredns,coredns' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=coredns IDB_DB=coredns PG_DB=coredns GHA2DB_LOCAL=1 ./gha2db 2017-11-08 2 today now 'coredns' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=coredns IDB_DB=coredns PG_DB=coredns GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
 ./grafana/influxdb_recreate.sh coredns
