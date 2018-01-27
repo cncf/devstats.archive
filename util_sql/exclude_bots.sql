0fd3951cc052fc3fb4f349be8ac021d3a2e318c7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-27 13:21:20 +0000

    New bot
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/exclude_bots.sql
+++ util_sql/exclude_bots.sql
@@ -1,1 +1,1 @@
-not like all(array['googlebot', 'coveralls', 'rktbot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%'])
+not like all(array['googlebot', 'coveralls', 'rktbot', 'web-flow', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%'])
