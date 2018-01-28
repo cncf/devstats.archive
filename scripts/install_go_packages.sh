86944eab61f00fcdd32dfb74f32a93a2aa02b6a0
Lukasz Gryglicki <lgryglicki@cncf.io>
2018-01-28 19:18:56 +0100

    Added script that adds all go stuff at once
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

---
+++ scripts/install_go_packages.sh
@@ -0,0 +1,23 @@
+echo "goimports..."
+go get golang.org/x/tools/cmd/goimports
+echo "goconst..."
+go get github.com/jgautheron/goconst/cmd/goconst
+echo "usedexports..."
+go get github.com/jgautheron/usedexports
+echo "errcheck..."
+go get github.com/kisielk/errcheck
+echo "influxdb..."
+go get github.com/influxdata/influxdb/client/v2
+echo "pq..."
+go get github.com/lib/pq
+echo "transform..."
+go get golang.org/x/text/transform
+echo "norm..."
+go get golang.org/x/text/unicode/norm
+echo "yaml..."
+go get gopkg.in/yaml.v2
+echo "github..."
+go get github.com/google/go-github/github
+echo "oauth..."
+go get golang.org/x/oauth2
+echo "Done"
