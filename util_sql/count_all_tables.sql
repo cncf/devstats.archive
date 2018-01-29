1a24cc86957590aef071bb9ed74db67022bb31ba
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-29 19:12:08 +0000

    Updated gaps config for 'All', fixed for 'CNCF', psql data generated, now generating Influx
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/count_all_tables.sql
+++ util_sql/count_all_tables.sql
@@ -5,8 +5,10 @@ select 'assets' as name, count(*) as count_value from gha_assets union
 select 'branches' as name, count(*) as count_value from gha_branches union
 select 'comments' as name, count(*) as count_value from gha_comments union
 select 'commits' as name, count(*) as count_value from gha_commits union
+select 'commits files' as name, count(*) as count_value from gha_commits_files union
 select 'companies' as name, count(*) as count_value from gha_companies union
 select 'events' as name, count(*) as count_value from gha_events union
+select 'events commits file' as name, count(*) as count_value from gha_events_commits_files union
 select 'forkees' as name, count(*) as count_value from gha_forkees union
 select 'issues' as name, count(*) as count_value from gha_issues union
 select 'issue assignees' as name, count(*) as count_value from gha_issues_assignees union
@@ -16,12 +18,14 @@ select 'milestones' as name, count(*) as count_value from gha_milestones union
 select 'orgs' as name, count(*) as count_value from gha_orgs union
 select 'pages' as name, count(*) as count_value from gha_pages union
 select 'payloads' as name, count(*) as count_value from gha_payloads union
+select 'postprocess scripts' as name, count(*) as count_value from gha_postprocess_scripts union
 select 'pull requests' as name, count(*) as count_value from gha_pull_requests union
 select 'pull request assignees' as name, count(*) as count_value from gha_pull_requests_assignees union
 select 'pull request requested reviewers' as name, count(*) as count_value from gha_pull_requests_requested_reviewers union
 select 'releases' as name, count(*) as count_value from gha_releases union
 select 'release assets' as name, count(*) as count_value from gha_releases_assets union
 select 'repos' as name, count(*) as count_value from gha_repos union
+select 'skip commits' as name, count(*) as count_value from gha_skip_commits union
 select 'teams' as name, count(*) as count_value from gha_teams union
 select 'team repositories' as name, count(*) as count_value from gha_teams_repositories union
 select 'issue events labels' as name, count(*) as count_value from gha_issues_events_labels union
