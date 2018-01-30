6a18b9931b295223f5a375b1a20ffa6817d8979f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-30 10:20:48 +0000

    Config to reinit 'All' project with repo groups being othe rprojects included in 'All'
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all/psql.sh
+++ all/psql.sh
@@ -6,6 +6,7 @@ GHA2DB_PROJECT=all IDB_DB=allprj PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_MGETC=y ./st
 GHA2DB_INPUT_DBS="gha,prometheus,opentracing,fluentd,linkerd,grpc,coredns,containerd,rkt,cni,envoy,jaeger,notary,tuf,rook" GHA2DB_OUTPUT_DB="allprj" ./merge_pdbs || exit 2
 GHA2DB_PROJECT=all IDB_DB=allprj PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
 ./grafana/influxdb_recreate.sh allprj
+./all/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 ./all/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 5
 ./all/import_affs.sh 2>>errors.txt | tee -a run.log || exit 6
 ./all/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
---
+++ scripts/all/repo_groups.sql
@@ -0,0 +1,180 @@
+-- Clear current repo groups (taken from merge of all other projects)
+update
+  gha_repos
+set
+  repo_group = null,
+  alias = null
+;
+
+-- Kubernetes
+update
+  gha_repos
+set
+  repo_group = 'Kubernetes',
+  alias = 'Kubernetes'
+where
+  org_login in (
+    'kubernetes', 'kubernetes-client', 'kubernetes-incubator', 'kubernetes-helm'
+  )
+  or name in (
+    'GoogleCloudPlatform/kubernetes', 'kubernetes' , 'kubernetes-client'
+ )
+;
+
+-- Prometheus
+update
+  gha_repos
+set
+  repo_group = 'Prometheus',
+  alias = 'Prometheus'
+where
+  org_login = 'prometheus'
+;
+
+-- OpenTracing
+update
+  gha_repos
+set
+  repo_group = 'OpenTracing',
+  alias = 'OpenTracing'
+where
+  org_login = 'opentracing'
+;
+
+-- Fluentd
+update
+  gha_repos
+set
+  repo_group = 'Fluentd',
+  alias = 'Fluentd'
+where
+  org_login = 'fluent'
+;
+
+-- Linkerd
+update
+  gha_repos
+set
+  repo_group = 'Linkerd',
+  alias = 'Linkerd'
+where
+  org_login = 'linkerd'
+  or name = 'BuoyantIO/linkerd'
+;
+
+-- gRPC
+update
+  gha_repos
+set
+  repo_group = 'gRPC',
+  alias = 'gRPC'
+where
+  org_login = 'grpc'
+;
+
+-- CoreDNS
+update
+  gha_repos
+set
+  repo_group = 'CoreDNS',
+  alias = 'CoreDNS'
+where
+  org_login = 'coredns'
+  or name = 'miekg/coredns'
+;
+
+-- containerd
+update
+  gha_repos
+set
+  repo_group = 'containerd',
+  alias = 'containerd'
+where
+  org_login = 'containerd'
+  or name = 'docker/containerd'
+;
+
+-- rkt
+update
+  gha_repos
+set
+  repo_group = 'rkt',
+  alias = 'rkt'
+where
+  org_login in ('rkt' , 'coreos', 'rktproject')
+  or name in ('rkt/Navigation_Drawer', 'rocket')
+;
+
+-- CNI
+update
+  gha_repos
+set
+  repo_group = 'CNI',
+  alias = 'CNI'
+where
+  org_login = 'containernetworking'
+  or name = 'appc/cni'
+;
+
+-- Envoy
+update
+  gha_repos
+set
+  repo_group = 'Envoy',
+  alias = 'Envoy'
+where
+  org_login = 'envoyproxy'
+  or name = 'lyft/envoy'
+;
+
+-- Jaeger
+update
+  gha_repos
+set
+  repo_group = 'Jaeger',
+  alias = 'Jaeger'
+where
+  org_login = 'jaegertracing'
+  or name = 'uber/jaeger'
+;
+
+-- Notary
+update
+  gha_repos
+set
+  repo_group = 'Notary',
+  alias = 'Notary'
+where
+  name in ('theupdateframework/notary', 'docker/notary')
+;
+
+-- TUF
+update
+  gha_repos
+set
+  repo_group = 'TUF',
+  alias = 'TUF'
+where
+  org_login = 'theupdateframework'
+  and name != 'theupdateframework/notary' 
+;
+
+-- Rook
+update
+  gha_repos
+set
+  repo_group = 'Rook',
+  alias = 'Rook'
+where
+  org_login = 'rook'
+;
+
+-- CNCF
+update
+  gha_repos
+set
+  repo_group = 'CNCF',
+  alias = 'CNCF'
+where
+  org_login = 'cncf'
+;
