baa67176569237592e8f39f5bc6f3d2ee3d17fb2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-30 15:38:24 +0000

    Include CNCF in 'All' project too
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all/psql.sh
+++ all/psql.sh
@@ -3,7 +3,7 @@ set -o pipefail
 > errors.txt
 > run.log
 GHA2DB_PROJECT=all IDB_DB=allprj PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_MGETC=y ./structure 2>>errors.txt | tee -a run.log || exit 1
-GHA2DB_INPUT_DBS="gha,prometheus,opentracing,fluentd,linkerd,grpc,coredns,containerd,rkt,cni,envoy,jaeger,notary,tuf,rook" GHA2DB_OUTPUT_DB="allprj" ./merge_pdbs || exit 2
+GHA2DB_INPUT_DBS="gha,prometheus,opentracing,fluentd,linkerd,grpc,coredns,containerd,rkt,cni,envoy,jaeger,notary,tuf,rook,cncf" GHA2DB_OUTPUT_DB="allprj" ./merge_pdbs || exit 2
 GHA2DB_PROJECT=all IDB_DB=allprj PG_DB=allprj GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
 ./grafana/influxdb_recreate.sh allprj
 ./all/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
