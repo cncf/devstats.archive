c509d701767a5b42ff2c9c501b114a094b3475d2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-30 20:55:03 +0000

    This script is not needed in 'All' project
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all/setup_scripts.sh
+++ all/setup_scripts.sh
@@ -2,7 +2,5 @@
 # Run this script from the repository top level.
 echo "Setting up default postprocess scripts"
 PG_DB=allprj ./runq util_sql/default_postprocess_scripts.sql
-echo "Setting up repository groups postprocess script (file level granularity)"
-PG_DB=allprj ./runq util_sql/repo_groups_postprocess_script.sql
 echo "Setting up repository groups postprocess script"
 PG_DB=allprj ./runq util_sql/repo_groups_postprocess_script_from_repos.sql
