7b4c2e2f3919ffc15e0b41edb4f55a23ca654344
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-01-31 15:04:58 +0000

    Added script to copy grafana DBs from the test server
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/get_grafana_dbs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+host=`hostname`
+if [ $host = "cncftest.io" ]
+then
+  all="k8s prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook all cncf"
+else
+  all="k8s prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook"
+fi
+for proj in $all
+do
+    echo "wget grafana.$proj.db"
+    rm -f grafana.$proj.db 2>/dev/null
+    wget https://cncftest.io/grafana.$proj.db || exit 1
+    ls -l grafana.$proj.db
+    cp grafana.$proj.db /var/lib/grafana.$proj/grafana.db || exit 2
+done
+echo 'OK'
+
