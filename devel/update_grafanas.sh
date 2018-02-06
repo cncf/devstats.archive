ea78f07e6eee7522c35de9dd69c198abbb665a7e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-06 05:56:29 +0000

    New script to automatically update grafanas
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/update_grafanas.sh
+++ devel/update_grafanas.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/master/grafana_5.0.0-11639beta1_amd64.deb
+rm -rf ~/grafana.v5.old
+mv ~/grafana.v5 ~/grafana.v5.old
+mkdir ~/grafana.v5
+killall grafana-server
+sudo dpkg -i grafana_5.0.0-11639beta1_amd64.deb
+mv /usr/share/grafana ~/grafana.v5/usr.share.grafana
+mv /var/lib/grafana ~/grafana.v5/var.lib.grafana
+mv /etc/grafana ~/grafana.v5/etc.grafana
 host=`hostname`
 if [ $host = "cncftest.io" ]
 then
@@ -6,18 +15,14 @@ then
 else
   all="k8s prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess"
 fi
-killall grafana-server
 for proj in $all
 do
     echo $proj
     rm -rf /usr/share/grafana.$proj
-    cp -R /usr/share/grafana /usr/share/grafana.$proj || exit 1
-    rm -rf /var/lib/grafana.$proj
-    cp -R /var/lib/grafana /var/lib/grafana.$proj || exit 2
+    cp -R ~/grafana.v5/usr.share.grafana/ /usr/share/grafana.$proj || exit 1
 done
 echo 'OK'
 ./grafana/change_title_and_icons_all.sh || exit 3
-./devel/get_grafana_dbs.sh || exit 4
 ./grafana/start_all_grafanas.sh || exit 5
 sleep 5
 ps -aux | grep 'grafana-server'
