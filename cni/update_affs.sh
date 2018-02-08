d0b7c1b201cc9924388bdfb6d254a8e7125299a0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-08 08:28:17 +0000

    Removed no longer needed gaps YAMLs and updated affiliations scripts not to require them
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cni/update_affs.sh
+++ cni/update_affs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_PROJECT=cni GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=cni IDB_DB=cni GHA2DB_METRICS_YAML=./metrics/cni/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/cni/gaps_affs.yaml GHA2DB_TAGS_YAML=./metrics/cni/tags_affs.yaml ./gha2db_sync
+GHA2DB_PROJECT=cni GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=cni IDB_DB=cni GHA2DB_METRICS_YAML=./metrics/cni/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/cni/gaps.yaml GHA2DB_TAGS_YAML=./metrics/cni/tags_affs.yaml ./gha2db_sync
--- tuf/update_affs.sh
+++ tuf/update_affs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_PROJECT=tuf GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=tuf IDB_DB=tuf GHA2DB_METRICS_YAML=./metrics/tuf/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/tuf/gaps_affs.yaml GHA2DB_TAGS_YAML=./metrics/tuf/tags_affs.yaml ./gha2db_sync
+GHA2DB_PROJECT=tuf GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=tuf IDB_DB=tuf GHA2DB_METRICS_YAML=./metrics/tuf/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/tuf/gaps.yaml GHA2DB_TAGS_YAML=./metrics/tuf/tags_affs.yaml ./gha2db_sync
