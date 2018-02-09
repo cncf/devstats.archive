fda0fb078ea12412c385bf6b8121a0d22f075f53
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-09 12:42:09 +0000

    Make import affs script fail when any problem detected
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cni/import_affs.sh
+++ cni/import_affs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=cni PG_DB=cni IDB_DB=cni ./runq scripts/clean_affiliations.sql
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=cni PG_DB=cni IDB_DB=cni ./import_affs github_users.json
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=cni PG_DB=cni IDB_DB=cni ./import_affs github_users.json || exit 1
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=cni PG_DB=cni IDB_DB=cni ./idb_tags
--- linkerd/import_affs.sh
+++ linkerd/import_affs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./runq scripts/clean_affiliations.sql
-GHA2DB_LOCAL=1 GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./import_affs github_users.json
+GHA2DB_LOCAL=1 GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./import_affs github_users.json || exit 1
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=linkerd PG_DB=linkerd IDB_DB=linkerd ./idb_tags
