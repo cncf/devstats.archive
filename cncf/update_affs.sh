50448ba85e273ef332f91fdc047a5d153b629c43
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-14 12:21:10 +0000

    Importing new cncf/gitdm affiliations
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cncf/update_affs.sh
+++ cncf/update_affs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_PROJECT=cncf GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=cncf IDB_DB=cncf GHA2DB_METRICS_YAML=./metrics/cncf/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/cncf/gaps.yaml GHA2DB_TAGS_YAML=./metrics/cncf/tags_affs.yaml ./gha2db_sync
+GHA2DB_PROJECT=cncf GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=cncf IDB_DB=cncf GHA2DB_METRICS_YAML=./metrics/cncf/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/cncf/gaps_affs.yaml GHA2DB_TAGS_YAML=./metrics/cncf/tags_affs.yaml ./gha2db_sync
--- kubernetes/update_affs.sh
+++ kubernetes/update_affs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_PROJECT=kubernetes GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=gha IDB_DB=gha GHA2DB_METRICS_YAML=./metrics/kubernetes/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/kubernetes/gaps.yaml GHA2DB_TAGS_YAML=./metrics/kubernetes/tags_affs.yaml ./gha2db_sync
+GHA2DB_PROJECT=kubernetes GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=gha IDB_DB=gha GHA2DB_METRICS_YAML=./metrics/kubernetes/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/kubernetes/gaps_affs.yaml GHA2DB_TAGS_YAML=./metrics/kubernetes/tags_affs.yaml ./gha2db_sync
