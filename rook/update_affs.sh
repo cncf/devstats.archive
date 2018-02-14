7f592219fd7c5de3409e339602e91df0dff66a7f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-14 12:21:10 +0000

    Importing new cncf/gitdm affiliations
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- rook/update_affs.sh
+++ rook/update_affs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GHA2DB_PROJECT=rook GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=rook IDB_DB=rook GHA2DB_METRICS_YAML=./metrics/rook/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/rook/gaps.yaml GHA2DB_TAGS_YAML=./metrics/rook/tags_affs.yaml ./gha2db_sync
+GHA2DB_PROJECT=rook GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESETIDB=1 PG_DB=rook IDB_DB=rook GHA2DB_METRICS_YAML=./metrics/rook/metrics_affs.yaml GHA2DB_GAPS_YAML=./metrics/rook/gaps_affs.yaml GHA2DB_TAGS_YAML=./metrics/rook/tags_affs.yaml ./gha2db_sync
