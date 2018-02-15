27abe8e555228b548706233ad3e8ee088011d465
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-15 12:25:50 +0000

    Added 'PR workload' chart dashboard SQL
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/kubernetes/pr_workload.sql
@@ -0,0 +1,67 @@
+create temp table pr_sizes as
+select
+  sub.issue_id,
+  sub.size
+from (
+  select distinct issue_id,
+    lower(substring(dup_label_name from '(?i)size/(.*)')) as size
+  from
+    gha_issues_labels
+  where
+    dup_created_at >= '{{from}}'
+    and dup_created_at < '{{to}}'
+  ) sub
+where
+  sub.size is not null
+;
+
+create temp table pr_sigs as
+select
+  sub.issue_id,
+  sub.sig
+from (
+  select distinct issue_id,
+    lower(substring(dup_label_name from '(?i)sig/(.*)')) as sig
+  from
+    gha_issues_labels
+  where
+    dup_created_at >= '{{from}}'
+    and dup_created_at < '{{to}}'
+  ) sub
+where
+  sub.sig is not null
+;
+
+select
+  'sig_pr_workload;' || sig.sig || ';issues,absolute_workload' as sig,
+  count(distinct sig.issue_id) as issues,
+  sum(
+    case coalesce(siz.size, 'nil')
+      when 'xs' then 0.25
+      when 's' then 0.5
+      when 'small' then 0.5
+      when 'm' then 1.0
+      when 'medium' then 1.0
+      when 'nil' then 1.0
+      when 'l' then 2.0
+      when 'large' then 2.0
+      when 'xl' then 4.0
+      when 'xxl' then 8.0
+      else 1.0
+    end
+  ) as absolute_workload
+from
+  pr_sigs sig
+left join
+  pr_sizes siz
+on
+  sig.issue_id = siz.issue_id
+group by
+  sig.sig
+order by
+  absolute_workload desc,
+  sig asc
+;
+
+drop table pr_sizes;
+drop table pr_sigs;
