0a86b68f7719f08231c9fcbe85c69b56826f18f2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-22 12:08:27 +0000

    Updated 'idb_tags' for milestones
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/milestones_tags_with_all.sql
+++ metrics/kubernetes/milestones_tags_with_all.sql
@@ -1,8 +1,21 @@
-select
-  'All' as title
-union select distinct title
-from
-  gha_milestones
-order by
-  title asc
+select 'All' as title
+union select distinct ml.title
+from (
+  select
+    milestone_id,
+    count(*) as cnt
+  from
+    gha_issues
+  where
+    milestone_id is not null
+    and created_at > now() - '2 years'::interval
+  group by
+    milestone_id
+  order by
+    cnt desc
+  limit {{lim}}
+  ) sub,
+  gha_milestones ml
+where
+  ml.id = sub.milestone_id
 ;
