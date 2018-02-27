fe0961ed20856940c863167ff29ad4d0d748c48a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-27 09:56:16 +0000

    Working on making prometheus dashbards use templating variables and generic datasource name
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/idb_vars_all.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ -z "${IDB_PASS}" ]
+then
+  echo "You need to set IDB_PASS environment variable to run this script"
+  exit 1
+fi
+host=`hostname`
+if [ $host = "cncftest.io" ]
+then
+  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers all cncf"
+else
+  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers"
+fi
+for f in $all
+do
+    db=$f
+    if [ $f = "kubernetes" ]
+    then
+      db="gha"
+    elif [ $f = "all" ]
+    then
+      db="allprj"
+    fi
+    echo "Project: $f, IDB: $db"
+    GHA2DB_LOCAL=1 GHA2DB_PROJECT=$f IDB_DB=$db ./idb_vars
+done
+echo 'OK'
--- grafana/start_all_grafanas.sh
+++ grafana/start_all_grafanas.sh
@@ -4,7 +4,7 @@ if [ $host = "cncftest.io" ]
 then
   all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers all cncf"
 else
-  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers"
+  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers all"
 fi
 for f in $all
 do
