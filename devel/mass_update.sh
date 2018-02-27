269623ce0c9c6cc7d4cee86b5ce636f7a3a00007
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-02-27 09:38:46 +0000

    Removed unneeded file
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/mass_update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Example `FROM=DS_PROMETHEUS TO=DS_GHA ./devel/mass_update.sh grafana/dashboards/prometheus/*.json`
+if [ -z "${FROM}" ]
+then
+  echo "You need to set FROM, example FROM=abc TO=xyz $0 ./some_dir/*"
+  exit 1
+fi
+if [ -z "${TO}" ]
+then
+  echo "You need to set TO, example FROM=abc TO=xyz $0 ./some_dir/*"
+  exit 2
+fi
+for f in $*
+do
+  ls -l "$f"
+  vim -c "%s/${FROM}/${TO}/g|wq" "$f"
+done
