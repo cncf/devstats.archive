e2c88e7aa784dc4aba52cec937848acad9b00022
lukaszgryglicki <lukaszgryglicki@o2.pl>
2018-02-28 09:55:31 +0100

    Added 'replacer' tool in golang
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

---
+++ cmd/replacer/replacer.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// replacer - replace regexp or string with regesp or string
+// possible modes:
+// rr, rr0: regexp to regexp, trailing 0 means that we're allowing no hits
+// rs, rs0: regexp to string (so in the TO patter you cannot use $1, $2 ... matchings from FROM)
+func replacer(from, to, fn, mode string) {
+	bytes, err := ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	contents := string(bytes)
+	var newContents string
+	switch mode {
+	case "rr", "rr0", "rs", "rs0":
+		re := regexp.MustCompile(from)
+		if mode[:2] == "rs" {
+			newContents = re.ReplaceAllLiteralString(contents, to)
+		} else {
+			newContents = re.ReplaceAllString(contents, to)
+		}
+		if contents == newContents {
+			fmt.Printf("Noting replaced in: %s\n", fn)
+			if mode == "re" {
+				os.Exit(1)
+			}
+			return
+		}
+	case "ss", "ss0":
+		newContents = strings.Replace(contents, from, to, -1)
+		if contents == newContents {
+			fmt.Printf("Noting replaced in: %s\n", fn)
+			if mode == "re" {
+				os.Exit(1)
+			}
+			return
+		}
+	default:
+		fmt.Printf("Unknown mode '%s'\n", mode)
+		os.Exit(1)
+	}
+	info, err := os.Stat(fn)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(fn, []byte(newContents), info.Mode())
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	from := os.Getenv("FROM")
+	if from == "" {
+		fmt.Printf("You need to set 'FROM' env variable\n")
+		os.Exit(1)
+	}
+	to := os.Getenv("TO")
+	if to == "" {
+		fmt.Printf("You need to set 'TO' env variable\n")
+		os.Exit(1)
+	}
+	mode := os.Getenv("MODE")
+	if mode == "" {
+		fmt.Printf("You need to set 'MODE' env variable\n")
+		os.Exit(1)
+	}
+	if len(os.Args) < 2 {
+		fmt.Printf("You need to provide a file name\n")
+		os.Exit(1)
+	}
+	fn := os.Args[1]
+	// fmt.Printf("File: '%s': '%s' -> '%s' (mode: %s)\n", fn, from, to, mode)
+	replacer(from, to, fn, mode)
+}
