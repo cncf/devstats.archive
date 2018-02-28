1c6c3de3b634f9ad81fb1240cade0afc9a60eaab
lukaszgryglicki <lukaszgryglicki@o2.pl>
2018-02-28 11:21:22 +0100

    Before mass update uid to increasing integer via bash script
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- devel/update_dashboards2.sh
+++ devel/update_dashboards2.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-for f in `ls grafana/dashboards/{cncf,cni,containerd,coredns,envoy,fluentd,grpc,jaeger,linkerd,notary,opencontainers,opentracing,rkt,rook,tuf,vitess}/*`
+#for f in `ls grafana/dashboards/{cncf,cni,containerd,coredns,envoy,fluentd,grpc,jaeger,linkerd,notary,opencontainers,opentracing,rkt,rook,tuf,vitess}/*`
+#do
+#  MODE=rs0 FROM='(?m)^.*"uid": "\w+",\n' TO='-' ./replacer $f
+#done
+repl='  "timezone": "",'
+all="cncf cni containerd coredns envoy fluentd grpc jaeger linkerd notary opencontainers opentracing rkt rook tuf vitess"
+for proj in $all
 do
-  MODE=rs0 FROM='(?m)^.*"uid": "\w+",\n' TO='-' ./replacer $f
+  uid=0
+  for f in `ls grafana/dashboards/${proj}/*.json`
+  do
+    uid=$((uid+1))
+    echo $repl > out
+    echo "  \"uid\": \"$uid\"," >> out
+    MODE=ss FROM='  "timezone": "",' TO=`cat out` ./replacer $f
+  done
 done
