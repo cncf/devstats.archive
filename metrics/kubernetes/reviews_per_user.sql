4297b568d6ac2f312e39bb57c1a08eadbd121533
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-05 09:52:34 +0000

    Change 'repo groups' to 'repos' as reqested for this dashboard
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/reviews_per_user.sql
+++ metrics/kubernetes/reviews_per_user.sql
@@ -9,8 +9,8 @@ where
 ;
 
 select
-  concat('lgtms_per_user,', dup_actor_login, '`All') as repo_group_user,
-  count(distinct id) as result
+  concat('lgtms_per_user,', dup_actor_login, '`All') as repo_user,
+  count(id) as result
 from
   gha_events
 where
@@ -29,8 +29,8 @@ where
   )
 group by
   dup_actor_login
-union select concat('reviews_per_user,', dup_actor_login, '`All') as repo_group_user,
-  count(distinct id) as result
+union select concat('reviews_per_user,', dup_actor_login, '`All') as repo_user,
+  count(id) as result
 from
   gha_events
 where
@@ -40,66 +40,40 @@ where
   and (dup_actor_login {{exclude_bots}})
 group by
   dup_actor_login
-union select sub.repo_group_user,
-  count(distinct sub.event_id) as result
-from (
-  select 'reviews_per_user,' || concat(e.dup_actor_login, '`', coalesce(ecf.repo_group, r.repo_group)) as repo_group_user,
-    coalesce(ecf.repo_group, r.repo_group) as repo_group,
-    e.id as event_id
-  from
-    gha_repos r,
-    gha_events e
-  left join
-    gha_events_commits_files ecf
-  on
-    ecf.event_id = e.id
-  where
-    e.repo_id = r.id
-    and (e.dup_actor_login {{exclude_bots}})
-    and e.type in ('PullRequestReviewCommentEvent')
-    and e.created_at >= '{{from}}'
-    and e.created_at < '{{to}}'
-  ) sub
+union select 'reviews_per_user,' || concat(dup_actor_login, '`', dup_repo_name) as repo_user,
+  count(id) as result
+from
+  gha_events
 where
-  sub.repo_group is not null
+  (dup_actor_login {{exclude_bots}})
+  and type in ('PullRequestReviewCommentEvent')
+  and created_at >= '{{from}}'
+  and created_at < '{{to}}'
 group by
-  sub.repo_group_user
-union select sub.repo_group_user,
-  count(distinct sub.event_id) as result
-from (
-  select 'lgtms_per_user,' || concat(e.dup_actor_login, '`', coalesce(ecf.repo_group, r.repo_group)) as repo_group_user,
-    coalesce(ecf.repo_group, r.repo_group) as repo_group,
-    e.id as event_id
-  from
-    gha_repos r,
-    gha_events e
-  left join
-    gha_events_commits_files ecf
-  on
-    ecf.event_id = e.id
-  where
-    e.repo_id = r.id
-    and (e.dup_actor_login {{exclude_bots}})
-    and e.id in (
-      select min(event_id)
-      from
-        gha_issues_events_labels
-      where
-        created_at >= '{{from}}'
-        and created_at < '{{to}}'
-        and label_name = 'lgtm'
-      group by
-        issue_id
-      union select event_id from lgtm_texts
-    )
-  ) sub
+  repo_user
+union select 'lgtms_per_user,' || concat(dup_actor_login, '`', dup_repo_name) as repo_user,
+  count(id) as result
+from
+  gha_events
 where
-  sub.repo_group is not null
+  (dup_actor_login {{exclude_bots}})
+  and id in (
+    select min(event_id)
+    from
+      gha_issues_events_labels
+    where
+      created_at >= '{{from}}'
+      and created_at < '{{to}}'
+      and label_name = 'lgtm'
+    group by
+      issue_id
+    union select event_id from lgtm_texts
+  )
 group by
-  sub.repo_group_user
+  repo_user
 order by
   result desc,
-  repo_group_user asc
+  repo_user asc
 ;
 
 drop table lgtm_texts;
