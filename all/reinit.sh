b1ca2509a96ea3bbe7ac13950c42c1d24c7bf195
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-07 10:48:19 +0000

    Regenerating 'All CNCF' project data with 'vitessio' org added
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all/reinit.sh
+++ all/reinit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ./grafana/influxdb_recreate.sh allprj_temp || exit 1
-GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=all PG_DB=allprj IDB_DB=allprj_temp ./gha2db_sync || exit 2
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=all IDB_DB=allprj_temp ./idb_vars || exit 3
+GHA2DB_CMDDEBUG=1 GHA2DB_RESETIDB=1 GHA2DB_LOCAL=1 GHA2DB_PROJECT=all PG_DB=allprj IDB_DB=allprj_temp ./gha2db_sync || exit 2
 ./grafana/influxdb_recreate.sh allprj || exit 4
 ./idb_backup allprj_temp allprj || exit 5
 ./grafana/influxdb_drop.sh allprj_temp
