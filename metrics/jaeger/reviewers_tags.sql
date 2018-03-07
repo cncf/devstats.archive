81e5d48b3be04fe7452ac77258803cbe05794953
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-07 12:42:02 +0000

    Adding 'User reviews' everywhere
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/jaeger/reviewers_tags.sql
@@ -0,0 +1,30 @@
+create temp table lgtm_texts as
+select event_id
+from gha_texts
+where
+  substring(body from '(?i)(?:^|\n|\r)\s*/(?:lgtm)\s*(?:\n|\r|$)') is not null
+  and (actor_login {{exclude_bots}})
+;
+
+select
+  distinct dup_actor_login
+from
+  gha_events
+where
+  (dup_actor_login {{exclude_bots}})
+  and (
+    type = 'PullRequestReviewCommentEvent'
+    or id in (
+      select min(event_id)
+      from
+        gha_issues_events_labels
+      where
+        label_name = 'lgtm'
+      group by
+        issue_id
+      union select event_id from lgtm_texts
+    )
+  )
+;
+
+drop table lgtm_texts;
