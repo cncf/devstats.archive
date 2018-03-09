9852e8f221deff6b94634afa13452ca904795926
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-09 06:26:18 +0000

    Adding wait for devstats scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/wait_for_devstats.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o pipefail
+pid=/tmp/devstats.pid
+trials=0
+maxTrials=5
+while true
+do
+  if [ -e $pid ]
+  then
+    if [ "$trials" -eq "0" ]
+    then
+      echo "DevStats is running: $pid exists, waiting"
+    fi
+    sleep 1
+    trials=$((trials+1))
+    if [ "$trials" -ge "$maxTrials" ]
+    then
+      echo "DevStats is still running: $pid exists, waited $maxTrials seconds, exiting"
+      exit 1
+  fi
+  else
+    break
+  fi
+done
+if [ "$trials" -gt "0"]
+then
+  echo "DevStats was running waited $trails seconds"
+fi
