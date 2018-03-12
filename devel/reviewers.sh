af4bc18b0eef9d49c7f4601a024500eb2cc86f01
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-12 08:26:21 +0000

    Added script to show top reviewer(s) to select it by default for 'User reviews' dashboard [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/reviewers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if ( [ -z "$1" ] || [ -z "$2" ] || [ -z "$PG_PASS" ] )
+then
+  echo "PG_PASS=... PG_DB=db $0 date_from date_to"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq metrics/prometheus/reviews_per_user.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{n}} 1 {{from}} "$1" {{to}} "$2"
