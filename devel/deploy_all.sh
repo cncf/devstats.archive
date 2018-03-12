9c1f2b0cee21d9bda5abc7afd3cb09c9336b0b43
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-12 14:26:38 +0000

    Change sync order, long waiting command should block another deploy
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/deploy_all.sh
+++ devel/deploy_all.sh
@@ -9,18 +9,19 @@ then
 fi
 host=`hostname`
 function finish {
-    rm -f /tmp/deploy.wip 2>/dev/null
     sync_unlock.sh
+    rm -f /tmp/deploy.wip 2>/dev/null
 }
 if [ -z "$TRAP" ]
 then
+  > /tmp/deploy.wip
   sync_lock.sh || exit -1
   trap finish EXIT
   export TRAP=1
-  > /tmp/deploy.wip
 fi
 
 # k8s prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess nats opencontainers all cncf
+
 PROJ=kubernetes PROJDB=gha PROJORG=kubernetes ORGNAME=Kubernetes PORT=2999 ICON=kubernetes GRAFSUFF=k8s GA="UA-108085315-1" ./devel/deploy_proj.sh || exit 2
 # TODO: ICON=nats - when CNCF updates artwork to include NATS icon.
 PROJ=nats PROJDB=nats PROJORG=nats-io ORGNAME=NATS PORT=3016 ICON=cncf GRAFSUFF=nats GA="UA-108085315-21" ./devel/deploy_proj.sh || exit 2
