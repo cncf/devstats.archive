304ac24d450f5675cb9eecd7741612674f9ab2d6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-12 10:30:33 +0000

    First auto deploy attempt - expect the unexpected
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/sync_unlock.sh
+++ devel/sync_unlock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cronctl.sh devstats on || exit 2
-cronctl.sh webhook on || exit 4
+cronctl.sh devstats on || exit 1
+cronctl.sh webhook on || exit 2
 echo 'All sync and deploy jobs enabled'
