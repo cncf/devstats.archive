bb79e31abac4d1230e187aa90d436e73b83ee0cb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-12 11:34:12 +0000

    Fix Grafana init script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- nats/create_grafana.sh
+++ nats/create_grafana.sh
@@ -43,15 +43,14 @@ cd ~/dev/cncf/artwork || exit 2
 git pull || exit 3
 cd $WD || exit 4
 
-cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$icon/public/img/grafana_icon.svg" || exit 5
-cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$icon/public/img/grafana_com_auth_icon.svg" || exit 6
-cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$icon/public/img/grafana_net_logo.svg" || exit 7
-cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$icon/public/img/grafana_mask_icon.svg" || exit 8
-convert "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.png" -resize 80x80  "/var/www/html/img/$icon-icon-color.png" || exit 9
-
 if [ ! -d "/usr/share/grafana.$proj/" ]
 then
   cp -R ~/grafana.v5/usr.share.grafana "/usr/share/grafana.$proj"/ || exit 10
+  cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$proj/public/img/grafana_icon.svg" || exit 5
+  cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$proj/public/img/grafana_com_auth_icon.svg" || exit 6
+  cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$proj/public/img/grafana_net_logo.svg" || exit 7
+  cp "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.svg" "/usr/share/grafana.$proj/public/img/grafana_mask_icon.svg" || exit 8
+  convert "$HOME/dev/cncf/artwork/$icon/icon/color/$icon-icon-color.png" -resize 80x80  "/var/www/html/img/$proj-icon-color.png" || exit 9
   GRAFANA_DATA=/usr/share/grafana.nats/ ./grafana/nats/change_title_and_icons.sh || exit 11
 fi
 
