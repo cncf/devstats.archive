bf8e3b28e823459c81e76f1c6fbfdb7595eb3a2d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-12 13:53:38 +0000

    Unify deploy script - make shared one which will deploy by using ENV variables [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/create_databases.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# PDB=1 (will generate Postgres DB)
+# PDROP=1 (will drop & create Postgres DB)
+# GET=1 (will use Postgres DB backup if available)
+# GAPS=1 (will update metrics/$PROJ/gaps.yaml with Top repo groups from PSQL database)
+# IDB=1 (will generate Influx DB)
+# IDROP=1 (will drop & create Influx DB)
+lim=70
+set -o pipefail
+if ( [ -z "$PG_PASS" ] || [ -z "$IDB_PASS" ] || [ -z "$IDB_HOST" ] )
+then
+  echo "$0: You need to set PG_PASS, IDB_PASS, IDB_HOST environment variables to run this script"
+  exit 1
+fi
+if ( [ -z "$PROJ" ] || [ -z "$PROJDB" ] )
+then
+  echo "$0: You need to set PROJ, PROJDB environment variables to run this script"
+  exit 2
+fi
+function finish {
+    rm -rf "$PROJDB.dump" >/dev/null 2>&1
+    sync_unlock.sh
+}
+if [ -z "$TRAP" ]
+then
+  sync_lock.sh || exit -1
+  trap finish EXIT
+  export TRAP=1
+fi
+if [ ! -z "$PDB" ]
+then
+  exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = '$PROJDB'"` || exit 3
+  if ( [ ! -z "$PDROP" ] && [ "$exists" = "1" ] )
+  then
+    echo "dropping postgres database $PROJDB"
+    sudo -u postgres psql -c "drop database $PROJDB" || exit 4
+  fi
+  exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = '$PROJDB'"` || exit 5
+  if [ ! "$exists" = "1" ]
+  then
+    echo "creating postgres database $PROJDB"
+    sudo -u postgres psql -c "create database $PROJDB" || exit 6
+    sudo -u postgres psql -c "grant all privileges on database \"$PROJDB\" to gha_admin" || exit 7
+    if [ ! -z "$GET" ]
+    then
+      echo "attempt to fetch postgres database $PROJDB from backup"
+      wget "https://cncftest.io/$PROJDB.dump" || exit 8
+      sudo -u postgres pg_restore -d "$PROJDB" "$PROJDB.dump" || exit 9
+      rm -f "$PROJ.dump" || exit 10
+    else
+      echo "generating postgres database $PROJDB"
+      GHA2DB_MGETC=y ./$PROJ/psql.sh || exit 11
+    fi
+  else
+    echo "postgres database $PROJDB already exists"
+  fi
+else
+  echo "postgres database $PROJDB generation skipped"
+fi
+if [ ! -z "$GAPS" ]
+then
+  sql=`sed -e "s/{{lim}}/$lim/g" ./util_sql/top_repo_groups.sql`
+  repo_groups=`sudo -u postgres psql "$PROJDB" -tAc "$sql"`
+  MODE=rs FROM=';;;(.*) # {{repo_groups}}' TO=";;;$repo_groups # {{repo_groups}}" ./replacer ./metrics/$PROJ/gaps.yaml || exit 12
+fi
+if [ ! -z "$IDB" ]
+then
+  exists=`echo 'show databases' | influx -host $IDB_HOST -username gha_admin -password $IDB_PASS | grep $PROJDB`
+  if ( [ ! -z "$IDROP" ] && [ "$exists" = "$PROJDB" ] )
+  then
+    echo "dropping influx database $PROJDB"
+    ./grafana/influxdb_drop.sh "$PROJDB" || exit 13
+  fi
+  exists=`echo 'show databases' | influx -host $IDB_HOST -username gha_admin -password $IDB_PASS | grep $PROJDB`
+  if [ ! "$exists" = "$PROJDB" ]
+  then
+    echo "generating influx database $PROJDB"
+    ./grafana/influxdb_recreate.sh "$PROJDB" || exit 14
+    ./$PROJ/reinit.sh || exit 15
+  else
+    echo "influx database $PROJDB already exists"
+  fi
+else
+  echo "influxdb database $PROJDB generation skipped"
+fi
+echo 'finished'
