defd51ab3a3f3ea5d0bd12e6c47acb23b7bb4f52
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-13 07:56:34 +0000

    Final NATS related tweaks (some scripts were not updated to support it) [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/top_repo_groups_all.sh
+++ devel/top_repo_groups_all.sh
@@ -7,9 +7,9 @@ fi
 host=`hostname`
 if [ $host = "cncftest.io" ]
 then
-  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers all cncf"
+  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess nats opencontainers all cncf"
 else
-  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess opencontainers"
+  all="kubernetes prometheus opentracing fluentd linkerd grpc coredns containerd rkt cni envoy jaeger notary tuf rook vitess nats opencontainers"
 fi
 for f in $all
 do
