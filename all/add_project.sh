db271eaa5ba1febe5e69056cd9d45805ae1ba94d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-19 13:12:58 +0000

    Final 'idb_backup' allowing to copy entire Influx databases test -> prod
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- all/add_project.sh
+++ all/add_project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # IDB=1 (will update InfluxDB)
+# IGET=1 (attempt to fetch Influx database from the test server)
 # AGET=1 (will fetch allprj database from backup - not recommended because local merge is faster)
 set -o pipefail
 if ( [ -z "$1" ] || [ -z "$2" ] )
@@ -12,13 +13,18 @@ then
   echo "$0: You need to set PG_PASS, IDB_PASS, IDB_HOST environment variables to run this script"
   exit 2
 fi
-exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'allprj'"` || exit 3
+if ( [ ! -z "$IGET" ] && [ -z "$IDB_PASS_SRC" ] )
+then
+  echo "$0: You need to set IDB_PASS_SRC environment variable when using IGET"
+  exit 3
+fi
+exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'allprj'"` || exit 4
 if [ -z "$exists" ]
 then
   echo "All CNCF Project database doesn't exist"
   exit 0
 fi
-added=`sudo -u postgres psql allprj -tAc "select name from gha_repos where name = '$2'"` || exit 4
+added=`sudo -u postgres psql allprj -tAc "select name from gha_repos where name = '$2'"` || exit 5
 if [ ! -z "$added" ]
 then
   echo "Project '$1' is already present in 'All CNCF', repo '$2' exists"
@@ -36,27 +42,39 @@ fi
 if [ ! -z "$AGET" ]
 then
   echo "attempt to fetch postgres database allprj from backup"
-  wget "https://cncftest.io/allprj.dump" || exit 5
-  sudo -u postgres pg_restore -d allprj allprj.dump || exit 6
-  rm -f allprj.dump || exit 7
+  wget "https://cncftest.io/allprj.dump" || exit 6
+  sudo -u postgres pg_restore -d allprj allprj.dump || exit 7
+  rm -f allprj.dump || exit 8
   echo "allprj backup restored"
 else
   echo "merging $1 into allprj"
-  GHA2DB_INPUT_DBS="$1" GHA2DB_OUTPUT_DB="allprj" ./merge_pdbs || exit 8
-  PG_DB="allprj" ./devel/remove_db_dups.sh || exit 9
-  ./all/get_repos.sh || exit 10
-  ./all/setup_repo_groups.sh || exit 11
+  GHA2DB_INPUT_DBS="$1" GHA2DB_OUTPUT_DB="allprj" ./merge_pdbs || exit 9
+  PG_DB="allprj" ./devel/remove_db_dups.sh || exit 10
+  ./all/get_repos.sh || exit 11
+  ./all/setup_repo_groups.sh || exit 12
 fi
 if [ -z "$IDB" ]
 then
-  ./all/top_n_repos_groups.sh 70 > out || exit 12
-  ./all/top_n_companies 70 >> out || exit 13
+  ./all/top_n_repos_groups.sh 70 > out || exit 13
+  ./all/top_n_companies 70 >> out || exit 14
   cat out
   echo 'Please update ./metrics/all/gaps*.yaml with new companies & repo groups data (also dont forget repo groups).'
   echo 'Then run ./all/reinit.sh.'
   echo 'Top 70 repo groups & companies are saved in "out" file.'
 else
-  echo "regenerating allprj influx database"
-  ./all/reinit.sh || exit 14
+  if [ -z "$IGET" ]
+  then
+    echo "regenerating allprj influx database"
+    ./all/reinit.sh || exit 15
+  else
+    echo 'fetching allprj database from cncftest.io (into allprj_temp database)'
+    ./grafana/influxdb_recreate.sh allprj_temp || exit 16
+    IDB_HOST_SRC=cncftest.io IDB_USER_SRC=ro_user IDB_DB_SRC=allprj IDB_DB_DST=allprj_temp ./idb_backup || exit 17
+    echo 'copying allprj_temp database to allprj'
+    ./grafana/influxdb_recreate.sh allprj || exit 18
+    unset IDB_PASS_SRC
+    IDB_DB_SRC=allprj_temp IDB_DB_DST=allprj ./idb_backup || exit 19
+    ./grafana/influxdb_drop.sh allprj_temp || exit 20
+  fi
 fi
 echo "$0: $1 finished"
