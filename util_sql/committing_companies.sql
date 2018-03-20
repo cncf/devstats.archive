bd8ccac28b33d3ffcddae25fd8d91900e8760470
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-20 14:15:00 +0000

    Added script to calculate number of contributing and committing companies
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/committing_companies.sql
@@ -0,0 +1,22 @@
+select
+  sub.company,
+  sub.events
+from (
+  select aa.company_name as company,
+    count(e.id) as events
+  from
+    gha_events e,
+    gha_actors_affiliations aa
+  where
+    e.actor_id = aa.actor_id
+    and e.type in ('PushEvent', 'PullRequestEvent')
+    and e.created_at >= '{{from}}'
+    and e.created_at < '{{to}}'
+    and (e.dup_actor_login {{exclude_bots}})
+  group by
+    aa.company_name
+  ) sub
+order by
+  sub.events desc,
+  sub.company asc
+;
---
+++ util_sql/contributing_companies.sql
@@ -0,0 +1,25 @@
+select
+  sub.company,
+  sub.events
+from (
+  select aa.company_name as company,
+    count(e.id) as events
+  from
+    gha_events e,
+    gha_actors_affiliations aa
+  where
+    e.actor_id = aa.actor_id
+    and e.type in (
+      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+    )
+    and e.created_at >= '{{from}}'
+    and e.created_at < '{{to}}'
+    and (e.dup_actor_login {{exclude_bots}})
+  group by
+    aa.company_name
+  ) sub
+order by
+  sub.events desc,
+  sub.company asc
+;
