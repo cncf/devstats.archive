6269f440f2eaf2f3cc5101f247ce4c2d87ab31e1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-22 08:06:43 +0000

    2nd SPIFFE test deploy attempt [deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- spiffe/import_affs.sh
+++ spiffe/import_affs.sh
@@ -10,4 +10,9 @@ then
 fi
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=spiffe PG_DB=spiffe IDB_DB=spiffe ./runq scripts/clean_affiliations.sql
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=spiffe PG_DB=spiffe IDB_DB=spiffe ./import_affs github_users.json || exit 1
+exists=`echo 'show databases' | influx -host $IDB_HOST -username gha_admin -password $IDB_PASS | grep spiffe`
+if [ -z "$exists" ]
+then
+  ./grafana/influxdb_recreate.sh spiffe || exit 2
+fi
 GHA2DB_LOCAL=1 GHA2DB_PROJECT=spiffe PG_DB=spiffe IDB_DB=spiffe ./idb_tags
