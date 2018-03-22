5b7b29f2cff98971ceefe5a23d030e5d4ce15fec
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-22 10:54:25 +0000

    SPIFFE auto deployed on the test, adding 'ghapi2db' tool [deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ cmd/ghapi2db/ghapi2db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	lib "devstats"
+)
+
+// Insert Postgres vars
+func ghapi() {
+	// Environment context parse
+	var ctx lib.Ctx
+	ctx.Init()
+
+	// Connect to Postgres DB
+	c := lib.PgConn(&ctx)
+	defer func() { lib.FatalOnError(c.Close()) }()
+
+	// Connect to GitHub API
+	gctx, gc := lib.GHClient(&ctx)
+
+  // Get RateLimits info
+  all, rem, wait := lib.GetRateLimits(gctx, gc, true)
+  fmt.Printf("all=%d, rem=%d, wait=%v\n", all, rem, wait)
+}
+
+func main() {
+	dtStart := time.Now()
+	ghapi()
+	dtEnd := time.Now()
+	lib.Printf("Time: %v\n", dtEnd.Sub(dtStart))
+}
