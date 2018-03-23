c54eb3b40f9b74499034da4dbaea79c00f781672
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-23 13:20:50 +0000

    Functionality finished, now need to battle test it, chekc for bugs and check how it reacts to no API points available
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/delete_artificial.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+PG_PASS=Admin123 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq ./util_sql/delete_artificial.sql
