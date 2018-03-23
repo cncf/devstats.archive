8fa456f93922740cd867cb1d2c5ff1147fe7ecc7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-23 08:03:27 +0000

    'ghapi2db' wip wip wip [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cmd/ghapi2db/ghapi2db.go
+++ cmd/ghapi2db/ghapi2db.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"strings"
@@ -14,9 +15,104 @@ import (
 type issueConfig struct {
 	repo        string
 	number      int
-	issueID     int
+	issueID     int64
 	pr          bool
 	milestoneID *int64
+}
+
+func milestoneEvent(c *sql.DB, ctx *lib.Ctx, milestone string, iid, eid int64) (err error) {
+	// Create artificial event, add 2^60 to eid
+	eventID := 1152921504606846976 + eid
+	now := time.Now()
+
+	// Start transaction
+	tc, err := c.Begin()
+	lib.FatalOnError(err)
+
+	// Create new issue state
+	lib.ExecSQLTxWithErr(
+		tc,
+		ctx,
+		fmt.Sprintf(
+			"insert into gha_issues("+
+				"id, event_id, assignee_id, body, closed_at, comments, created_at, "+
+				"locked, milestone_id, number, state, title, updated_at, user_id, "+
+				"dup_actor_id, dup_actor_login, dup_repo_id, dup_repo_name, dup_type, dup_created_at, "+
+				"dup_user_login, dupn_assignee_login, is_pull_request) "+
+				"select id, %s, assignee_id, body, closed_at, comments, created_at, "+
+				"locked, %s, number, state, title, %s, 0, "+
+				"0, 'devstats-bot', dup_repo_id, dup_repo_name, 'MilestonesEvent', %s, "+
+				"'devstats-bot', dupn_assignee_login, is_pull_request "+
+				"from gha_issues where id = %s and event_id = %s",
+			lib.NValue(1),
+			milestone,
+			lib.NValue(2),
+			lib.NValue(3),
+			lib.NValue(4),
+			lib.NValue(5),
+		),
+		lib.AnyArray{
+			eventID,
+			now,
+			now,
+			iid,
+			eid,
+		}...,
+	)
+
+	// Create artificial 'MilestonesEvent' event
+	lib.ExecSQLTxWithErr(
+		tc,
+		ctx,
+		fmt.Sprintf(
+			"insert into gha_events("+
+				"id, type, actor_id, repo_id, public, created_at, "+
+				"dup_actor_login, dup_repo_name, org_id, forkee_id) "+
+				"select %s, 'MilestonesEvent', 0, repo_id, public, %s, "+
+				"'devstats-bot', dup_repo_name, org_id, forkee_id "+
+				"from gha_events where id = %s",
+			lib.NValue(1),
+			lib.NValue(2),
+			lib.NValue(3),
+		),
+		lib.AnyArray{
+			eventID,
+			now,
+			eid,
+		}...,
+	)
+
+	// Create artificial event's payload
+	lib.ExecSQLTxWithErr(
+		tc,
+		ctx,
+		fmt.Sprintf(
+			"insert into gha_payloads("+
+				"event_id, push_id, size, ref, head, befor, action, "+
+				"issue_id, pull_request_id, comment_id, ref_type, master_branch, commit, "+
+				"description, number, forkee_id, release_id, member_id, "+
+				"dup_actor_id, dup_actor_login, dup_repo_id, dup_repo_name, dup_type, dup_created_at) "+
+				"select %s, null, null, null, null, null, 'stubbed', "+
+				"issue_id, pull_request_id, null, null, null, null, "+
+				"null, number, null, null, null, "+
+				"0, 'devstats-bot', dup_repo_id, dup_repo_name, 'MilestonesEvent', %s "+
+				"from gha_payloads where issue_id = %s and event_id = %s",
+			lib.NValue(1),
+			lib.NValue(2),
+			lib.NValue(3),
+			lib.NValue(4),
+		),
+		lib.AnyArray{
+			eventID,
+			now,
+			iid,
+			eid,
+		}...,
+	)
+
+	// Final commit
+	lib.FatalOnError(tc.Commit())
+	return
 }
 
 // Insert Postgres vars
@@ -58,23 +154,28 @@ func ghapi() {
 
 	// Get issues/PRs to check
 	// repo, number, issueID, is_pr
-	repo, number, issueID, pr := "", 0, 0, false
 	m := make(map[int]issueConfig)
-	nIssues := 0
-	for rows.Next() {
-		lib.FatalOnError(rows.Scan(&repo, &number, &issueID, &pr))
-		cfg := issueConfig{repo: repo, number: number, issueID: issueID, pr: pr}
-		v, ok := m[issueID]
-		if ok {
-			if ctx.Debug > 0 {
-				lib.Printf("Warning: we already have issue config for id=%d: %v, skipped new config: %v\n", issueID, v, cfg)
+	// TODO:
+	/*
+		repo, number, issueID, pr := "", 0, 0, false
+		nIssues := 0
+		for rows.Next() {
+			lib.FatalOnError(rows.Scan(&repo, &number, &issueID, &pr))
+			cfg := issueConfig{repo: repo, number: number, issueID: issueID, pr: pr}
+			v, ok := m[issueID]
+			if ok {
+				if ctx.Debug > 0 {
+					lib.Printf("Warning: we already have issue config for id=%d: %v, skipped new config: %v\n", issueID, v, cfg)
+				}
+				continue
 			}
-			continue
+			m[issueID] = cfg
+			nIssues++
 		}
-		m[issueID] = cfg
-		nIssues++
-	}
-	lib.FatalOnError(rows.Err())
+		lib.FatalOnError(rows.Err())
+	*/
+	nIssues := 1
+	m[307893875] = issueConfig{repo: "kubernetes/kubernetes", number: 61579, issueID: 307893875, pr: false}
 
 	// GitHub don't like MT quering - they say that:
 	// 403 You have triggered an abuse detection mechanism. Please wait a few minutes before you try again
@@ -167,13 +268,13 @@ func ghapi() {
 			}
 			var (
 				ghaMilestoneID *int64
-				ghaEventID     int
+				ghaEventID     int64
 			)
 			apiMilestoneID := cfg.milestoneID
 			rows := lib.QuerySQLWithErr(
 				c,
 				&ctx,
-				fmt.Sprintf("select milestone_id, event_id from gha_issues where id = %s order by event_id desc limit 1", lib.NValue(1)),
+				fmt.Sprintf("select milestone_id, event_id from gha_issues where id = %s order by updated_at desc limit 1", lib.NValue(1)),
 				cfg.issueID,
 			)
 			defer func() { lib.FatalOnError(rows.Close()) }()
@@ -182,16 +283,16 @@ func ghapi() {
 			}
 			lib.FatalOnError(rows.Err())
 
-			// update will be non-empty when we detect that something needs to be updated
-			update := ""
+			// newMilestone will be non-empty when we detect that something needs to be updated
+			newMilestone := ""
 			if apiMilestoneID == nil && ghaMilestoneID != nil {
-				update = fmt.Sprintf("update gha_issues set milestone_id = null where id = %s and event_id = %s", lib.NValue(1), lib.NValue(2))
+				newMilestone = "null"
 				if ctx.Debug > 0 {
 					lib.Printf("Updating issue '%v' milestone to null, it was %d\n", cfg, *ghaMilestoneID)
 				}
 			}
 			if apiMilestoneID != nil && (ghaMilestoneID == nil || *apiMilestoneID != *ghaMilestoneID) {
-				update = fmt.Sprintf("update gha_issues set milestone_id = %d where id = %s and event_id = %s", *apiMilestoneID, lib.NValue(1), lib.NValue(2))
+				newMilestone = fmt.Sprintf("%d", *apiMilestoneID)
 				if ctx.Debug > 0 {
 					if ghaMilestoneID != nil {
 						lib.Printf("Updating issue '%v' milestone to %d, it was %d\n", cfg, *apiMilestoneID, *ghaMilestoneID)
@@ -201,8 +302,8 @@ func ghapi() {
 				}
 			}
 			// Do the update if needed
-			if update != "" {
-				lib.ExecSQLWithErr(c, &ctx, update, lib.AnyArray{cfg.issueID, ghaEventID}...)
+			if newMilestone != "" {
+				lib.FatalOnError(milestoneEvent(c, &ctx, newMilestone, cfg.issueID, ghaEventID))
 				updates++
 			}
 
