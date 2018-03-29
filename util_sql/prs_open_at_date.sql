1bd1232fa3e74268f8d9d1bd32c2723dcb9e23fc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-29 11:22:04 +0000

    Now I have fast SQL to get Issues/PRs opened at date - finally [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/prs_open_at_date.sql
+++ util_sql/prs_open_at_date.sql
@@ -1,75 +1,67 @@
-create temp table issues as
-select i.id as issue_id,
-  i.event_id,
-  i.updated_at
-from
-  gha_issues i
-where
-  i.is_pull_request = true
-  and i.id > 0
-  and i.event_id > 0
-  and i.closed_at is null
-  and i.created_at < '{{date}}'
-  and i.event_id = (
-    select inn.event_id
+with issues as (
+  select sub.issue_id,
+    sub.event_id,
+    sub.updated_at
+  from (
+    select distinct
+      id as issue_id,
+      last_value(event_id) over issues_ordered_by_update as event_id,
+      last_value(closed_at) over issues_ordered_by_update as closed_at,
+      last_value(updated_at) over issues_ordered_by_update as updated_at
     from
-      gha_issues inn
+      gha_issues
     where
-      inn.id = i.id
-      and inn.id > 0
-      and inn.event_id > 0
-      and inn.created_at < '{{date}}'
-      and inn.is_pull_request = true
-      and inn.updated_at < '{{date}}'
-    order by
-      inn.updated_at desc,
-      inn.event_id desc
-    limit
-      1
-  )
-;
-
-create temp table prs as
-select i.issue_id,
-  i.event_id as last_issue_event_id,
-  i.updated_at as issue_last_updated,
-  pr.id as pr_id,
-  pr.event_id as last_pr_event_id,
-  pr.updated_at as pr_last_updated
-from
-  issues i,
-  gha_issues_pull_requests ipr,
-  gha_pull_requests pr
-where
-  ipr.issue_id = i.issue_id
-  and ipr.pull_request_id = pr.id
-  and pr.id > 0
-  and pr.event_id > 0
-  and pr.closed_at is null
-  and pr.merged_at is null
-  and pr.created_at < '{{date}}'
-  and pr.event_id = (
-    select inn.event_id
+      created_at < '{{date}}'
+      and updated_at < '{{date}}'
+      and is_pull_request = true
+    window
+      issues_ordered_by_update as (
+        partition by id
+        order by
+          updated_at asc,
+          event_id asc
+        range between current row
+        and unbounded following
+      )
+    ) sub
+  where
+    sub.closed_at is null
+), prs as (
+  select i.issue_id,
+    i.event_id as last_issue_event_id,
+    i.updated_at as issue_last_updated,
+    pr.pr_id,
+    pr.event_id as last_pr_event_id,
+    pr.updated_at as pr_last_updated
+  from (
+    select distinct
+      id as pr_id,
+      last_value(event_id) over prs_ordered_by_update as event_id,
+      last_value(closed_at) over prs_ordered_by_update as closed_at,
+      last_value(merged_at) over prs_ordered_by_update as merged_at,
+      last_value(updated_at) over prs_ordered_by_update as updated_at
     from
-      gha_pull_requests inn
+      gha_pull_requests
     where
-      inn.id = pr.id
-      and inn.id > 0
-      and inn.event_id > 0
-      and inn.created_at < '{{date}}'
-      and inn.updated_at < '{{date}}'
-    order by
-      inn.updated_at desc,
-      inn.event_id desc
-    limit
-      1
-  )
-order by
-  i.updated_at desc,
-  pr.updated_at desc
-;
-
+      created_at < '{{date}}'
+      and updated_at < '{{date}}'
+      and event_id > 0
+    window
+      prs_ordered_by_update as (
+        partition by id
+        order by
+          updated_at asc,
+          event_id asc
+        range between current row
+        and unbounded following
+      )
+    ) pr,
+    issues i,
+    gha_issues_pull_requests ipr
+  where
+    ipr.issue_id = i.issue_id
+    and ipr.pull_request_id = pr.pr_id
+    and pr.closed_at is null
+    and pr.merged_at is null
+)
 select * from prs;
-
-drop table prs;
-drop table issues;
