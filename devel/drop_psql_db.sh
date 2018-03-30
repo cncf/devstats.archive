10da5ed1142516b487c37728074ae1dff00530e7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-03-30 06:36:15 +0000

    Just started automatic deploy of SPIFFE & SPIRE (2 at once) on the test [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/drop_psql_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide db name"
+  exit 1
+fi
+sudo -u postgres psql -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = '$1'"
+sudo -u postgres psql -c "drop database $1"
