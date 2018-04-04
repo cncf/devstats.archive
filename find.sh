9dc9030339131dc20f023f0c3370575134bc050a
lukaszgryglicki <lukaszgryglicki@o2.pl>
2018-04-04 10:24:55 +0200

    'find.sh' script update [no deploy]
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- find.sh
+++ find.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-  echo "You need to provide file name pattern as a first argument"
+  echo "You need to provide path as first arument"
   exit 1
 fi
 if [ -z "$2" ]
 then
+  echo "You need to provide file name pattern as a first argument"
+  exit 1
+fi
+if [ -z "$3" ]
+then
   echo "You need to provide regexp pattern to search for as a second argument"
   exit 1
 fi
-find . -iname "$1" -exec grep -EHIn "$2" "{}" \; | tee -a out
+find "$1" -iname "$2" -exec grep -EHIn "$3" "{}" \; | tee -a out
