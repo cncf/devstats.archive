45054c1e0f8c8c64fd9b6cad241a2802782df976
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-05 13:25:42 +0000

    Mass rename all projects dashboards to their SQLite3 slug name
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/get_all_sqlite_jsons.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ -z "$ONLY" ]
+then
+  host=`hostname`
+  if [ $host = "cncftest.io" ]
+  then
+    all=`cat ./devel/all_test_projects.txt`
+  else
+    all=`cat ./devel/all_prod_projects.txt`
+  fi
+else
+  all=$ONLY
+fi
+for proj in $all
+do
+    db=$proj
+    if [ "$proj" = "kubernetes" ]
+    then
+      db="k8s"
+    fi
+    echo "Project: $proj, GrafanaDB: $db"
+    rm -f sqlite/* 2>/dev/null
+    ./import_json /var/lib/grafana.$db/grafana.db || exit 1
+    rm -f grafana/dashboards/$proj/*.json || exit 2
+    mv sqlite/*.json grafana/dashboards/$proj/ || exit 3
+done
+echo 'OK'
