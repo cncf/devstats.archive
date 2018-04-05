d7b6deda2842ea1b555a0c5548cc9f380836f8bf
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-05 13:17:47 +0000

    Added dump all dashboards by their slug name mode
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cmd/import_json/import_json.go
+++ cmd/import_json/import_json.go
@@ -19,23 +19,54 @@ type dashboard struct {
 	UID   string `json:"uid"`
 }
 
-// importJsons uses sqlFile database to update list of JSONs
-func importJsons(sqlFile string, jsons []string) {
+// outputJsons uses dbFile database to dump all dashboards as JSONs
+func outputJsons(dbFile string) {
+	// Environment context parse
+	var ctx lib.Ctx
+	ctx.Init()
+
+	// Connect to SQLite3
+	db, err := sql.Open("sqlite3", dbFile)
+	lib.FatalOnError(err)
+	defer func() { lib.FatalOnError(db.Close()) }()
+
+	// Get all dashboards
+	rows, err := db.Query("select slug, title, data from dashboard")
+	lib.FatalOnError(err)
+	defer func() { lib.FatalOnError(rows.Close()) }()
+	var (
+		slug  string
+		title string
+		data  string
+	)
+	for rows.Next() {
+		err = rows.Scan(&slug, &title, &data)
+		lib.FatalOnError(err)
+		fn := "sqlite/" + slug + ".json"
+		lib.FatalOnError(ioutil.WriteFile(fn, lib.PrettyPrintJSON([]byte(data)), 0644))
+		lib.Printf("Written '%s' to %s\n", title, fn)
+	}
+	err = rows.Err()
+	lib.FatalOnError(err)
+}
+
+// importJsons uses dbFile database to update list of JSONs
+func importJsons(dbFile string, jsons []string) {
 	// Environment context parse
 	var ctx lib.Ctx
 	ctx.Init()
 
 	// Make DB backup:
-	contents, err := lib.ReadFile(&ctx, sqlFile)
+	contents, err := lib.ReadFile(&ctx, dbFile)
 	lib.FatalOnError(err)
 	lib.FatalOnError(ioutil.WriteFile(
-		fmt.Sprintf("%s.%v", sqlFile, time.Now().UnixNano()),
+		fmt.Sprintf("%s.%v", dbFile, time.Now().UnixNano()),
 		contents,
 		0644,
 	))
 
 	// Connect to SQLite3
-	db, err := sql.Open("sqlite3", sqlFile)
+	db, err := sql.Open("sqlite3", dbFile)
 	lib.FatalOnError(err)
 	defer func() { lib.FatalOnError(db.Close()) }()
 	var (
@@ -81,11 +112,16 @@ func importJsons(sqlFile string, jsons []string) {
 
 func main() {
 	dtStart := time.Now()
-	if len(os.Args) < 3 {
-		lib.Printf("Required sql file name and list of jsons to import.\n")
+	if len(os.Args) < 2 {
+		lib.Printf("%s: required args: grafana.db file name and list of jsons to import.\n", os.Args[0])
+		lib.Printf("%s: if only db file name given, it will output all dashboards to jsons\n", os.Args[0])
 		os.Exit(1)
 	}
-	importJsons(os.Args[1], os.Args[2:])
+	if len(os.Args) > 2 {
+		importJsons(os.Args[1], os.Args[2:])
+	} else {
+		outputJsons(os.Args[1])
+	}
 	dtEnd := time.Now()
 	lib.Printf("Time: %v\n", dtEnd.Sub(dtStart))
 }
