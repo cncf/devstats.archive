71d31c6734afd4eb736375df1ee1866c08cee525
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-05 13:17:47 +0000

    Added dump all dashboards by their slug name mode
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/get_from_sqlite.sh
+++ devel/get_from_sqlite.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 if [ -z "$GRAFANA" ]
 then
-  echo "$0: need to set GRAFANA env variable"
+  echo "$0: you need to set GRAFANA env variable"
   exit 1
 fi
-cp "/var/lib/grafana.$GRAFANA/grafana.db" a.db || exit 2
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide at least one dashboard JSON"
+  exit 2
+fi
+cp "/var/lib/grafana.$GRAFANA/grafana.db" a.db || exit 3
 function finish {
     rm -f a.db* 2>/dev/null
 }
 trap finish EXIT
-./import_json a.db $* || exit 3
+./import_json a.db $* || exit 4
 ./devel/update_from_sqlite.sh
