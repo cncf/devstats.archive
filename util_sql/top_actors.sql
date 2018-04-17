c8e474d3fef1361ff28d1a5bc3e206bb66413407
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-17 08:17:10 +0000

    New top actors script [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/top_actors.sql
@@ -0,0 +1,24 @@
+select
+  e.dup_actor_login,
+  a.company_name,
+  a.dt_from,
+  a.dt_to,
+  count(distinct e.id) as cnt
+from
+  gha_events e
+left join
+  gha_actors_affiliations a
+on
+  e.actor_id = a.actor_id
+where
+  e.dup_actor_login not like all(array['googlebot', 'coveralls', 'rktbot', 'coreosbot', 'web-flow', 'openstack-gerrit', 'prometheus-roobot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%'])
+group by
+  e.dup_actor_login,
+  a.company_name,
+  a.dt_from,
+  a.dt_to
+order by
+  cnt desc
+limit
+  30
+;
