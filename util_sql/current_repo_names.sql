a0b5c1db504ee552ab4487c69467961a7f39ecc5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-20 12:23:43 +0000

    Optimize SQL
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/current_repo_names.sql
+++ util_sql/current_repo_names.sql
@@ -1,15 +1,27 @@
 select
-  distinct last_value(dup_repo_name) over events_by_date as repo_name
-from
-  gha_events
+  distinct last_value(dup_repo_name) over by_date as repo_name
+from (
+  select
+    repo_id,
+    dup_repo_name,
+    max(created_at) as max_date
+  from
+    gha_events
+  where
+    dup_repo_name like '%/%'
+  group by
+    repo_id,
+    dup_repo_name
+) sub
 window
-  events_by_date as (
+  by_date as (
     partition by
       repo_id
     order by
-      created_at asc
-    range between current row
-    and unbounded following
+      max_date asc
+    range
+      between unbounded preceding
+      and unbounded following
   )
 order by
   repo_name
