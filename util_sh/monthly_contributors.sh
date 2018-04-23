e4d57f549824b696680a744deebba9dc8f1bc11c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 12:12:12 +0000

    New contributors for KubeCon.EU report [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/monthly_contributors.sh
+++ util_sh/monthly_contributors.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=gha GHA2DB_CSVOUT="monthly_contributors.csv" ./runq util_sql/monthly_contributors.sql {{start_date}} '2014-01-01'
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide start date argument"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=gha GHA2DB_CSVOUT="monthly_contributors.csv" ./runq util_sql/monthly_contributors.sql {{start_date}} "$1"
