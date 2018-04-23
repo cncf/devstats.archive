a5afe2d118b20a2df15dfde30771a17f6b0df1b1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 13:24:09 +0000

    Requed velocity stats scripts [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/velocity.sql
@@ -0,0 +1,34 @@
+with days as (
+  select
+    date_part('day', '{{date_to}}'::timestamp - '{{date_from}}'::timestamp) as days
+)
+select
+  e.dup_repo_name as repo,
+  count(e.id) / d.days as commits_per_day
+from
+  gha_events e,
+  days d
+where
+  e.type in ({{types}})
+  and (e.dup_actor_login {{exclude_bots}})
+  and e.created_at >= '{{date_from}}'
+  and e.created_at < '{{date_to}}'
+group by
+  e.dup_repo_name,
+  d.days
+union select 'All' as repo,
+  count(e.id) / d.days as commits_per_day
+from
+  gha_events e,
+  days d
+where
+  e.type in ({{types}})
+  and (e.dup_actor_login {{exclude_bots}})
+  and e.created_at >= '{{date_from}}'
+  and e.created_at < '{{date_to}}'
+group by
+  d.days
+order by
+  commits_per_day desc,
+  repo asc
+;
