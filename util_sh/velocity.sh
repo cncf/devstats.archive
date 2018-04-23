9041687c7d1694c8252236b013a1ae428d236843
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 13:24:09 +0000

    Requed velocity stats scripts [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/velocity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+if ( [ -z "$1" ] || [ -z "$2" ]  || [ -z "$3" ] )
+then
+  echo "$0: you need to provide types, date from and date to arguments"
+  echo "$0: example: \"'PushEvent', 'IssuesEvent', 'PullRequestEvent'\" '2017-07-01 2017-08-01"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 GHA2DB_CSVOUT="velocity.csv" ./runq util_sql/velocity.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{types}} "$1" {{date_from}} "$2" {{date_to}} "$3"
