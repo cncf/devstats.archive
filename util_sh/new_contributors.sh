8c461f085150aa359be73362be525012ef71e8e2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 12:12:12 +0000

    New contributors for KubeCon.EU report [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/new_contributors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide date argument"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=gha GHA2DB_CSVOUT="new_contributors.csv" ./runq util_sql/new_contributors.sql {{date}} "$1"
---
+++ util_sql/new_contributors.sql
@@ -0,0 +1,23 @@
+select 
+  distinct dup_actor_login,
+  count(*) as contributions
+from
+  gha_events
+where
+  type in ('PushEvent', 'IssuesEvent', 'PullRequestEvent')
+  and created_at > '{{date}}'
+  and dup_actor_login not in(
+  select
+    distinct dup_actor_login
+  from
+    gha_events
+  where
+    type in ('PushEvent', 'IssuesEvent', 'PullRequestEvent')
+    and created_at < '{{date}}'
+  )
+group by
+  dup_actor_login
+order by
+  contributions desc,
+  dup_actor_login asc
+;
