d32a5965043596485452a6a38ea6b1bcc4f8f528
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 08:14:57 +0000

    K8s analysis for KubeCon wip [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/top_committing_companies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide number of companies to display as an arg"
+  exit 1
+fi
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to set PG_PASS to run this script"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq util_sql/top_committing_companies.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{lim}} "$1"
---
+++ util_sql/top_committing_companies.sql
@@ -0,0 +1,27 @@
+select
+  string_agg(sub.name, ',') from (
+  select c.name as name,
+    count(distinct e.actor_id) as acnt,
+    count(distinct e.id) as ecnt
+  from
+    gha_companies c,
+    gha_actors_affiliations aa,
+    gha_events e
+  where
+    aa.company_name = c.name
+    and e.actor_id = aa.actor_id
+    and c.name not in (
+      '(Unknown)'
+    )
+    and e.type = 'PushEvent'
+    and e.created_at > now() - '3 years'::interval
+    and (e.dup_actor_login {{exclude_bots}})
+  group by
+    c.name
+  order by
+    acnt desc,
+    ecnt desc,
+    name asc
+  limit {{lim}}
+) sub
+;
