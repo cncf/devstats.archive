9314275327d419bf18fbf0cce1e3e42233a21662
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-24 13:03:31 +0000

    Consolidating GH stats per repo group - query ready [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/github_stats_by_repo_groups.sql
+++ metrics/kubernetes/github_stats_by_repo_groups.sql
@@ -1,3 +1,20 @@
+with matching as (
+  select event_id
+  from
+    gha_texts
+  where
+    created_at >= '{{from}}'
+    and created_at < '{{to}}'
+    and substring(body from '(?i)(?:^|\n|\r)\s*/(?:lgtm|approve)\s*(?:\n|\r|$)') is not null
+), reviews as (
+  select id as event_id
+  from
+    gha_events
+  where
+    created_at >= '{{from}}'
+    and created_at < '{{to}}'
+    and type in ('PullRequestReviewCommentEvent')
+)
 select
   sub.repo_group,
   round(count(distinct sub.sha) / {{n}}, 2) as metric
@@ -83,6 +100,121 @@ from (
     and pr.merged_at is not null
     and pr.merged_at >= '{{from}}'
     and pr.merged_at < '{{to}}'
+  ) sub
+where
+  sub.repo_group is not null
+group by
+  sub.repo_group
+union select sub.repo_group,
+  round(count(distinct sub.id) / {{n}}, 2) as metric
+from (
+  select 'gh_stats_repo_groups_prs_closed,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    r.name = pr.dup_repo_name
+    and pr.merged_at is null
+    and pr.closed_at >= '{{from}}'
+    and pr.closed_at < '{{to}}'
+  ) sub
+where
+  sub.repo_group is not null
+group by
+  sub.repo_group
+union select 'gh_stats_repo_groups_pr_comments,' || r.repo_group as repo_group,
+  round(count(distinct i.event_id) / {{n}}, 2) as metric
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and r.repo_group is not null
+  and i.dup_created_at >= '{{from}}'
+  and i.dup_created_at < '{{to}}'
+  and i.dup_type = 'IssueCommentEvent'
+  and i.is_pull_request = false
+  and (i.dup_actor_login {{exclude_bots}})
+group by
+  r.repo_group
+union select 'gh_stats_repo_groups_pr_commenters,' || r.repo_group as repo_group,
+  round(count(distinct i.dup_actor_id) / {{n}}, 2) as metric
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and r.repo_group is not null
+  and i.dup_created_at >= '{{from}}'
+  and i.dup_created_at < '{{to}}'
+  and i.dup_type = 'IssueCommentEvent'
+  and i.is_pull_request = false
+  and (i.dup_actor_login {{exclude_bots}})
+group by
+  r.repo_group
+union select 'gh_stats_repo_groups_issue_comments,' || r.repo_group as repo_group,
+  round(count(distinct i.event_id) / {{n}}, 2) as metric
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and r.repo_group is not null
+  and i.dup_created_at >= '{{from}}'
+  and i.dup_created_at < '{{to}}'
+  and i.dup_type = 'IssueCommentEvent'
+  and i.is_pull_request = true
+  and (i.dup_actor_login {{exclude_bots}})
+group by
+  r.repo_group
+union select 'gh_stats_repo_groups_issue_commenters,' || r.repo_group as repo_group,
+  round(count(distinct i.dup_actor_id) / {{n}}, 2) as metric
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and r.repo_group is not null
+  and i.dup_created_at >= '{{from}}'
+  and i.dup_created_at < '{{to}}'
+  and i.dup_type = 'IssueCommentEvent'
+  and i.is_pull_request = true
+  and (i.dup_actor_login {{exclude_bots}})
+group by
+  r.repo_group
+union select sub.repo_group,
+  count(distinct sub.actor) as metric
+from (
+  select 'gh_stats_repo_groups_reviewers,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    e.dup_actor_login as actor
+  from
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    e.repo_id = r.id
+    and (e.dup_actor_login {{exclude_bots}})
+    and e.id in (
+      select min(event_id)
+      from
+        gha_issues_events_labels
+      where
+        created_at >= '{{from}}'
+        and created_at < '{{to}}'
+        and label_name in ('lgtm', 'approved')
+      group by
+        issue_id
+      union select event_id from matching
+      union select event_id from reviews
+    )
   ) sub
 where
   sub.repo_group is not null
