81ae8099695f43473bc342c0cbea1a541cd35f2a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-24 12:28:16 +0000

    Consolidating charts wip [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/github_stats_by_repo_groups.sql
+++ metrics/kubernetes/github_stats_by_repo_groups.sql
@@ -33,6 +33,61 @@ where
   and i.closed_at < '{{to}}'
 group by
   r.repo_group
+union select 'gh_stats_repo_groups_issues_opened,' || r.repo_group as repo_group,
+  round(count(distinct i.id) / {{n}}, 2) as metric
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and r.repo_group is not null
+  and i.created_at >= '{{from}}'
+  and i.created_at < '{{to}}'
+group by
+  r.repo_group
+union select sub.repo_group,
+  round(count(distinct sub.id) / {{n}}, 2) as metric
+from (
+    select 'gh_stats_repo_groups_new_prs,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    pr.dup_repo_id = r.id
+    and pr.created_at >= '{{from}}'
+    and pr.created_at < '{{to}}'
+  ) sub
+where
+  sub.repo_group is not null
+group by
+  sub.repo_group
+union select sub.repo_group,
+  round(count(distinct sub.id) / {{n}}, 2) as metric
+from (
+  select 'gh_stats_repo_groups_prs_merged,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    pr.id
+  from
+    gha_repos r,
+    gha_pull_requests pr
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = pr.event_id
+  where
+    r.name = pr.dup_repo_name
+    and pr.merged_at is not null
+    and pr.merged_at >= '{{from}}'
+    and pr.merged_at < '{{to}}'
+  ) sub
+where
+  sub.repo_group is not null
+group by
+  sub.repo_group
 order by
   repo_group asc
 ;
