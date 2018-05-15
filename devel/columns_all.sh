ee7ce4a0d8b5a95ec6170284b1cb8953ed7d754b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-15 05:07:37 +0000

    Missing columns in commits dahsboards + script to update all projects
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/columns_all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+if [ -z "$ONLY" ]
+then
+  host=`hostname`
+  if [ $host = "cncftest.io" ]
+  then
+    all=`cat ./devel/all_test_projects.txt`
+  else
+    all=`cat ./devel/all_prod_projects.txt`
+  fi
+else
+  all=$ONLY
+fi
+for proj in $all
+do
+    db=$proj
+    if [ "$proj" = "kubernetes" ]
+    then
+      db="gha"
+    elif [ "$proj" = "all" ]
+    then
+      db="allprj"
+    fi
+    echo "Project: $proj, PDB: $db"
+    GHA2DB_LOCAL=1 GHA2DB_PROJECT=$proj PG_DB=$db ./columns || exit 2
+done
+echo 'OK'
