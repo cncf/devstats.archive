ddb0980720de2b0628b7b8de86e4f210a2a67fed
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-15 04:46:37 +0000

    Made script to list all CNCF projects contributors list
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/contributing_actors.sql
+++ util_sql/contributing_actors.sql
@@ -1,18 +1,27 @@
 select
   -- string_agg(sub.actor, ',')
-  sub.actor
+  sub.id,
+  coalesce(sub.name, '-') as name,
+  coalesce(sub.email, '-') as email
 from (
-  select distinct a.login as actor
+  select distinct a.login as id,
+    a.name,
+    ae.email
   from
     gha_events e,
     gha_actors a
+  left join
+    gha_actors_emails ae
+  on
+    ae.actor_id = a.id
   where
     (e.actor_id = a.id or e.dup_actor_login = a.login)
     and type in (
-      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
-      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+      'PushEvent', 'PullRequestEvent', 'IssuesEvent'
     )
   order by
-    actor asc
+    id asc,
+    name asc,
+    email asc
   ) sub
 ;
