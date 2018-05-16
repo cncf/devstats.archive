acb4901e36845229404ebf358a4b9e4ac4e5faa6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-16 09:34:04 +0000

    PRs authors ported - had uid 23 != 21 related problem - fixed
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/copy_dashboards_replace_tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if ( [ -z "${SRC}" ] || [ -z "${FILE}" ] || [ -z "${ONLY}" ] )
+then
+  echo "You need to set ONLY, SRC, FILE environment variables to run this script"
+  echo 'ONLY="proj1 proj2" SRC="proj3" FILE="filename.json" $0'
+  exit 2
+fi
+for proj in $ONLY
+do
+    db=$proj
+    cp grafana/dashboards/$SRC/$FILE grafana/dashboards/$proj/$FILE || exit 3
+    FROM="\"$SRC\"" TO="\"$proj\"" MODE=ss0 ./replacer grafana/dashboards/$proj/$FILE || exit 4
+done
+echo 'OK'
+
