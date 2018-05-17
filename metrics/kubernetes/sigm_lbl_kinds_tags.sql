8202e8f59bfa0c03d09709adb2e65978bd74c0b4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-17 07:39:10 +0000

    tags tweak, I've removed too much
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/kubernetes/sigm_lbl_kinds_tags.sql
@@ -0,0 +1,22 @@
+select
+  -- string_agg(sub.kind, ',')
+  sub.kind
+from (
+  select sel.kind as kind,
+    count(distinct issue_id) as cnt
+  from (
+    select distinct issue_id,
+      lower(substring(dup_label_name from '(?i)kind/(.*)')) as kind
+    from
+      gha_issues_labels
+    ) sel
+  where
+    sel.kind is not null
+  group by
+    kind
+  order by
+    cnt desc,
+    kind asc
+) sub
+limit {{lim}}
+;
