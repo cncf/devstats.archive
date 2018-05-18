c75774106aab9b84e473193c8000d83a390d0bc6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-18 11:16:58 +0000

    Fix script [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/restore_db.sh
+++ devel/restore_db.sh
@@ -4,6 +4,6 @@ then
   echo "$0: you need to provide database name"
   exit 1
 fi
-sudo -u postgres dropdb $1
+./devel/drop_psql_db.sh $1
 sudo -u postgres createdb $1
 sudo -u postgres pg_restore -d $1 $1.dump
