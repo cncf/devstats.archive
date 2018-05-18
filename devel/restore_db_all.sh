513d3b56a43dc87b693c96b98bb5c6e46849e60f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-18 11:11:24 +0000

    Added restore DB scripts [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/restore_db_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$ONLY" ]
+then
+  host=`hostname`
+  if [ $host = "cncftest.io" ]
+  then
+    all=`cat ./devel/all_test_dbs.txt`
+  else
+    all=`cat ./devel/all_prod_dbs.txt`
+  fi
+else
+  all=$ONLY
+fi
+for db in $all
+do
+    echo "DB: $db"
+    ./devel/restore_db.sh $db || exit 2
+done
+echo 'OK'
