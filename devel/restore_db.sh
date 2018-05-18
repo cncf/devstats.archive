417ec01b16295f290a80e413f79b47aabcfefc10
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-18 11:21:08 +0000

    Restore DB files from prod [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/restore_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide database name"
+  exit 1
+fi
+./devel/drop_psql_db.sh $1
+sudo -u postgres createdb $1
+sudo -u postgres pg_restore -d $1 $1.dump
