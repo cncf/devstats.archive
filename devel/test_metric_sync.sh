5499f77743df6430e56038b58e0a2a946032e762
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-05-24 08:40:34 +0000

    No more need GAPS config when reimporting affiliations [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/test_metric_sync.sh
+++ devel/test_metric_sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make || exit 1
 ./devel/drop_ts_tables.sh test || exit 2
-#GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=2 GHA2DB_DEBUG=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_GAPS_YAML=devel/test_gaps.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=gha PG_DB=test ./gha2db_sync
-GHA2DB_PROJECT=kubernetes GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=1 GHA2DB_DEBUG=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_GAPS_YAML=devel/test_gaps.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=gha PG_DB=test ./gha2db_sync
+#GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=2 GHA2DB_DEBUG=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=gha PG_DB=test ./gha2db_sync
+GHA2DB_PROJECT=kubernetes GHA2DB_SKIPPDB=1 GHA2DB_CMDDEBUG=1 GHA2DB_DEBUG=1 GHA2DB_RESETTSDB=1 GHA2DB_METRICS_YAML=devel/test_metrics.yaml GHA2DB_TAGS_YAML=devel/test_tags.yaml GHA2DB_LOCAL=1 PG_DB=gha PG_DB=test ./gha2db_sync
