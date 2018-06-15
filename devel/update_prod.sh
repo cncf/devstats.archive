61ef8a7093ac9cc4c3e343a38c320b160c67f6b1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-06-15 05:44:34 +0000

    Update master-production sync script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/update_prod.sh
+++ devel/update_prod.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-git checkout production && git pull && git merge master && git push && make install && git checkout master && git diff production
-
+git checkout master && git pull && git checkout production && git pull && git merge master && git push && make install && git checkout master && git diff production
