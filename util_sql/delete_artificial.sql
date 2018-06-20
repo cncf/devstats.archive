d8b2d68b5b44ac6ec32221d78a7d5ad6c6abaca5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-06-20 10:19:19 +0000

    Update had no sense, other approach [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/delete_artificial.sql
+++ util_sql/delete_artificial.sql
@@ -1,4 +1,5 @@
 delete from gha_issues_labels where event_id > 281474976710656;
+delete from gha_issues_assignees where event_id > 281474976710656;
 delete from gha_issues where event_id > 281474976710656;
 delete from gha_milestones where event_id > 281474976710656;
 delete from gha_payloads where event_id > 281474976710656;
