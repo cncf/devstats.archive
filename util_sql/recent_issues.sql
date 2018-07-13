0bd508ae2e13dfed0ccb4f5c138b7236e71cec27
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-07-13 11:44:17 +0000

    SQL for recent issues to fix [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/recent_issues.sql
+++ util_sql/recent_issues.sql
@@ -1,25 +1,20 @@
 select
-  distinct i.dup_repo_name,
-  i.number
+  distinct dup_repo_name,
+  number
 from
-  gha_issues i,
-  gha_issues_labels il
+  gha_issues
 where
-  il.issue_id = i.id
-  and il.dup_label_name = 'cncf-cla: yes'
-  and i.dup_repo_name = 'kubernetes/kubernetes'
+  dup_repo_name = 'kubernetes/kubernetes'
   and (
-    i.milestone_id in (
+    milestone_id in (
       select id
       from
         gha_milestones
       where
         title in ('v1.11', 'v1.12')
     ) or (
-      i.milestone_id is null
-      and i.updated_at > now() - '1 month'::interval
+      milestone_id is null
+      and updated_at > now() - '1 week'::interval
     )
   )
-limit
-  32
 ;
