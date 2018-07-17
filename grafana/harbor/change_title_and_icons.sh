75f6056d69e63498b3f39b24d85000775192ee58
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-07-17 07:27:46 +0000

    Grafana shell scripts [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ grafana/harbor/change_title_and_icons.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# GRAFANA_DATA=/usr/share/grafana.harbor/
+if [ -z "${GRAFANA_DATA}" ]
+then
+  echo "You need to set GRAFANA_DATA environment variable to run this script"
+  exit 1
+fi
+for f in `find ${GRAFANA_DATA} -type f -exec grep -l "'Grafana - '" "{}" \; | sort | uniq`
+do
+  ls -l "$f"
+  vim -c "%s/'Grafana - '/'Harbor DevStats - '/g|wq" "$f"
+done
+for f in `find ${GRAFANA_DATA} -type f -exec grep -l '"Grafana - "' "{}" \; | sort | uniq`
+do
+  ls -l "$f"
+  vim -c '%s/"Grafana - "/"Harbor DevStats - "/g|wq' "$f"
+done
+cp -n ${GRAFANA_DATA}/public/img/grafana_icon.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg.bak
+cp grafana/img/harbor.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg.bak
+cp grafana/img/harbor.svg ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/grafana_net_logo.svg ${GRAFANA_DATA}/public/img/grafana_net_logo.svg.bak
+cp grafana/img/harbor.svg ${GRAFANA_DATA}/public/img/grafana_net_logo.svg || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav32.png ${GRAFANA_DATA}/public/img/fav32.png.bak
+cp grafana/img/harbor32.png ${GRAFANA_DATA}/public/img/fav32.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav16.png ${GRAFANA_DATA}/public/img/fav16.png.bak
+cp grafana/img/harbor32.png ${GRAFANA_DATA}/public/img/fav16.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav_dark_16.png ${GRAFANA_DATA}/public/img/fav_dark_16.png.bak
+cp grafana/img/harbor32.png ${GRAFANA_DATA}/public/img/fav_dark_16.png || exit 1
+cp -n ${GRAFANA_DATA}/public/img/fav_dark_32.png ${GRAFANA_DATA}/public/img/fav_dark_32.png.bak
+cp grafana/img/harbor32.png ${GRAFANA_DATA}/public/img/fav_dark_32.png || exit 1
+echo 'OK'
