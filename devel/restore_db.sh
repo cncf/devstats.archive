7d2e82fe469b12d389435b04bdb29d1181f622a7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-03 09:16:27 +0000

    updated restore_db.sh script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/restore_db.sh
+++ devel/restore_db.sh
@@ -2,10 +2,11 @@
 if [ -z "$1" ]
 then
   echo "$0: you need to provide database name"
+  echo "When running manually please copy/link /var/www/html/dbname.dump to current directory and remove after restore"
   exit 1
 fi
 ./devel/drop_psql_db.sh $1
 echo "Creating $1"
 sudo -u postgres createdb $1
-sudo -u postgres pg_restore -d $1 /var/www/html/$1.dump
+sudo -u postgres pg_restore -d $1 $1.dump
 echo "Created $1"
