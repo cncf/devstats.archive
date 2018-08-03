d99b620220d6940dfc2187daf201380c448a2850
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-03 10:22:45 +0000

    Basckport all etcd related data and hide [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/etcd/repo_groups.sql
@@ -0,0 +1,36 @@
+-- Add repository groups
+update
+  gha_repos r
+set
+  alias = coalesce((
+    select e.dup_repo_name
+    from
+      gha_events e
+    where
+      e.repo_id = r.id
+    order by
+      e.created_at desc
+    limit 1
+  ), name)
+;
+update gha_repos set repo_group = alias;
+
+update gha_repos
+set repo_group = 'etcd', alias = 'etcd'
+where name in (
+  'etcd',
+  'coreos/etcd'
+);
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
