3a2ca85f5eab90a24dd2a3559c149d72c49df96f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-13 05:12:52 +0000

    Grants for gha_admin to current_schema [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/current_state_grants.sql
@@ -0,0 +1,5 @@
+grant usage on schema current_state to gha_admin;
+grant select on current_state.milestones to gha_admin;
+grant select on current_state.issue_labels to gha_admin;
+grant select on current_state.prs to gha_admin;
+grant select on current_state.issues to gha_admin;
---
+++ util_sql/mview_issue_labels.sql
@@ -0,0 +1,21 @@
+with label_fields as (
+          select gha_issues_labels.issue_id,
+             gha_issues_labels.label_id,
+             gha_issues_labels.event_id,
+             gha_issues_labels.dup_label_name
+            from gha_issues_labels
+         ), event_rank as (
+          select gha_issues_labels.issue_id,
+             gha_issues_labels.event_id,
+             row_number() over (partition by gha_issues_labels.issue_id order by gha_issues_labels.event_id desc) as rank
+            from gha_issues_labels
+           group by gha_issues_labels.issue_id, gha_issues_labels.event_id
+         )
+select label_fields.issue_id,
+     label_fields.label_id,
+     label_fields.dup_label_name as full_label,
+     current_state.label_prefix((label_fields.dup_label_name)::text) as prefix,
+     current_state.label_suffix((label_fields.dup_label_name)::text) as label
+    from (label_fields
+      join event_rank on (((label_fields.issue_id = event_rank.issue_id) and (label_fields.event_id = event_rank.event_id) and (event_rank.rank = 1))))
+order by label_fields.issue_id, (current_state.label_prefix((label_fields.dup_label_name)::text)), (current_state.label_suffix((label_fields.dup_label_name)::text));
