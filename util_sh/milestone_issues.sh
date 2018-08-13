6979e0dbf6d8f360b46954945faf49b60116b59f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-13 05:17:03 +0000

    Final state of sync check scripts [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/milestone_issues.sh
+++ util_sh/milestone_issues.sh
@@ -4,5 +4,4 @@ then
   echo "PG_PASS=... $0 milestone_name"
   exit 1
 fi
-sudo -u postgres psql -c "refresh materialized view current_state.issues"
 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq util_sql/milestone_issues.sql {{milestone}} "$1"
