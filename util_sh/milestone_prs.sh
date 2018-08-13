d49f8f7f21556e58407129f7923474cd2412d075
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-13 05:12:52 +0000

    Grants for gha_admin to current_schema [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/milestone_prs.sh
+++ util_sh/milestone_prs.sh
@@ -4,5 +4,5 @@ then
   echo "PG_PASS=... $0 milestone_name"
   exit 1
 fi
-sudo -u postgres psql -c "refresh materialized view current_state.prs"
+sudo -u postgres psql gha -c "refresh materialized view current_state.prs"
 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq util_sql/milestone_prs.sql {{milestone}} "$1"
---
+++ util_sql/mview_milestones.sql
@@ -0,0 +1,22 @@
+with milestone_latest as (
+          select gha_milestones.id,
+             gha_milestones.title as milestone,
+             gha_milestones.state,
+             gha_milestones.created_at,
+             gha_milestones.updated_at,
+             gha_milestones.closed_at,
+             gha_milestones.event_id,
+             gha_milestones.dup_repo_id as repo_id,
+             gha_milestones.dup_repo_name as repo_name,
+             row_number() over (partition by gha_milestones.id order by gha_milestones.updated_at desc, gha_milestones.event_id desc) as rank
+            from gha_milestones
+         )
+select milestone_latest.id,
+     milestone_latest.milestone,
+     milestone_latest.state,
+     milestone_latest.created_at,
+     milestone_latest.updated_at,
+     milestone_latest.closed_at,
+     milestone_latest.repo_id,
+     milestone_latest.repo_name
+from milestone_latest;
