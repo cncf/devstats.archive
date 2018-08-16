81c2cf4012c8965f3184b98710bbc6c5176b24c4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-16 12:22:14 +0000

    Some fixes [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/contributors_and_emails.sql
+++ util_sql/contributors_and_emails.sql
@@ -15,7 +15,8 @@ from (
   where
     (e.actor_id = a.id or e.dup_actor_login = a.login)
     and e.type in (
-      'PushEvent', 'PullRequestEvent', 'IssuesEvent'
+      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
     )
     and (lower(e.dup_actor_login) {{exclude_bots}})
   order by
