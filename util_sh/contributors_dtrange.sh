d0497f40d7deb514a4dd1e35049d85a5ac86754e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-16 12:09:38 +0000

    New contributors for date range scripts [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/contributors_dtrange.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+if ( [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] )
+then
+  echo "$0: you need to specify dtfrom dtto n"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_HOST=cncftest.io PG_DB=gha GHA2DB_CSVOUT="contributors_dtrange.csv" ./runq ./util_sql/contributors_dtrange.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{from}} "$1" {{to}} "$2" {{n}} "$3" > out
