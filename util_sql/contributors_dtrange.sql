69a8940c6abeb071905d9e65640a174c00134d92
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-20 07:22:48 +0000

    backport election scripts and kubespray update from master [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/contributors_dtrange.sql
@@ -0,0 +1,40 @@
+select
+  i2.github_handle,
+  string_agg(i2.email, ',') as emails,
+  i2.contributions
+from (
+  select distinct
+    i.github_handle,
+    i.contributions,
+    ae.email
+  from (
+    select a.login as github_handle,
+      a.id as actor_id,
+      count(distinct e.id) as contributions
+    from
+      gha_events e,
+      gha_actors a
+    where
+      e.dup_actor_login = a.login
+      and (lower(a.login) {{exclude_bots}})
+      and e.created_at >= '{{from}}'
+      and e.created_at < '{{to}}'
+      and e.type in (
+        'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+        'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+      )
+    group by
+      a.login,
+      a.id
+    ) i,
+    gha_actors_emails ae
+  where
+    ae.actor_id = i.actor_id
+    and i.contributions >= {{n}}
+  ) i2
+group by
+  i2.github_handle,
+  i2.contributions
+order by
+  i2.contributions desc
+;
