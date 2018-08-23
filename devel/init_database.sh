4d7c0ce8dc9493eea2776a56e5afbaf28a4e7ff2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-08-23 05:52:18 +0000

    Fix to init_database.sh discovered when creating devstats-example [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/init_database.sh
+++ devel/init_database.sh
@@ -22,18 +22,18 @@ echo "$0 start"
 if [ ! -z "$UDROP" ]
 then
   echo "dropping users"
-  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh gha_admin || exit 1
-  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh ro_user || exit 2
-  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh devstats_team || exit 3
+  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh gha_admin
+  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh ro_user
+  ONLY=devstats DROP=1 NOCREATE=1 devel/create_psql_user.sh devstats_team
   echo "dropping done"
 fi
 
-exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'devstats'"` || exit 4
+exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'devstats'"` || exit 1
 if ( [ ! -z "$LDROP" ] && [ "$exists" = "1" ] )
 then
   echo "dropping postgres database devstats (logs)"
-  sudo -u postgres psql -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'devstats'" || exit 5
-  sudo -u postgres psql -c "drop database devstats" || exit 6
+  sudo -u postgres psql -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'devstats'" || exit 2
+  sudo -u postgres psql -c "drop database devstats" || exit 3
 fi
 
 if [ ! -z "$NOCREATE" ]
@@ -42,19 +42,19 @@ then
   exit 0
 fi
 
-exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'devstats'"` || exit 7
+exists=`sudo -u postgres psql -tAc "select 1 from pg_database WHERE datname = 'devstats'"` || exit 4
 if [ ! "$exists" = "1" ]
 then
   echo "creating postgres database devstats (logs)"
-  sudo -u postgres psql -c "create database devstats" || exit 8
-  sudo -u postgres psql -c "create user gha_admin with password '$PG_PASS'" || exit 9
-  sudo -u postgres psql -c "create user ro_user with password '$PG_PASS_RO'" || exit 10
-  sudo -u postgres psql -c "create user devstats_team with password '$PG_PASS_TEAM'" || exit 11
-  sudo -u postgres psql -c "grant all privileges on database \"devstats\" to gha_admin" || exit 12
-  sudo -u postgres psql -c "alter user gha_admin createdb" || exit 13
-  sudo -u postgres psql devstats < ./util_sql/devstats_log_table.sql || exit 14
-  ./devel/ro_user_grants.sh devstats || exit 15
-  ./devel/psql_user_grants.sh "devstats_team" "devstats" || exit 16
+  sudo -u postgres psql -c "create database devstats" || exit 5
+  sudo -u postgres psql -c "create user gha_admin with password '$PG_PASS'" || exit 6
+  sudo -u postgres psql -c "create user ro_user with password '$PG_PASS_RO'" || exit 7
+  sudo -u postgres psql -c "create user devstats_team with password '$PG_PASS_TEAM'" || exit 8
+  sudo -u postgres psql -c "grant all privileges on database \"devstats\" to gha_admin" || exit 9
+  sudo -u postgres psql -c "alter user gha_admin createdb" || exit 10
+  sudo -u postgres psql devstats < ./util_sql/devstats_log_table.sql
+  ./devel/ro_user_grants.sh devstats || exit 11
+  ./devel/psql_user_grants.sh "devstats_team" "devstats" || exit 12
 else
   echo "postgres database devstats (logs) already exists"
 fi
