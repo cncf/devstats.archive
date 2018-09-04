0f6299feaf90e9f01973fd0c12969563c425cf74
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-04 06:12:13 +0000

    Backports from master [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/k8s_contributors_and_emails.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_HOST=cncftest.io PG_DB=gha GHA2DB_CSVOUT="k8s_contributors_and_emails.csv" ./runq util_sql/contributors_and_emails.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
