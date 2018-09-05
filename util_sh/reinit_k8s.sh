2680ef686b650fd5bc9c53b13d97c4f3b1a9bfa8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-05 13:22:08 +0000

    Reinit k8s full worklfow script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/reinit_k8s.sh
+++ util_sh/reinit_k8s.sh
@@ -7,7 +7,7 @@ fi
 ./kubernetes/psql.sh &&\
 ./devel/restore_artificial.sh gha &&\
 sudo -u postgres psql gha -f util_sql/current_state_all.sql &&\
-#./util_sh/refresh_mviews.sh &&\
+./util_sh/refresh_mviews.sh &&\
 ONLY=kubernetes SKIPTEMP=1 ./devel/reinit.sh &&\
 ONLY=kubernetes ./devel/vars_all.sh &&\
 ONLY=gha cron/cron_db_backup_all.sh
