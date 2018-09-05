4efeb5e09ed1297659d41f7597f784c0e667dc9b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-05 08:09:30 +0000

    Regenerating data [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/label_suffix_func.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION current_state.label_suffix(some_label text) RETURNS text
+    LANGUAGE sql IMMUTABLE
+    AS $_$
+SELECT CASE WHEN $1 LIKE '%/%'
+  THEN substring($1 FROM '/(.*)')
+ELSE
+  $1
+END;
+$_$;
