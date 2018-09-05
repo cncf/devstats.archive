fbc3db2684d513591c3b28a6ccf95ce4dcfade96
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-05 13:45:11 +0000

    Fixed bug that could only happen on a clean database with excluded repos removed [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- ghapi.go
+++ ghapi.go
@@ -312,7 +312,7 @@ func ghMilestone(con *sql.Tx, ctx *Ctx, eid int64, ic *IssueConfig, maybeHide fu
 					"dupn_creator_login) values("+
 					"%s, %s, %s, %s, %s, %s, "+
 					"%s, %s, %s, %s, %s, %s, %s, "+
-					"%s, %s, (select max(id) from gha_repos where name = %s), %s, %s, %s, "+
+					"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, %s, %s, "+
 					"%s)",
 				NValue(1),
 				NValue(2),
@@ -479,7 +479,7 @@ func ArtificialPREvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig, pr *github.PullReq
 				"%s, %s, %s, %s, %s, %s, %s, %s, "+
 				"%s, %s, %s, %s, %s, "+
 				"%s, %s, %s, %s, %s, "+
-				"%s, %s, (select max(id) from gha_repos where name = %s), %s, %s, %s, "+
+				"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, %s, %s, "+
 				"%s, %s, %s)",
 			NValue(1),
 			NValue(2),
@@ -565,8 +565,8 @@ func ArtificialPREvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig, pr *github.PullReq
 				"into gha_events("+
 					"id, type, actor_id, repo_id, public, created_at, "+
 					"dup_actor_login, dup_repo_name, org_id, forkee_id) "+
-					"values(%s, %s, %s, (select max(id) from gha_repos where name = %s), true, %s, "+
-					"%s, %s, (select max(org_id) from gha_repos where name = %s), null)",
+					"values(%s, %s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), true, %s, "+
+					"%s, %s, (select max(org_id) from gha_events where dup_repo_name = %s), null)",
 				NValue(1),
 				NValue(2),
 				NValue(3),
@@ -603,7 +603,7 @@ func ArtificialPREvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig, pr *github.PullReq
 					"values(%s, null, null, null, null, null, %s, "+
 					"%s, %s, null, null, null, null, "+
 					"null, %s, null, null, null, "+
-					"%s, %s, (select max(id) from gha_repos where name = %s), %s, %s, %s)",
+					"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, %s, %s)",
 				NValue(1),
 				NValue(2),
 				NValue(3),
@@ -770,7 +770,7 @@ func ArtificialEvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig) (err error) {
 				"dup_user_login, dupn_assignee_login, is_pull_request) "+
 				"values(%s, %s, %s, %s, %s, %s, %s, "+
 				"%s, %s, %s, %s, %s, %s, %s, "+
-				"%s, %s, (select max(id) from gha_repos where name = %s), %s, %s, %s, "+
+				"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, %s, %s, "+
 				"%s, %s, %s) ",
 			NValue(1),
 			NValue(2),
@@ -837,8 +837,8 @@ func ArtificialEvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig) (err error) {
 				"into gha_events("+
 					"id, type, actor_id, repo_id, public, created_at, "+
 					"dup_actor_login, dup_repo_name, org_id, forkee_id) "+
-					"values(%s, %s, %s, (select max(id) from gha_repos where name = %s), true, %s, "+
-					"%s, %s, (select max(org_id) from gha_repos where name = %s), null)",
+					"values(%s, %s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), true, %s, "+
+					"%s, %s, (select max(org_id) from gha_events where dup_repo_name = %s), null)",
 				NValue(1),
 				NValue(2),
 				NValue(3),
@@ -875,7 +875,7 @@ func ArtificialEvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig) (err error) {
 					"values(%s, null, null, null, null, null, %s, "+
 					"%s, null, null, null, null, null, "+
 					"null, %s, null, null, null, "+
-					"%s, %s, (select max(id) from gha_repos where name = %s), %s, %s, %s)",
+					"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, %s, %s)",
 				NValue(1),
 				NValue(2),
 				NValue(3),
@@ -912,7 +912,7 @@ func ArtificialEvent(c *sql.DB, ctx *Ctx, cfg *IssueConfig) (err error) {
 					"dup_actor_id, dup_actor_login, dup_repo_id, dup_repo_name, "+
 					"dup_type, dup_created_at, dup_issue_number, dup_label_name) "+
 					"values(%s, %s, %s, "+
-					"%s, %s, (select max(id) from gha_repos where name = %s), %s, "+
+					"%s, %s, (select max(repo_id) from gha_events where dup_repo_name = %s), %s, "+
 					"%s, %s, %s, %s)",
 				NValue(1),
 				NValue(2),
