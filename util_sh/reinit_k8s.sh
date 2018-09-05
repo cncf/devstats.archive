fa69dd701b92206aea1aa47b2352a94905007f0b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-05 15:18:03 +0000

    Add missing part [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/reinit_k8s.sh
+++ util_sh/reinit_k8s.sh
@@ -7,6 +7,7 @@ fi
 ./kubernetes/psql.sh &&\
 ./devel/restore_artificial.sh gha &&\
 sudo -u postgres psql gha -f util_sql/current_state_all.sql &&\
+sudo -u postgres psql gha -f ./util_sql/current_state_grants.sql &&\
 ./util_sh/refresh_mviews.sh &&\
 ONLY=kubernetes SKIPTEMP=1 ./devel/reinit.sh &&\
 ONLY=kubernetes ./devel/vars_all.sh &&\
