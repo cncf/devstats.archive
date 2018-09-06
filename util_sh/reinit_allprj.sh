1518a94b4e7882ae5334d4805ff7e6de9edc8fa1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-06 06:05:18 +0000

    Backport changes from master [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/reinit_allprj.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+./devel/drop_psql_db.sh allprj &&\
+./all/psql.sh &&\
+./devel/restore_artificial.sh allprj &&\
+ONLY=all SKIPTEMP=1 ./devel/reinit.sh &&\
+ONLY=all ./devel/vars_all.sh &&\
+ONLY=allprj cron/cron_db_backup_all.sh
