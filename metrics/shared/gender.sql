5ec6870a0a8d1d2d3f3aca1fcb4120d3630ad3a5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-12 08:02:58 +0000

    New metric [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/gender.sql
+++ metrics/shared/gender.sql
@@ -2,7 +2,7 @@ select
   inn.name,
   inn.n
 from (
-  select 'gender_contributions,' || a.sex as name,
+  select 'sexevents,' || a.sex as name,
     round(count(distinct e.id) / {{n}}, 2) as n
   from
     gha_events e,
@@ -14,9 +14,11 @@ from (
     and a.sex is not null
     and a.sex != ''
     and a.sex_prob >= 0.85
+    and e.created_at >= '{{from}}'
+    and e.created_at < '{{to}}'
   group by
     a.sex
-  union select 'gender_contributors,' || a.sex as name,
+  union select 'sexactors,' || a.sex as name,
     round(count(distinct e.actor_id) / {{n}}, 2) as n
   from
     gha_events e,
@@ -28,6 +30,38 @@ from (
     and a.sex is not null
     and a.sex != ''
     and a.sex_prob >= 0.85
+    and e.created_at >= '{{from}}'
+    and e.created_at < '{{to}}'
+  group by
+    a.sex
+  union select 'sexeventscum,' || a.sex as name,
+    round(count(distinct e.id) / {{n}}, 2) as n
+  from
+    gha_events e,
+    gha_actors a
+  where
+    e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
+    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and a.id = e.actor_id
+    and a.sex is not null
+    and a.sex != ''
+    and a.sex_prob >= 0.85
+    and e.created_at < '{{to}}'
+  group by
+    a.sex
+  union select 'sexactorscum,' || a.sex as name,
+    round(count(distinct e.actor_id) / {{n}}, 2) as n
+  from
+    gha_events e,
+    gha_actors a
+  where
+    e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
+    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and a.id = e.actor_id
+    and a.sex is not null
+    and a.sex != ''
+    and a.sex_prob >= 0.85
+    and e.created_at < '{{to}}'
   group by
     a.sex
 ) inn
