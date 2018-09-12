63075189ff487f42b5bffb17a3908857c1ee54ce
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-12 12:53:08 +0000

    Gender dashboard [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/gender.sql
+++ metrics/shared/gender.sql
@@ -1,9 +1,13 @@
 select
-  inn.name,
-  inn.n
+  concat(inn.type, ';', case inn.sex when 'm' then 'Male' when 'f' then 'Female' end, '`', inn.repo_group, ';contributors,contributions') as name,
+  inn.contributors,
+  inn.contributions
 from (
-  select 'sexevents,' || a.sex as name,
-    round(count(distinct e.id) / {{n}}, 2) as n
+  select 'sex' as type,
+    a.sex,
+    'All' as repo_group,
+    count(distinct e.actor_id) as contributors,
+    round(count(distinct e.id) / {{n}}, 2) as contributions
   from
     gha_events e,
     gha_actors a
@@ -13,13 +17,16 @@ from (
     and a.id = e.actor_id
     and a.sex is not null
     and a.sex != ''
-    and a.sex_prob >= 0.85
+    and a.sex_prob >= 0.75
     and e.created_at >= '{{from}}'
     and e.created_at < '{{to}}'
   group by
     a.sex
-  union select 'sexactors,' || a.sex as name,
-    round(count(distinct e.actor_id) / {{n}}, 2) as n
+  union select 'sexcum' as type,
+    a.sex,
+    'all' as repo_group,
+    count(distinct e.actor_id) as contributors,
+    round(count(distinct e.id) / {{n}}, 2) as contributions
   from
     gha_events e,
     gha_actors a
@@ -29,42 +36,64 @@ from (
     and a.id = e.actor_id
     and a.sex is not null
     and a.sex != ''
-    and a.sex_prob >= 0.85
+    and a.sex_prob >= 0.75
+    and e.created_at < '{{to}}'
+  group by
+    a.sex
+  union select 'sex' as type,
+    a.sex,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    count(distinct e.actor_id) as contributors,
+    round(count(distinct e.id) / {{n}}, 2) as contributions
+  from
+    gha_repos r,
+    gha_actors a,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    r.id = e.repo_id
+    and e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
+    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and a.id = e.actor_id
+    and a.sex is not null
+    and a.sex != ''
+    and a.sex_prob >= 0.75
     and e.created_at >= '{{from}}'
     and e.created_at < '{{to}}'
   group by
-    a.sex
-  union select 'sexeventscum,' || a.sex as name,
-    round(count(distinct e.id) / {{n}}, 2) as n
+    a.sex,
+    coalesce(ecf.repo_group, r.repo_group)
+  union select 'sexcum' as type,
+    a.sex,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    count(distinct e.actor_id) as contributors,
+    round(count(distinct e.id) / {{n}}, 2) as contributions
   from
-    gha_events e,
-    gha_actors a
+    gha_repos r,
+    gha_actors a,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
   where
-    e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
+    r.id = e.repo_id
+    and e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
     and (lower(e.dup_actor_login) {{exclude_bots}})
     and a.id = e.actor_id
     and a.sex is not null
     and a.sex != ''
-    and a.sex_prob >= 0.85
+    and a.sex_prob >= 0.75
     and e.created_at < '{{to}}'
   group by
-    a.sex
-  union select 'sexactorscum,' || a.sex as name,
-    round(count(distinct e.actor_id) / {{n}}, 2) as n
-  from
-    gha_events e,
-    gha_actors a
-  where
-    e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')
-    and (lower(e.dup_actor_login) {{exclude_bots}})
-    and a.id = e.actor_id
-    and a.sex is not null
-    and a.sex != ''
-    and a.sex_prob >= 0.85
-    and e.created_at < '{{to}}'
-  group by
-    a.sex
+    a.sex,
+    coalesce(ecf.repo_group, r.repo_group)
 ) inn
+where
+  inn.repo_group is not null 
 order by
-  inn.name asc
+  name
 ;
