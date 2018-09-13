122e999f65deac6d09f3d394eb586fec8ac97211
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-13 05:04:39 +0000

    Add more stats [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/gender_cum.sql
+++ metrics/shared/gender_cum.sql
@@ -1,9 +1,23 @@
 select
-  concat(inn.type, ';', case inn.sex when 'm' then 'Male' when 'f' then 'Female' end, '`', inn.repo_group, ';contributors,contributions,users,events') as name,
+  concat(inn.type, ';', case inn.sex when 'm' then 'Male' when 'f' then 'Female' end, '`', inn.repo_group, ';contributors,contributions,users,events,committers,commits,prcreators,prs,issucreators,issues,commenters,comments,reviewers,reviews,watchers,watches,forkers,forks') as name,
   inn.contributors,
   inn.contributions,
   inn.users,
-  inn.events
+  inn.events,
+  inn.committers,
+  inn.commits,
+  inn.prcreators,
+  inn.prs,
+  inn.issuecreators,
+  inn.issues,
+  inn.commenters,
+  inn.comments,
+  inn.reviewers,
+  inn.reviews,
+  inn.watchers,
+  inn.watches,
+  inn.forkers,
+  inn.forks
 from (
   select 'sexcum' as type,
     a.sex,
@@ -11,7 +25,21 @@ from (
     count(distinct e.actor_id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributors,
     count(distinct e.id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributions,
     count(distinct e.actor_id) as users,
-    count(distinct e.id) as events
+    count(distinct e.id) as events,
+    count(distinct e.actor_id) filter (where e.type = 'PushEvent') as committers,
+    count(distinct e.id) filter (where e.type = 'PushEvent') as commits,
+    count(distinct e.actor_id) filter (where e.type = 'PullRequestEvent') as prcreators,
+    count(distinct e.id) filter (where e.type = 'PullRequestEvent') as prs,
+    count(distinct e.actor_id) filter (where e.type = 'IssuesEvent') as issuecreators,
+    count(distinct e.id) filter (where e.type = 'IssuesEvent') as issues,
+    count(distinct e.actor_id) filter (where e.type in ('CommitCommentEvent', 'IssueCommentEvent')) as commenters,
+    count(distinct e.id) filter (where e.type in ('CommitCommentEvent', 'IssueCommentEvent')) as comments,
+    count(distinct e.actor_id) filter (where e.type = 'PullRequestReviewCommentEvent') as reviewers,
+    count(distinct e.id) filter (where e.type = 'PullRequestReviewCommentEvent') as reviews,
+    count(distinct e.actor_id) filter (where e.type = 'WatchEvent') as watchers,
+    count(distinct e.id) filter (where e.type = 'WatchEvent') as watches,
+    count(distinct e.actor_id) filter (where e.type = 'ForkEvent') as forkers,
+    count(distinct e.id) filter (where e.type = 'ForkEvent') as forks
   from
     gha_events e,
     gha_actors a
@@ -30,7 +58,21 @@ from (
     count(distinct e.actor_id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributors,
     count(distinct e.id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributions,
     count(distinct e.actor_id) as users,
-    count(distinct e.id) as events
+    count(distinct e.id) as events,
+    count(distinct e.actor_id) filter (where e.type = 'PushEvent') as committers,
+    count(distinct e.id) filter (where e.type = 'PushEvent') as commits,
+    count(distinct e.actor_id) filter (where e.type = 'PullRequestEvent') as prcreators,
+    count(distinct e.id) filter (where e.type = 'PullRequestEvent') as prs,
+    count(distinct e.actor_id) filter (where e.type = 'IssuesEvent') as issuecreators,
+    count(distinct e.id) filter (where e.type = 'IssuesEvent') as issues,
+    count(distinct e.actor_id) filter (where e.type in ('CommitCommentEvent', 'IssueCommentEvent')) as commenters,
+    count(distinct e.id) filter (where e.type in ('CommitCommentEvent', 'IssueCommentEvent')) as comments,
+    count(distinct e.actor_id) filter (where e.type = 'PullRequestReviewCommentEvent') as reviewers,
+    count(distinct e.id) filter (where e.type = 'PullRequestReviewCommentEvent') as reviews,
+    count(distinct e.actor_id) filter (where e.type = 'WatchEvent') as watchers,
+    count(distinct e.id) filter (where e.type = 'WatchEvent') as watches,
+    count(distinct e.actor_id) filter (where e.type = 'ForkEvent') as forkers,
+    count(distinct e.id) filter (where e.type = 'ForkEvent') as forks
   from
     gha_repos r,
     gha_actors a,
