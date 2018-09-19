55ad14cfb8e13c5d4a2c0a53c23023d104b3931f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-19 05:13:18 +0000

    Drop Affiliations TSDB tables when regenerating affiliations [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/drop_tsdb_affs_tables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: need database name argument"
+  exit 1
+fi
+proj=$1
+# snum_stats scompany_activity shcom* shpr_comps* ssex ssexcum scountries scountriescum
+# sudo -u postgres psql $proj -c "drop table snum_stats" || exit 1
+# sudo -u postgres psql $proj -c "drop table scompany_activity" || exit 2
+# sudo -u postgres psql $proj -c "drop table ssex" || exit 3
+# sudo -u postgres psql $proj -c "drop table ssexcum" || exit 4
+# sudo -u postgres psql $proj -c "drop table scountries" || exit 5
+# sudo -u postgres psql $proj -c "drop table scountriescum" || exit 6
+tables=`sudo -u postgres psql $proj -qAntc '\dt' | cut -d\| -f2`
+for table in $tables
+do
+  base1=${table:0:5}
+  base2=${table:0:10}
+  if ( [ "$base1" = "shcom" ] || [ "$base2" = "shpr_comps" ] )
+  then
+    sudo -u postgres psql $proj -c "drop table $table" || exit 1
+    echo "dropped $table"
+  fi
+done
