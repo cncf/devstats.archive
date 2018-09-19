0b7c8ab587b102089e014d166e99f38ab0e39b27
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-19 09:22:05 +0000

    New index files [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_rb/generate_partial.rb
+++ util_rb/generate_partial.rb
@@ -4,17 +4,28 @@ def generate_partial(outfile, tfile, pfile, tpref, tsuff, ppref, psuff, repl_fro
   replaces = repl_from.split(',').map(&:strip)
   tlines = []
   plines = []
+  tlines_ws = []
+  plines_ws = []
   File.readlines(tfile).each do |line|
-    line = line.strip
     replaces.each { |repl| line = line.gsub(repl, repl_to) }
-    tlines << line
+    sline = line.strip
+    i = line.index(sline)
+    ws = ''
+    ws = line[0..(i-1)] if i > 0
+    tlines << sline
+    tlines_ws << ws
   end
   File.readlines(pfile).each do |line|
-    line = line.strip
     replaces.each { |repl| line = line.gsub(repl, repl_to) }
-    plines << line
+    sline = line.strip
+    i = line.index(sline)
+    ws = ''
+    ws = line[0..(i-1)] if i > 0
+    plines << sline
+    plines_ws << ws
   end
   outlines = []
+  outlines_ws = []
   t = 0
   p = 0
   l = 0
@@ -24,6 +35,7 @@ def generate_partial(outfile, tfile, pfile, tpref, tsuff, ppref, psuff, repl_fro
   while !finished
     if tlines[t] == plines[p]
       outlines << tlines[t]
+      outlines_ws << tlines_ws[t]
       t += 1
       p += 1
       l += 1
@@ -43,36 +55,49 @@ def generate_partial(outfile, tfile, pfile, tpref, tsuff, ppref, psuff, repl_fro
       if !p_found && !t_found
         outlines << "#{tpref}#{tlines[t]}#{tsuff}"
         outlines << "#{ppref}#{plines[t]}#{psuff}"
+        outlines_ws << tlines_ws[t]
+        outlines_ws << plines_ws[t]
         t += 1
         p += 1
       elsif p_found && !t_found
         for i in p...pp
           outlines << "#{ppref}#{plines[i]}#{psuff}"
+          outlines_ws << plines_ws[i]
         end
         outlines << plines[pp]
+        outlines_ws << plines_ws[pp]
         t += 1
         p = pp + 1
       elsif !p_found && t_found
         for i in t...tt
           outlines << "#{tpref}#{tlines[i]}#{tsuff}"
+          outlines_ws << tlines_ws[i]
         end
         outlines << tlines[tt]
+        outlines_ws << tlines_ws[tt]
         p += 1
         t = tt + 1
       elsif p_found && t_found
         for i in p...pp
           outlines << "#{ppref}#{plines[i]}#{psuff}"
+          outlines_ws << plines_ws[i]
         end
         for i in t...tt
           outlines << "#{tpref}#{tlines[i]}#{tsuff}"
+          outlines_ws << tlines_ws[i]
         end
         outlines << plines[pp]
         outlines << tlines[tt]
+        outlines_ws << plines_ws[pp]
+        outlines_ws << tlines_ws[tt]
         p = pp + 1
         t = tt + 1
       end
     end
     finished = true if p >= pl && t >= tl
+  end
+  outlines.each_with_index do |line, idx|
+    outlines[idx] = "#{outlines_ws[idx]}#{outlines[idx]}"
   end
   # pretty print HTML
   binding.pry
