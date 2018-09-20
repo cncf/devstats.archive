2bf4c13193b05f0485415a7334fc3a0b4ea26a56
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-20 07:20:41 +0000

    Optimize SQLs [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/gender_cum.sql
+++ metrics/shared/gender_cum.sql
@@ -44,7 +44,7 @@ from (
     gha_events e,
     gha_actors a
   where
-    (lower(e.dup_actor_login) {{exclude_bots}})
+    (lower(a.login) {{exclude_bots}})
     and a.id = e.actor_id
     and a.sex is not null
     and a.sex != ''
@@ -54,7 +54,7 @@ from (
     a.sex
   union select 'sexcum' as type,
     a.sex,
-    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    r.repo_group,
     count(distinct e.actor_id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributors,
     count(distinct e.id) filter (where e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent')) as contributions,
     count(distinct e.actor_id) as users,
@@ -77,13 +77,9 @@ from (
     gha_repos r,
     gha_actors a,
     gha_events e
-  left join
-    gha_events_commits_files ecf
-  on
-    ecf.event_id = e.id
   where
     r.id = e.repo_id
-    and (lower(e.dup_actor_login) {{exclude_bots}})
+    and (lower(a.login) {{exclude_bots}})
     and a.id = e.actor_id
     and a.sex is not null
     and a.sex != ''
@@ -91,7 +87,7 @@ from (
     and e.created_at < '{{to}}'
   group by
     a.sex,
-    coalesce(ecf.repo_group, r.repo_group)
+    r.repo_group
 ) inn
 where
   inn.repo_group is not null 
