2663c9a2e83fd46981fc1e4721f0c7b39784894d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-21 15:57:24 +0000

    Add TZ dashboard to all non-k8s projects [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/replace_proj_name_tag.sh
+++ util_sh/replace_proj_name_tag.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo 'This is an example'
+if [ -z "$1" ]
+then
+  echo "$0: required dashboard file name"
+  exit 1
+fi
 for f in `cat devel/all_test_projects.txt`
 do
-  FROM='"cncf"' TO="\"$f\"" FILES=`find grafana/dashboards/$f/ -iname "countries-stats.json"` MODE=ss0 ./devel/mass_replace.sh
+  FROM='"cncf"' TO="\"$f\"" FILES=`find grafana/dashboards/$f/ -iname "$1"` MODE=ss0 ./devel/mass_replace.sh
 done
