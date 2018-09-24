9fdb6a40e9d23aea23f1eb025d3e1f8cefd628c1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-24 10:31:26 +0000

    Missing drop TZs table on affiliations reimport [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/drop_tsdb_affs_tables.sh
+++ devel/drop_tsdb_affs_tables.sh
@@ -12,6 +12,7 @@ sudo -u postgres psql $proj -c "drop table ssex" || exit 3
 sudo -u postgres psql $proj -c "drop table ssexcum" || exit 4
 sudo -u postgres psql $proj -c "drop table scountries" || exit 5
 sudo -u postgres psql $proj -c "drop table scountriescum" || exit 6
+sudo -u postgres psql $proj -c "drop table stz" || exit 7
 tables=`sudo -u postgres psql $proj -qAntc '\dt' | cut -d\| -f2`
 for table in $tables
 do
@@ -19,7 +20,7 @@ do
   base2=${table:0:10}
   if ( [ "$base1" = "shcom" ] || [ "$base2" = "shpr_comps" ] )
   then
-    sudo -u postgres psql $proj -c "drop table \"$table\"" || exit 1
+    sudo -u postgres psql $proj -c "drop table \"$table\"" || exit 8
     echo "dropped $table"
   fi
 done
