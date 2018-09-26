0af354061173a0acea41d31a4d50bffc29808a75
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-26 07:26:26 +0000

    Adding new dashboards to K8s [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/kubernetes/committers_tz.sql
@@ -0,0 +1,62 @@
+with committers_data as (
+  select a.tz_offset,
+    'all' as repo_group,
+    count(distinct a.login) as rcommitters,
+    count(distinct c.sha) as rcommits
+  from
+    gha_actors a,
+    gha_commits c
+  where
+    (
+      c.author_name = a.name
+      or
+      (
+        a.login = c.dup_actor_login
+        and (lower(a.login) {{exclude_bots}})
+        and (lower(c.dup_actor_login) {{exclude_bots}})
+      )
+    )
+    and a.tz_offset is not null
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+  group by
+    a.tz_offset
+  union select a.tz_offset,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    count(distinct a.login) as rcommitters,
+    count(distinct c.sha) as rcommits
+  from
+    gha_repos r,
+    gha_actors a,
+    gha_commits c
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = c.event_id
+  where
+    (
+      c.author_name = a.name
+      or
+      (
+        a.login = c.dup_actor_login
+        and (lower(a.login) {{exclude_bots}})
+        and (lower(c.dup_actor_login) {{exclude_bots}})
+      )
+    )
+    and r.id = c.dup_repo_id
+    and a.tz_offset is not null
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+  group by
+    a.tz_offset,
+    coalesce(ecf.repo_group, r.repo_group)
+)
+select
+  'tz;' || round(tz_offset / 60.0, 1) || '`' || repo_group || ';rcommitters,rcommits' as name,
+  rcommitters,
+  rcommits
+from
+  committers_data
+where
+  repo_group is not null
+;
