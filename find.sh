e0c46cd331f410970c24b5238cfede7c4dacfd47
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-26 07:01:08 +0000

    backports from master [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- find.sh
+++ find.sh
@@ -6,12 +6,12 @@ then
 fi
 if [ -z "$2" ]
 then
-  echo "You need to provide file name pattern as a first argument"
+  echo "You need to provide file name pattern as a second argument"
   exit 1
 fi
 if [ -z "$3" ]
 then
-  echo "You need to provide regexp pattern to search for as a second argument"
+  echo "You need to provide regexp pattern to search for as a third argument"
   exit 1
 fi
 find "$1" -type f -iname "$2" -not -name "out" -not -path '*.git/*' -exec grep -EHIn "$3" "{}" \; | tee -a out
