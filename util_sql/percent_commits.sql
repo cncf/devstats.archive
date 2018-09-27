7adfe9280459a8862bfd90cab6e49304e6ad04cb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-27 11:06:42 +0000

    Percent commits analysis [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/percent_commits.sql
@@ -0,0 +1,35 @@
+select
+  i.company,
+  i.commits
+from (
+  select
+    coalesce(af.company_name, 'Unknown') as company,
+    count(distinct c.sha) as commits,
+    count(distinct a.id) as committers
+  from
+    gha_commits c,
+    gha_actors a,
+    gha_events e
+  left join
+    gha_actors_affiliations af
+  on
+    e.actor_id = af.actor_id
+    and af.dt_from <= e.created_at
+    and af.dt_to > e.created_at
+  where
+    e.id = c.event_id
+    and (
+      c.author_name = a.name
+      or (
+        a.login = c.dup_actor_login
+        and (lower(a.login) {{exclude_bots}})
+        and (lower(c.dup_actor_login) {{exclude_bots}})
+      )
+    )
+  group by
+    coalesce(af.company_name, 'Unknown')
+) i
+order by
+  i.commits desc,
+  i.company asc
+;
