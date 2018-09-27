dfcd8618ff53d5291d17530f4db2fd8c0b18d797
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-27 19:29:38 +0000

    Fix script order [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/drop_tsdb_affs_tables.sh
+++ devel/drop_tsdb_affs_tables.sh
@@ -6,13 +6,6 @@ then
 fi
 proj=$1
 # snum_stats scompany_activity shcom* shpr_comps* ssex ssexcum scountries scountriescum
-sudo -u postgres psql $proj -c "drop table snum_stats" || exit 1
-sudo -u postgres psql $proj -c "drop table scompany_activity" || exit 2
-sudo -u postgres psql $proj -c "drop table ssex" || exit 3
-sudo -u postgres psql $proj -c "drop table ssexcum" || exit 4
-sudo -u postgres psql $proj -c "drop table scountries" || exit 5
-sudo -u postgres psql $proj -c "drop table scountriescum" || exit 6
-sudo -u postgres psql $proj -c "drop table stz" || exit 7
 tables=`sudo -u postgres psql $proj -qAntc '\dt' | cut -d\| -f2`
 for table in $tables
 do
@@ -20,7 +13,14 @@ do
   base2=${table:0:10}
   if ( [ "$base1" = "shcom" ] || [ "$base2" = "shpr_comps" ] )
   then
-    sudo -u postgres psql $proj -c "drop table \"$table\"" || exit 8
+    sudo -u postgres psql $proj -c "drop table \"$table\"" || exit 2
     echo "dropped $table"
   fi
 done
+sudo -u postgres psql $proj -c "drop table snum_stats" || exit 3
+sudo -u postgres psql $proj -c "drop table scompany_activity" || exit 4
+sudo -u postgres psql $proj -c "drop table scountries" || exit 5
+sudo -u postgres psql $proj -c "drop table scountriescum" || exit 6
+sudo -u postgres psql $proj -c "drop table stz" || exit 7
+sudo -u postgres psql $proj -c "drop table ssex" || exit 8
+sudo -u postgres psql $proj -c "drop table ssexcum" || exit 9
