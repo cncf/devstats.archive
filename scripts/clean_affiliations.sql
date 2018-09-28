64f763cec0e84f73f3dde429d43f5678b92b07d7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-09-28 12:46:55 +0000

    Backport from master [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/clean_affiliations.sql
+++ scripts/clean_affiliations.sql
@@ -1,4 +1,14 @@
 delete from gha_actors_emails;
 delete from gha_actors_affiliations;
 delete from gha_companies;
-update gha_actors set name = null;
+update
+  gha_actors
+set
+  name = null,
+  country_id = null,
+  sex = null,
+  sex_prob = null,
+  tz = null,
+  tz_offset = null,
+ country_name  = null
+;
