9a32138e8fae9dc48fb2fe5cb6f35bd66b7b522d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-01 09:27:53 +0000

    new scripts to recalculate given periods from a given date [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/recalculate_periods_from.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -z "${PG_DB}" ]
+then
+  echo "$0: you need to set PG_DB environment variable to run this script"
+  exit 1
+fi
+if [ -z "${GHA2DB_PROJECT}" ]
+then
+  echo "$0: you need to set GHA2DB_PROJECT environment variable to run this script"
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to provide date start as a 1st argument: YYYY-MM-DD"
+  exit 1
+fi
+if [ -z "$2" ]
+then
+  echo "$0: you need to provide periods to calculate as a 2nd argument: 'w:f,m:f,q:f,y:f'"
+  exit 1
+fi
+function finish {
+    sync_unlock.sh
+}
+if [ -z "$TRAP" ]
+then
+  sync_lock.sh || exit -1
+  trap finish EXIT
+  export TRAP=1
+fi
+GHA2DB_LOCAL=1 GHA2DB_STARTDT_FORCE=1 GHA2DB_STARTDT="$1" GHA2DB_RESETTSDB=1 GHA2DB_FORCE_PERIODS="$2" ./gha2db_synca
