6cd864384459abb3706629b964cb1004bb7eb00f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-03 05:45:46 +0000

    Backport buildpacks from master [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/all/change_title_and_icons.sh
+++ grafana/all/change_title_and_icons.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # GRAFANA_DATA=/usr/share/grafana.all/
-for f in `find ${GRAFANA_DATA} -type f -exec grep -l "'Grafana - '" "{}" \; | sort | uniq`
-do
-  ls -l "$f"
-  vim -c "%s/'Grafana - '/'All DevStats - '/g|wq" "$f"
-done
-for f in `find ${GRAFANA_DATA} -type f -exec grep -l '"Grafana - "' "{}" \; | sort | uniq`
-do
-  ls -l "$f"
-  vim -c '%s/"Grafana - "/"All DevStats - "/g|wq' "$f"
-done
+if [ -z "${GRAFANA_DATA}" ]
+then
+  echo "You need to set GRAFANA_DATA environment variable to run this script"
+  exit 1
+fi
+if [ -z "${SKIPVIM}" ]
+then
+  for f in `find ${GRAFANA_DATA} -type f -exec grep -l "'Grafana - '" "{}" \; | sort | uniq`
+  do
+    ls -l "$f"
+    vim -c "%s/'Grafana - '/'All DevStats - '/g|wq" "$f"
+  done
+  for f in `find ${GRAFANA_DATA} -type f -exec grep -l '"Grafana - "' "{}" \; | sort | uniq`
+  do
+    ls -l "$f"
+    vim -c '%s/"Grafana - "/"All DevStats - "/g|wq' "$f"
+  done
+fi
 cp -n ${GRAFANA_DATA}/public/img/grafana_icon.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg.bak
 cp grafana/img/all.svg ${GRAFANA_DATA}/public/img/grafana_icon.svg || exit 1
 cp -n ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg ${GRAFANA_DATA}/public/img/grafana_com_auth_icon.svg.bak
