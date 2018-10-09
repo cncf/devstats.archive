e1a83e5e158d70a6e37465f1cf57ea8a63b1f10c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-09 11:53:55 +0000

    Renaming domain [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/contributors_and_emails.sh
+++ util_sh/contributors_and_emails.sh
@@ -4,4 +4,4 @@ then
   echo "$0: you need to specify PG_PASS env variable"
   exit 1
 fi
-GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_HOST=cncftest.io PG_DB=allprj GHA2DB_CSVOUT="contributors_and_emails.csv" ./runq util_sql/contributors_and_emails.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_HOST=teststats.cncf.io PG_DB=allprj GHA2DB_CSVOUT="contributors_and_emails.csv" ./runq util_sql/contributors_and_emails.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
