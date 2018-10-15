6b9d184f01487e8398b6bb5148f93cef3f17c200
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-15 15:33:02 +0000

    GH API commits 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/test_ghapi_commits.sh
+++ util_sh/test_ghapi_commits.sh
@@ -5,3 +5,4 @@ then
   exit 1
 fi
 FROM="2018-10-15" REPO="cncf/devstats" GHA2DB_DEBUG=1 GHA2DB_GHAPISKIPEVENTS=1 PG_DB=cncf ./ghapi2db
+# REPO="cncf/devstats" GHA2DB_DEBUG=1 GHA2DB_GHAPISKIPEVENTS=1 GHA2DB_QOUT=1 PG_DB=cncf ./ghapi2db
