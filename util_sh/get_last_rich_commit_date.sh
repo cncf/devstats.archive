687cd4e798c53e89de1afab11c40e4b92d592e34
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-16 10:20:50 +0000

    few helper scripts [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/get_last_rich_commit_date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "You need to provide repo name as an argument"
+  exit 2
+fi
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq util_sql/get_last_rich_commit_date.sql {{repo}} "'$1'"
