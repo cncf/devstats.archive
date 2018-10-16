1638098e41ee3f5d1385b3fd2b960e4571708882
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-16 10:20:50 +0000

    few helper scripts [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/get_last_rich_commit_date.sql
+++ util_sql/get_last_rich_commit_date.sql
@@ -1,22 +1,17 @@
 select
   coalesce(
-    max(dup_created_at), (
-      select min(dup_created_at) from gha_commits
-      where dup_repo_name = {{repo}} or dup_repo_id = (
-        select max(id) from gha_repos where name = {{repo}}
-      )
-    ), (
-      select min(dup_created_at) from gha_commits
-    )
+    max(dup_created_at),
+    (select min(dup_created_at) from gha_commits where dup_repo_name = {{repo}})
   )
 from
   gha_commits
 where
   author_email != ''
-  and (
-    dup_repo_name = {{repo}}
-    or dup_repo_id = (
-      select max(id) from gha_repos where name = {{repo}}
-    )
-  )
+  and dup_repo_name = {{repo}}
+union select
+  max(dup_created_at)
+from
+  gha_commits
+where
+  dup_repo_name = {{repo}}
 ;
