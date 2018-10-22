0fbc1bd30e5394e311d87751fa7c777215559ae6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-22 11:57:44 +0000

    Remove old non-merged tables [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/remove_unmerged_tables.sh
+++ util_sh/remove_unmerged_tables.sh
@@ -1,3 +1,9 @@
 #./util_sh/drop_tables_by_name_all.sh shcom
 #./util_sh/drop_tables_by_name_all.sh spr_auth
-./util_sh/drop_tables_by_name_all.sh shdev_
+#./util_sh/drop_tables_by_name_all.sh shdev_
+./util_sh/drop_tables_by_name_all.sh shdeva
+./util_sh/drop_tables_by_name_all.sh shdevc
+./util_sh/drop_tables_by_name_all.sh shdevg
+./util_sh/drop_tables_by_name_all.sh shdevi
+./util_sh/drop_tables_by_name_all.sh shdevp
+./util_sh/drop_tables_by_name_all.sh shdevr
