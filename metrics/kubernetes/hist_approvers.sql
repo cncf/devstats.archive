2d9aad850e4329be65a7490149f0d8f31e0e0d42
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-22 08:32:21 +0000

    Approvers & Reviewers metric [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/hist_approvers.sql
+++ metrics/kubernetes/hist_approvers.sql
@@ -7,11 +7,11 @@ with matching as (
     and substring(body from '(?i)(?:^|\n|\r)\s*/(?:approve)\s*(?:\n|\r|$)') is not null
 )
 select
-  sub.repo_group,
+  'hdev_approves,' || sub.repo_group || '_All' as metric,
   sub.actor,
   count(distinct sub.id) as approves
 from (
-  select 'hdev_approves,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
     e.dup_actor_login as actor,
     e.id
   from
@@ -37,7 +37,7 @@ group by
   sub.actor
 having
   count(distinct sub.id) >= 1
-union select 'hdev_approves,All' as repo_group,
+union select 'hdev_approves,All_All' as metric,
   dup_actor_login as actor,
   count(distinct id) as approves
 from
@@ -53,8 +53,63 @@ group by
   dup_actor_login
 having
   count(distinct id) >= 1
+union select 'hdev_approves,All_' || a.country_name as metric,
+  a.login as actor,
+  count(distinct e.id) as approves
+from
+  gha_actors a,
+  gha_events e
+where
+  (e.actor_id = a.id or e.dup_actor_login = a.login)
+  and e.id in (
+    select event_id
+    from
+      matching
+  )
+  and (lower(a.login) {{exclude_bots}})
+  and a.country_name is not null
+group by
+  a.country_name,
+  a.login
+having
+  count(distinct e.id) >= 1
+union select 'hdev_approves,' || sub.repo_group || '_' || sub.country as metric,
+  sub.actor,
+  count(distinct sub.id) as approves
+from (
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    a.country_name as country,
+    a.login as actor,
+    e.id
+  from
+    gha_actors a,
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    (e.actor_id = a.id or e.dup_actor_login = a.login)
+    and e.repo_id = r.id
+    and (lower(a.login) {{exclude_bots}})
+    and e.id in (
+      select event_id
+      from
+        matching
+      )
+    ) sub
+where
+  sub.repo_group is not null
+  and sub.country is not null
+group by
+  sub.repo_group,
+  sub.country,
+  sub.actor
+having
+  count(distinct sub.id) >= 1
 order by
   approves desc,
-  repo_group asc,
+  metric asc,
   actor asc
 ;
--- metrics/kubernetes/hist_reviewers.sql
+++ metrics/kubernetes/hist_reviewers.sql
@@ -14,11 +14,11 @@ with matching as (
     and type in ('PullRequestReviewCommentEvent')
 )
 select
-  sub.repo_group,
+  'hdev_reviews,' || sub.repo_group || '_All' as metric ,
   sub.actor,
   count(distinct sub.id) as reviews
 from (
-  select 'hdev_reviews,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
     e.dup_actor_login as actor,
     e.id
   from
@@ -51,7 +51,7 @@ group by
   sub.actor
 having
   count(distinct sub.id) >= 1
-union select 'hdev_reviews,All' as repo_group,
+union select 'hdev_reviews,All_All' as metric,
   dup_actor_login as actor,
   count(distinct id) as reviews
 from
@@ -74,8 +74,77 @@ group by
   dup_actor_login
 having
   count(distinct id) >= 1
+union select 'hdev_reviews,' || sub.repo_group || '_' || sub.country as metric,
+  sub.actor,
+  count(distinct sub.id) as reviews
+from (
+  select coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    a.country_name as country,
+    a.login as actor,
+    e.id
+  from
+    gha_actors a,
+    gha_repos r,
+    gha_events e
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = e.id
+  where
+    (e.actor_id = a.id or e.dup_actor_login = a.login)
+    and e.repo_id = r.id
+    and (lower(a.login) {{exclude_bots}})
+    and e.id in (
+      select min(event_id)
+      from
+        gha_issues_events_labels
+      where
+        {{period:created_at}}
+        and label_name in ('lgtm', 'approved')
+      group by
+        issue_id
+      union select event_id from matching
+      union select event_id from reviews
+    )
+  ) sub
+where
+  sub.repo_group is not null
+  and sub.country is not null
+group by
+  sub.country,
+  sub.repo_group,
+  sub.actor
+having
+  count(distinct sub.id) >= 1
+union select 'hdev_reviews,All_' || a.country_name as metric,
+  a.login as actor,
+  count(distinct e.id) as reviews
+from
+  gha_actors a,
+  gha_events e
+where
+  (e.actor_id = a.id or e.dup_actor_login = a.login)
+  and e.id in (
+    select min(event_id)
+    from
+      gha_issues_events_labels
+    where
+      {{period:created_at}}
+      and label_name in ('lgtm', 'approved')
+    group by
+      issue_id
+    union select event_id from matching
+    union select event_id from reviews
+  )
+  and (lower(a.login) {{exclude_bots}})
+  and a.country_name is not null
+group by
+  a.country_name,
+  a.login
+having
+  count(distinct e.id) >= 1
 order by
   reviews desc,
-  repo_group asc,
+  metric asc,
   actor asc
 ;
