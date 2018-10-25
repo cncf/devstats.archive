bf48939f06b15e0360ad9d6380c6ff6a2290f1c6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-25 11:16:23 +0000

    ES raw index for issues [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/es_raw_issues.sql
+++ util_sql/es_raw_issues.sql
@@ -2,11 +2,39 @@ select
   i.id,
   i.event_id,
   i.dup_created_at,
-  i.created_at
+  i.created_at,
+  coalesce(i.body, ''),
+  i.closed_at,
+  i.comments,
+  i.locked,
+  i.number,
+  i.state,
+  i.title,
+  i.updated_at,
+  i.is_pull_request,
+  i.dup_type,
+-- milestone_id        | bigint                      |           |          |
+-- user_id             | bigint                      |           | not null |
+-- dup_actor_id        | bigint                      |           | not null |
+-- dup_actor_login     | character varying(120)      |           | not null |
+-- dup_repo_id         | bigint                      |           | not null |
+-- dup_repo_name       | character varying(160)      |           | not null |
+-- dup_user_login      | character varying(120)      |           | not null |
+  coalesce(dupn_assignee_login, assignee.login, ''),
+  coalesce(assignee.name, ''),
+  coalesce(assignee.country_id, ''),
+  coalesce(assignee.sex, ''),
+  assignee.sex_prob,
+  coalesce(assignee.tz, ''),
+  assignee.tz_offset,
+  coalesce(assignee.country_name, '')
 from
   gha_issues i
+left join
+  gha_actors assignee
+on
+  i.assignee_id = assignee.id
 where
   i.dup_created_at >= '{{from}}'
   and i.dup_created_at < '{{to}}'
 ;
-
