412bf0ba54458e6085777686bf5e778e444135f0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-10-25 07:42:24 +0000

    Enrich more - commit actor data duplicated [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/es_raw_commits.sql
+++ util_sql/es_raw_commits.sql
@@ -14,7 +14,14 @@ select
   c.dup_committer_login,
   r.org_login,
   r.repo_group,
-  r.alias
+  r.alias,
+  coalesce(a.name, ''),
+  coalesce(a.country_id, ''),
+  coalesce(a.sex, ''),
+  a.sex_prob,
+  coalesce(a.tz, ''),
+  a.tz_offset,
+  coalesce(a.country_name, '')
 from
   gha_commits c
 left join
@@ -22,6 +29,10 @@ left join
 on
   c.dup_repo_id = r.id
   and c.dup_repo_name = r.name
+left join
+  gha_actors a
+on
+  c.dup_actor_id = a.id
 where
   dup_created_at >= '{{from}}'
   and dup_created_at < '{{to}}'
