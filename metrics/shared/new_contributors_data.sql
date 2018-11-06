0efd110ce809fe06a202222926648663aeb08091
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-06 11:12:47 +0000

    Update metric to only count merged PRs [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/new_contributors_data.sql
+++ metrics/shared/new_contributors_data.sql
@@ -3,30 +3,28 @@ with prev as (
   from
     gha_pull_requests
   where
-    created_at < '{{from}}'
+    merged = true
+    and merged_at < '{{from}}'
     and (lower(dup_actor_login) {{exclude_bots}})
 ), contributors as (
   select distinct pr.user_id,
-    first_value(pr.created_at) over prs_by_created_at as created_at,
-    first_value(pr.dup_repo_id) over prs_by_created_at as repo_id,
-    first_value(pr.dup_repo_name) over prs_by_created_at as repo_name,
-    first_value(pr.event_id) over prs_by_created_at as event_id
+    first_value(pr.merged_at) over prs_by_merged_at as merged_at,
+    first_value(pr.dup_repo_id) over prs_by_merged_at as repo_id,
+    first_value(pr.dup_repo_name) over prs_by_merged_at as repo_name,
+    first_value(pr.event_id) over prs_by_merged_at as event_id
   from
     gha_pull_requests pr
-  left join
-    prev pc
-  on
-    pc.user_id = pr.user_id
   where
-    pr.created_at >= '{{from}}'
-    and pr.created_at < '{{to}}'
+    pr.merged = true
+    and pr.merged_at >= '{{from}}'
+    and pr.merged_at < '{{to}}'
     and pr.user_id not in (select user_id from prev)
     and (lower(pr.dup_actor_login) {{exclude_bots}})
   window
-    prs_by_created_at as (
+    prs_by_merged_at as (
       partition by pr.user_id
       order by
-        pr.created_at asc,
+        pr.merged_at asc,
         pr.event_id asc
       range between unbounded preceding
       and current row
@@ -34,7 +32,7 @@ with prev as (
 )
 select
   'ncd,All' as metric,
-  c.created_at,
+  c.merged_at,
   0.0 as value,
   case a.name is null when true then a.login else case a.name when '' then a.login else a.name || ' (' || a.login || ')' end end as contributor
 from
@@ -43,7 +41,7 @@ from
 where
   c.user_id = a.id
 union select 'ncd,' || coalesce(ecf.repo_group, r.repo_group) as metric,
-  c.created_at,
+  c.merged_at,
   0.0 as value,
   case a.name is null when true then a.login else case a.name when '' then a.login else a.name || ' (' || a.login || ')' end end as contributor
 from
@@ -61,6 +59,6 @@ where
   and r.repo_group is not null
 order by
   metric asc,
-  created_at asc,
+  merged_at asc,
   contributor asc
 ;
