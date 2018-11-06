29643b4fcbc72931ec118093f41b589ab6afec11
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-06 09:55:45 +0000

    new contributors data sql ready [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/new_contributors_data.sql
+++ metrics/shared/new_contributors_data.sql
@@ -6,7 +6,10 @@ with prev as (
     created_at < '{{from}}'
 ), contributors as (
   select distinct pr.user_id,
-    min(pr.created_at) as created_at
+    first_value(pr.created_at) over prs_by_created_at as created_at,
+    first_value(pr.dup_repo_id) over prs_by_created_at as repo_id,
+    first_value(pr.dup_repo_name) over prs_by_created_at as repo_name,
+    first_value(pr.event_id) over prs_by_created_at as event_id
   from
     gha_pull_requests pr
   left join
@@ -17,11 +20,18 @@ with prev as (
     pr.created_at >= '{{from}}'
     and pr.created_at < '{{to}}'
     and pr.user_id not in (select user_id from prev)
-  group by
-    pr.user_id
+  window
+    prs_by_created_at as (
+      partition by pr.user_id
+      order by
+        pr.created_at asc,
+        pr.event_id asc
+      range between unbounded preceding
+      and current row
+    )
 )
 select
-  'new_contributors_data,All' as metric,
+  'ncd,All' as metric,
   c.created_at,
   0.0 as value,
   case a.name is null when true then a.login else case a.name when '' then a.login else a.name || ' (' || a.login || ')' end end as contributor
@@ -30,7 +40,25 @@ from
   gha_actors a
 where
   c.user_id = a.id
+union select 'ncd,' || coalesce(ecf.repo_group, r.repo_group) as metric,
+  c.created_at,
+  0.0 as value,
+  case a.name is null when true then a.login else case a.name when '' then a.login else a.name || ' (' || a.login || ')' end end as contributor
+from
+  gha_actors a,
+  gha_repos r,
+  contributors c
+left join
+  gha_events_commits_files ecf
+on
+  ecf.event_id = c.event_id
+where
+  c.user_id = a.id
+  and c.repo_id = r.id
+  and c.repo_name = r.name
+  and r.repo_group is not null
 order by
-  c.created_at asc,
+  metric asc,
+  created_at asc,
   contributor asc
 ;
