4160bbd88f2bcf9a5a42622a76351d7e15f6cef9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-08 15:49:49 +0000

    Docker deploy script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ docker/docker_deploy_all.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ARTWORK
+# GET=1 (attempt to fetch Postgres database from the test server)
+# INIT=1 (needs PG_PASS_RO, PG_PASS_TEAM, initialize from no postgres database state, creates postgres logs database and users)
+# SKIPVARS=1 (if set it will skip final Postgres vars regeneration)
+set -o pipefail
+exec > >(tee run.log)
+exec 2> >(tee errors.txt)
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+if ( [ ! -z "$INIT" ] && ( [ -z "$PG_PASS_RO" ] || [ -z "$PG_PASS_TEAM" ] ) )
+then
+  echo "$0: You need to set PG_PASS_RO, PG_PASS_TEAM when using INIT"
+  exit 1
+fi
+
+if [ -z "$PG_HOST" ]
+then
+  echo "$0: you need to set PG_HOST to run this script"
+  exit 1
+fi
+
+if [ -z "$PG_PORT" ]
+then
+  echo "$0: you need to set PG_PORT to run this script"
+  exit 1
+fi
+
+ONLY=buildpacks
+export ONLY
+
+if [ ! -z "$INIT" ]
+then
+  ./devel/init_database.sh || exit 1
+fi
+
+PROJ=buildpacks PROJDB=buildpacks PROJREPO="buildpack/lifecycle" ORGNAME=Buildpacks PORT=3001 ICON="-" GA="-" GRAFSUFF="-" SKIPGRAFANA=1 ./devel/deploy_proj.sh || exit 30
+
+if [ -z "$SKIPVARS" ]
+then
+  ./devel/vars_all.sh || exit 3
+fi
+echo "$0: All deployments finished"
