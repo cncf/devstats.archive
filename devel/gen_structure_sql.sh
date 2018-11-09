6dd28442bd561d2dcc8458f31834bfaea3e9c692
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-09 07:30:14 +0000

    Updating all shell scripts to use db.sh helper [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/gen_structure_sql.sh
+++ devel/gen_structure_sql.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-if [ -z "$PG_HOST" ]
-then
-  PG_HOST=127.0.0.1
-fi
-
-if [ -z "$PG_PORT" ]
-then
-  PG_PORT=5432
-fi
 if [ -z "${PG_PASS}" ]
 then
   echo "You need to set PG_PASS environment variable to run this script"
@@ -16,8 +7,8 @@ fi
 ./devel/drop_psql_db.sh temp &&\
 GHA2DB_LOCAL=1 PG_DB=temp ./structure &&\
 GHA2DB_LOCAL=1 PG_DB=temp GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 GHA2DB_MGETC=y ./structure &&\
-sudo -u postgres psql -h "$PG_HOST" -p "$PG_PORT" temp -f util_sql/current_state_all.sql &&\
-sudo -u postgres psql -h "$PG_HOST" -p "$PG_PORT" temp -f util_sql/current_state_grants.sql &&\
-#sudo -u postgres pg_dump -s temp > structure.sql &&\
-sudo -u postgres pg_dump temp > structure.sql &&\
+./devel/db.sh psql temp -f util_sql/current_state_all.sql &&\
+./devel/db.sh psql temp -f util_sql/current_state_grants.sql &&\
+#./devel/db.sh pg_dump -s temp > structure.sql &&\
+./devel/db.sh pg_dump temp > structure.sql &&\
 ./devel/drop_psql_db.sh temp && echo 'structure.sql generated'
--- util_sh/contributors.sh
+++ util_sh/contributors.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-if [ -z "$PG_HOST" ]
-then
-  PG_HOST=127.0.0.1
-fi
-
-if [ -z "$PG_PORT" ]
-then
-  PG_PORT=5432
-fi
 if [ "$1" = "" ]
 then
   echo "Usage: $0 dbname"
   exit 1
 fi
-sudo -u postgres psql -h "$PG_HOST" -p "$PG_PORT" -tA $1 < ./util_sql/actors.sql > actors.txt
+./devel/db.sh psql -tA $1 < ./util_sql/actors.sql > actors.txt
 cat actors.txt | sort | uniq > actors.tmp
 mv actors.tmp actors.txt
 cat actors.txt | wc -l
