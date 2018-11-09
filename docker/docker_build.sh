cfa7da0a63d2b6db9d2c566b7f50bd82a8186bde
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-09 09:32:41 +0000

    More stuff to make deployment from the container possible 3 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -f devstats.tar 2>/dev/null
-tar cf devstats.tar cmd git metrics docker devel util_sql all buildpacks shared scripts vendor util_sh/touch docs/touch partials/touch cron/net_tcp_config.sh *.go projects.yaml Makefile
+tar cf devstats.tar cmd git metrics docker devel util_sql all buildpacks shared scripts partials docs vendor util_sh/touch cron/net_tcp_config.sh *.go projects.yaml github_users.json Makefile
 docker build -t devstats .
 rm -f devstats.tar
