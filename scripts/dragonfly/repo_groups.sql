68a932512ec263cceec9f686cacbadb123ef9d6d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-13 18:33:45 +0800

    Adding Dragonfly using mac binaries [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/dragonfly/repo_groups.sql
@@ -0,0 +1,35 @@
+-- Add repository groups
+update
+  gha_repos r
+set
+  alias = coalesce((
+    select e.dup_repo_name
+    from
+      gha_events e
+    where
+      e.repo_id = r.id
+    order by
+      e.created_at desc
+    limit 1
+  ), name)
+;
+update gha_repos set repo_group = alias;
+
+update gha_repos
+set repo_group = 'Dragonfly', alias = 'Dragonfly'
+where name in (
+  'alibaba/Dragonfly'
+);
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
