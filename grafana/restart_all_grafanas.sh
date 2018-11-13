30cf5c3d0176232763e07f73a197ce6ac199a512
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-13 16:40:24 +0000

    Conditional start for contrib project [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/restart_all_grafanas.sh
+++ grafana/restart_all_grafanas.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 killall grafana-server
 ./grafana/start_all_grafanas.sh
-./util_sh/start_contrib.sh
+host=`hostname`
+if [ $host = "teststats.cncf.io" ]
+then
+  ./util_sh/start_contrib.sh
+fi
