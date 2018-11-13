1ec31d1b672426438ef8feacd49393a9494c4e34
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-13 18:26:03 +0800

    Adding Dragonfly using local development [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dragonfly/psql.sh
+++ dragonfly/psql.sh
@@ -11,14 +11,14 @@ then
 fi
 > errors.txt
 > run.log
-GHA2DB_PROJECT=falco PG_DB=falco GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
-./devel/db.sh psql falco -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=falco PG_DB=falco GHA2DB_LOCAL=1 ./gha2db 2016-05-17 0 today now 'falcosecurity,draios/falco' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=falco PG_DB=falco GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=falco PG_DB=falco ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
-GHA2DB_PROJECT=falco PG_DB=falco ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
-GHA2DB_PROJECT=falco PG_DB=falco ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
-GHA2DB_PROJECT=falco PG_DB=falco ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
-GHA2DB_PROJECT=falco PG_DB=falco GHA2DB_LOCAL=1 ./vars || exit 8
-./devel/ro_user_grants.sh falco || exit 9
-./devel/psql_user_grants.sh devstats_team falco || exit 10
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
+./devel/db.sh psql dragonfly -c "create extension if not exists pgcrypto" || exit 1
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly GHA2DB_LOCAL=1 ./gha2db 2017-11-19 0 today now 'alibaba/Dragonfly' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
+GHA2DB_PROJECT=dragonfly PG_DB=dragonfly GHA2DB_LOCAL=1 ./vars || exit 8
+./devel/ro_user_grants.sh dragonfly || exit 9
+./devel/psql_user_grants.sh devstats_team dragonfly || exit 10
