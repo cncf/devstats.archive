48f6de60df4489891c58d1c94bb3fdee89c33f25
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-22 10:21:24 +0000

    Fixing env variables name collision [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- buildpacks/psql.sh
+++ buildpacks/psql.sh
@@ -13,7 +13,8 @@ fi
 > run.log
 GHA2DB_PROJECT=buildpacks PG_DB=buildpacks GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql buildpacks -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=buildpacks PG_DB=buildpacks GHA2DB_LOCAL=1 ./gha2db 2018-06-01 0 today now buildpack 2>>errors.txt | tee -a run.log || exit 2
+# GHA2DB_PROJECT=buildpacks PG_DB=buildpacks GHA2DB_LOCAL=1 ./gha2db 2018-06-01 0 today now buildpack 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=buildpacks PG_DB=buildpacks GHA2DB_LOCAL=1 ./gha2db 2018-11-11 0 today now buildpack 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=buildpacks PG_DB=buildpacks GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=buildpacks PG_DB=buildpacks ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=buildpacks PG_DB=buildpacks ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
--- docker/docker_test_all.sh
+++ docker/docker_test_all.sh
@@ -5,14 +5,14 @@ then
   exit 1
 fi
 
-if [ "${FROM}" = "host" ]
+if [ "${DEPLOY_FROM}" = "host" ]
 then
   echo "Testing deployment from the host"
-elif [ "${FROM}" = "container" ]
+elif [ "${DEPLOY_FROM}" = "container" ]
 then
   echo "Testing deployment from the container"
 else
-  echo "$0: set the deployment type via FROM=host or FROM=container"
+  echo "$0: set the deployment type via DEPLOY_FROM=host or DEPLOY_FROM=container"
   exit 2
 fi
 
@@ -31,10 +31,10 @@ fi
 ./docker/docker_remove_psql.sh
 ./docker/docker_psql.sh || exit 3
 ./docker/docker_build.sh || exit 4
-if [ "${FROM}" = "host" ]
+if [ "${DEPLOY_FROM}" = "host" ]
 then
   PG_PASS="${PASS}" PG_PASS_RO="${PASS}" PG_PASS_TEAM="${PASS}" ./docker/docker_deploy_from_host.sh || exit 5
-elif [ "${FROM}" = "container" ]
+elif [ "${DEPLOY_FROM}" = "container" ]
 then
   PG_PASS="${PASS}" PG_PASS_RO="${PASS}" PG_PASS_TEAM="${PASS}" ./docker/docker_deploy_from_container.sh || exit 6
 fi
@@ -43,3 +43,4 @@ PG_PASS="${PASS}" ./docker/docker_devstats.sh || exit 8
 PG_PASS="${PASS}" ./docker/docker_display_logs.sh || exit 9
 PG_PASS="${PASS}" ./docker/docker_health.sh || exit 10
 echo 'All OK'
+echo 'You can call docker/docker_remove_psql.sh and docker/docker_remove.sh to cleanup'
