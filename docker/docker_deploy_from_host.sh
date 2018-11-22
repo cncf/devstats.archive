b88c935315a83ebcbae181aa62ec5a27aa905ecb
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-22 09:28:13 +0000

    Adding GH API support to dockerized devstats image [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_deploy_from_host.sh
+++ docker/docker_deploy_from_host.sh
@@ -4,4 +4,11 @@ then
   echo "$0: you need to set PG_PASS=... PG_PASS_RO=... PG_PASS_TEAM=... environment variables"
   exit 1
 fi
-INIT=1 GHA2DB_GHAPISKIP=1 SKIPTEMP=1 NOLOCK=1 SKIPADDALL=1 NOBACKUP=1 PG_HOST=127.0.0.1 PG_PORT=65432 ./docker/docker_deploy_all.sh
+if ( [ -z "${GHA2DB_GITHUB_OAUTH}" ] && [ -z "${GHA2DB_GHAPISKIP}" ] )
+then
+  echo "$0: warning no GitHub API key provided via GHA2DB_GITHUB_OAUTH=..., falling back to public API (very limited)"
+  echo "You can also skip GitHub API processing by setting GHA2DB_GHAPISKIP=1"
+  GHA2DB_GITHUB_OAUTH="-"
+  export GHA2DB_GITHUB_OAUTH
+fi
+INIT=1 SKIPTEMP=1 NOLOCK=1 SKIPADDALL=1 NOBACKUP=1 PG_HOST=127.0.0.1 PG_PORT=65432 ./docker/docker_deploy_all.sh
