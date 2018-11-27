cc15592de48c14295b36ae3c29536a7567bd83ab
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-27 08:29:48 +0000

    Support custom projects/dbs list files and specifying prod/test server names 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/create_images.sh
+++ grafana/create_images.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 # ARTWORK
 # This script assumes that You have cncf/artwork cloned in ~/dev/cncf/artwork 
-if [ -z "$ONLY" ]
-then
-  host=`hostname`
-  if [ $host = "teststats.cncf.io" ]
-  then
-    all=`cat ./devel/all_test_projects.txt`
-  else
-    all=`cat ./devel/all_prod_projects.txt`
-  fi
-  all="${all} devstats"
-else
-  all=$ONLY
-fi
+. ./devel/all_projs.sh || exit 2
 for proj in $all
 do
   if [ "$proj" = "opencontainers" ]
