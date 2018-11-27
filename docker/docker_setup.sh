2fe3cb84b28b90152a3eeeb82f888e600b694ab6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-27 13:12:08 +0000

    Instructions how to create env to run tests for dockerized instance [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ docker/docker_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update || exit 1
+sudo apt upgrade || exit 2
+sudo apt install -y git || exit 3
+git clone https://github.com/cncf/devstats.git || exit 4
+cd devstats || exit 5
+sudo apt install -y curl lsb-release software-properties-common apt-transport-https || exit 6
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - || exit 7
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" || exit 8
+sudo apt update || exit 9
+sudo apt-cache policy docker-ce || exit 10
+sudo apt install -y docker-ce || exit 11
