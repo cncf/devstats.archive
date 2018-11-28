f5e1b84f1abf6e7e43d5a5dff5fbced87b6bc602
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-28 09:15:56 +0000

    Ignore kill result [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/get_grafana_dbs.sh
+++ devel/get_grafana_dbs.sh
@@ -9,7 +9,7 @@ for proj in $all
 do
     if [ ! -z "$ONLY" ]
     then
-      kill `ps -aux | grep grafana-server | grep $proj | awk '{print $2}'`
+      kill `ps -aux | grep grafana-server | grep $proj | awk '{print $2}'` 2>/dev/null
     fi
     echo "wget grafana.$proj.db"
     rm -f grafana.$proj.db 2>/dev/null
