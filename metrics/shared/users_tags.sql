324521b8dbdce7af7d265d5f68b7a59b6b9b1d17
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-28 18:49:59 +0000

    make sure 'tusers' tag is always present [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/users_tags.sql
+++ metrics/shared/users_tags.sql
@@ -1,22 +1,33 @@
 select
-  -- string_agg(sub.name, ',') from (
-  sub.name from (
-  select dup_actor_login as name,
-    count(distinct id) as ecnt
-  from
-    gha_events
-  where
-    type in (
-      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
-      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
-    )
-    and created_at > now() - '2 months'::interval
-    and (lower(dup_actor_login) {{exclude_bots}})
-  group by
-    dup_actor_login
-  order by
-    ecnt desc,
-    name asc
-  limit {{lim}}
-) sub
+  i.name
+from (
+  select
+    -- string_agg(sub.name, ',') from (
+    0 as ord,
+    sub.name as name
+  from (
+    select dup_actor_login as name,
+      count(distinct id) as ecnt
+    from
+      gha_events
+    where
+      type in (
+        'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+        'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+      )
+      and created_at > now() - '2 months'::interval
+      and (lower(dup_actor_login) {{exclude_bots}})
+    group by
+      dup_actor_login
+    order by
+      ecnt desc,
+      name asc
+    limit {{lim}}
+    ) sub
+  union select 1 as ord,
+    'None' as name
+) i
+order by
+  i.ord asc
+limit {{lim}}
 ;
