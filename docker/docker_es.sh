af56e8b8d50aad058c94431ba42727100f180844
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-28 16:56:48 +0000

    Should fix ES file descriptors errors [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_es.sh
+++ docker/docker_es.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -p 19200:9200 -p 19300:9300 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
+# docker run -p 19200:9200 -p 19300:9300 -e ES_JAVA_OPTS="-Xms2g -Xmx2g" --ulimit nofile=65536:65536 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
+docker run -p 19200:9200 -p 19300:9300 --ulimit nofile=65536:65536 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
