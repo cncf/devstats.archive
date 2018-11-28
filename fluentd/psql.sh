a163b0d8ee2ee6e511286df9d86ec81257c5281b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-28 13:30:50 +0000

    Split LFN projects into separate ones with a special summary project that contains combined data [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fluentd/psql.sh
+++ fluentd/psql.sh
@@ -13,8 +13,8 @@ set -o pipefail
 > run.log
 GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql fluentd -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 ./gha2db 2015-01-01 0 today now 'fluent' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 ./gha2db 2014-01-02 0 2014-12-31 23 'fluent' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 ./gha2db 2015-01-01 0 today now fluent 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 ./gha2db 2014-01-02 0 2014-12-31 23 fluent 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=fluentd PG_DB=fluentd GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=fluentd PG_DB=fluentd ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=fluentd PG_DB=fluentd ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
---
+++ p4lang/psql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+function finish {
+    sync_unlock.sh
+}
+if [ -z "$TRAP" ]
+then
+  sync_lock.sh || exit -1
+  trap finish EXIT
+  export TRAP=1
+fi
+set -o pipefail
+> errors.txt
+> run.log
+GHA2DB_PROJECT=p4lang PG_DB=p4lang GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
+./devel/db.sh psql p4lang -c "create extension if not exists pgcrypto" || exit 1
+./devel/ro_user_grants.sh p4lang || exit 2
+GHA2DB_PROJECT=p4lang PG_DB=p4lang GHA2DB_LOCAL=1 ./gha2db 2018-11-25 0 today now p4lang 2>>errors.txt | tee -a run.log || exit 3
+# GHA2DB_PROJECT=p4lang PG_DB=p4lang GHA2DB_LOCAL=1 ./gha2db 2016-12-17 0 today now p4lang 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=p4lang PG_DB=p4lang GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=p4lang PG_DB=p4lang ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
+GHA2DB_PROJECT=p4lang PG_DB=p4lang ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
+GHA2DB_PROJECT=p4lang PG_DB=p4lang ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 8
+GHA2DB_PROJECT=p4lang PG_DB=p4lang ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 9
+GHA2DB_PROJECT=p4lang PG_DB=p4lang GHA2DB_LOCAL=1 ./vars || exit 10
