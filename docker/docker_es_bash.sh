026033007626d70b579a67585ad42ac2c2053467
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 06:16:14 +0000

    Use volume maps when basing into the containers [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_es_bash.sh
+++ docker/docker_es_bash.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -it elasticsearch:6.5.1 /bin/bash
+mkdir /data 2>/dev/null
+mkdir /data/es 2>/dev/null
+docker run -v /data/es:/var/lib/elasticsearch -it elasticsearch:6.5.1 /bin/bash
