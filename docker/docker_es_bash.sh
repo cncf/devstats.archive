a86deed84a39c1711b09b79c256208e987450ed9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 06:24:36 +0000

    new mount syntax [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_es_bash.sh
+++ docker/docker_es_bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /data 2>/dev/null
 mkdir /data/es 2>/dev/null
-docker run -v /data/es:/var/lib/elasticsearch -it elasticsearch:6.5.1 /bin/bash
+docker run --mount 'src="/data/es",target="/var/lib/elasticsearch",type=bind' -it elasticsearch:6.5.1 /bin/bash
