8c96a768242555c5f063950b5039d3d72d7c5a11
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 21:18:26 +0000

    Fix psql shared memory segment size bug [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_psql.sh
+++ docker/docker_psql.sh
@@ -5,5 +5,5 @@ then
   exit 1
 fi
 ./docker/docker_make_mount_dirs.sh
-docker run --mount src="/data/psql",target="/var/lib/postgresql/data",type=bind -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
+docker run --shm-size=1g --mount src="/data/psql",target="/var/lib/postgresql/data",type=bind -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
 
