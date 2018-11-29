107827a40a0de510cd218251ed286687b3dc62a7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 09:36:52 +0000

    Fix for ES [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_make_mount_dirs.sh
+++ docker/docker_make_mount_dirs.sh
@@ -2,4 +2,4 @@
 mkdir /data 2>/dev/null
 mkdir /data/devstats 2>/dev/null
 mkdir /data/psql 2>/dev/null
-mkdir /data/es 2>/dev/null
+mkdir /data/es 2>/dev/null && chown 1000:1000 /data/es 2>/dev/null
