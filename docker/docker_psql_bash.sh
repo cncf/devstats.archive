4a4c90a1764f196bb4a21b9699d1ff69ceeeb36f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 06:35:00 +0000

    Postgres persist data path fix [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_psql_bash.sh
+++ docker/docker_psql_bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /data 2>/dev/null
 mkdir /data/psql 2>/dev/null
-docker run --mount src="/data/psql",target="/var/lib/postgresql",type=bind -it postgres:11 /bin/bash
+docker run --mount src="/data/psql",target="/var/lib/postgresql/data",type=bind -it postgres:11 /bin/bash
