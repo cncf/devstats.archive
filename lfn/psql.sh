7b134880f12a72ef4d51ee5d4be9ceda97bf7d4f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 20:50:33 +0000

    Fix org name in the summary project [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- lfn/psql.sh
+++ lfn/psql.sh
@@ -14,9 +14,9 @@ set -o pipefail
 GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql lfn -c "create extension if not exists pgcrypto" || exit 1
 ./devel/ro_user_grants.sh lfn || exit 2
-# GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 ./gha2db 2018-11-25 0 today now 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,openbmp,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 ./gha2db 2015-01-01 0 today now 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,openbmp,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 ./gha2db 2014-01-01 0 2014-12-31 23 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,openbmp,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 4
+# GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 ./gha2db 2018-11-25 0 today now 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,OpenBMP,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 ./gha2db 2015-01-01 0 today now 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,OpenBMP,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 ./gha2db 2014-01-01 0 2014-12-31 23 'iovisor,mininet,opennetworkinglab,opensecuritycontroller,open-switch,p4lang,OpenBMP,tungstenfabric,opencord' 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=lfn PG_DB=lfn GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=lfn PG_DB=lfn ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
 GHA2DB_PROJECT=lfn PG_DB=lfn ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
