81d06f923a14ebe8fea61701fa2906d45dde29b7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 06:35:00 +0000

    Postgres persist data path fix [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM golang:1.11
-RUN apt-get update -y && apt-get upgrade -y && apt-get install -y ca-certificates openssh-client postgresql-client git curl make
+RUN apt-get update -y && apt-get upgrade -y && apt-get install -y ca-certificates openssh-client postgresql-client apt-utils git curl make
 WORKDIR /go/src/devstats
 ADD devstats.tar .
 RUN make dockerinstall
--- docker/docker_psql.sh
+++ docker/docker_psql.sh
@@ -6,5 +6,5 @@ then
 fi
 mkdir /data 2>/dev/null
 mkdir /data/psql 2>/dev/null
-docker run --mount src="/data/psql",target="/var/lib/postgresql",type=bind -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
+docker run --mount src="/data/psql",target="/var/lib/postgresql/data",type=bind -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
 
