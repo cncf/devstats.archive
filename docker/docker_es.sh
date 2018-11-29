705a7740f30cbefef9f154e5235927f974dfc192
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-29 05:59:50 +0000

    Try with voulmes [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_es.sh
+++ docker/docker_es.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # docker run -p 19200:9200 -p 19300:9300 -e ES_JAVA_OPTS="-Xms2g -Xmx2g" --ulimit nofile=65536:65536 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
-docker run -p 19200:9200 -p 19300:9300 --ulimit nofile=65536:65536 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
+mkdir /data 2>/dev/null
+mkdir /data/es 2>/dev/null
+docker run -v /data/es:/var/lib/elasticsearch -p 19200:9200 -p 19300:9300 --ulimit nofile=65536:65536 elasticsearch:6.5.1 1>/tmp/docker.es.log 2>/tmp/docker.es.err &
--- docker/docker_psql.sh
+++ docker/docker_psql.sh
@@ -4,4 +4,6 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
-docker run -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
+mkdir /data 2>/dev/null
+mkdir /data/psql 2>/dev/null
+docker run -v /data/psql:/var/lib/postgresql -e POSTGRES_PASSWORD="${PG_PASS}" -p 65432:5432 postgres:11 1>/tmp/docker.psql.log 2>/tmp/docker.psql.err &
