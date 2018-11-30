8c742b1527aa36e8af8f51509851c6a043f8a336
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-11-30 14:31:02 +0000

    Update committers definition in 3 more dashboards: 'countries stats', 'timezone stats', 'gender stats' [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/committers_countries_cum.sql
+++ metrics/shared/committers_countries_cum.sql
@@ -1,3 +1,52 @@
+with commits_data as (
+  select c.dup_repo_id as repo_id,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    c.sha,
+    c.dup_actor_id as actor_id
+  from
+    gha_repos r,
+    gha_commits c
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = c.event_id
+  where
+    c.dup_repo_id = r.id
+    and c.dup_created_at < '{{to}}'
+    and (lower(c.dup_actor_login) {{exclude_bots}})
+  union select c.dup_repo_id as repo_id,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    c.sha,
+    c.author_id as actor_id
+  from
+    gha_repos r,
+    gha_commits c
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = c.event_id
+  where
+    c.dup_repo_id = r.id
+    and c.author_id is not null
+    and c.dup_created_at < '{{to}}'
+    and (lower(c.dup_author_login) {{exclude_bots}})
+  union select c.dup_repo_id as repo_id,
+    coalesce(ecf.repo_group, r.repo_group) as repo_group,
+    c.sha,
+    c.committer_id as actor_id
+  from
+    gha_repos r,
+    gha_commits c
+  left join
+    gha_events_commits_files ecf
+  on
+    ecf.event_id = c.event_id
+  where
+    c.dup_repo_id = r.id
+    and c.committer_id is not null
+    and c.dup_created_at < '{{to}}'
+    and (lower(c.dup_committer_login) {{exclude_bots}})
+)
 select
   concat(inn.type, ';', inn.country_name, '`', inn.repo_group, ';rcommitters,rcommits') as name,
   inn.rcommitters,
@@ -6,52 +55,34 @@ from (
   select 'countriescum' as type,
     a.country_name,
     'all' as repo_group,
-    count(distinct a.login) as rcommitters,
+    count(distinct c.actor_id) as rcommitters,
     count(distinct c.sha) as rcommits
   from
-    gha_actors a,
-    gha_commits c
+    commits_data c,
+    gha_actors a
   where
-    (
-      c.author_name = a.name
-      or
-      (
-        a.login = c.dup_actor_login
-        and (lower(a.login) {{exclude_bots}})
-        and (lower(c.dup_actor_login) {{exclude_bots}})
-      )
-    )
+    (lower(a.login) {{exclude_bots}})
+    and a.id = c.actor_id
     and a.country_name is not null
     and a.country_name != ''
-    and c.dup_created_at < '{{to}}'
   group by
     a.country_name
   union select 'countriescum' as type,
     a.country_name,
-    r.repo_group,
-    count(distinct a.login) as rcommitters,
+    c.repo_group,
+    count(distinct c.actor_id) as rcommitters,
     count(distinct c.sha) as rcommits
   from
-    gha_repos r,
-    gha_actors a,
-    gha_commits c
+    commits_data c,
+    gha_actors a
   where
-    (
-      c.author_name = a.name
-      or
-      (
-        a.login = c.dup_actor_login
-        and (lower(a.login) {{exclude_bots}})
-        and (lower(c.dup_actor_login) {{exclude_bots}})
-      )
-    )
-    and r.id = c.dup_repo_id
+    (lower(a.login) {{exclude_bots}})
+    and a.id = c.actor_id
     and a.country_name is not null
     and a.country_name != ''
-    and c.dup_created_at < '{{to}}'
   group by
     a.country_name,
-    r.repo_group
+    c.repo_group
 ) inn
 where
   inn.repo_group is not null 
