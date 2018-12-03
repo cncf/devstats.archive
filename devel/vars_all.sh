004ae4b7aafe605cc1cb2a7533a5a30b00e5da8e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-03 15:31:57 +0000

    Fix one more call [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/vars_all.sh
+++ devel/vars_all.sh
@@ -5,6 +5,11 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
+user=gha_admin
+if [ ! -z "${PG_USER}" ]
+then
+  user="${PG_USER}"
+fi
 . ./devel/all_projs.sh || exit 2
 for proj in $all
 do
@@ -19,7 +24,7 @@ do
     echo "Project: $proj, PDB: $db"
     if [ -z "${SKIPDEL}" ]
     then
-      ./devel/db.sh psql "$db" -c "delete from gha_vars" || exit 1
+      PG_USER="${user}" ./devel/db.sh psql "$db" -c "delete from gha_vars" || exit 1
     fi
     GHA2DB_LOCAL=1 GHA2DB_PROJECT=$proj PG_DB=$db ./vars || exit 2
     GHA2DB_LOCAL=1 GHA2DB_PROJECT=$proj PG_DB=$db GHA2DB_VARS_FN_YAML="sync_vars.yaml" ./vars || exit 3
