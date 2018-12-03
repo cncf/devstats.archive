f5a0cb2d6df81de86ec2a23fca624a86a4e920e6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-03 11:12:41 +0000

    More permissions fixes [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/psql_user_grants.sh
+++ devel/psql_user_grants.sh
@@ -9,6 +9,10 @@ then
   echo "$0: need database name argument"
   exit 1
 fi
+if [ ! -z "${PG_USER}" ]
+then
+  user="${PG_USER}"
+fi
 proj=$2
 ./devel/db.sh psql postgres -c "grant connect on database \"$proj\" to \"$1\"" || exit 1
 ./devel/db.sh psql postgres -c "grant usage on schema \"public\" to \"$1\"" || exit 1
@@ -16,5 +20,5 @@ tables=`./devel/db.sh psql $proj -qAntc '\dt' | cut -d\| -f2`
 for table in $tables
 do
   echo -n "$proj: $table "
-  ./devel/db.sh psql $proj -c "grant select on $table to \"$1\"" || exit 1
+  PG_USER="${user}" ./devel/db.sh psql $proj -c "grant select on $table to \"$1\"" || exit 1
 done
