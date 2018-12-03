c89c3c35f1d42aa33129245eec55b6991618bfd8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-03 11:43:06 +0000

    This works [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/drop_psql_user.sql
+++ util_sql/drop_psql_user.sql
@@ -1,7 +1,6 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "{{user}}";
 REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM "{{user}}";
 REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM "{{user}}";
-revoke all privileges on schema public from "{{user}}";
--- REASSIGN OWNED BY "{{user}}" TO "{{admin_user}}";
--- DROP OWNED BY "{{user}}";
+REASSIGN OWNED BY "{{user}}" TO "{{admin_user}}";
+DROP OWNED BY "{{user}}";
 DROP USER "{{user}}";
