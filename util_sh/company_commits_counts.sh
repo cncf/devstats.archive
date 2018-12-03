c57691a70191af3fe97bbb8f84bd19d5a838b18f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-03 06:40:15 +0000

    Company commits counts for all CNCF projects combined [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/company_commits_counts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: you need to specify PG_PASS env variable"
+  exit 1
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify number of companies to show as a first argument"
+  exit 2
+fi
+GHA2DB_CSVOUT="company_commits_counts.csv" GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=allprj ./runq ./temp.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{lim}} $1
