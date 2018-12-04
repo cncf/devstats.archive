052319e55b307719096892c83b230a76c393d487
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-04 13:01:24 +0000

    remove interactive mode where not needed [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- docker/docker_psql_wait.sh
+++ docker/docker_psql_wait.sh
@@ -9,10 +9,10 @@ if [ "${DEPLOY_FROM}" = "container" ]
 then
   if [ -z "$AURORA" ]
   then
-    host=`docker run -it devstats ip route show 2>/dev/null | awk '/default/ {print $3}'`
-    docker run -e PG_PORT=65432 -e PG_HOST="${host}" -e PG_PASS="${PG_PASS}" -it devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null && exit 0
+    host=`docker run devstats ip route show 2>/dev/null | awk '/default/ {print $3}'`
+    docker run -e PG_PORT=65432 -e PG_HOST="${host}" -e PG_PASS="${PG_PASS}" devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null && exit 0
   else
-    docker run -e PG_ADMIN_USER=sa -e PG_PORT=5432 -e PG_HOST="dev-analytics-api-devstats-dev.cluster-czqvov18pw9a.us-west-2.rds.amazonaws.com" -e PG_PASS="${PG_PASS}" -it devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null && exit 0
+    docker run -e PG_ADMIN_USER=sa -e PG_PORT=5432 -e PG_HOST="dev-analytics-api-devstats-dev.cluster-czqvov18pw9a.us-west-2.rds.amazonaws.com" -e PG_PASS="${PG_PASS}" devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null && exit 0
   fi
 else
   if [ -z "$AURORA" ]
@@ -29,11 +29,11 @@ do
   then
     if [ -z "$AURORA" ]
     then
-      host=`docker run -it devstats ip route show 2>/dev/null | awk '/default/ {print $3}'`
-      docker run -e PG_PORT=65432 -e PG_HOST="${host}" -e PG_PASS="${PG_PASS}" -it devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null
+      host=`docker run devstats ip route show 2>/dev/null | awk '/default/ {print $3}'`
+      docker run -e PG_PORT=65432 -e PG_HOST="${host}" -e PG_PASS="${PG_PASS}" devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null
       r=$?
     else
-      docker run -e PG_ADMIN_USER=sa -e PG_PORT=5432 -e PG_HOST="dev-analytics-api-devstats-dev.cluster-czqvov18pw9a.us-west-2.rds.amazonaws.com" -e PG_PASS="${PG_PASS}" -it devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null
+      docker run -e PG_ADMIN_USER=sa -e PG_PORT=5432 -e PG_HOST="dev-analytics-api-devstats-dev.cluster-czqvov18pw9a.us-west-2.rds.amazonaws.com" -e PG_PASS="${PG_PASS}" devstats db.sh psql postgres -c 'select 1' 1>/dev/null 2>/dev/null
       r=$?
     fi
   else
