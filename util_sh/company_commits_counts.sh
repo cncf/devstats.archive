b3c96ef89f4c725c58f6687c373491aed057f5b4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-12-10 13:28:27 +0000

    K8s only commits [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/company_commits_counts.sh
+++ util_sh/company_commits_counts.sh
@@ -9,4 +9,8 @@ then
   echo "$0: you need to specify number of companies to show as a first argument"
   exit 2
 fi
-GHA2DB_CSVOUT="company_commits_counts.csv" GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=allprj ./runq ./util_sql/company_commits_counts.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{lim}} $1
+if [ -z "$PG_DB" ]
+then
+  export PG_DB=allprj
+fi
+GHA2DB_CSVOUT="company_commits_counts.csv" GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 ./runq ./util_sql/company_commits_counts.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`" {{lim}} $1
