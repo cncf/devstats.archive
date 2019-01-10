71e2ad57bb520933dce0ea1aec3d40f0025df10a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-10 12:31:06 +0000

    Fetching Linux data started [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/count_everything.sh
+++ util_sh/count_everything.sh
@@ -4,11 +4,16 @@ then
   echo "$0: need database name argument"
   exit 1
 fi
+if [ -z "$PG_PASS" ]
+then
+  echo "$0: You need to set PG_PASS environment variable to run this script"
+  exit 2
+fi
 tables=`./devel/db.sh psql $1 -qAntc '\dt' | cut -d\| -f2`
 > out
 for table in $tables
 do
   echo -n "$table: " >> out
-  ./devel/db.sh psql $1 -qAntc "select count(*) from \"$table\"" >> out|| exit 1
+  ./devel/db.sh psql $1 -qAntc "select count(*) from \"$table\"" >> out || exit 3
 done
 cat out | sort -k 2 -n
