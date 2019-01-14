5b9af798b7807cb53d04b6d932916b516824e764
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-14 08:45:21 +0000

    resizing home dashboads everywhere [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/dashboards_replace_from_to.sh
+++ devel/dashboards_replace_from_to.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+if [ -z "$FROM" ]
+then
+  export FROM=`cat ./FROM`
+fi
+if [ -z "$TO" ]
+then
+  export FROM=`cat ./TO`
+fi
 if [ -z "$1" ]
 then
-  FROM=`cat ./FROM` TO=`cat ./TO` FILES=`find grafana/dashboards/ -iname "*.json"` MODE=ss0 ./devel/mass_replace.sh
+  FILES=`find grafana/dashboards/ -iname "*.json"` MODE=ss0 ./devel/mass_replace.sh
 else
-  FROM=`cat ./FROM` TO=`cat ./TO` FILES=`find grafana/dashboards/ -iname "$1"` MODE=ss0 ./devel/mass_replace.sh
+  FILES=`find grafana/dashboards/ -iname "$1"` MODE=ss0 ./devel/mass_replace.sh
 fi
