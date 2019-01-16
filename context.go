a550308819a93ced61bce1fa1cf9a995550cea60
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-16 09:23:37 +0000

    Longer API periods because we now have 4 API keys [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- context.go
+++ context.go
@@ -80,8 +80,8 @@ type Ctx struct {
 	OutputDB            string                       // From GHA2DB_OUTPUT_DB, merge_dbs tool - output database to merge into
 	TmOffset            int                          // From GHA2DB_TMOFFSET, gha2db_sync tool - uses time offset to decide when to calculate various metrics, default offset is 0 which means UTC, good offset for USA is -6, and for Poland is 1 or 2
 	DefaultHostname     string                       // "devstats.cncf.io"
-	RecentRange         string                       // From GHA2DB_RECENT_RANGE, ghapi2db tool, default '2 hours'. This is a recent period to check open issues/PR to fix their labels and milestones.
-	RecentReposRange    string                       // From GHA2DB_RECENT_REPOS_RANGE, ghapi2db tool, default '1 day'. This is a recent period to check modified repositories.
+	RecentRange         string                       // From GHA2DB_RECENT_RANGE, ghapi2db tool, default '4 hours'. This is a recent period to check open issues/PR to fix their labels and milestones.
+	RecentReposRange    string                       // From GHA2DB_RECENT_REPOS_RANGE, ghapi2db tool, default '2 days'. This is a recent period to check modified repositories.
 	MinGHAPIPoints      int                          // From GHA2DB_MIN_GHAPI_POINTS, ghapi2db tool, minimum GitHub API points, before waiting for reset.
 	MaxGHAPIWaitSeconds int                          // From GHA2DB_MAX_GHAPI_WAIT, ghapi2db tool, maximum wait time for GitHub API points reset (in seconds).
 	MaxGHAPIRetry       int                          // From GHA2DB_MAX_GHAPI_RETRY, ghapi2db tool, maximum wait retries
@@ -636,11 +636,11 @@ func (ctx *Ctx) Init() {
 	// RecentRange - ghapi2db will check issues/PRs from now() - this range to now()
 	ctx.RecentRange = os.Getenv("GHA2DB_RECENT_RANGE")
 	if ctx.RecentRange == "" {
-		ctx.RecentRange = "2 hours"
+		ctx.RecentRange = "4 hours"
 	}
 	ctx.RecentReposRange = os.Getenv("GHA2DB_RECENT_REPOS_RANGE")
 	if ctx.RecentReposRange == "" {
-		ctx.RecentReposRange = "1 day"
+		ctx.RecentReposRange = "2 days"
 	}
 
 	ctx.CSVFile = os.Getenv("GHA2DB_CSVOUT")
