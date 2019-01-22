c7e6d26ca3f756c441712f98896fd338f7519ddc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-01-22 14:14:34 +0000

    Update affiliations import scripts to call devstats more often [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_prod.sh
+++ util_sh/affs_prod.sh
@@ -4,13 +4,29 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
-ONLY="kubernetes prometheus opentracing" ./devel/all_affs.sh
-GHA2DB_TMOFFSET="-4" devstats
+ONLY="kubernetes" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="prometheus opentracing" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
 ONLY="fluentd linkerd grpc coredns containerd" ./devel/all_affs.sh
-GHA2DB_TMOFFSET="-4" devstats
-ONLY="rkt cni envoy jaeger notary tuf rook vitess nats opa" ./devel/all_affs.sh
-GHA2DB_TMOFFSET="-4" devstats
-ONLY="spiffe spire cloudevents telepresence helm openmetrics harbor etcd tikv cortex" ./devel/all_affs.sh
-GHA2DB_TMOFFSET="-4" devstats
-ONLY="buildpacks falco dragonfly virtualkubelet all" ./devel/all_affs.sh
-GHA2DB_TMOFFSET="-4" devstats
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="rkt cni envoy jaeger notary" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="tuf rook vitess nats opa" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="spiffe spire cloudevents telepresence" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="helm openmetrics harbor etcd tikv cortex" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="buildpacks falco dragonfly virtualkubelet" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="all" ./devel/all_affs.sh
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
