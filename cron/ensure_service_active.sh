c4383f4ee41a927940285d2ed35cf7115740ca3d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-11 07:29:55 +0000

    Added script to ensure that required services are running [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ cron/ensure_service_active.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: required service name"
+  exit 1
+fi
+function restart {
+  echo "Stopping $1"
+  service "$1" stop
+  echo "Starting $1"
+  service "$1" start
+  echo "Restarted $1"
+}
+fn="/tmp/ensure_$1"
+if [ -f "$fn" ]
+then
+  echo "Ensure process already running $fn file present"
+  exit 2
+fi
+function finish {
+  echo "Removing $fn"
+  rm -f "$fn"
+}
+trap finish EXIT
+> "$fn"
+while true
+do
+  service "$1" status | grep 'Active: active' || restart "$1"
+  sleep 30
+done
