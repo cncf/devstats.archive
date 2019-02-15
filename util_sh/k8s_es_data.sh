b9876021e12e7cd8797fcf2411909abf0f4d120f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-15 11:57:09 +0000

    Script to get example data described in the email [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/k8s_es_data.sh
+++ util_sh/k8s_es_data.sh
@@ -9,3 +9,7 @@ fi
 ./docker/docker_es_query.sh d_kubernetes _doc "type:tvars AND vname:sig_mentions_docs_html"
 ./docker/docker_es_query.sh d_kubernetes _doc "type:tsig_mentions_texts AND sig_mentions_texts_value:apps"
 ./docker/docker_es_query.sh d_kubernetes _doc "type:issig_mentions AND name:api-machinery AND period:y AND ivalue:4599"
+./docker/docker_es_query.sh d_kubernetes _doc "type:tquick_ranges AND quick_ranges_suffix:a_0_1"
+./docker/docker_es_query.sh d_kubernetes _doc "type:tvars AND vname:project_stats_docs_html"
+./docker/docker_es_query.sh d_kubernetes _doc "type:tall_repo_groups AND all_repo_group_value:signode"
+./docker/docker_es_query.sh d_kubernetes _doc "type:ispstat AND series:pstatsignode AND period:y10 AND svalue:Contributors"
