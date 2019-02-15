f986b38ceb5adb3de7ee6d714882fdad1ed95a35
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-15 08:19:46 +0000

    Support for adding ES only metrics [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/add_single_metric_all_es.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+if [ -z "$GHA2DB_ES_URL" ]
+then
+  echo "$0: you need to set GHA2DB_ES_URL=..."
+  exit 2
+fi
+
+export TRAP=1
+
+. ./devel/all_projs.sh || exit 3
+
+for proj in $all
+do
+  db=$proj
+  if [ "$db" = "kubernetes" ]
+  then
+    db="gha"
+  elif [ "$db" = "all" ]
+  then
+    db="allprj"
+  fi
+  GHA2DB_SKIPTSDB=1 GHA2DB_USE_ES_ONLY=1 GHA2DB_USE_ES=1 GHA2DB_PROJECT=$proj PG_DB=$db ./devel/add_single_metric.sh || exit 4
+done
+
+echo 'OK'
