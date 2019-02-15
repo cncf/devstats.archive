9babc14d12c1252c07a303b4a6bf4e2c686f9448
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-15 12:56:57 +0000

    Finished TZ description [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/k8s_es_data.sh
+++ util_sh/k8s_es_data.sh
@@ -13,3 +13,6 @@ fi
 ./docker/docker_es_query.sh d_kubernetes _doc "type:tvars AND vname:project_stats_docs_html"
 ./docker/docker_es_query.sh d_kubernetes _doc "type:tall_repo_groups AND all_repo_group_value:signode"
 ./docker/docker_es_query.sh d_kubernetes _doc "type:ispstat AND series:pstatsignode AND period:y10 AND svalue:Contributors"
+./docker/docker_es_query.sh d_kubernetes _doc "type:tvars AND vname:tzs_stats_docs_html"
+./docker/docker_es_query.sh d_kubernetes _doc "type:ttz_offsets AND tz_offset:1.0"
+./docker/docker_es_query.sh d_kubernetes _doc "type:istz AND period:y AND series:tzsignodecontributions AND name:1.0 AND ivalue:3414"
