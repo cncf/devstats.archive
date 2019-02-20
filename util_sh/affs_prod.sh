1b1e9b40ae193700fd2944003a38377889831d29
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-20 16:26:15 +0000

    Missing test for exit codes and return on errors [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_prod.sh
+++ util_sh/affs_prod.sh
@@ -4,29 +4,29 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
-ONLY="kubernetes" ./devel/all_affs.sh
+ONLY="kubernetes" ./devel/all_affs.sh || exit 1
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="prometheus opentracing" ./devel/all_affs.sh
+ONLY="prometheus opentracing" ./devel/all_affs.sh || exit 2
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="fluentd linkerd grpc coredns containerd" ./devel/all_affs.sh
+ONLY="fluentd linkerd grpc coredns containerd" ./devel/all_affs.sh || exit 3
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="rkt cni envoy jaeger notary" ./devel/all_affs.sh
+ONLY="rkt cni envoy jaeger notary" ./devel/all_affs.sh || exit 4
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="tuf rook vitess nats opa" ./devel/all_affs.sh
+ONLY="tuf rook vitess nats opa" ./devel/all_affs.sh || exit 5
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="spiffe spire cloudevents telepresence" ./devel/all_affs.sh
+ONLY="spiffe spire cloudevents telepresence" ./devel/all_affs.sh || exit 6
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="helm openmetrics harbor etcd tikv cortex" ./devel/all_affs.sh
+ONLY="helm openmetrics harbor etcd tikv cortex" ./devel/all_affs.sh || exit 7
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="buildpacks falco dragonfly virtualkubelet" ./devel/all_affs.sh
+ONLY="buildpacks falco dragonfly virtualkubelet" ./devel/all_affs.sh || exit 8
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="all" ./devel/all_affs.sh
+ONLY="all" ./devel/all_affs.sh || exit 9
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
--- util_sh/affs_test.sh
+++ util_sh/affs_test.sh
@@ -4,26 +4,26 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
-ONLY="kubernetes" ./devel/all_affs.sh
+ONLY="kubernetes" ./devel/all_affs.sh || exit 1
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="prometheus opentracing" ./devel/all_affs.sh
+ONLY="prometheus opentracing" ./devel/all_affs.sh || exit 2
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="fluentd linkerd grpc coredns containerd rkt cni" ./devel/all_affs.sh
+ONLY="fluentd linkerd grpc coredns containerd rkt cni" ./devel/all_affs.sh || exit 3
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="envoy jaeger notary tuf rook vitess nats opa spiffe spire" ./devel/all_affs.sh
+ONLY="envoy jaeger notary tuf rook vitess nats opa spiffe spire" ./devel/all_affs.sh || exit 4
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="cloudevents telepresence helm openmetrics harbor etcd" ./devel/all_affs.sh
+ONLY="cloudevents telepresence helm openmetrics harbor etcd" ./devel/all_affs.sh || exit 5
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="tikv cortex buildpacks falco dragonfly virtualkubelet cncf" GHA2DB_PROJECTS_OVERRIDE="+cncf" ./devel/all_affs.sh
+ONLY="tikv cortex buildpacks falco dragonfly virtualkubelet cncf" GHA2DB_PROJECTS_OVERRIDE="+cncf" ./devel/all_affs.sh || exit 6
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="opencontainers istio spinnaker knative" GHA2DB_PROJECTS_OVERRIDE="+opencontainers,+istio,+spinnaker,+knative" ./devel/all_affs.sh
+ONLY="opencontainers istio spinnaker knative" GHA2DB_PROJECTS_OVERRIDE="+opencontainers,+istio,+spinnaker,+knative" ./devel/all_affs.sh || exit 7
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
 
-ONLY="linux zephyr all" GHA2DB_PROJECTS_OVERRIDE="+linux,+zephyr" ./devel/all_affs.sh
+ONLY="linux zephyr all" GHA2DB_PROJECTS_OVERRIDE="+linux,+zephyr" ./devel/all_affs.sh || exit 8
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+spinnaker,+knative,+linux,+zephyr" devstats
