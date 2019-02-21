f87d8d3b75a9bfd7d17e59ed541042ca048a880d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-21 12:21:58 +0000

    Creating minimal DevStats image for sync and full image for provisioning 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/build_images.sh
+++ k8s/build_images.sh
@@ -7,11 +7,11 @@ fi
 rm -f devstats.tar 2>/dev/null
 tar cf devstats.tar cmd git metrics docker devel util_sql all lfn shared iovisor mininet opennetworkinglab opensecuritycontroller openswitch p4lang openbmp tungstenfabric cord scripts partials docs cron vendor util_sh/touch *.go projects.yaml companies.yaml linux.yaml zephyr.yaml github_users.json Makefile
 docker build -f Dockerfile -t "${DOCKER_USER}/devstats" .
+docker build -f docker/Dockerfile.minimal.debug -t "${DOCKER_USER}/devstats-minimal" .
 # Dockerfile.minimal.debug creates container with a working '/bin/sh' command. Dockerfile.minimal creates DevStats binaries only, even without any shell.
-#docker build -f docker/Dockerfile.minimal.debug -t "${DOCKER_USER}/devstats-minimal" .
-docker build -f docker/Dockerfile.minimal -t "${DOCKER_USER}/devstats-minimal" .
+#docker build -f docker/Dockerfile.minimal -t "${DOCKER_USER}/devstats-minimal" .
 rm -f devstats.tar
-# docker tag devstats "${DOCKER_USER}/devstats"
-# docker tag devstats-minimal "${DOCKER_USER}/devstats-minimal"
+docker tag devstats "${DOCKER_USER}/devstats"
+docker tag devstats-minimal "${DOCKER_USER}/devstats-minimal"
 docker push "${DOCKER_USER}/devstats"
 docker push "${DOCKER_USER}/devstats-minimal"
---
+++ k8s/test_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z "${AWS_PROFILE}" ]
+then
+  echo "$0: you need to set AWS_PROFILE=... to run this script"
+  exit 1
+fi
+if [ -z "${DOCKER_USER}" ]
+then
+  echo "$0: you need to set docker user via DOCKER_USER=username"
+  exit 2
+fi
+if [ -z "$1" ]
+then
+  echo "$0: you need to pass devstats image name as an agument, possible options are: devstats or devstats-minimal"
+  echo "$0: if you've used docker/Dockerfile.minila instead of docker/Dockerfile.minimal.debug there will be no /bin/sh command to run"
+  exit 3
+fi
+command="$2"
+if [ -z "${command}" ]
+then
+  export command=/bin/sh
+fi
+kubectl run -i --tty $1 --restart=Never --rm --image="${DOCKER_USER}/$1" --command "$command"
