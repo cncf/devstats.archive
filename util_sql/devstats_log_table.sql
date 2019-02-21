15815ae98caa0de93686bad3a58973cdcf6f9ef6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-21 20:54:36 +0000

    Devstats logs table: columns order and indexes [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/devstats_log_table.sql
+++ util_sql/devstats_log_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE gha_logs (
     id integer NOT NULL,
     dt timestamp without time zone DEFAULT now(),
-    msg text,
     prog character varying(32) not null default '',
     proj character varying(32) not null,
-    run_dt timestamp without time zone not null
+    run_dt timestamp without time zone not null,
+    msg text
 );
 ALTER TABLE gha_logs OWNER TO gha_admin;
 CREATE SEQUENCE gha_logs_id_seq
