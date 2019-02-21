70b8e101645c918e7f8817b36338d6ffeb669cd4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-21 20:54:36 +0000

    Devstats logs table: columns order and indexes [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/devstats_log_table_as_owner.sql
+++ util_sql/devstats_log_table_as_owner.sql
@@ -2,3 +2,6 @@ ALTER SEQUENCE gha_logs_id_seq OWNED BY gha_logs.id;
 ALTER TABLE ONLY gha_logs ALTER COLUMN id SET DEFAULT nextval('gha_logs_id_seq'::regclass);
 CREATE INDEX logs_dt_idx ON gha_logs USING btree (dt);
 CREATE INDEX logs_id_idx ON gha_logs USING btree (id);
+CREATE INDEX logs_prog_idx ON gha_logs USING btree (prog);
+CREATE INDEX logs_proj_idx ON gha_logs USING btree (proj);
+CREATE INDEX logs_run_dt_idx ON gha_logs USING btree (run_dt);
