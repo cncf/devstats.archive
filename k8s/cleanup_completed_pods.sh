2f1bdffed873854e884da3bc63aa30f7d8e85043
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-25 19:37:21 +0000

    Optimize delete finished pods script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/cleanup_completed_pods.sh
+++ k8s/cleanup_completed_pods.sh
@@ -4,6 +4,7 @@ then
   echo "$0: you need to set AWS_PROFILE=... to run this script"
   exit 1
 fi
+pods=""
 for data in `kubectl get po -o=jsonpath='{range .items[*]}{.metadata.name}{";"}{.status.phase}{"\n"}{end}'`
 do
   IFS=';'
@@ -14,6 +15,11 @@ do
   # echo "$data -> $pod $status"
   if [ "$status" = "Succeeded" ]
   then
-    kubectl delete pod "$pod"
+    pods="${pods} ${pod}"
   fi
 done
+if [ ! -z "$pods" ]
+then
+  echo "Deleting pods: ${pods}"
+  kubectl delete pod ${pods}
+fi
