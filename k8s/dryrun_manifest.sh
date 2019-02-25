c1ab51e045c1898167cc510eb16b390b2aa03da9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-25 12:54:30 +0000

    More manifests work [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/dryrun_manifest.sh
+++ k8s/dryrun_manifest.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # This dry-runs all manifests given, doing environment subsititution and setting TIMESTAMP to the current time with microsecond resolution
+if [ -z "${AWS_PROFILE}" ]
+then
+  echo "$0: you need to set AWS_PROFILE=... to run this script"
+  exit 1
+fi
 export TIMESTAMP=`date +'%s%N'`
 for f in "$@"
 do
