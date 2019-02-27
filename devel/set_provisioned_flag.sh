306d18989ae39b119f3b681b5b0bc69e1cad89fa
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-27 08:52:43 +0000

    Delete errored pods, update set provisioned flag script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/set_provisioned_flag.sh
+++ devel/set_provisioned_flag.sh
@@ -9,5 +9,10 @@ then
   echo "$0: You need to set PG_PASS environment variable to run this script"
   exit 2
 fi
-./devel/db.sh psql "$1" -c "insert into gha_computed(metric, dt) select 'provisioned', now() where not exists(select 1 from gha_computed where metric = 'provisioned')" || exit 3
+user=gha_admin
+if [ ! -z "${PG_USER}" ]
+then
+  user="${PG_USER}"
+fi
+PG_USER="${user}" ./devel/db.sh psql "$1" -c "insert into gha_computed(metric, dt) select 'provisioned', now() where not exists(select 1 from gha_computed where metric = 'provisioned')" || exit 3
 echo "database '$1' marked as provisioned"
