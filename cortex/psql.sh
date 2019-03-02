19debec82a7ecff84fdd83dacfe974d075fca89d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-02 07:32:04 +0000

    Fixed Cortext provision bug [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cortex/psql.sh
+++ cortex/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=cortex PG_DB=cortex GHA2DB_LOCAL=1 ./structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql cortex -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=cortex PG_DB=cortex GHA2DB_LOCAL=1 ./gha2db 2016-09-09 0 today now weaveworks 'cortexproject,weaveworks/cortex,weaveworks/prism,weaveworks/frankenstein' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=cortex PG_DB=cortex GHA2DB_LOCAL=1 ./gha2db 2016-09-09 0 today now 'cortexproject,weaveworks/cortex,weaveworks/prism,weaveworks/frankenstein' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=cortex PG_DB=cortex GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 ./structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=cortex PG_DB=cortex ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=cortex PG_DB=cortex ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
