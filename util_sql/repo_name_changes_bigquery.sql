92dc232793b2ed7f47b9edb829a220da2402465b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-05 07:15:12 +0000

    Adding 4 new projects [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/repo_name_changes_bigquery.sql
+++ util_sql/repo_name_changes_bigquery.sql
@@ -6,26 +6,21 @@ select
   min(created_at) as date_from,
   max(created_at) as date_to
 from
-  [githubarchive:month.201810],
-  [githubarchive:month.201809],
-  [githubarchive:month.201808],
-  [githubarchive:month.201807],
-  [githubarchive:month.201806],
-  [githubarchive:month.201805],
-  [githubarchive:month.201804],
-  [githubarchive:month.201803],
-  [githubarchive:month.201802],
-  [githubarchive:month.201801],
+  [githubarchive:month.201902],
+  [githubarchive:month.201901],
+  [githubarchive:year.2018],
   [githubarchive:year.2017],
   [githubarchive:year.2016],
   [githubarchive:year.2015],
-  [githubarchive:year.2014]
+  [githubarchive:year.2014],
+  [githubarchive:year.2013],
+  [githubarchive:year.2012]
 where
   repo.id = (
     select
       repo.id
     from
-      [githubarchive:month.201810]
+      [githubarchive:month.201902]
     where
       repo.name = '{{org_repo}}'
     group by
