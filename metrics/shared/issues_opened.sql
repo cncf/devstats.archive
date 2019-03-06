a6c96bed7438af6bf1723000719a34bea6252845
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-06 13:51:36 +0000

    fix next metrics [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/issues_opened.sql
+++ metrics/shared/issues_opened.sql
@@ -13,6 +13,7 @@ from
   gha_repos r
 where
   i.dup_repo_id = r.id
+  and r.name in (select repo_name from trepos)
   and r.repo_group is not null
   and i.created_at >= '{{from}}'
   and i.created_at < '{{to}}'
