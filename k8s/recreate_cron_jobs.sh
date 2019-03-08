72583017e9f226a9bd01900947d34ba34dfd39e0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-08 07:01:25 +0000

    Recreated cronjobs and added script to do this automatically [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ k8s/recreate_cron_jobs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -z "$AWS_PROFILE" ]
+then
+  echo "$0: you need to set AWS_PROFILE=..."
+  exit 1
+fi
+./k8s/delete_devstats_cron_jobs.sh || exit 2
+helm install ./devstats-helm --set skipSecrets=1,skipPV=1,skipBootstrap=1,skipProvisions=1 || exit 3
+echo 'OK'
