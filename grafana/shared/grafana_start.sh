2531c2d4109652d0a0f123e66e2d8ba6b17b0f0d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-08 12:22:53 +0000

    Autoprovisioning datasources [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/shared/grafana_start.sh
+++ grafana/shared/grafana_start.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-if ( [ -z "$GF_SECURITY_ADMIN_USER" ] || [ -z "$GF_SECURITY_ADMIN_PASSWORD" ] || [ -z "$PROJ" ] )
+if ( [ -z "$PG_HOST" ] || [ -z "$PG_PORT" ] || [ -z "$PG_PASS" ] || [ -z "$PG_DB" ] || [ -z "$GF_SECURITY_ADMIN_USER" ] || [ -z "$GF_SECURITY_ADMIN_PASSWORD" ] || [ -z "$PROJ" ] )
 then
-  echo "$0: you need to set GF_SECURITY_ADMIN_USER=..., GF_SECURITY_ADMIN_PASSWORD=... and PROJ=..."
+  echo "$0: you need to set PG_HOST=..., PG_PORT=..., PG_PASS=..., PG_DB=..., GF_SECURITY_ADMIN_USER=..., GF_SECURITY_ADMIN_PASSWORD=... and PROJ=..."
   exit 1
 fi
 cwd=`pwd`
 cd /usr/share/grafana
+echo 'Updating provisioning yaml'
+cfile="/usr/share/grafana/conf/provisioning/datasources/datasources.yaml"
+MODE=ss FROM='{{url}}' TO="${PG_HOST}:${PG_PORT}" replacer "$cfile" || exit 2
+MODE=ss FROM='{{PG_PASS}}' TO="${PG_PASS}" replacer "$cfile" || exit 3
+MODE=ss FROM='{{PG_DB}}' TO="${PG_DB}" replacer "$cfile" || exit 4
+MODE=ss FROM='{{PG_USER}}' TO="ro_user" replacer "$cfile" || exit 5
+echo 'Starting Grafana'
 GF_SECURITY_ADMIN_USER="${GF_SECURITY_ADMIN_USER}" GF_SECURITY_ADMIN_PASSWORD="${GF_SECURITY_ADMIN_PASSWORD}" grafana-server -config /etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana 1>/var/log/grafana.log 2>/var/log/grafana.err &
 cd "$cwd"
 n=0
@@ -19,7 +26,7 @@ do
     if [ "$n" = "30" ]
     then
       echo "Waited too long, exiting"
-      exit 1
+      exit 6
     fi
     continue
   fi
@@ -27,14 +34,14 @@ do
   if [ -z "$pid" ]
   then
     echo "Grafana not found, existing"
-    exit 2
+    exit 7
   else
     break
   fi
 done
 echo 'Provisioning dashboards'
-sqlitedb /var/lib/grafana/grafana.db grafana/dashboards/$PROJ/*.json || exit 3
+sqlitedb /var/lib/grafana/grafana.db grafana/dashboards/$PROJ/*.json || exit 8
 echo 'Provisioning other preferences'
-sqlite3 /var/lib/grafana/grafana.db < grafana/$PROJ/update_sqlite.sql || exit 4
+sqlite3 /var/lib/grafana/grafana.db < grafana/$PROJ/update_sqlite.sql || exit 9
 echo 'OK'
 wait
