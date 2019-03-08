b985f2d04cc21f57485ef3a88dcfe3ab952d0e1f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-08 13:20:07 +0000

    Now should be able to deploy any project [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/kubernetes/update_sqlite.sql
+++
@@ -1,4 +0,0 @@
-update org set name = 'Kubernetes' where id = 1;
-insert into star(id, user_id, dashboard_id) select 1, 1, id from dashboard where uid = 12;
-insert into preferences select 1,1,1,0,(select id from dashboard where uid = 12),'','dark',datetime('now'),datetime('now'),0;
-insert into preferences select 2,1,0,0,(select id from dashboard where uid = 12),'','dark',datetime('now'),datetime('now'),0;
