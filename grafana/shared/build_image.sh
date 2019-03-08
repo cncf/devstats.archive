346411397cd93813df25436dc3e48620807b4c89
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-08 13:13:38 +0000

    Made devstats-grafana image project independent [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ grafana/shared/build_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z "${DOCKER_USER}" ]
+then
+  echo "$0: you need to set docker user via DOCKER_USER=username"
+  exit 1
+fi
+rm -f devstats-grafana.tar 2>/dev/null
+tar cf devstats-grafana.tar replacer sqlitedb grafana/shared grafana/img/*.svg grafana/img/*.png grafana/*/change_title_and_icons.sh grafana/*/update_sqlite.sql grafana/dashboards/*/*.json
+docker build -f grafana/shared/Dockerfile -t "${DOCKER_USER}/devstats-grafana" .
+rm -f devstats-grafana.tar
+#docker push "${DOCKER_USER}/devstats-grafana"
--- grafana/shared/grafana_start.sh
+++ grafana/shared/grafana_start.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
-if ( [ -z "$PG_HOST" ] || [ -z "$PG_PORT" ] || [ -z "$PG_PASS" ] || [ -z "$PG_DB" ] || [ -z "$GF_SECURITY_ADMIN_USER" ] || [ -z "$GF_SECURITY_ADMIN_PASSWORD" ] || [ -z "$PROJ" ] )
+if ( [ -z "$ICON" ] || [ -z "$ORGNAME" ] || [ -z "$PG_HOST" ] || [ -z "$PG_PORT" ] || [ -z "$PG_PASS" ] || [ -z "$PG_DB" ] || [ -z "$GF_SECURITY_ADMIN_USER" ] || [ -z "$GF_SECURITY_ADMIN_PASSWORD" ] || [ -z "$PROJ" ] )
 then
-  echo "$0: you need to set PG_HOST=..., PG_PORT=..., PG_PASS=..., PG_DB=..., GF_SECURITY_ADMIN_USER=..., GF_SECURITY_ADMIN_PASSWORD=... and PROJ=..."
+  echo "$0: you need to set PG_HOST=..., PG_PORT=..., PG_PASS=..., PG_DB=..., GF_SECURITY_ADMIN_USER=..., GF_SECURITY_ADMIN_PASSWORD=..., ICON=..., ORGNAME=... and PROJ=..."
   exit 1
 fi
-cwd=`pwd`
-cd /usr/share/grafana
+
+# Patch Grafana per our project
+echo 'Patching Grafana'
+host=`hostname`
+if [ -z "$GA" ]
+then
+  ga=";"
+else
+  ga="google_analytics_ua_id = $GA"
+fi
+cp "grafana/img/$ICON.svg" "/usr/share/grafana/public/img/grafana_icon.svg" || exit 10
+cp "grafana/img/$ICON.svg" "/usr/share/grafana/public/img/grafana_com_auth_icon.svg" || exit 11
+cp "grafana/img/$ICON.svg" "/usr/share/grafana/public/img/grafana_net_logo.svg" || exit 12
+cp "grafana/img/$ICON.svg" "/usr/share/grafana/public/img/grafana_mask_icon.svg" || exit 13
+GRAFANA_DATA="/usr/share/grafana/" ./grafana/$PROJ/change_title_and_icons.sh || exit 14
+cp ./grafana/shared/datasource.yaml.example /usr/share/grafana/conf/provisioning/datasources/datasources.yaml || exit 15
+cfile="/etc/grafana/grafana.ini"
+cp ./grafana/shared/grafana.ini.example "$cfile" || exit 16
+MODE=ss FROM='{{project}}' TO="$PROJ" replacer "$cfile" || exit 17
+MODE=ss FROM='{{url}}' TO="$host" replacer "$cfile" || exit 18
+MODE=ss FROM='{{ga}}' TO="$ga" replacer "$cfile" || exit 19
+MODE=ss FROM='{{org}}' TO="$ORGNAME" replacer "$cfile" || exit 20
+
+# Setup Grafana provisioning
 echo 'Updating provisioning yaml'
 cfile="/usr/share/grafana/conf/provisioning/datasources/datasources.yaml"
 MODE=ss FROM='{{url}}' TO="${PG_HOST}:${PG_PORT}" replacer "$cfile" || exit 2
 MODE=ss FROM='{{PG_PASS}}' TO="${PG_PASS}" replacer "$cfile" || exit 3
 MODE=ss FROM='{{PG_DB}}' TO="${PG_DB}" replacer "$cfile" || exit 4
 MODE=ss FROM='{{PG_USER}}' TO="ro_user" replacer "$cfile" || exit 5
+
 echo 'Starting Grafana'
+cwd=`pwd`
+cd /usr/share/grafana
 GF_SECURITY_ADMIN_USER="${GF_SECURITY_ADMIN_USER}" GF_SECURITY_ADMIN_PASSWORD="${GF_SECURITY_ADMIN_PASSWORD}" grafana-server -config /etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana 1>/var/log/grafana.log 2>/var/log/grafana.err &
 cd "$cwd"
+
+# Wait for start and update its SQLite database after configured provisioning is finished
 n=0
 while true
 do
@@ -39,9 +66,15 @@ do
     break
   fi
 done
+
+# Provision dashboards
 echo 'Provisioning dashboards'
 sqlitedb /var/lib/grafana/grafana.db grafana/dashboards/$PROJ/*.json || exit 8
+
+# Set organization name and home dashboard
 echo 'Provisioning other preferences'
 sqlite3 /var/lib/grafana/grafana.db < grafana/$PROJ/update_sqlite.sql || exit 9
+
+# Switch to already started Grafana
 echo 'OK'
 wait
