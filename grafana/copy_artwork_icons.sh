ef3b5130df04e96b742e67d565699d5309531459
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-11 05:44:59 +0000

    Adding All CDF 3 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -18,6 +18,10 @@ do
   then
     icon="cncf"
   fi
+  if [ "$icon" = "allcdf" ]
+  then
+    icon="cdf"
+  fi
   # TODO: remove when we have icons
   if ( [ "$icon" = "istio" ] || [ "$icon" = "knative" ] )
   then
