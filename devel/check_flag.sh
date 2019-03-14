77c92aa9319be7fe365a6bc668f0a42a1d0f7a3a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-14 08:36:30 +0000

    Processing flags to safely allow import data when cron job is not running, and when importing clear provisioned job so the cron job won't disturb [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/check_flag.sh
+++ devel/check_flag.sh
@@ -9,10 +9,15 @@ then
   echo "$0: you need to provide flag as a second argument"
   exit 2
 fi
+if [ -z "$3" ]
+then
+  echo "$0: you need to provide flag expected value (0 or 1)"
+  exit 3
+fi
 if [ -z "$PG_PASS" ]
 then
   echo "$0: You need to set PG_PASS environment variable to run this script"
-  exit 3
+  exit 4
 fi
 user=gha_admin
 if [ ! -z "${PG_USER}" ]
@@ -20,9 +25,9 @@ then
   user="${PG_USER}"
 fi
 
-exists=`PG_USER="${user}" ./devel/db.sh psql "$1" -tAc "select 1 from gha_computed where metric = '$2'"` || exit 4
-if [ ! "$exists" = "1" ]
+exists=`PG_USER="${user}" ./devel/db.sh psql "$1" -tAc "select 1 from gha_computed where metric = '$2' union select 0 order by 1 desc limit 1"` || exit 5
+if [ ! "$exists" = "$3" ]
 then
-  echo "No $2 flag on $1 database"
-  exit 5
+  echo "$1 expecting $2 to be $3, got $exists"
+  exit 6
 fi
