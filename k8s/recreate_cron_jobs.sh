763e8ebb906073ed25e790a69fe4d2cf8cf37a39
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-15 11:12:22 +0000

    update script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/recreate_cron_jobs.sh
+++ k8s/recreate_cron_jobs.sh
@@ -5,5 +5,5 @@ then
   exit 1
 fi
 ./k8s/delete_devstats_cron_jobs.sh || exit 2
-helm install ./devstats-helm --set skipSecrets=1,skipPV=1,skipBootstrap=1,skipProvisions=1 || exit 3
+helm install ./devstats-helm --set skipSecrets=1,skipPV=1,skipBootstrap=1,skipProvisions=1,skipGrafanas=1,skipServices=1 || exit 3
 echo 'OK'
