0b03861cad34d67587f62d74e75a3d56e10068f4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-15 08:43:55 +0000

    Missing SQLite3 insert column names [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/shared/update_sqlite.sql
+++ grafana/shared/update_sqlite.sql
@@ -1,4 +1,4 @@
 update org set name = '{{org}}' where id = 1;
 insert into star(id, user_id, dashboard_id) select 1, 1, id from dashboard where uid = {{uid}};
-insert into preferences select 1, 1, 1, 0, (select id from dashboard where uid = {{uid}}), '', 'dark', datetime('now'), datetime('now'), 0;
-insert into preferences select 2, 1, 0, 0, (select id from dashboard where uid = {{uid}}), '', 'dark', datetime('now'), datetime('now'), 0;
+insert into preferences(id, org_id, user_id, version, home_dashboard_id, timezone, theme, created, updated) select 1, 1, 1, 0, (select id from dashboard where uid = {{uid}}), '', 'dark', datetime('now'), datetime('now');
+insert into preferences(id, org_id, user_id, version, home_dashboard_id, timezone, theme, created, updated) select 2, 1, 0, 0, (select id from dashboard where uid = {{uid}}), '', 'dark', datetime('now'), datetime('now');
