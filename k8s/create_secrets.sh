7060763ae21dbb165ee17792db6a091978362927
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-21 13:07:44 +0000

    Split kubernetes deployment into devstats-docker-images (will be split more later) [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- k8s/create_secrets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl delete secret pg-db
-kubectl delete secret es-db
-kubectl delete secret github-oauth
-kubectl create secret generic pg-db --from-file=./k8s/secrets/PG_HOST.secret --from-file=./k8s/secrets/PG_PASS.secret --from-file=./k8s/secrets/PG_PASS_RO.secret --from-file=./k8s/secrets/PG_PASS_TEAM.secret --from-file=./k8s/secrets/PG_PORT.secret --from-file=./k8s/secrets/PG_ADMIN_USER.secret
-kubectl create secret generic es-db --from-file=./k8s/secrets/GHA2DB_ES_URL.secret --from-file=./k8s/secrets/ES_PROTO.secret --from-file=./k8s/secrets/ES_HOST.secret --from-file=./k8s/secrets/ES_PORT.secret
-kubectl create secret generic github-oauth --from-file=./k8s/secrets/GHA2DB_GITHUB_OAUTH.secret
