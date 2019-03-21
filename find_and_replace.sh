7c66bb4393e9db54186d94ae7cfe2560c94990c8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-21 05:56:35 +0000

    New util script 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- find_and_replace.sh
+++ find_and_replace.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-if [ -z "$1" ]
-then
-  echo "You need to provide path as first arument"
-  exit 1
-fi
-if [ -z "$2" ]
-then
-  echo "You need to provide file name pattern as a second argument"
-  exit 1
-fi
-if [ -z "$3" ]
-then
-  echo "You need to provide regexp pattern to search for as a third argument"
-  exit 1
-fi
 if [ -z "$FROM" ]
 then
   export FROM="`cat ./FROM`"
@@ -28,6 +13,21 @@ then
 fi
 if [ -z "$FILES" ]
 then
+  if [ -z "$1" ]
+  then
+    echo "You need to provide path as first arument"
+    exit 1
+  fi
+  if [ -z "$2" ]
+  then
+    echo "You need to provide file name pattern as a second argument"
+    exit 2
+  fi
+  if [ -z "$3" ]
+  then
+    echo "You need to provide regexp pattern to search for as a third argument"
+    exit 3
+  fi
   find "$1" -type f -iname "$2" -not -name "out" -not -path '*.git/*' -exec grep -El "$3" "{}" \; > out
   export FILES=`cat out`
 fi
