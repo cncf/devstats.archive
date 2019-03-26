ddab5f46220ae6d3d24ea9dcf3ddd2913d658db5
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-26 15:06:07 +0000

    Support for reimporting affiliations for CDF projects [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/affs_cdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+
+export GHA2DB_PROJECTS_YAML="cdf_projects.yaml"
+
+ONLY="spinnaker tekton jenkins-x" ./devel/all_affs.sh || exit 2
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+
+ONLY="jenkins allcdf" ./devel/all_affs.sh || exit 3
+GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
