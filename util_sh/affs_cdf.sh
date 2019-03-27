cd8a144aeddec6c45654ba624390a20ee5a47529
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-27 10:58:34 +0000

    Fixed typo [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_cdf.sh
+++ util_sh/affs_cdf.sh
@@ -7,7 +7,7 @@ fi
 
 export GHA2DB_PROJECTS_YAML="cdf/projects.yaml"
 
-ONLY="spinnaker tekton jenkins-x" ./devel/all_affs.sh || exit 2
+ONLY="spinnaker tekton jenkinsx" ./devel/all_affs.sh || exit 2
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
 ONLY="jenkins allcdf" ./devel/all_affs.sh || exit 3
