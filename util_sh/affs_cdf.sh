4f1376789ab498d05f446fe276d0a87f7f63a657
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-27 12:08:16 +0000

    Fix broken script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_cdf.sh
+++ util_sh/affs_cdf.sh
@@ -5,10 +5,8 @@ then
   exit 1
 fi
 
-export GHA2DB_PROJECTS_YAML="cdf/projects.yaml"
+GHA2DB_PROJECTS_YAML="cdf/projects.yaml" ONLY="spinnaker tekton jenkinsx" ./devel/all_affs.sh || exit 2
+GHA2DB_PROJECTS_YAML="cdf_projects.yaml" GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
 
-ONLY="spinnaker tekton jenkinsx" ./devel/all_affs.sh || exit 2
-GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
-
-ONLY="jenkins allcdf" ./devel/all_affs.sh || exit 3
-GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
+GHA2DB_PROJECTS_YAML="cdf/projects.yaml" ONLY="jenkins allcdf" ./devel/all_affs.sh || exit 3
+GHA2DB_PROJECTS_YAML="cdf_projects.yaml" GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
