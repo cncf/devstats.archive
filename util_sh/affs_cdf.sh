d8e7c098dc7982f494d47a7c1def2b2dce3964f1
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-27 10:39:28 +0000

    Support CDF import affs [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_cdf.sh
+++ util_sh/affs_cdf.sh
@@ -5,7 +5,7 @@ then
   exit 1
 fi
 
-export GHA2DB_PROJECTS_YAML="cdf_projects.yaml"
+export GHA2DB_PROJECTS_YAML="cdf/projects.yaml"
 
 ONLY="spinnaker tekton jenkins-x" ./devel/all_affs.sh || exit 2
 GHA2DB_RECENT_RANGE="4 hours" GHA2DB_TMOFFSET="-4" devstats
