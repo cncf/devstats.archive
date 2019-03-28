d34641a8260c873975da64874e63c8d82b751c17
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-28 08:03:15 +0000

    Support separate devstats instances for the biggest projects [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/sync_lock.sh
+++ devel/sync_lock.sh
@@ -12,12 +12,14 @@ then
     exit 1
   fi
   wait_for_command.sh devstats 600 || exit 2
-  cronctl.sh devstats off || exit 3
-  cronctl.sh contrib.sh off || exit 4
+  wait_for_command.sh devstats_others 600 || exit 3
+  wait_for_command.sh devstats_kubernetes 600 || exit 4
+  wait_for_command.sh devstats_allprj 600 || exit 5
+  cronctl.sh devstats off || exit 6
   if [ -z "$FROM_WEBHOOK" ]
   then
-    wait_for_command.sh webhook 600 || exit 5
-    cronctl.sh webhook off || exit 6
+    wait_for_command.sh webhook 600 || exit 7
+    cronctl.sh webhook off || exit 8
     killall webhook
   fi
   echo 'All sync and deploy jobs stopped and disabled'
