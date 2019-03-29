0243ff3ac396fab3ec8a6e4f8c239aa6d5744033
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-29 07:10:53 +0000

    Updated check running stuff [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/sync_lock.sh
+++ devel/sync_lock.sh
@@ -11,15 +11,12 @@ then
     echo "another deploy process is running, exiting"
     exit 1
   fi
-  wait_for_command.sh devstats 600 || exit 2
-  wait_for_command.sh devstats_others 600 || exit 3
-  wait_for_command.sh devstats_kubernetes 600 || exit 4
-  wait_for_command.sh devstats_allprj 600 || exit 5
-  cronctl.sh devstats off || exit 6
+  wait_for_command.sh 'devstats,devstats_others,devstats_kubernetes,devstats_allprj' 900 || exit 2
+  cronctl.sh devstats off || exit 3
   if [ -z "$FROM_WEBHOOK" ]
   then
-    wait_for_command.sh webhook 600 || exit 7
-    cronctl.sh webhook off || exit 8
+    wait_for_command.sh webhook 900 || exit 4
+    cronctl.sh webhook off || exit 5
     killall webhook
   fi
   echo 'All sync and deploy jobs stopped and disabled'
