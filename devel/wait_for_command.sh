cee68471c20c9071158c3f897d36404368d5de7d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-29 07:10:53 +0000

    Updated check running stuff [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/wait_for_command.sh
+++ devel/wait_for_command.sh
@@ -5,37 +5,76 @@ then
   exit 0
 fi
 set -o pipefail
+
 if ([ -z "$1" ] || [ -z "$2" ])
 then
   echo "Usage $0 command max_seconds"
+  echo "Usage $0 'command1,command2,..,commandN' max_seconds"
   exit 1
 fi
-command=$1
-pid="/tmp/$command.pid"
+
+commands=${1//,/ }
 trials=0
 maxTrials=$2
+
 while true
 do
-  if [ -e $pid ]
+  info=""
+  running=0
+  all=0
+  for cmd in $commands
+  do
+    pid="/tmp/$cmd.pid"
+    if [ -e $pid ]
+    then
+      if [ -z "$info" ]
+      then
+        info="${cmd} is running: ${pid} exists"
+      else
+        info="${info}, ${cmd} is running: ${pid} exists"
+      fi
+      running=$((running+1))
+    fi
+    all=$((all+1))
+  done
+  if [ "$running" -ge "1" ]
   then
+    info="${info}, ${running}/${all} running"
     if [ "$trials" -eq "0" ]
     then
-      echo "$command is running: $pid exists, waiting"
+      echo "${info}, waiting"
     fi
     sleep 1
     trials=$((trials+1))
     if [ "$trials" -ge "$maxTrials" ]
     then
-      echo "$command is still running: $pid exists, waited $maxTrials seconds, exiting"
+      echo "${info}, waited $maxTrials seconds, exiting"
       exit 1
     fi
   else
     break
   fi
 done
+
+s=0
+for cmd in $commands
+do
+  s=$((s+1))
+done
+
 if [ "$trials" -gt "0" ]
 then
-  echo "$command was running waited $trials seconds"
+  if [ "$s" -ge "2" ]
+  then
+    echo "$1 were running waited $trials seconds"
+  else
+    echo "$1 was running waited $trials seconds"
+  fi
 else
-  echo "$command was not running"
+  if [ "$s" -ge "2" ]
+  then
+    echo "$1 were not running"
+  else
+    echo "$1 was not running"
+  fi
 fi
