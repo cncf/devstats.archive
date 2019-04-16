70755ef63bb06894bdc50f999f1d52a9c010d039
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-16 10:36:27 +0000

    Import affs on special projects too [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/affs_test_all_at_once.sh
+++ util_sh/affs_test_all_at_once.sh
@@ -4,5 +4,5 @@ then
   echo "You need to set PG_PASS environment variable to run this script"
   exit 1
 fi
-./devel/all_affs.sh || exit 2
+GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+knative,+linux,+zephyr" ./devel/all_affs.sh || exit 2
 GHA2DB_RECENT_RANGE="12 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+knative,+linux,+zephyr" devstats
