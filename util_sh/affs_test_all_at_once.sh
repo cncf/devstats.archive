df95fa92bb637b375f49454587d69098b3f508e6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-16 05:11:51 +0000

    All-at-once import affiliations script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/affs_test_all_at_once.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z "${PG_PASS}" ]
+then
+  echo "You need to set PG_PASS environment variable to run this script"
+  exit 1
+fi
+./devel/all_affs.sh || exit 2
+GHA2DB_RECENT_RANGE="12 hours" GHA2DB_TMOFFSET="-4" GHA2DB_PROJECTS_OVERRIDE="+cncf,+opencontainers,+istio,+knative,+linux,+zephyr" devstats
