e02e429571dd080be41d3c939cb1fa4b1e15705e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-29 08:14:41 +0000

    Update commit author using name if that is unique [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/update_commits_actors_data.sql
+++ util_sql/update_commits_actors_data.sql
@@ -1,2 +1,3 @@
+update gha_commits c set author_id = (select an.actor_id from gha_actors_names an where an.name = c.author_name and an.actor_id != 0) where c.author_id is null and c.author_name != '' and (select count(*) from gha_actors_names an where an.name = c.author_name) = 1;
 update gha_commits c set dup_author_login = (select a.login from gha_actors a where a.id = c.author_id) where c.dup_author_login = '' and c.author_id is not null;
 update gha_commits c set dup_committer_login = (select a.login from gha_actors a where a.id = c.committer_id) where c.dup_committer_login = '' and c.committer_id is not null;
