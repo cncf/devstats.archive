f6d5cf4b77ca86b90c7b11259025df2df724b3ca
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-29 08:57:09 +0000

    Reorganize [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/default_postprocess_scripts.sql
+++ util_sql/default_postprocess_scripts.sql
@@ -1,3 +1,4 @@
 insert into gha_postprocess_scripts(ord, path) select 1, 'util_sql/postprocess_texts.sql' on conflict do nothing;
 insert into gha_postprocess_scripts(ord, path) select 2, 'util_sql/postprocess_labels.sql' on conflict do nothing;
 insert into gha_postprocess_scripts(ord, path) select 3, 'util_sql/postprocess_issues_prs.sql' on conflict do nothing;
+insert into gha_postprocess_scripts(ord, path) select 6, 'util_sql/postprocess_commits.sql' on conflict do nothing;
