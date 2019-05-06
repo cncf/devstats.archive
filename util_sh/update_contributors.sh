f9dcc4e751bb1c3a88357c29c11caf204e12ea61
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-06 11:03:48 +0000

    Generated contributors list [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/update_contributors.sh
+++ util_sh/update_contributors.sh
@@ -10,4 +10,5 @@ fi
 ./util_sh/k8s_contributors_and_emails.sh || exit 5
 ./util_sh/top_50_k8s_yearly_contributors.sh || exit 6
 ./util_sh/k8s_yearly_contributors_with_50.sh || exit 7
+zip -9 ~/contrib.zip contributors_and_emails.csv contributing_actors.csv contributing_actors_data.csv k8s_contributors_and_emails.csv top_50_k8s_yearly_contributors.csv k8s_yearly_contributors_with_50.csv || exit 8
 echo 'OK'
