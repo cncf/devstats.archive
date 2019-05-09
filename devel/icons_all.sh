190aac52becc2fea3dda97a8cadc740d6c4ba1b7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-09 04:52:53 +0000

    Update icons script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/icons_all.sh
+++ devel/icons_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./apache/www/copy_icons.sh || exit 1
-./grafana/create_images.sh || exit 2
-./grafana/copy_artwork_icons.sh || exit 3
+./devel/update_artwork.sh || exit 1
+./apache/www/copy_icons.sh || exit 2
+./grafana/create_images.sh || exit 3
+./grafana/copy_artwork_icons.sh || exit 4
 echo 'Icons updated'
