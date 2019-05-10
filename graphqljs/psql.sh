b04a3083e1dc64f7ca4e2fe5e2aab98ba84709d7
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-10 08:22:01 +0000

    Adding GraphQL [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ graphqljs/psql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function finish {
+    sync_unlock.sh
+}
+if [ -z "$TRAP" ]
+then
+  sync_lock.sh || exit -1
+  trap finish EXIT
+  export TRAP=1
+fi
+set -o pipefail
+> errors.txt
+> run.log
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
+./devel/db.sh psql graphqljs -c "create extension if not exists pgcrypto" || exit 1
+./devel/ro_user_grants.sh graphqljs || exit 2
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs GHA2DB_LOCAL=1 gha2db 2015-07-02 0 today now 'graphql/graphql-js' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 8
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 9
+GHA2DB_PROJECT=graphqljs PG_DB=graphqljs GHA2DB_LOCAL=1 vars || exit 10
