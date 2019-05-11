a5847bc4e4a6a86ff5bba55de550e7b2f88cec88
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-11 12:21:29 +0000

    Adding graphiql expressgraphql graphqlspec [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/graphql/repo_groups.sql
@@ -0,0 +1,35 @@
+-- Add repository groups
+-- This is a stub, repo_group = repo name in gRPC
+update
+  gha_repos r
+set
+  alias = coalesce((
+    select e.dup_repo_name
+    from
+      gha_events e
+    where
+      e.repo_id = r.id
+    order by
+      e.created_at desc
+    limit 1
+  ), name)
+;
+update gha_repos set repo_group = alias;
+
+update gha_repos set alias = 'GraphQL JavaScript', repo_group = 'GraphQL JavaScript' where name = 'graphql/graphql-js';
+update gha_repos set alias = 'GraphQL IDE', repo_group = 'GraphQL IDE' where name = 'graphql/graphiql';
+update gha_repos set alias = 'Express GraphQL', repo_group = 'Express GraphQL' where name = 'graphql/express-graphql';
+update gha_repos set alias = 'GraphQL Spec', repo_group = 'GraphQL Spec' where name in ('graphql/graphql-spec', 'facebook/graphql');
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
