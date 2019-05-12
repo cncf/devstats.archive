f299b5e907a971c728df703fd94f4553a355d7ec
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-12 15:12:41 +0000

    Merging master [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/generate_actors_gql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run inside 'devstats' image pod shell:
+if ( [ -z "${PG_PASS}" ] || [ -z "${PG_HOS}T" ] )
+then
+  echo "$0: you need to set PG_HOST=... and PG_PASS=... to run this script"
+  exit 1
+fi
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA graphql < ./util_sql/actors.sql
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA graphqljs < ./util_sql/actors.sql
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA graphiql < ./util_sql/actors.sql
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA expressgraphql < ./util_sql/actors.sql
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA graphqlspec < ./util_sql/actors.sql
--- devel/get_all_sqlite_jsons.sh
+++ devel/get_all_sqlite_jsons.sh
@@ -4,16 +4,16 @@ mkdir sqlite 1>/dev/null 2>/dev/null
 touch sqlite/touch
 for proj in $all
 do
-    db=$proj
-    if [ "$proj" = "kubernetes" ]
-    then
-      db="k8s"
-    fi
-    echo "Project: $proj, GrafanaDB: $db"
-    rm -f sqlite/* 2>/dev/null
-    touch sqlite/touch
-    sqlitedb /var/lib/grafana.$db/grafana.db || exit 1
-    rm -f grafana/dashboards/$proj/*.json || exit 2
-    mv sqlite/*.json grafana/dashboards/$proj/ || exit 3
+  db=$proj
+  if [ "$proj" = "kubernetes" ]
+  then
+    db="k8s"
+  fi
+  echo "Project: $proj, GrafanaDB: $db"
+  rm -f sqlite/* 2>/dev/null
+  touch sqlite/touch
+  sqlitedb /var/lib/grafana.$db/grafana.db || exit 1
+  rm -f grafana/dashboards/$proj/*.json || exit 2
+  mv sqlite/*.json grafana/dashboards/$proj/ || exit 3
 done
 echo 'OK'
