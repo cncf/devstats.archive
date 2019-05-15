f3e0e21bba805276b35d5772df40460d464a661c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-15 11:41:37 +0000

    Fix situation where there are no actors countries set [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/countries_tags.sql
+++ metrics/shared/countries_tags.sql
@@ -5,4 +5,13 @@ from
 where
   country_name is not null
   and country_name != ''
+union select 'None'
+where (
+  select count(country_name)
+  from
+    gha_actors
+  where
+    country_name is not null
+    and country_name != ''
+  ) = 0;
 ;
