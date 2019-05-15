e0514d375c454e3c0c9055c83c5408c26dfcdaec
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-15 08:28:32 +0000

    Fix grafana health check [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/shared/grafana_health_check.sh
+++ grafana/shared/grafana_health_check.sh
@@ -4,4 +4,4 @@ then
   echo "$0: need to specify server url as an argument"
   exit 1
 fi
-wget -qO- "https://${1}/public/img/grafana_icon.svg" | grep svg
+wget -qO- "${1}/public/img/grafana_icon.svg" | grep svg
