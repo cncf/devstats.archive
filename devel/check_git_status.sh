b9f924355b423511daf1fde7e68ed53bcfca54b8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-31 11:02:48 +0000

    list new project [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/check_git_status.sh
+++ devel/check_git_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-for f in devstats devstatscode devstats-docker-images devstats-docker-lf devstats-example devstats-helm-lf devstats-k8s-lf devstats-reports devstats-helm-example devstats-helm-graphql devstats-kubernetes-dashboard json2hat-helm
+for f in devstats devstatscode devstats-docker-images devstats-docker-lf devstats-example devstats-helm devstats-helm-lf devstats-k8s-lf devstats-reports devstats-helm-example devstats-helm-graphql devstats-kubernetes-dashboard json2hat-helm
 do
   cd $f
   echo ">>>>> status: $f <<<<<"
