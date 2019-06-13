e223602aada9c83f1a70c7a06c693a0c3fd5ba81
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-13 15:22:20 +0000

    Scripts to generate non-LF actors [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/k8s_generate_actors_nonlf.sh
+++ devel/k8s_generate_actors_nonlf.sh
@@ -10,4 +10,4 @@ then
   exit 1
 fi
 ts=`date +'%s%N'`
-kubectl run --env="PG_HOST=${PG_HOST}" --env="PG_PASS=${PG_PASS}" -i --tty "devstats-actors-${ts}" --restart=Never --rm --image="lukaszgryglicki/devstats-test" --command "./devel/generate_actors_lf.sh"
+kubectl run --env="PG_HOST=${PG_HOST}" --env="PG_PASS=${PG_PASS}" -i --tty "devstats-actors-${ts}" --restart=Never --rm --image="lukaszgryglicki/devstats-test" --command "./devel/generate_actors_nonlf.sh"
