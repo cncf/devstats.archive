dc7e543401126fdd4b06ce98c15182037fc21a24
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-06-19 05:45:32 +0000

    Add missing org [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- hyperledger/psql.sh
+++ hyperledger/psql.sh
@@ -14,7 +14,7 @@ set -o pipefail
 GHA2DB_PROJECT=hyperledger PG_DB=hyperledger GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql hyperledger -c "create extension if not exists pgcrypto" || exit 1
 ./devel/ro_user_grants.sh hyperledger || exit 2
-GHA2DB_PROJECT=hyperledger PG_DB=hyperledger GHA2DB_LOCAL=1 gha2db 2015-01-01 0 today now 'hyperledger,hyperledger-labs' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=hyperledger PG_DB=hyperledger GHA2DB_LOCAL=1 gha2db 2015-01-01 0 today now 'hyperledger,hyperledger-labs,hyperledger-cicd' 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=hyperledger PG_DB=hyperledger GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=hyperledger PG_DB=hyperledger ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=hyperledger PG_DB=hyperledger ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
