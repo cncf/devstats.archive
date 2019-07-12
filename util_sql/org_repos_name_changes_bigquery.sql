7aa3c42712ae8ba707df7df8d7060da6193b47f8
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-12 08:33:18 +0000

    Update repo finder bigquery [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/org_repos_name_changes_bigquery.sql
+++ util_sql/org_repos_name_changes_bigquery.sql
@@ -6,24 +6,26 @@ select
   max(created_at) as date_to
 from (
   select * from
-    [githubarchive:month.201807],
-    [githubarchive:month.201806],
-    [githubarchive:month.201805],
-    [githubarchive:month.201804],
-    [githubarchive:month.201803],
-    [githubarchive:month.201802],
-    [githubarchive:month.201801],
+    [githubarchive:month.201906],
+    [githubarchive:month.201905],
+    [githubarchive:month.201904],
+    [githubarchive:month.201903],
+    [githubarchive:month.201902],
+    [githubarchive:month.201901],
+    [githubarchive:year.2018],
     [githubarchive:year.2017],
     [githubarchive:year.2016],
     [githubarchive:year.2015],
-    [githubarchive:year.2014]
+    [githubarchive:year.2014],
+    [githubarchive:year.2013],
+    [githubarchive:year.2012]
  )
 where
   repo.id in (
     select
       repo.id
     from
-      [githubarchive:month.201802]
+      [githubarchive:month.201906]
     where
       repo.name like 'org_name/%'
     group by
