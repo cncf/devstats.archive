0aceb23fdeea5eb9bbe7387c00504b085737841a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-16 04:52:49 +0000

    Make contributors script able to run on k8s and bare metal deployments [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/k8s_yearly_contributors_with_50.sh
+++ util_sh/k8s_yearly_contributors_with_50.sh
@@ -4,4 +4,8 @@ then
   echo "$0: you need to specify PG_PASS env variable"
   exit 1
 fi
-GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_HOST=teststats.cncf.io PG_DB=gha GHA2DB_CSVOUT="k8s_yearly_contributors_with_50.csv" runq ./util_sql/k8s_yearly_contributors_with_50.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
+if [ -z "$PG_HOST" ]
+then
+  export PG_HOST=teststats.cncf.io
+fi
+GHA2DB_LOCAL=1 GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 PG_DB=gha GHA2DB_CSVOUT="k8s_yearly_contributors_with_50.csv" runq ./util_sql/k8s_yearly_contributors_with_50.sql {{exclude_bots}} "`cat util_sql/exclude_bots.sql`"
