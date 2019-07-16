634e9ed1df7afa598f780d8e83ead221ff79470f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-16 05:08:38 +0000

    Add NOZIP option [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/update_contributors.sh
+++ util_sh/update_contributors.sh
@@ -10,5 +10,8 @@ fi
 ./util_sh/k8s_contributors_and_emails.sh || exit 5
 ./util_sh/top_50_k8s_yearly_contributors.sh || exit 6
 ./util_sh/k8s_yearly_contributors_with_50.sh || exit 7
-zip -9 ~/contrib.zip contributors_and_emails.csv contributing_actors.csv contributing_actors_data.csv k8s_contributors_and_emails.csv top_50_k8s_yearly_contributors.csv k8s_yearly_contributors_with_50.csv || exit 8
+if [ -z "$NOZIP" ]
+then
+  zip -9 ~/contrib.zip contributors_and_emails.csv contributing_actors.csv contributing_actors_data.csv k8s_contributors_and_emails.csv top_50_k8s_yearly_contributors.csv k8s_yearly_contributors_with_50.csv || exit 8
+fi
 echo 'OK'
