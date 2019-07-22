cf031029d90f142dcfcf341432fe68906888578f
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-22 06:51:49 +0000

    Thanos moved its main repo to the new org 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ scripts/thanos/repo_groups.sql
@@ -0,0 +1,37 @@
+-- Add repository groups
+update
+  gha_repos r
+set
+  alias = coalesce((
+    select e.dup_repo_name
+    from
+      gha_events e
+    where
+      e.repo_id = r.id
+    order by
+      e.created_at desc
+    limit 1
+  ), name)
+;
+update gha_repos set repo_group = alias;
+
+update gha_repos
+set repo_group = 'Thanos', alias = 'Thanos'
+where name in (
+  'improbable-eng/promlts',
+  'improbable-eng/thanos',
+  'thanos-io/thanos'
+);
+
+select
+  repo_group,
+  count(*) as number_of_repos
+from
+  gha_repos
+where
+  repo_group is not null
+group by
+  repo_group
+order by
+  number_of_repos desc,
+  repo_group asc;
