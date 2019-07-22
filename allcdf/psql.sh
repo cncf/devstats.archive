1caa9b2f8699cdc00e97c0b607716e3f0453b16c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-22 06:10:33 +0000

    Add support for non-merge mode for 'All CDF' project [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- allcdf/psql.sh
+++ allcdf/psql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# MERGE_MODE=1 (use merge DBs mode instead of generating data via 'gha2db')
 function finish {
     sync_unlock.sh
 }
@@ -13,12 +14,18 @@ set -o pipefail
 > run.log
 GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_MGETC=y structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql allcdf -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_INPUT_DBS="spinnaker,tekton,jenkins,jenkinsx" GHA2DB_OUTPUT_DB="allcdf" merge_dbs || exit 2
-GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
-GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 5
-GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 6
-GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
-GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_EXCLUDE_VARS="projects_health_partial_html" vars || exit 8
-./devel/ro_user_grants.sh allcdf || exit 10
-./devel/psql_user_grants.sh devstats_team allcdf || exit 11
+if [ ! -z "$MERGE_MODE" ]
+then
+  GHA2DB_INPUT_DBS="spinnaker,tekton,jenkins,jenkinsx" GHA2DB_OUTPUT_DB="allcdf" merge_dbs || exit 2
+else
+  GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 gha2db 2015-01-01 0 today now 'spinnaker,tektoncd,jenkinsci,jenkins-infra,jenkins-x,knative/build' 2>>errors.txt | tee -a run.log || exit 3
+  GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 gha2db 2012-07-01 0 2014-12-31 23 'jenkinsci/jenkins,jenkins,jenkins-infra' 2>>errors.txt | tee -a run.log || exit 4
+fi
+GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
+GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
+GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 8
+GHA2DB_PROJECT=allcdf PG_DB=allcdf ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 9
+GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_EXCLUDE_VARS="projects_health_partial_html" vars || exit 10
+./devel/ro_user_grants.sh allcdf || exit 11
+./devel/psql_user_grants.sh devstats_team allcdf || exit 12
