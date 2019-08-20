3a6deb917dc3abb01949b04009a1b04dcd943e01
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-08-20 05:20:37 +0000

    Use client level copy instead of DB level copy which has a lot more permissions issues [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/restore_artificial.sh
+++ devel/restore_artificial.sh
@@ -19,14 +19,14 @@ rm -f $db.tar* || exit 2
 cp /var/www/html/$db.tar.xz . || exit 3
 xz -d $db.tar.xz || exit 4
 tar xf $db.tar || exit 5
-./devel/db.sh psql $db -tAc "copy gha_events from '/tmp/$db.events.tsv'" || exit 6
-./devel/db.sh psql $db -tAc "copy gha_payloads from '/tmp/$db.payloads.tsv'" || exit 7
-./devel/db.sh psql $db -tAc "copy gha_issues from '/tmp/$db.issues.tsv'" || exit 8
-./devel/db.sh psql $db -tAc "copy gha_pull_requests from '/tmp/$db.prs.tsv'" || exit 9
-./devel/db.sh psql $db -tAc "copy gha_milestones from '/tmp/$db.milestones.tsv'" || exit 10
-./devel/db.sh psql $db -tAc "copy gha_issues_labels from '/tmp/$db.labels.tsv'" || exit 11
-./devel/db.sh psql $db -tAc "copy gha_issues_assignees from '/tmp/$db.issue_assignees.tsv'" || exit 12
-./devel/db.sh psql $db -tAc "copy gha_pull_requests_assignees from '/tmp/$db.pr_assignees.tsv'" || exit 13
-./devel/db.sh psql $db -tAc "copy gha_pull_requests_requested_reviewers from '/tmp/$db.pr_reviewers.tsv'" || exit 14
-./devel/db.sh psql $db -tAc "copy gha_issues_events_labels from '/tmp/$db.issues_events_labels.tsv'" || exit 15
-./devel/db.sh psql $db -tAc "copy gha_texts from '/tmp/$db.texts.tsv'" || exit 16
+./devel/db.sh psql $db -tAc "\copy gha_events from '/tmp/$db.events.tsv'" || exit 6
+./devel/db.sh psql $db -tAc "\copy gha_payloads from '/tmp/$db.payloads.tsv'" || exit 7
+./devel/db.sh psql $db -tAc "\copy gha_issues from '/tmp/$db.issues.tsv'" || exit 8
+./devel/db.sh psql $db -tAc "\copy gha_pull_requests from '/tmp/$db.prs.tsv'" || exit 9
+./devel/db.sh psql $db -tAc "\copy gha_milestones from '/tmp/$db.milestones.tsv'" || exit 10
+./devel/db.sh psql $db -tAc "\copy gha_issues_labels from '/tmp/$db.labels.tsv'" || exit 11
+./devel/db.sh psql $db -tAc "\copy gha_issues_assignees from '/tmp/$db.issue_assignees.tsv'" || exit 12
+./devel/db.sh psql $db -tAc "\copy gha_pull_requests_assignees from '/tmp/$db.pr_assignees.tsv'" || exit 13
+./devel/db.sh psql $db -tAc "\copy gha_pull_requests_requested_reviewers from '/tmp/$db.pr_reviewers.tsv'" || exit 14
+./devel/db.sh psql $db -tAc "\copy gha_issues_events_labels from '/tmp/$db.issues_events_labels.tsv'" || exit 15
+./devel/db.sh psql $db -tAc "\copy gha_texts from '/tmp/$db.texts.tsv'" || exit 16
