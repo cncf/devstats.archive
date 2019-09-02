3f6b2c62e7cd1ee822718bda07f38116e6d87a37
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-02 08:59:35 +0000

    Issues opened/closed -> issues/PRs split [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/issues_opened.sql
+++ metrics/shared/issues_opened.sql
@@ -6,6 +6,7 @@ from
 where
   created_at >= '{{from}}'
   and created_at < '{{to}}'
+  and is_pull_request = false
 union select 'iopened,' || r.repo_group as name,
   round(count(distinct i.id) / {{n}}, 2) as cnt
 from
@@ -18,6 +19,30 @@ where
   and r.repo_group is not null
   and i.created_at >= '{{from}}'
   and i.created_at < '{{to}}'
+  and is_pull_request = false
+group by
+  r.repo_group
+union select 'propened,All' as name,
+  round(count(distinct id) / {{n}}, 2) as cnt
+from
+  gha_issues
+where
+  created_at >= '{{from}}'
+  and created_at < '{{to}}'
+  and is_pull_request = true
+union select 'propened,' || r.repo_group as name,
+  round(count(distinct i.id) / {{n}}, 2) as cnt
+from
+  gha_issues i,
+  gha_repos r
+where
+  i.dup_repo_id = r.id
+  and i.dup_repo_name = r.name
+  and r.name in (select repo_name from trepos)
+  and r.repo_group is not null
+  and i.created_at >= '{{from}}'
+  and i.created_at < '{{to}}'
+  and is_pull_request = true
 group by
   r.repo_group
 order by
