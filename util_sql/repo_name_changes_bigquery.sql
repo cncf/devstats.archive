849501561adba7dac4c4cef3f18b082c8849e758
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-03 05:38:48 +0000

    Update org/repo finder [no deploy]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/repo_name_changes_bigquery.sql
+++ util_sql/repo_name_changes_bigquery.sql
@@ -6,6 +6,7 @@ select
   min(created_at) as date_from,
   max(created_at) as date_to
 from
+  [githubarchive:month.201908],
   [githubarchive:month.201907],
   [githubarchive:month.201906],
   [githubarchive:month.201905],
@@ -25,7 +26,7 @@ where
     select
       repo.id
     from
-      [githubarchive:month.201907]
+      [githubarchive:month.201908]
     where
       repo.name = '{{org_repo}}'
     group by
