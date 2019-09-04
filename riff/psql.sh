1ea98b24669464369ca039663b5d6801c01020c4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-04 17:39:10 +0000

    Fix wrong org name [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- riff/psql.sh
+++ riff/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=riff PG_DB=riff GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql riff -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=riff PG_DB=riff GHA2DB_LOCAL=1 gha2db 2017-07-28 0 today now 'riff,markfisher/sk8s,jldec/projectriff.io,pivotal-cf/pfs-system-test,ericbottard/sk8s-go,ericbottard/sk8s-types,ericbottard/kubernetes-model,ericbottard/sk8s-http-gateway,ericbottard/function-sidecar,ericbottard/sk8s-topic-controller' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=riff PG_DB=riff GHA2DB_LOCAL=1 gha2db 2017-07-28 0 today now 'projectriff,markfisher/sk8s,jldec/projectriff.io,pivotal-cf/pfs-system-test,ericbottard/sk8s-go,ericbottard/sk8s-types,ericbottard/kubernetes-model,ericbottard/sk8s-http-gateway,ericbottard/function-sidecar,ericbottard/sk8s-topic-controller' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=riff PG_DB=riff GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=riff PG_DB=riff ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=riff PG_DB=riff ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
