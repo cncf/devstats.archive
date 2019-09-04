354f266b07f07f6053fe26ccf2303444621a130d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-04 13:51:44 +0000

    Fix bug [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- fn/psql.sh
+++ fn/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=fn PG_DB=fn GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql fn -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=fn PG_DB=fn GHA2DB_LOCAL=1 gha2db 2017-07-25 0 today now 'fn,fnhub/katacoda-scenarios,treeder/fn-plugin-example,treeder/fn-ext-dns,treeder/fn-ext-example,treeder/fn-ext-auth' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=fn PG_DB=fn GHA2DB_LOCAL=1 gha2db 2017-07-25 0 today now 'fnproject,fnhub/katacoda-scenarios,treeder/fn-plugin-example,treeder/fn-ext-dns,treeder/fn-ext-example,treeder/fn-ext-auth' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=fn PG_DB=fn GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=fn PG_DB=fn ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=fn PG_DB=fn ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
