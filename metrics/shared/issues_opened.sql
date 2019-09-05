e4ca2db8c1979e9eed268ef4c1d674536a33c682
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-05 07:41:20 +0000

    Fix PR opened/closed calculations [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/issues_opened.sql
+++ metrics/shared/issues_opened.sql
@@ -25,24 +25,22 @@ group by
 union select 'propened,All' as name,
   round(count(distinct id) / {{n}}, 2) as cnt
 from
-  gha_issues
+  gha_pull_requests
 where
   created_at >= '{{from}}'
   and created_at < '{{to}}'
-  and is_pull_request = true
 union select 'propened,' || r.repo_group as name,
-  round(count(distinct i.id) / {{n}}, 2) as cnt
+  round(count(distinct pr.id) / {{n}}, 2) as cnt
 from
-  gha_issues i,
+  gha_pull_requests pr,
   gha_repos r
 where
-  i.dup_repo_id = r.id
-  and i.dup_repo_name = r.name
+  pr.dup_repo_id = r.id
+  and pr.dup_repo_name = r.name
   and r.name in (select repo_name from trepos)
   and r.repo_group is not null
-  and i.created_at >= '{{from}}'
-  and i.created_at < '{{to}}'
-  and is_pull_request = true
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
 group by
   r.repo_group
 order by
