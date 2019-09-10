c701a163ee5443a4427250a6243de567a7544606
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-10 07:39:04 +0000

    Hist Reviewers metric updated to get affiliations info [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/hist_reviewers.sql
+++ metrics/kubernetes/hist_reviewers.sql
@@ -15,11 +15,12 @@ with matching as (
 )
 select
   'hdev_reviews,' || sub.repo_group || '_All' as metric ,
-  sub.actor,
+  sub.actor || '$$$' || sub.company as actor_and_company,
   count(distinct sub.id) as reviews
 from (
   select coalesce(ecf.repo_group, r.repo_group) as repo_group,
     e.dup_actor_login as actor,
+    coalesce(aa.company_name, '') as company,
     e.id
   from
     gha_repos r,
@@ -28,6 +29,12 @@ from (
     gha_events_commits_files ecf
   on
     ecf.event_id = e.id
+  left join
+    gha_actors_affiliations aa
+  on
+    aa.actor_id = e.actor_id
+    and aa.dt_from <= e.created_at
+    and aa.dt_to > e.created_at
   where
     e.repo_id = r.id
     and e.dup_repo_name = r.name
@@ -49,16 +56,23 @@ where
   sub.repo_group is not null
 group by
   sub.repo_group,
-  sub.actor
+  sub.actor,
+  sub.company
 having
   count(distinct sub.id) >= 1
 union select 'hdev_reviews,All_All' as metric,
-  dup_actor_login as actor,
-  count(distinct id) as reviews
+  e.dup_actor_login || '$$$' || coalesce(aa.company_name, '') as actor_and_company,
+  count(distinct e.id) as reviews
 from
-  gha_events
+  gha_events e
+left join
+  gha_actors_affiliations aa
+on
+  aa.actor_id = e.actor_id
+  and aa.dt_from <= e.created_at
+  and aa.dt_to > e.created_at
 where
-  id in (
+  e.id in (
     select min(event_id)
     from
       gha_issues_events_labels
@@ -72,16 +86,18 @@ where
   )
   and (lower(dup_actor_login) {{exclude_bots}})
 group by
-  dup_actor_login
+  e.dup_actor_login,
+  aa.company_name
 having
   count(distinct id) >= 1
 union select 'hdev_reviews,' || sub.repo_group || '_' || sub.country as metric,
-  sub.actor,
+  sub.actor || '$$$' || sub.company as actor_and_company,
   count(distinct sub.id) as reviews
 from (
   select coalesce(ecf.repo_group, r.repo_group) as repo_group,
     a.country_name as country,
     a.login as actor,
+    coalesce(aa.company_name, '') as company,
     e.id
   from
     gha_actors a,
@@ -91,6 +107,12 @@ from (
     gha_events_commits_files ecf
   on
     ecf.event_id = e.id
+  left join
+    gha_actors_affiliations aa
+  on
+    aa.actor_id = e.actor_id
+    and aa.dt_from <= e.created_at
+    and aa.dt_to > e.created_at
   where
     (e.actor_id = a.id or e.dup_actor_login = a.login)
     and e.repo_id = r.id
@@ -115,15 +137,22 @@ where
 group by
   sub.country,
   sub.repo_group,
-  sub.actor
+  sub.actor,
+  sub.company
 having
   count(distinct sub.id) >= 1
 union select 'hdev_reviews,All_' || a.country_name as metric,
-  a.login as actor,
+  a.login || '$$$' || coalesce(aa.company_name, '') as actor_and_company,
   count(distinct e.id) as reviews
 from
   gha_actors a,
   gha_events e
+left join
+  gha_actors_affiliations aa
+on
+  aa.actor_id = e.actor_id
+  and aa.dt_from <= e.created_at
+  and aa.dt_to > e.created_at
 where
   (e.actor_id = a.id or e.dup_actor_login = a.login)
   and e.id in (
@@ -142,11 +171,12 @@ where
   and a.country_name is not null
 group by
   a.country_name,
-  a.login
+  a.login,
+  aa.company_name
 having
   count(distinct e.id) >= 1
 order by
   reviews desc,
   metric asc,
-  actor asc
+  actor_and_company asc
 ;
