46af3d6b9877f21febac6a25de1df8faf467fcb0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-16 09:11:33 +0000

    Add current companies.yaml SHA to mark it as imported [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/parsed_1.sql
+++ util_sql/parsed_1.sql
@@ -5,3 +5,4 @@ create table public.gha_imported_shas(
 alter table public.gha_imported_shas owner to gha_admin;
 alter table only public.gha_imported_shas add constraint gha_imported_shas_pkey primary key(sha);
 insert into gha_imported_shas(sha) select '14c6ea2be153c24af67734899d2178fe16ef7d937abb92c3c4f346eafd81138c';
+insert into gha_imported_shas(sha) select 'f772a5c56fe3bbaae5d05ff53fffb8b6480de47d5a6fe10f0a1dc09152ef42fd';
