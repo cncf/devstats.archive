76e14934cb4156e9e2286243dd1317d3d43f3607
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-26 13:24:35 +0000

    Add OpenEBS backup script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ devel/backup_from_openebs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+git pull || exit 1
+. ./devel/all_dbs.sh || exit 2
+# found=`find /var/www/html -iname "*.tar.xz"` || exit 3
+found=`find /var/openebs/local -iname "*.tar.xz"` || exit 3
+mkdir ~/backups 1>/dev/null 2>/dev/null
+for db in $all
+do
+  db="$db.tar.xz"
+  hit=''
+  for f in $found
+  do
+    fa=(${f//\// })
+    f2=${fa[-1]}
+    if [ "$db" = "$f2" ]
+    then
+      hit="$f"
+      break
+    fi
+  done
+  if [ -z "$hit" ]
+  then
+    echo "$db backup not found"
+  else
+    echo "copying $f"
+    cp "$f" ~/backups/ || exit 4
+  fi
+done
