5c0198abe5a4ca4f06f7cc03e561a3235d92d5b4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-09-26 13:49:55 +0000

    handle backing-up only artificial events [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- cron/cron_db_backup.sh
+++ cron/cron_db_backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# ONLY_ARTIFICIAL=1 - only backup artificial events
 if [ ! -z "${NOBACKUP}" ]
 then
   exit 0
 fi
 echo "Backup start:" && date
-db.sh pg_dump -Fc $1 -f /tmp/$1.dump || exit 1
-mv /tmp/$1.dump /var/www/html/ || exit 2
+if [ -z "$ONLY_ARTIFICIAL" ]
+then
+  db.sh pg_dump -Fc $1 -f /tmp/$1.dump || exit 1
+  mv /tmp/$1.dump /var/www/html/ || exit 2
+fi
 backup_artificial.sh "$1" || exit 3
 date && echo "Backup OK"
 
