fc92882bb5f15122726927bb7b8a56caea848aea
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-17 12:52:41 +0000

    Update util script [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/delete_all_range.sql
+++ util_sql/delete_all_range.sql
@@ -1,3 +1,6 @@
+delete from gha_pull_requests_assignees where event_id in (select id from gha_events where created_at > '{{from}}' and created_at < '{{to}}');
+delete from gha_pull_requests_requested_reviewers where event_id in (select id from gha_events where created_at > '{{from}}' and created_at < '{{to}}');
+delete from gha_issues_assignees where event_id in (select id from gha_events where created_at > '{{from}}' and created_at < '{{to}}');
 delete from gha_events where created_at > '{{from}}' and created_at < '{{to}}';
 delete from gha_assets where dup_created_at > '{{from}}' and dup_created_at < '{{to}}';
 delete from gha_branches where dup_created_at > '{{from}}' and dup_created_at < '{{to}}';
