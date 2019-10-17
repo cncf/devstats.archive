18bddf536bc362230ff7888340030311b36a3475
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-17 10:13:57 +0000

    Adding CII 6 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/cii/opencontainers/grafana_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /usr/share/grafana.opencontainers
-grafana-server -config /etc/grafana.opencontainers/grafana.ini cfg:default.paths.data=/var/lib/grafana.opencontainers 1>/var/log/grafana.opencontainers.log 2>&1
