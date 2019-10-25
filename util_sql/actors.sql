67150e42edca220a239f946b7f66e06474d55c3c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-25 07:54:37 +0000

    Update actors.sql to also include commit's authors and committers in addition to actors/pushers [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/actors.sql
+++ util_sql/actors.sql
@@ -2,18 +2,35 @@ select
   -- string_agg(sub.actor, ',')
   sub.actor
 from (
-  select distinct a.login as actor
-  from
-    gha_events e,
-    gha_actors a
-  where
-    (e.actor_id = a.id or e.dup_actor_login = a.login)
-    and e.type in (
-      'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
-      'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
-    )
-    and lower(a.login) not like all(array['angular-builds', 'devstats-sync', 'googlebot', 'coveralls', 'rktbot', 'coreosbot', 'web-flow', 'prometheus-roobot', 'cncf-bot', 'kernelprbot', 'istio-testing', 'spinnakerbot', 'pikbot', 'spinnaker-release', 'docker-jenkins', 'jenkins-x-bot', 'golangcibot', 'opencontrail-ci-admin', 'titanium-octobot', 'asfgit', 'travis%bot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%', '%clabot%', '%cla-bot%', '%-gerrit', '%-bot-%', '%envoy-filter-example%'])
+  select distinct sub2.actor
+  from (
+    select a.login as actor
+    from
+      gha_events e,
+      gha_actors a
+    where
+      (e.actor_id = a.id or e.dup_actor_login = a.login)
+      and e.type in (
+        'PullRequestReviewCommentEvent', 'PushEvent', 'PullRequestEvent',
+        'IssuesEvent', 'IssueCommentEvent', 'CommitCommentEvent'
+      )
+      and lower(a.login) not like all(array['angular-builds', 'devstats-sync', 'googlebot', 'coveralls', 'rktbot', 'coreosbot', 'web-flow', 'prometheus-roobot', 'cncf-bot', 'kernelprbot', 'istio-testing', 'spinnakerbot', 'pikbot', 'spinnaker-release', 'docker-jenkins', 'jenkins-x-bot', 'golangcibot', 'opencontrail-ci-admin', 'titanium-octobot', 'asfgit', 'travis%bot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%', '%clabot%', '%cla-bot%', '%-gerrit', '%-bot-%', '%envoy-filter-example%'])
+    union select a.login as actor
+    from
+      gha_commits c,
+      gha_actors a
+    where
+      (c.committer_id = a.id or c.dup_committer_login = a.login)
+      and lower(a.login) not like all(array['angular-builds', 'devstats-sync', 'googlebot', 'coveralls', 'rktbot', 'coreosbot', 'web-flow', 'prometheus-roobot', 'cncf-bot', 'kernelprbot', 'istio-testing', 'spinnakerbot', 'pikbot', 'spinnaker-release', 'docker-jenkins', 'jenkins-x-bot', 'golangcibot', 'opencontrail-ci-admin', 'titanium-octobot', 'asfgit', 'travis%bot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%', '%clabot%', '%cla-bot%', '%-gerrit', '%-bot-%', '%envoy-filter-example%'])
+    union select a.login as actor
+    from
+      gha_commits c,
+      gha_actors a
+    where
+      (c.author_id = a.id or c.dup_author_login = a.login)
+      and lower(a.login) not like all(array['angular-builds', 'devstats-sync', 'googlebot', 'coveralls', 'rktbot', 'coreosbot', 'web-flow', 'prometheus-roobot', 'cncf-bot', 'kernelprbot', 'istio-testing', 'spinnakerbot', 'pikbot', 'spinnaker-release', 'docker-jenkins', 'jenkins-x-bot', 'golangcibot', 'opencontrail-ci-admin', 'titanium-octobot', 'asfgit', 'travis%bot', 'k8s-%', '%-bot', '%-robot', 'bot-%', 'robot-%', '%[bot]%', '%-jenkins', '%-ci%bot', '%-testing', 'codecov-%', '%clabot%', '%cla-bot%', '%-gerrit', '%-bot-%', '%envoy-filter-example%'])
+    ) sub2
   order by
-    actor asc
+    sub2.actor asc
   ) sub
 ;
