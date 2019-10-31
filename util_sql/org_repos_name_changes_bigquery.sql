1a68574cbc48b2c01773921f4ced4f6c4b6ecce2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-31 05:33:46 +0000

    Update repo/org finder scripts [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/org_repos_name_changes_bigquery.sql
+++ util_sql/org_repos_name_changes_bigquery.sql
@@ -6,6 +6,10 @@ select
   max(created_at) as date_to
 from (
   select * from
+    [githubarchive:month.201910],
+    [githubarchive:month.201909],
+    [githubarchive:month.201908],
+    [githubarchive:month.201907],
     [githubarchive:month.201906],
     [githubarchive:month.201905],
     [githubarchive:month.201904],
@@ -25,7 +29,9 @@ where
     select
       repo.id
     from
-      [githubarchive:month.201906]
+      [githubarchive:month.201910],
+      [githubarchive:month.201909],
+      [githubarchive:month.201908]
     where
       repo.name like 'org_name/%'
     group by
--- util_sql/repo_name_changes_bigquery.sql
+++ util_sql/repo_name_changes_bigquery.sql
@@ -8,6 +8,7 @@ select
   max(created_at) as date_to
 from
   -- TABLE_DATE_RANGE([githubarchive:day.], TIMESTAMP('2018-01-01'), TIMESTAMP('2019-08-01'))
+  [githubarchive:month.201910],
   [githubarchive:month.201909],
   [githubarchive:month.201908],
   [githubarchive:month.201907],
@@ -30,6 +31,7 @@ where
       repo.id
     from
       -- TABLE_DATE_RANGE([githubarchive:day.], TIMESTAMP('2018-01-01'), TIMESTAMP('2019-08-01'))
+      [githubarchive:month.201910],
       [githubarchive:month.201909],
       [githubarchive:month.201908],
       [githubarchive:month.201907],
