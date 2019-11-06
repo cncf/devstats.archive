f7dc9d5ba7389788720e2ca5e775b7f60d13b6ff
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-11-06 10:12:19 +0000

    Add env config for some metrics [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/drop_tsdb_affs_tables.sh
+++ devel/drop_tsdb_affs_tables.sh
@@ -8,7 +8,6 @@ fi
 ./devel/drop_ts_tables_staring_with.sh "$1" "scompany_activity"
 ./devel/drop_ts_tables_staring_with.sh "$1" "snum_stats"
 ./devel/drop_ts_tables_staring_with.sh "$1" "shcom"
-./devel/drop_ts_tables_staring_with.sh "$1" "ssex"
 ./devel/drop_ts_tables_staring_with.sh "$1" "scountries"
 ./devel/drop_ts_tables_staring_with.sh "$1" "scompany_prs"
 ./devel/drop_ts_tables_staring_with.sh "$1" "shdev"
@@ -19,5 +18,6 @@ fi
 
 if [ ! "$1" = "gha" ]
 then
+  ./devel/drop_ts_tables_staring_with.sh "$1" "ssex"
   ./devel/drop_ts_tables_staring_with.sh "$1" "shpr_comps"
 fi
