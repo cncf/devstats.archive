8dd92e7355a8da3730954250577f9a36a05f93d9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-02 11:30:38 +0000

    Update dates [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/repo_name_changes_bigquery.sql
+++ util_sql/repo_name_changes_bigquery.sql
@@ -8,6 +8,7 @@ select
   max(created_at) as date_to
 from
   -- TABLE_DATE_RANGE([githubarchive:day.], TIMESTAMP('2018-01-01'), TIMESTAMP('2019-08-01'))
+  [githubarchive:month.201911],
   [githubarchive:month.201910],
   [githubarchive:month.201909],
   [githubarchive:month.201908],
@@ -31,6 +32,7 @@ where
       repo.id
     from
       -- TABLE_DATE_RANGE([githubarchive:day.], TIMESTAMP('2018-01-01'), TIMESTAMP('2019-08-01'))
+      [githubarchive:month.201911],
       [githubarchive:month.201910],
       [githubarchive:month.201909],
       [githubarchive:month.201908],
