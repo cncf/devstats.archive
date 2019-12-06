9631cf874748aaedabe2050ad40b1a1ce2dcdb54
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-06 06:14:53 +0000

    Support LFS diff vs uncommitted yet file [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- git-lfs-diff.sh
+++ git-lfs-diff.sh
@@ -14,12 +14,17 @@ object() {
     File=$2
     Object=""
 
+    if [ "$Rev" = "." ]; then
+        echo "$File"
+        return
+    fi
+
     Oid=$(git show $Rev:$File 2> /dev/null | grep "sha256" | cut -d ":" -f 2)
     if [ "$Oid" != "" ]; then
         Oid12=$(echo $Oid | cut -b 1-2)
         Oid34=$(echo $Oid | cut -b 3-4)
         Object=.git/lfs/objects/$Oid12/$Oid34/$Oid
-        if [ ! -e "$Object" ] ; then
+        if [ ! -e "$Object" ]; then
             echo "Missing file $File at revision $Rev"
             exit 2
         fi
