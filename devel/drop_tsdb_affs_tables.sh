891ca51215cb08bccb17064297e87c04a70ce344
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-09 10:22:00 +0000

    Handle drop differently 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/drop_tsdb_affs_tables.sh
+++ devel/drop_tsdb_affs_tables.sh
@@ -7,11 +7,12 @@ fi
 
 ./devel/drop_ts_tables_staring_with.sh "$1" "scompany_activity"
 ./devel/drop_ts_tables_staring_with.sh "$1" "snum_stats"
-./devel/drop_ts_tables_staring_with.sh "$1" "shcom"
 ./devel/drop_ts_tables_staring_with.sh "$1" "scountries"
-./devel/drop_ts_tables_staring_with.sh "$1" "scompany_prs"
-./devel/drop_ts_tables_staring_with.sh "$1" "shdev"
 ./devel/drop_ts_tables_staring_with.sh "$1" "stz"
+# Histograms handle delete internally
+# ./devel/drop_ts_tables_staring_with.sh "$1" "shcom"
+# ./devel/drop_ts_tables_staring_with.sh "$1" "shdev"
+# ./devel/drop_ts_tables_staring_with.sh "$1" "scompany_prs"
 # Tags are auto-dropped when recalculating
 # ./devel/drop_ts_tables_staring_with.sh "$1" "ttz_offsets"
 # ./devel/drop_ts_tables_staring_with.sh "$1" "tcompanies"
@@ -20,7 +21,7 @@ fi
 if [ ! "$1" = "gha" ]
 then
   ./devel/drop_ts_tables_staring_with.sh "$1" "ssex"
-  ./devel/drop_ts_tables_staring_with.sh "$1" "shpr_comps"
   ./devel/drop_ts_tables_staring_with.sh "$1" "scompany_commits_data"
   ./devel/drop_ts_tables_staring_with.sh "$1" "sdoc_committers"
+  # ./devel/drop_ts_tables_staring_with.sh "$1" "shpr_comps"
 fi
