f7f0e88e9bea05e46e2a1f40e09e2eaa5aa8372b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-09 07:54:32 +0000

    Support better handling of affiliations errors 2 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- shared/update_affs.sh
+++ shared/update_affs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# TSDB_DROP=1 - drop TSDB tables before reinitializing data (new approach handles that per-metric via `drop:` property
 if ( [ -z "$GHA2DB_PROJECT" ] || [ -z "$PG_DB" ] || [ -z "$PG_PASS" ] )
 then
   echo "$0: you need to set GHA2DB_PROJECT, PG_DB, PG_PASS env variables to use this script"
@@ -14,6 +15,9 @@ then
   export TRAP=1
 fi
 proj=$GHA2DB_PROJECT
-./devel/drop_tsdb_affs_tables.sh "$PG_DB"
+if [ ! -z "$TSDB_DROP" ]
+then
+  ./devel/drop_tsdb_affs_tables.sh "$PG_DB"
+fi
 # GHA2DB_COLUMNS_YAML=metrics/$GHA2DB_PROJECT/columns_affs.yaml GHA2DB_LOCAL=1 columns
 GHA2DB_CMDDEBUG=1 GHA2DB_LOCAL=1 GHA2DB_RESET_ES_RAW=1 GHA2DB_RESETTSDB=1 GHA2DB_COLUMNS_YAML=./metrics/$proj/columns_affs.yaml GHA2DB_METRICS_YAML=./metrics/$proj/metrics_affs.yaml GHA2DB_TAGS_YAML=./metrics/$proj/tags_affs.yaml gha2db_sync
