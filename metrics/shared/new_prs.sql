1a07f1a29a93fcdc08b526bfa852db1741303ce0
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-01-22 06:41:20 +0000

    With and without bots [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/shared/new_prs.sql
+++ metrics/shared/new_prs.sql
@@ -21,6 +21,26 @@ where
   and (lower(pr.dup_user_login) {{exclude_bots}})
 group by
   r.repo_group
+union select 'new_prs_bots,All' as repo_group,
+  round(count(distinct id) / {{n}}, 2) as new
+from
+  gha_pull_requests
+where
+  created_at >= '{{from}}'
+  and created_at < '{{to}}'
+union select 'new_prs_bots,' || r.repo_group as repo_group,
+  round(count(distinct pr.id) / {{n}}, 2) as new
+from
+  gha_pull_requests pr,
+  gha_repos r
+where
+  pr.dup_repo_id = r.id
+  and pr.dup_repo_name = r.name
+  and r.repo_group is not null
+  and pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+group by
+  r.repo_group
 order by
   new desc,
   repo_group asc
