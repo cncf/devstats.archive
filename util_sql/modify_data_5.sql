0cec1cbb7d5ec504b7e3a0314ddd5160cc804b65
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-11 07:23:21 +0000

    programming languages table [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sql/modify_data_5.sql
@@ -0,0 +1,13 @@
+create table public.gha_repos_langs (
+    repo_name character varying(160) not null,
+    lang_name character varying(60) not null,
+    lang_loc integer not null,
+    lang_perc double precision not null,
+    dt timestamp without time zone default now()
+);
+alter table public.gha_repos_langs owner to gha_admin;
+alter table only public.gha_repos_langs add constraint gha_repos_langs_pkey primary key(repo_name, lang_name);
+create index repos_langs_lang_loc_idx on public.gha_repos_langs using btree(lang_loc);
+create index repos_langs_lang_name_idx on public.gha_repos_langs using btree(lang_name);
+create index repos_langs_lang_perc_idx on public.gha_repos_langs using btree(lang_perc);
+create index repos_langs_narepo_me_idx on public.gha_repos_langs using btree(repo_name);
