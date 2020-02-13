d46076b1dc539cdfcd114620b0ad7c1f5877e983
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-02-13 07:43:28 +0000

    Fix typo [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sql/modify_data_6.sql
+++ util_sql/modify_data_6.sql
@@ -1,4 +1,4 @@
-alter table gha_repos add created_at timestamp without time zone DEFAULT now();
-alter table gha_repos add updated_at timestamp without time zone DEFAULT now();
+alter table gha_repos add created_at timestamp without time zone default now();
+alter table gha_repos add updated_at timestamp without time zone default now();
 create index repos_created_at_idx on public.gha_repos using btree(created_at);
 create index repos_updated_at_idx on public.gha_repos using btree(updated_at);
