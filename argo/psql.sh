de30182984ac4aca92d585c0e78f23219f223818
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-20 11:03:06 +0000

    Adding Argo 14 [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- argo/psql.sh
+++ argo/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=argo PG_DB=argo GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql argo -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=argo PG_DB=argo GHA2DB_LOCAL=1 gha2db 2019-05-21 0 today now argoproj 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=argo PG_DB=argo GHA2DB_LOCAL=1 gha2db 2017-07-27 0 today now argoproj 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=argo PG_DB=argo GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=argo PG_DB=argo ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=argo PG_DB=argo ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
