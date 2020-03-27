7c2c103ec8cf0dac48b71fd69c746aeb146c62f2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-03-27 07:36:51 +0000

    Updated Argo artwork
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -26,7 +26,7 @@ do
     icon="servicemeshinterface"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "argo" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
