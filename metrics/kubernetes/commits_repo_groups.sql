e00aff081cb783c1646d669991bad37b23fa54a5
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-14 10:51:58 +0000

    Updates to commits repo groups metric [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/commits_repo_groups.sql
+++ metrics/kubernetes/commits_repo_groups.sql
@@ -1,23 +1,63 @@
+with commits as (
+  select r.repo_group as repo_group,
+    c.event_id,
+    c.sha,
+    c.dup_actor_login as login
+  from
+    gha_repos r,
+    gha_commits c
+  where
+    c.dup_repo_id = r.id
+    and c.dup_repo_name = r.name
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+    and r.name in (select repo_name from trepos)
+    and (lower(c.dup_actor_login) {{exclude_bots}})
+  union select r.repo_group as repo_group,
+    c.event_id,
+    c.sha,
+    c.dup_author_login as login
+  from
+    gha_repos r,
+    gha_commits c
+  where
+    c.dup_repo_id = r.id
+    and c.dup_repo_name = r.name
+    and c.author_id is not null
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+    and r.name in (select repo_name from trepos)
+    and (lower(c.dup_author_login) {{exclude_bots}})
+  union select r.repo_group as repo_group,
+    c.event_id,
+    c.sha,
+    c.dup_committer_login as login
+  from
+    gha_repos r,
+    gha_commits c
+  where
+    c.dup_repo_id = r.id
+    and c.dup_repo_name = r.name
+    and c.committer_id is not null
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+    and r.name in (select repo_name from trepos)
+    and (lower(c.dup_committer_login) {{exclude_bots}})
+)
 select
   sub.repo_group,
   round(count(distinct sub.sha) / {{n}}, 2) as commits
 from (
-  select 'commits,' || coalesce(ecf.repo_group, r.repo_group) as repo_group,
+  select 'commits,' || coalesce(ecf.repo_group, c.repo_group) as repo_group,
     c.sha
   from
-    gha_repos r,
-    gha_commits c
+    commits c
   left join
     gha_events_commits_files ecf
   on
     ecf.event_id = c.event_id
   where
-    r.name = c.dup_repo_name
-    and r.id = c.dup_repo_id
-    and r.name in (select repo_name from trepos)
-    and c.dup_created_at >= '{{from}}'
-    and c.dup_created_at < '{{to}}'
-    and (lower(c.dup_actor_login) {{exclude_bots}})
+    lower(c.login) {{exclude_bots}}
   ) sub
 where
   sub.repo_group is not null
