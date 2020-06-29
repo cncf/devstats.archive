99e540e6ee84f2dd334af8b19eed6aea7134eb94
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-06-29 09:19:46 +0000

    Artwork (temp) for new 11 Sandbox projects [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -26,7 +26,7 @@ do
     icon="servicemeshinterface"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "cnigenie" ] || [ "$icon" = "keptn" ] || [ "$icon" = "kudo" ] || [ "$icon" = "cloudcustodian" ] || [ "$icon" = "dex" ] || [ "$icon" = "litmuschaos" ] || [ "$icon" = "artifacthub" ] || [ "$icon" = "kuma" ] || [ "$icon" = "parsec" ] || [ "$icon" = "bfe" ] || [ "$icon" = "crossplane" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
