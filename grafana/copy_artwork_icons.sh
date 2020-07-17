0395aac00d0213023a511c20e52f4897158a0470
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-17 07:19:45 +0000

    Adding Chaos Mesh and Serverless Workflow - 2 new CNCF Sandbox projects 4 [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -31,7 +31,7 @@ do
     icon="servicemeshinterface"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "cnigenie" ] || [ "$icon" = "keptn" ] || [ "$icon" = "cloudcustodian" ] || [ "$icon" = "litmuschaos" ] || [ "$icon" = "kuma" ] || [ "$icon" = "contour" ] || [ "$icon" = "operatorframework" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "cnigenie" ] || [ "$icon" = "keptn" ] || [ "$icon" = "cloudcustodian" ] || [ "$icon" = "litmuschaos" ] || [ "$icon" = "kuma" ] || [ "$icon" = "contour" ] || [ "$icon" = "operatorframework" ] || [ "$icon" = "chaosmesh" ] || [ "$icon" = "serverlessworkflow" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
