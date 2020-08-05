e1a4b4af142be0b3367e7abf9aa319bebbdb0fb4
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-08-05 05:24:33 +0000

    Pre update artwork [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/create_images.sh
+++ grafana/create_images.sh
@@ -36,7 +36,7 @@ do
     icon="litmus"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "cnigenie" ] || [ "$icon" = "cloudcustodian" ] || [ "$icon" = "operatorframework" ] || [ "$icon" = "chaosmesh" ] || [ "$icon" = "serverlessworkflow" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "cnigenie" ] || [ "$icon" = "operatorframework" ] || [ "$icon" = "serverlessworkflow" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
