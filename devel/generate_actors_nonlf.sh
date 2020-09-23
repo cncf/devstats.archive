1c6f65e76c8a5a461f4460c48570dc85ac555fa9
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-09-23 09:49:26 +0000

    Adding Keylime - a new CNCF Sandbox project 5 [wip]
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/generate_actors_nonlf.sh
+++ devel/generate_actors_nonlf.sh
@@ -76,6 +76,7 @@ PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA metal3 < ./util_sq
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA porter < ./util_sql/actors.sql
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA openyurt < ./util_sql/actors.sql
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA openservicemesh < ./util_sql/actors.sql
+PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA keylime < ./util_sql/actors.sql
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA cncf < ./util_sql/actors.sql
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA allprj < ./util_sql/actors.sql
 PGPASSWORD="${PG_PASS}" psql -h "${PG_HOST}" -U gha_admin -tA spinnaker < ./util_sql/actors.sql
