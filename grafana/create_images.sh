77df81a98694f531e1fa8620ad37dc17be831b54
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-11-18 06:01:31 +0000

    SchemaHero changed non-standard naming
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/create_images.sh
+++ grafana/create_images.sh
@@ -40,10 +40,6 @@ do
   then
     icon="cert-manager"
   fi
-  if [ "$icon" = "schemahero" ]
-  then
-    mid="logomark"
-  fi
   # TODO: remove when we have icons
   if ( [ "$icon" = "kyverno" ] || [ "$icon" = "openkruise" ] || [ "$icon" = "cdk8s" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
