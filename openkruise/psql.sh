01ff6a4cf18ce88569b1f40459f6a2473e1a8d59
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-11-18 09:49:49 +0000

    Fix typo
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- openkruise/psql.sh
+++ openkruise/psql.sh
@@ -11,14 +11,14 @@ then
 fi
 > errors.txt
 > run.log
-GHA2DB_PROJECT=opencruise PG_DB=opencruise GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
-./devel/db.sh psql opencruise -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=opencruise PG_DB=opencruise GHA2DB_LOCAL=1 gha2db 2019-05-28 0 today now 'opencruise,kruiseio' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=opencruise PG_DB=opencruise GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
-GHA2DB_PROJECT=opencruise PG_DB=opencruise ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
-GHA2DB_PROJECT=opencruise PG_DB=opencruise ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
-GHA2DB_PROJECT=opencruise PG_DB=opencruise ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
-GHA2DB_PROJECT=opencruise PG_DB=opencruise ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
-GHA2DB_PROJECT=opencruise PG_DB=opencruise GHA2DB_LOCAL=1 vars || exit 8
-./devel/ro_user_grants.sh opencruise || exit 9
-./devel/psql_user_grants.sh devstats_team opencruise || exit 10
+GHA2DB_PROJECT=openkruise PG_DB=openkruise GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
+./devel/db.sh psql openkruise -c "create extension if not exists pgcrypto" || exit 1
+GHA2DB_PROJECT=openkruise PG_DB=openkruise GHA2DB_LOCAL=1 gha2db 2019-05-28 0 today now 'openkruise,kruiseio' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=openkruise PG_DB=openkruise GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=openkruise PG_DB=openkruise ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
+GHA2DB_PROJECT=openkruise PG_DB=openkruise ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
+GHA2DB_PROJECT=openkruise PG_DB=openkruise ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 6
+GHA2DB_PROJECT=openkruise PG_DB=openkruise ./shared/get_repos.sh 2>>errors.txt | tee -a run.log || exit 7
+GHA2DB_PROJECT=openkruise PG_DB=openkruise GHA2DB_LOCAL=1 vars || exit 8
+./devel/ro_user_grants.sh openkruise || exit 9
+./devel/psql_user_grants.sh devstats_team openkruise || exit 10
