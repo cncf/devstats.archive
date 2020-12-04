0e5ea79b2755dd88b95e0b92ed4252bdc54db880
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-12-04 10:07:20 +0000

    Update JSON and artwork
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -40,7 +40,7 @@ do
     icon="cert-manager"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "kyverno" ] || [ "$icon" = "openkruise" ] || [ "$icon" = "cdk8s" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "openkruise" ] || [ "$icon" = "cdk8s" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
