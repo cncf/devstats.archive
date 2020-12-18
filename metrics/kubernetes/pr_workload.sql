9a696c0518c1154bb9942afeefaf0dc960e57954
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-12-18 11:06:03 +0000

    Speedup PR workload chart metric
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- metrics/kubernetes/pr_workload.sql
+++ metrics/kubernetes/pr_workload.sql
@@ -1,6 +1,4 @@
-with dtfrom as (
-  select '{{to}}'::timestamp - '1 year'::interval as dtfrom
-), issues as (
+create temp table issues{{rnd}} as
   select sub.issue_id,
     sub.event_id
   from (
@@ -9,10 +7,9 @@ with dtfrom as (
       last_value(event_id) over issues_ordered_by_update as event_id,
       last_value(closed_at) over issues_ordered_by_update as closed_at
     from
-      gha_issues,
-      dtfrom
+      gha_issues
     where
-      created_at >= dtfrom
+      created_at >= '{{to}}'::timestamp - '1 year'::interval
       and created_at < '{{to}}'
       and updated_at < '{{to}}'
       and is_pull_request = true
@@ -27,8 +24,9 @@ with dtfrom as (
       )
     ) sub
   where
-    sub.closed_at is null
-), prs as (
+    sub.closed_at is null;
+create index on issues{{rnd}}(issue_id);
+create temp table prs{{rnd}} as
   select i.issue_id,
     i.event_id
   from (
@@ -36,10 +34,9 @@ with dtfrom as (
       last_value(closed_at) over prs_ordered_by_update as closed_at,
       last_value(merged_at) over prs_ordered_by_update as merged_at
     from
-      gha_pull_requests,
-      dtfrom
+      gha_pull_requests
     where
-      created_at >= dtfrom
+      created_at >= '{{to}}'::timestamp - '1 year'::interval
       and created_at < '{{to}}'
       and updated_at < '{{to}}'
       and event_id > 0
@@ -53,14 +50,16 @@ with dtfrom as (
         and unbounded following
       )
     ) pr,
-    issues i,
+    issues{{rnd}} i,
     gha_issues_pull_requests ipr
   where
     ipr.issue_id = i.issue_id
     and ipr.pull_request_id = pr.pr_id
     and pr.closed_at is null
-    and pr.merged_at is null
-), pr_sizes as (
+    and pr.merged_at is null;
+create index on prs{{rnd}}(issue_id);
+create index on prs{{rnd}}(event_id);
+create temp table pr_sizes{{rnd}} as
   select sub.issue_id,
     sub.size
   from (
@@ -68,14 +67,15 @@ with dtfrom as (
       lower(substring(il.dup_label_name from '(?i)size/(.*)')) as size
     from
       gha_issues_labels il,
-      prs pr
+      prs{{rnd}} pr
     where
       il.issue_id = pr.issue_id
       and il.event_id = pr.event_id
     ) sub
   where
-    sub.size is not null
-), pr_sigs as (
+    sub.size is not null;
+create index on pr_sizes{{rnd}}(issue_id);
+create temp table pr_sigs{{rnd}} as
   select sub2.issue_id,
     case sub2.sig
       when 'aws' then 'cloud-provider'
@@ -96,7 +96,7 @@ with dtfrom as (
         lower(substring(il.dup_label_name from '(?i)sig/(.*)')) as sig
       from
         gha_issues_labels il,
-        prs pr
+        prs{{rnd}} pr
       where
         il.issue_id = pr.issue_id
         and il.event_id = pr.event_id
@@ -109,8 +109,9 @@ with dtfrom as (
         'cluster-fifecycle', 'rktnetes'
       )
       and sub.sig not like '%use-only-as-a-last-resort'
-  ) sub2
-), reviewers_text as (
+  ) sub2;
+create index on pr_sigs{{rnd}}(issue_id);
+create temp table reviewers_text{{rnd}} as
   select t.event_id,
     pl.issue_id
   from
@@ -121,8 +122,8 @@ with dtfrom as (
     and t.created_at >= '{{to}}'::date - '1 month'::interval
     and t.event_id = pl.event_id
     and substring(t.body from '(?i)(?:^|\n|\r)\s*/(?:lgtm|approve)\s*(?:\n|\r|$)') is not null
-    and (lower(t.actor_login) {{exclude_bots}})
-), issue_events as (
+    and (lower(t.actor_login) {{exclude_bots}});
+create temp table issue_events{{rnd}} as
   select distinct sub.event_id,
     sub.issue_id
   from (
@@ -137,9 +138,19 @@ with dtfrom as (
       and (lower(dup_actor_login) {{exclude_bots}})
     group by
       issue_id
-    union select event_id, issue_id from reviewers_text
-    ) sub
-), sig_reviewers as (
+    union select event_id, issue_id from reviewers_text{{rnd}}
+    ) sub;
+create index on issue_events{{rnd}}(issue_id);
+create index on issue_events{{rnd}}(event_id);
+create temp table issue_sig_labels{{rnd}} as
+  select distinct issue_id,
+    lower(substring(dup_label_name from '(?i)sig/(.*)')) as sig
+  from
+    gha_issues_labels
+  where
+    dup_created_at >= '{{to}}'::timestamp - '1 year'::interval;
+create index on issue_sig_labels{{rnd}}(issue_id);
+create temp table sig_reviewers{{rnd}} as
   select sub2.sig,
     count(distinct sub2.dup_actor_login) as rev
   from (
@@ -155,13 +166,9 @@ with dtfrom as (
         else sub.sig
       end as sig,
       e.dup_actor_login
-    from (
-      select distinct issue_id,
-        lower(substring(dup_label_name from '(?i)sig/(.*)')) as sig
-      from
-        gha_issues_labels
-      ) sub,
-      issue_events ie,
+    from
+      issue_sig_labels{{rnd}} sub,
+      issue_events{{rnd}} ie,
       gha_events e
     where
       sub.sig is not null
@@ -175,10 +182,10 @@ with dtfrom as (
       and ie.event_id = e.id
   ) sub2
   group by
-    sub2.sig
-)
+    sub2.sig;
+create index on sig_reviewers{{rnd}}(sig);
 select
-  'sig_pr_wl;' || sub.sig || ';iss,abs,rev,rel',
+  'sig_pr_wl;' || sub.sig || ';iss,abs,rev,rel' as metric,
   sub.iss,
   sub.abs,
   coalesce(sr.rev, 0) as rev,
@@ -205,16 +212,16 @@ from (
       end
     ) as abs
   from
-    pr_sigs sig
+    pr_sigs{{rnd}} sig
   left join
-    pr_sizes siz
+    pr_sizes{{rnd}} siz
   on
     sig.issue_id = siz.issue_id
   group by
     sig.sig
   ) sub
 left join
-  sig_reviewers sr
+  sig_reviewers{{rnd}} sr
 on
   sub.sig = sr.sig
 where
