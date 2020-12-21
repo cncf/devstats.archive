55749228cf914caa17de59f8a2563a95137b51b9
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-12-21 10:01:21 +0000

    Fix JSON update script to save JSONs with their keys sorted
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/update_json_value.sh
+++ util_sh/update_json_value.sh
@@ -4,11 +4,11 @@ then
   echo "$0: you need to provide a JSON file to do the update"
   exit 1
 fi
-curr_h=`cat "$1" | jq '.panels[] | select(.title == "Dashboard documentation") | .gridPos.h' 2>/dev/null` || { echo "$1: property not found" ; exit 0; }
+curr_h=`cat "$1" | jq -S '.panels[] | select(.title == "Dashboard documentation") | .gridPos.h' 2>/dev/null` || { echo "$1: property not found" ; exit 0; }
 if [ -z "$curr_h" ]
 then
   exit 0
 fi
 scaled=`echo "$curr_h*1.2" | bc`
 scaled=`printf "%.0f\n" $scaled`
-jq "(.panels[] | select(.title == \"Dashboard documentation\") | .gridPos.h) |= $scaled" "$1" > out && mv out "$1" && echo "$1 $curr_h -> $scaled"
+jq -S "(.panels[] | select(.title == \"Dashboard documentation\") | .gridPos.h) |= $scaled" "$1" > out && mv out "$1" && echo "$1 $curr_h -> $scaled"
