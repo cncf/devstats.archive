8c10be2f7740e17da71fa60a5fe5192b5d00465b
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-01-29 18:15:04 +0000

    Fix grafana db dump permissions
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/shared/expose_grafana_db.sh
+++ grafana/shared/expose_grafana_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ( [ -z "$1" ] || [ -z "$2" ] )
 then
-  echo "$0: required two arguments: project_name seel_interval_in_seconds"
+  echo "$0: required two arguments: project_name sleep_interval_in_seconds"
   exit 1
 fi
 
@@ -9,4 +9,5 @@ while true
 do
   sleep $2 || exit 2
   cp /var/lib/grafana/grafana.db "/root/grafana.$1.db" || exit 3
+  chmod ugo+r "/root/grafana.$1.db" || exit 4
 done
