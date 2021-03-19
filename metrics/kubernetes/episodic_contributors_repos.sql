566ba43eff4d4c0fb04ce3b33a955a18d7ce8480
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-03-19 11:14:17 +0000

    Update dashboard 'New and episodic PR creators' - episodic PRs part
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ metrics/kubernetes/episodic_contributors_repos.sql
@@ -0,0 +1,57 @@
+with prev as (
+  select distinct user_id
+  from
+    gha_pull_requests
+  where
+    created_at >= date '{{from}}' - '3 months'::interval
+    and created_at < '{{from}}'
+), prev_cnt as (
+  select user_id, count(distinct id) as cnt
+  from
+    gha_pull_requests
+  where
+    created_at < '{{from}}'
+  group by
+    user_id
+)
+select
+  'epis_contrib;All;contrib,prs' as name,
+  round(count(distinct pr.user_id) / {{n}}, 2) as contributors,
+  round(count(distinct pr.id) / {{n}}, 2) as prs
+from
+  gha_pull_requests pr
+left join
+  prev_cnt pc
+on
+  pc.user_id = pr.user_id
+where
+  pr.created_at >= '{{from}}'
+  and pr.created_at < '{{to}}'
+  and pr.user_id not in (select user_id from prev)
+  and (pc.user_id is null or pc.cnt <= 12)
+union select sub.name,
+  round(count(distinct sub.user_id) / {{n}}, 2) as contributors,
+  round(count(distinct sub.id) / {{n}}, 2) as prs
+from (
+    select 'epis_contrib;' || pr.dup_repo_name || ';contrib,prs' as name,
+    pr.user_id,
+    pr.id
+  from
+    gha_pull_requests pr
+  left join
+    prev_cnt pc
+  on
+    pc.user_id = pr.user_id
+  where
+    pr.created_at >= '{{from}}'
+    and pr.created_at < '{{to}}'
+    and pr.user_id not in (select user_id from prev)
+    and (pc.user_id is null or pc.cnt <= 12)
+    and pr.dup_repo_name in (select repo_name from trepos)
+  ) sub
+group by
+  sub.name
+order by
+  prs desc,
+  name asc
+;
