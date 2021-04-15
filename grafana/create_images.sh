178f5cbe8ce4dfc8d7d052097c4962e1f8bee553
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-04-15 09:14:31 +0000

    Adding Emissary-ingress a new CNCF incubating project 6
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/create_images.sh
+++ grafana/create_images.sh
@@ -45,7 +45,7 @@ do
     icon="kube-ovn"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "ingraind" ] || [ "$icon" = "kuberhealthy" ] || [ "$icon" = "k8gb" ] || [ "$icon" = "trickster" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "distribution" ] || [ "$icon" = "gitopswg" ] || [ "$icon" = "openkruise" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "emissaryingress" ] || [ "$icon" = "ingraind" ] || [ "$icon" = "kuberhealthy" ] || [ "$icon" = "k8gb" ] || [ "$icon" = "trickster" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "distribution" ] || [ "$icon" = "gitopswg" ] || [ "$icon" = "openkruise" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
