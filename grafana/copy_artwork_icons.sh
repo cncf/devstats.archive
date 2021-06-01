78ef3fd31e6e3ae9d9e14f39e623f53de8cdc885
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-01 08:41:20 +0000

    Updating 3 projects artwork
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -48,7 +48,7 @@ do
     icon="opengitops"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "wasmedge" ] || [ "$icon" = "chaosblade" ] || [ "$icon" = "vineyard" ] || [ "$icon" = "antrea" ] || [ "$icon" = "submariner" ] || [ "$icon" = "emissaryingress" ] || [ "$icon" = "ingraind" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "distribution" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "wasmedge" ] || [ "$icon" = "chaosblade" ] || [ "$icon" = "emissaryingress" ] || [ "$icon" = "ingraind" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "distribution" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
