aeeddfe9795dd5fc315ccad79a64549d9ff10cfa
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-07 05:08:11 +0000

    Update Antrea main repo/org 3
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- vineyard/psql.sh
+++ vineyard/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=vineyard PG_DB=vineyard GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql vineyard -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=vineyard PG_DB=vineyard GHA2DB_LOCAL=1 gha2db 2020-10-23 0 today now 'alibaba/v6d,alibaba/libvineyard' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=vineyard PG_DB=vineyard GHA2DB_LOCAL=1 gha2db 2020-10-23 0 today now 'v6d-io,alibaba/v6d,alibaba/libvineyard' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=vineyard PG_DB=vineyard GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=vineyard PG_DB=vineyard ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=vineyard PG_DB=vineyard ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
