9c42fa2fc227ae631692fbf75010bba1bea39e77
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-23 12:19:08 +0000

    Fix the typo
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- kubevip/psql.sh
+++ kubevip/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=kubevip PG_DB=kubevip GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql kubevip -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=kubevip PG_DB=kubevip GHA2DB_LOCAL=1 gha2db 2020-02-02 0 today now 'kube-vip,plunder-app/kubevip' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=kubevip PG_DB=kubevip GHA2DB_LOCAL=1 gha2db 2020-02-02 0 today now 'kube-vip,plunder-app/kube-vip' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=kubevip PG_DB=kubevip GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=kubevip PG_DB=kubevip ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=kubevip PG_DB=kubevip ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
