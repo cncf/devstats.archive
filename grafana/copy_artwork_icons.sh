9e799ae5d7b03e07430fa328c1ae78040fe297bc
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-06-23 09:11:54 +0000

    Adding 6 new CNCF projects [wip] 7
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- grafana/copy_artwork_icons.sh
+++ grafana/copy_artwork_icons.sh
@@ -61,7 +61,7 @@ do
     icon="wasm-edge-runtime"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "chaosblade" ] || [ "$icon" = "ingraind" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "pixie" ] || [ "$icon" = "meshery" ] || [ "$icon" = "servicemeshperformance" ] || [ "$icon" = "kubevela" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "kubedl" ] || [ "$icon" = "chaosblade" ] || [ "$icon" = "ingraind" ] || [ "$icon" = "k8dash" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
