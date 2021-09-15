64cc1212bf1f06835709292a11d545534ee5ad4f
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-09-15 16:43:22 +0000

    Fix typos
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- shared/setup_scripts.sh
+++ shared/setup_scripts.sh
@@ -12,9 +12,9 @@ fi
 proj=$GHA2DB_PROJECT
 echo "Setting up $proj repository groups sync script"
 PG_USER="${user}" ./devel/db.sh psql $PG_DB -c "insert into gha_postprocess_scripts(ord, path) select 0, 'scripts/$proj/repo_groups.sql' on conflict do nothing"
-echo "Setting $proj up default postprocess scripts"
+echo "Setting up $proj default postprocess scripts"
 GHA2DB_LOCAL=1 runq util_sql/default_postprocess_scripts.sql
-echo "Setting $proj up repository groups postprocess script"
+echo "Setting up $proj repository groups postprocess script"
 GHA2DB_LOCAL=1 runq util_sql/repo_groups_postprocess_script_from_repos.sql
-echo "Initial emails/names origins"
+echo "Initial $proj emails/names origins"
 GHA2DB_LOCAL=1 runq "scripts/$proj/origins.sql"
