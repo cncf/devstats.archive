1c974e2f90129fad273008796ac795f1bdc18410
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-10-14 13:05:19 +0000

    Updated Karmada artwork
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -68,7 +68,7 @@ do
     icon="inclavare"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "akri" ] || [ "$icon" = "metallb" ] || [ "$icon" = "karmada" ] || [ "$icon" = "superedge" ] || [ "$icon" = "cilium" ] || [ "$icon" = "krator" ] || [ "$icon" = "oras" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "akri" ] || [ "$icon" = "metallb" ] || [ "$icon" = "superedge" ] || [ "$icon" = "cilium" ] || [ "$icon" = "krator" ] || [ "$icon" = "oras" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "istio" ] || [ "$icon" = "knative" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
