9f22f0da7b0fcc3b8351da8781924a0f96782514
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-10-15 07:17:57 +0000

    Update K3s config 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- k3s/psql.sh
+++ k3s/psql.sh
@@ -14,7 +14,7 @@ fi
 GHA2DB_PROJECT=k3s PG_DB=k3s GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql k3s -c "create extension if not exists pgcrypto" || exit 1
 # GHA2DB_PROJECT=k3s PG_DB=k3s GHA2DB_LOCAL=1 gha2db 2018-05-31 0 today now 'regexp:(?i)^((ibuildthecloud|rancher)\/.*k3(s|d).*|k3s-io\/.*)$' 2>>errors.txt | tee -a run.log || exit 2
-GHA2DB_PROJECT=k3s PG_DB=k3s GHA2DB_LOCAL=1 gha2db 2018-05-31 0 today now 'k3s-io' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_EXCLUDE_REPOS='k3s-io/klog,k3s-io/containerd,k3s-io/cri-tools,k3s-io/etcd,k3s-io/flannel,k3s-io/go-powershell,k3s-io/kubernetes,k3s-io/nocode' GHA2DB_PROJECT=k3s PG_DB=k3s GHA2DB_LOCAL=1 gha2db 2018-05-31 0 today now 'k3s-io' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=k3s PG_DB=k3s GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=k3s PG_DB=k3s ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=k3s PG_DB=k3s ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
