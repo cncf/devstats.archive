1cec73bc7a84cef6ca6cc0d9a146f0bd2d19f6aa
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-10-18 06:03:37 +0000

    New metric wip
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ docs/dashboards/kyverno/health_stats.md
@@ -0,0 +1,14 @@
+<h1 id="dashboard-header">[[full_name]] community health dashboard</h1>
+<p>Links:</p>
+<ul>
+<li>Stars metric <a href="https://github.com/cncf/devstats/blob/master/metrics/shared/watchers_by_alias.sql" target="_blank">SQL file</a>.</li>
+<li>Other metrics <a href="https://github.com/cncf/devstats/blob/master/metrics/kyverno/community_health.sql" target="_blank">SQL file</a>.</li>
+<li>TSDB <a href="https://github.com/cncf/devstats/blob/master/metrics/shared/metrics.yaml" target="_blank">series definition</a>. Search for <code>community_health</code></li>
+<li>Grafana dashboard <a href="https://github.com/cncf/devstats/blob/master/grafana/dashboards/[[lower_name]]/community-health.json" target="_blank">JSON</a>.</li>
+</ul>
+<h1 id="description">Description</h1>
+<ul>
+<li>This dashboard shows counts for stargazers, issue creators, code committers, code commenters for a given repository</li>
+<li>It shows those values changing over time with daily resolution, also includes summory for all project repositories combined.</li>
+<li>Bots are skipped when calculating statistics, see <a href="https://github.com/cncf/devstats/blob/master/docs/excluding_bots.md" target="_blank">excluding bots</a> for details.</li>
+</ul>
---
+++ metrics/kyverno/community_health.sql
@@ -0,0 +1,74 @@
+with last_date as (
+  select dup_repo_id as repo_id, 
+    max(updated_at) as dt
+  from
+    gha_forkees
+  where
+    updated_at < '{{to}}'
+    and updated_at >= '{{to}}'::timestamp - '6 months'::interval
+    and dup_repo_name like '%_/_%'
+    and dup_repo_name not like '%/%/%'
+    and watchers > 0
+    and forks > 0
+    and open_issues > 0
+  group by
+    repo_id
+), last_event as (
+  select
+    ld.repo_id,
+    ld.dt,
+    max(f.event_id) as event_id
+  from
+    gha_forkees f,
+    last_date ld
+  where
+    ld.repo_id = f.dup_repo_id
+    and ld.dt = f.updated_at
+    and watchers > 0
+    and forks > 0
+    and open_issues > 0
+  group by
+    ld.dt,
+    ld.repo_id
+)
+select
+  'watch;' || r.alias || ';watch,forks,opiss' as name,
+  sum(f.watchers) as watchers,
+  sum(f.forks) as forks,
+  sum(f.open_issues) as open_issues
+from
+  gha_forkees f,
+  gha_repos r,
+  last_event le
+where
+  r.alias is not null
+  and r.id = le.repo_id
+  and r.id = f.dup_repo_id
+  and r.name = f.dup_repo_name
+  and le.repo_id = f.dup_repo_id
+  and le.event_id = f.event_id
+  and le.dt = f.updated_at
+ group by
+  r.alias
+union select 'allwatch;all;watch,forks,opiss' as name,
+  sum(f.watchers) as watchers,
+  sum(f.forks) as forks,
+  sum(f.open_issues) as open_issues
+from
+  gha_forkees f,
+  gha_repos r,
+  last_event le
+where
+  r.id = le.repo_id
+  and r.alias is not null
+  and r.id = f.dup_repo_id
+  and r.name = f.dup_repo_name
+  and le.repo_id = f.dup_repo_id
+  and le.event_id = f.event_id
+  and le.dt = f.updated_at
+order by
+  watchers desc,
+  forks desc,
+  open_issues desc,
+  name asc
+;
