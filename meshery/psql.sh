9c120cf52a9b0ab6b331d4a65a3ee096fa5d29b5
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-01-14 07:56:33 +0000

    Update meshery/layer5io projects configs
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- meshery/psql.sh
+++ meshery/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=meshery PG_DB=meshery GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql meshery -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=meshery PG_DB=meshery GHA2DB_LOCAL=1 gha2db 2018-11-14 0 today now 'regexp:(?i)layer5io\/.*meshery' 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=meshery PG_DB=meshery GHA2DB_LOCAL=1 gha2db 2018-11-14 0 today now 'regexp:(?i)^(layer5io\/.*meshery|meshery\/.*)' 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=meshery PG_DB=meshery GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=meshery PG_DB=meshery ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=meshery PG_DB=meshery ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 5
