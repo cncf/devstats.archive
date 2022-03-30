f311bb1df63eb076f6f50bfd12ec0ae3fa6a19bf
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2022-03-30 05:40:32 +0000

    new script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/execute_on_all_databases_k8s.sh
+++ devel/execute_on_all_databases_k8s.sh
@@ -4,13 +4,32 @@ then
   echo "$0: at least one SQL script required"
   exit 1
 fi
+if [ -z "${kenv}" ]
+then
+  echo "$0: you need to specify kenv=test|prod"
+  exit 2
+fi
+if [ "${kenv}" = "test" ]
+then
+  export TEST_SERVER=1
+  export member=$(kubectl exec -itn devstats-${kenv} devstats-postgres-0 -- patronictl list -f json | jq -rS '.[]  | select(.Role == "Leader") | .Member')
+  echo "R/W member on ${kenv} is ${member}"
+elif [ "${kenv}" = "prod" ]
+then
+  export PROD_SERVER=1
+  export member=$(kubectl exec -itn devstats-${kenv} devstats-postgres-0 -- patronictl list -f json | jq -rS '.[]  | select(.Role == "Leader") | .Member')
+  echo "R/W member on ${kenv} is ${member}"
+else
+  echo "kenv must be test or prod, got: ${kenv}"
+  exit 3
+fi
 . ./devel/all_dbs.sh || exit 2
 for db in $all
 do
   for sql in $*
   do
     echo "Execute script '$sql' on '$db' database"
-    kubectl exec -itn "${kenv}" "${kn}" -- psql "$db" < "$sql" || exit 2
+    kubectl exec -in "devstats-${kenv}" "${member}" -- psql "$db" < "$sql"
   done
 done
 echo 'OK'
