e0b1668176c63244bd1548aed72762c16aefaaf0
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-03-30 05:16:55 +0000

    Add all DBs exec util script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ devel/execute_on_all_databases_k8s.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: at least one SQL script required"
+  exit 1
+fi
+. ./devel/all_dbs.sh || exit 2
+for db in $all
+do
+  for sql in $*
+  do
+    echo "Execute script '$sql' on '$db' database"
+    kubectl exec -itn "${kenv}" "${kn}" -- psql "$db" < "$sql" || exit 2
+  done
+done
+echo 'OK'
+
