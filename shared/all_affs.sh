b44023354b957cca307847c4b926cdd13bc2c240
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-05-13 12:39:17 +0000

    Add support for percent value for skip imp/upd affs
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- shared/all_affs.sh
+++ shared/all_affs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SKIPTEMP=1 skip regenerating data into temporary database and use current database directly
-# SKIP_IMP_AFFS=1 skip import_affs.sh phase
-# SKIP_UPD_AFFS=1 skip update_affs.sh phase
+# SKIP_IMP_AFFS=percent - % chance to skip import_affs.sh phase
+# SKIP_UPD_AFFS=percent - % chance to skip update_affs.sh phase
 if ( [ -z "$GHA2DB_PROJECT" ] || [ -z "$PG_DB" ] || [ -z "$PG_PASS" ] )
 then
   echo "$0: you need to set GHA2DB_PROJECT, PG_DB, PG_PASS env variables to use this script"
@@ -27,6 +27,40 @@ else
   ./devel/db.sh pg_dump -Fc $db -f /tmp/$tdb.dump || exit 1
   mv /tmp/$tdb.dump . || exit 2
   ./devel/restore_db.sh $tdb || exit 3
+fi
+
+if [ "$SKIP_IMP_AFFS" = "0" ]
+then
+  export SKIP_IMP_AFFS=''
+fi
+if [ ! -z "$SKIP_IMP_AFFS" ]
+then
+  rval=$(((RANDOM%100)+1))
+  if [ "$rval" -gt "$SKIP_IMP_AFFS" ]
+  then
+    echo "Random value: $rval > $SKIP_IMP_AFFS, skipping importing affiliations"
+    export SKIP_IMP_AFFS=1
+  else
+    echo "Random value: $rval <= $SKIP_IMP_AFFS, importing"
+    export SKIP_IMP_AFFS=''
+  fi
+fi
+
+if [ "$SKIP_UPD_AFFS" = "0" ]
+then
+  export SKIP_UPD_AFFS=''
+fi
+if [ ! -z "$SKIP_UPD_AFFS" ]
+then
+  rval=$(((RANDOM%100)+1))
+  if [ "$rval" -gt "$SKIP_UPD_AFFS" ]
+  then
+    echo "Random value: $rval > $SKIP_UPD_AFFS, skipping TSDB regenerate (company related)"
+    export SKIP_UPD_AFFS=1
+  else
+    echo "Random value: $rval <= $SKIP_UPD_AFFS, performing TSDB regenerate (company related)"
+    export SKIP_UPD_AFFS=''
+  fi
 fi
 
 proj=$GHA2DB_PROJECT
