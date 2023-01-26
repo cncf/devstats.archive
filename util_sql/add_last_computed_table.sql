770b5ab2bc8892113c232108ef9f62cc1cd6c1c4
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-01-26 13:32:12 +0000

    Add grants
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- util_sql/add_last_computed_table.sql
+++ util_sql/add_last_computed_table.sql
@@ -5,3 +5,5 @@ CREATE TABLE public.gha_last_computed (
 ALTER TABLE public.gha_last_computed OWNER TO gha_admin;
 ALTER TABLE ONLY public.gha_last_computed
     ADD CONSTRAINT gha_last_computed_pkey PRIMARY KEY (metric);
+grant select on gha_last_computed to "devstats_team";
+grant select on gha_last_computed to "ro_user";
